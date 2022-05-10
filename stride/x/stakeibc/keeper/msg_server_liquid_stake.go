c877c09266d1eedfe04d967ba16eb1e1e52e3c91
Aidan Salzmann <aidan96@gmail.com>
2022-05-11 00:23:48 +0200

    fix errors

--- stride/x/stakeibc/keeper/msg_server_liquid_stake.go
+++ stride/x/stakeibc/keeper/msg_server_liquid_stake.go
@@ -2,6 +2,7 @@ package keeper
 
 import (
 	"context"
+	"strconv"
 
 	"github.com/Stride-labs/stride/x/stakeibc/types"
 	sdk "github.com/cosmos/cosmos-sdk/types"
@@ -11,7 +12,22 @@ import (
 func (k msgServer) LiquidStake(goCtx context.Context, msg *types.MsgLiquidStake) (*types.MsgLiquidStakeResponse, error) {
 	ctx := sdk.UnwrapSDKContext(goCtx)
 
-	// safety checks
+	// Init variables
+	// get the sender address
+	sender, err := sdk.AccAddressFromBech32(msg.Creator)
+	if err != nil {
+		return nil, sdkerrors.Wrap(sdkerrors.ErrInvalidAddress, "address invalid")
+	}
+	// get the coins to send, they need to be in the format {amount}{denom}
+	// NOTE: int is an int32 or int64 (depending on machine type) so converting from int32 -> int
+	// is safe. The converse is not true.
+	coinString := strconv.Itoa(int(msg.Amount)) + msg.Denom
+	coins, err := sdk.ParseCoinsNormalized(coinString)
+	if err != nil {
+		return nil, sdkerrors.Wrapf(sdkerrors.ErrInvalidType, "failed to parse %s coins", coins)
+	}
+	
+	// Safety checks
 	// ensure Amount is non-negative, liquid staking 0 or less tokens is invalid
 	if msg.Amount < 1 {
 		return nil, sdkerrors.Wrapf(sdkerrors.ErrInvalidCoins, "amount must be non-negative")
@@ -22,21 +38,11 @@ func (k msgServer) LiquidStake(goCtx context.Context, msg *types.MsgLiquidStake)
 		return nil, sdkerrors.Wrapf(types.ErrInvalidToken, "invalid token denom (%s)", msg.Denom)
 	}
 
-	// get the sender address
-	sender, err := sdk.AccAddressFromBech32(msg.Creator)
-	if err != nil {
-		return nil, sdkerrors.Wrap(sdkerrors.ErrInvalidAddress, "address invalid")
-	}
 	
-	// get the coins to send, they need to be in the format {amount}{denom}
-	coinString := string(msg.Amount) + msg.Denom
-	coins, err := sdk.ParseCoinsNormalized(coinString)
-	if err != nil {
-		return nil, sdkerrors.Wrapf(sdkerrors.ErrInvalidType, "failed to parse %s coins", coins)
-	}
 
 	// deposit `amount` of `denom` token to the stakeibc module
 	// NOTE: Should we add an additional check here? This is a pretty important line of code
+	// NOTE: If sender doesn't have enough coins, this panics (error is hard to interpret)
 	sdkerror := k.bankKeeper.SendCoinsFromAccountToModule(ctx, sender, types.ModuleName, coins)
 	if sdkerror != nil {
 		panic(sdkerror)
@@ -44,24 +50,24 @@ func (k msgServer) LiquidStake(goCtx context.Context, msg *types.MsgLiquidStake)
 
 	// mint user `amount` of the corresponding stAsset
 	// NOTE: We should ensure that denoms are unique - we don't want anyone spoofing denoms
-	success := k.MintStAsset(ctx, sender, msg.Amount, msg.Denom)
-	if !success {
-		return nil, sdkerrors.Wrap(sdkerrors.ErrPanic, "failed to mint stAssets to user")
+	err = k.MintStAsset(ctx, sender, msg.Amount, msg.Denom)
+	if err != nil {
+		return nil, sdkerrors.Wrap(sdkerrors.ErrInvalidCoins, "failed to mint stAssets to user")
 	}
 
 	return &types.MsgLiquidStakeResponse{}, nil
 }
 
-func (k msgServer) MintStAsset(ctx sdk.Context, sender sdk.AccAddress, amount int32, denom string) bool {
+func (k msgServer) MintStAsset(ctx sdk.Context, sender sdk.AccAddress, amount int32, denom string) error {
 	// repeat safety checks from LiquidStake in case MintStAsset is called from another site
 	// ensure Amount is non-negative, liquid staking 0 or less tokens is invalid
 	if amount < 1 {
-		return false
+		return nil
 	}
 	// check that the token is an IBC token
 	isIbcToken := types.IsIBCToken(denom)
 	if !isIbcToken {
-		return false
+		return nil
 	}
 
 	// NOTE: should we pass in a zone to this function and pull the stAssetDenom off of that object?
@@ -70,10 +76,12 @@ func (k msgServer) MintStAsset(ctx sdk.Context, sender sdk.AccAddress, amount in
 	// get the denom of the st asset type to transfer
 	stAssetDenom := prefix + denom
 	
-	coinString := string(amount) + stAssetDenom
+	// TODO(TEST-7): Add an exchange rate here! What object should we store the exchange rate on?
+	// How can we ensure that the exchange rate is not manipulated?
+	coinString := strconv.Itoa(int(amount)) + stAssetDenom
 	stCoins, err := sdk.ParseCoinsNormalized(coinString)
 	if err != nil {
-		panic(err)
+		return sdkerrors.Wrap(sdkerrors.ErrInvalidCoins, "Failed to parse coins")
 	}
 
 	// mint new coins of the asset type
@@ -81,12 +89,12 @@ func (k msgServer) MintStAsset(ctx sdk.Context, sender sdk.AccAddress, amount in
 	// It will panic if the module account does not exist or is unauthorized.
 	sdkerror := k.bankKeeper.MintCoins(ctx, types.ModuleName, stCoins)
 	if sdkerror != nil {
-		panic(sdkerror)
+		return sdkerrors.Wrap(sdkerrors.ErrInvalidCoins, "Failed to mint coins")
 	}
 	// transfer those coins to the user
 	sdkerror = k.bankKeeper.SendCoinsFromModuleToAccount(ctx, types.ModuleName, sender, stCoins)
 	if sdkerror != nil {
-		panic(sdkerror)
+		return sdkerrors.Wrap(sdkerrors.ErrInvalidCoins, "Failed to send coins from module to account")
 	}
-	return true
+	return nil
 }
