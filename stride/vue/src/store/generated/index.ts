5dddd1d2a7b6274d0cd8db1ae807874413ee3110
= <rileyedmunds@gmail.com>
2022-05-10 11:01:00 -0400

    ignite scaffold module interchainquery

--- stride/vue/src/store/generated/index.ts
+++ stride/vue/src/store/generated/index.ts
@@ -1,5 +1,6 @@
 // THIS FILE IS GENERATED AUTOMATICALLY. DO NOT MODIFY.
 
+import StrideLabsStrideStridelabsStrideInterchainquery from './Stride-labs/stride/Stridelabs.stride.interchainquery'
 import StrideLabsStrideStridelabsStrideStakeibc from './Stride-labs/stride/Stridelabs.stride.stakeibc'
 import CosmosCosmosSdkCosmosAuthV1Beta1 from './cosmos/cosmos-sdk/cosmos.auth.v1beta1'
 import CosmosCosmosSdkCosmosBankV1Beta1 from './cosmos/cosmos-sdk/cosmos.bank.v1beta1'
@@ -24,6 +25,7 @@ import CosmosIbcGoIbcCorePortV1 from './cosmos/ibc-go/ibc.core.port.v1'
 
 
 export default { 
+  StrideLabsStrideStridelabsStrideInterchainquery: load(StrideLabsStrideStridelabsStrideInterchainquery, 'Stridelabs.stride.interchainquery'),
   StrideLabsStrideStridelabsStrideStakeibc: load(StrideLabsStrideStridelabsStrideStakeibc, 'Stridelabs.stride.stakeibc'),
   CosmosCosmosSdkCosmosAuthV1Beta1: load(CosmosCosmosSdkCosmosAuthV1Beta1, 'cosmos.auth.v1beta1'),
   CosmosCosmosSdkCosmosBankV1Beta1: load(CosmosCosmosSdkCosmosBankV1Beta1, 'cosmos.bank.v1beta1'),
