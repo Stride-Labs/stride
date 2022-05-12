80f8716f1ec25e72d600dd5707ddffb092dbb394
riley-stride <riley@stridelabs.co>
2022-05-12 12:17:22 -0400

    copied x/mint folder from other

---
+++ stride/x/mint/genesis.go
@@ -0,0 +1,29 @@
+package mint
+
+import (
+	"github.com/ingenuity-build/quicksilver/x/mint/keeper"
+	"github.com/ingenuity-build/quicksilver/x/mint/types"
+
+	sdk "github.com/cosmos/cosmos-sdk/types"
+)
+
+// InitGenesis new mint genesis.
+func InitGenesis(ctx sdk.Context, keeper keeper.Keeper, ak types.AccountKeeper, bk types.BankKeeper, data *types.GenesisState) {
+	data.Minter.EpochProvisions = data.Params.GenesisEpochProvisions
+	keeper.SetMinter(ctx, data.Minter)
+	keeper.SetParams(ctx, data.Params)
+
+	if !ak.HasAccount(ctx, ak.GetModuleAddress(types.ModuleName)) {
+		ak.GetModuleAccount(ctx, types.ModuleName)
+	}
+
+	keeper.SetLastReductionEpochNum(ctx, data.ReductionStartedEpoch)
+}
+
+// ExportGenesis returns a GenesisState for a given context and keeper.
+func ExportGenesis(ctx sdk.Context, keeper keeper.Keeper) *types.GenesisState {
+	minter := keeper.GetMinter(ctx)
+	params := keeper.GetParams(ctx)
+	lastReductionEpoch := keeper.GetLastReductionEpochNum(ctx)
+	return types.NewGenesisState(minter, params, lastReductionEpoch)
+}
