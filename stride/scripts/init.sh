ca7de8bdc794e69e9aa8b22166c937ff3fbb8741
vish-stride <vishal@stridelabs.co>
2022-05-20 18:20:22 -0400

    all nodes are validators and talk to each other

--- stride/scripts/init.sh
+++ stride/scripts/init.sh
@@ -15,36 +15,51 @@ for chain_name in ${STRIDE_CHAINS[@]}; do
 done
 
 # fetch the stride node ids
-node1=$($ST1_RUN tendermint show-node-id)@stride1:26656
-node2=$($ST2_RUN tendermint show-node-id)@stride2:26656
-node3=$($ST3_RUN tendermint show-node-id)@stride3:26656
-echo ${node1}
-STRIDE_NODES=($node1 $node2 $node3)
-
+STRIDE_NODES=()
 # then, we initialize our chains 
 echo 'Initializing chains...'
-for i in "${!STRIDE_CHAINS[@]}"; do
+for i in ${!STRIDE_CHAINS[@]}; do
     chain_name=${STRIDE_CHAINS[i]}
     vkey=${VKEYS[i]}
     val_acct=${VAL_ACCTS[i]}
+    st_cmd=${ST_CMDS[i]}
     echo "\t$chain_name"
-    $BASE_RUN init test --chain-id $chain_name --overwrite --home "$STATE/$chain_name" 2> /dev/null
+    $st_cmd init test --chain-id $chain_name --overwrite 2> /dev/null
     sed -i -E 's|"stake"|"ustrd"|g' "${STATE}/${chain_name}/config/genesis.json"
     # add VALidator account
-    echo $vkey | $BASE_RUN keys add $val_acct --recover --keyring-backend=test --home "$STATE/$chain_name" > /dev/null
+    echo $vkey | $st_cmd keys add $val_acct --recover --keyring-backend=test > /dev/null
     # get validator address
-    VAL_ADDR=$($BASE_RUN keys show $val_acct --keyring-backend test -a --home "$STATE/$chain_name")
+    VAL_ADDR=$($st_cmd keys show $val_acct --keyring-backend test -a)
     # add money for this validator account
-    # $BASE_RUN add-genesis-account ${VAL_ADDR} 500000000000ustrd --home "$STATE/$main_chain"
-    $BASE_RUN add-genesis-account ${VAL_ADDR} 500000000000ustrd --home "$STATE/$chain_name"
+    # $st_cmd add-genesis-account ${VAL_ADDR} 500000000000ustrd
+    $st_cmd add-genesis-account ${VAL_ADDR} 500000000000ustrd
     # actually set this account as a validator
-    yes | $BASE_RUN gentx $val_acct 1000000000ustrd --chain-id $chain_name --keyring-backend test --home "$STATE/$chain_name"
+    yes | $st_cmd gentx $val_acct 1000000000ustrd --chain-id $main_chain --keyring-backend test
     # this is just annoying, but we need to move the validator tx
     # cp "./${STATE}/${chain_name}/config/gentx/*.json" "./${STATE}/${chain_name}/config/gentx/"
     # now we process these txs 
-    $BASE_RUN collect-gentxs --home "$STATE/$chain_name" 2> /dev/null
-    # add peers in config.toml so that nodes can find each other by constructing a fully connected
-    # graph of nodes
+    $st_cmd collect-gentxs 2> /dev/null
+    # now we grab the relevatn node id
+    dock_name=${STRIDE_DOCKER_NAMES[i]}
+    node_id=$($st_cmd tendermint show-node-id)@$dock_name:$PORT_ID
+    STRIDE_NODES+=( $node_id )
+
+    if [ $i -ne $MAIN_ID ]
+    then
+        $main_cmd add-genesis-account ${VAL_ADDR} 500000000000ustrd
+        # yes | $main_cmd gentx $val_acct 1000000000ustrd --chain-id $chain_name --keyring-backend test
+        cp ./${STATE}/${chain_name}/config/gentx/*.json ./${STATE}/${main_chain}/config/gentx/
+    fi
+done
+
+# strided --home state/STRIDE_1 add-genesis-account ${VAL_ADDR} 500000000000ustrd
+# strided --home state/STRIDE_1 collect-gentxs 
+
+$main_cmd collect-gentxs 2> /dev/null
+# add peers in config.toml so that nodes can find each other by constructing a fully connected
+# graph of nodes
+for i in ${!STRIDE_CHAINS[@]}; do
+    chain_name=${STRIDE_CHAINS[i]}
     peers=""
     for j in "${!STRIDE_NODES[@]}"; do
         if [ $j -ne $i ]
@@ -57,14 +72,16 @@ for i in "${!STRIDE_CHAINS[@]}"; do
     sed -i -E "s|persistent_peers = \"\"|persistent_peers = \"$peers\"|g" "${STATE}/${chain_name}/config/config.toml"
 done
 
-
-# make sure all Stride chains have the same genesis 
+# make sure all Stride chains have the same genesis
 for i in "${!STRIDE_CHAINS[@]}"; do
-    cp ./${STATE}/${main_chain}/config/genesis.json ./${STATE}/${STRIDE_CHAINS[i]}/config/genesis.json
+    if [ $i -ne $MAIN_ID ]
+    then
+        cp ./${STATE}/${main_chain}/config/genesis.json ./${STATE}/${STRIDE_CHAINS[i]}/config/genesis.json
+    fi
 done
 # strided start --home state/STRIDE_1  # TESTING ONLY
 # next we build our docker images
-docker build --no-cache --pull --tag stridezone:stride -f Dockerfile.stride .  # builds from scratch
+# docker build --no-cache --pull --tag stridezone:stride -f Dockerfile.stride .  # builds from scratch
 # docker build --tag stridezone:stride -f Dockerfile.stride .  # uses cache to speed things up
 
 # finally we serve our docker images
