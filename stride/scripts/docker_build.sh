0cb2d610950954b297b417443b1ac2fcee07091c
vish-stride <vishal@stridelabs.co>
2022-05-21 17:33:26 -0400

    INTERMEDIATE COMMIT - DOES NOT RUN

--- stride/scripts/docker_build.sh
+++ stride/scripts/docker_build.sh
@@ -6,10 +6,15 @@ while getopts f flag; do
     esac
 done
 
+echo $btype
+
 if [ "$btype" == "cached" ]; then
     docker build --tag stridezone:stride -f Dockerfile.stride .
+    docker build --tag stridezone:gaia -f Dockerfile.gaia .
 elif [ "$btype" == "full" ]; then
     docker build --no-cache --pull --tag stridezone:stride -f Dockerfile.stride .
+    docker build --no-cache --pull --tag stridezone:gaia -f Dockerfile.gaia .
 else
     echo "No docker build."
 fi
+
