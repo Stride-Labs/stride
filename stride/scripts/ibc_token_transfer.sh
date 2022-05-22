530e8e665fd7e011e4b641f19d0f159d4c1040ab
Aidan Salzmann <aidan96@gmail.com>
2022-05-23 01:39:50 +0200

    typo, remove unused line from go.mod

--- stride/scripts/ibc_token_transfer.sh
+++ stride/scripts/ibc_token_transfer.sh
@@ -4,7 +4,7 @@ echo "$STRIDE_ADDRESS_1 balances"
 $STR1_EXEC q bank balances $STRIDE_ADDRESS_1 --denom ustrd
 
 # transfer 100strd from val1 on stride to gval1 on gaia
-echo "transferring 100strd tokens..."
+echo "transferring 1000strd tokens..."
 $STR1_EXEC tx ibc-transfer transfer transfer channel-1 $GAIA_ADDRESS_1 1000ustrd --home /stride/.strided --keyring-backend test --from val1 --chain-id STRIDE_1 -y
 
 sleep 9
