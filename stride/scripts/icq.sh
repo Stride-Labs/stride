fe82c13e8a21f3cf31ee8a2e86bc187e314955c8
Riley Edmunds <riley@stridelabs.co>
2022-05-22 22:03:04 -0400

    TEST-50 add empty tester

---
+++ stride/scripts/icq.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+echo "Testing icq to read gaia address ${GAIA_ADDRESS_1} from stride"
+# strided query interchainquery interchainaccounts connection-0 $GAIA_ADDRESS_1 --home /stride/.strided --keyring-backend test --from val1 --chain-id STRIDE_1 -y
+sleep 9
+
