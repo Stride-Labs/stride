efa0ee3a8e6abb5984f9e3ac3f4b2d7cf3251f55
Riley Edmunds <riley@stridelabs.co>
2022-05-27 04:55:52 -0400

    TEST-50 basic test for ICQ added (no return values, only checks icqs don't fail

--- stride/scripts/icq.sh
+++ stride/scripts/icq.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
 
 echo "Testing icq to read gaia address ${GAIA_ADDRESS_1} from stride"
-# strided query interchainquery query-balance connection-0 $GAIA_ADDRESS_1 --home /stride/.strided --keyring-backend test --from val1 --chain-id STRIDE_1 -y
-strided tx interchainquery query-balance GAIA_1 cosmos1t2aqq3c6mt8fa6l5ady44manvhqf77sywjcldv uatom --connection-id connection-1 --from val1 --home /stride/.strided --keyring-backend test --chain-id STRIDE_1 -y
+# TXHASH=$(strided tx interchainquery query-balance GAIA_1 cosmos1t2aqq3c6mt8fa6l5ady44manvhqf77sywjcldv uatom --connection-id connection-1  --home /stride/.strided --keyring-backend test --chain-id STRIDE_1 -y --from val1 | tail -c 65)
+
+TXHASH=$(strided tx interchainquery query-balance GAIA_1 $GAIA_ADDRESS_1 uatom --connection-id connection-1  --home /stride/.strided --keyring-backend test --chain-id STRIDE_1 -y --from val1 | tail -c 65)
+strided q tx $TXHASH
 sleep 9
 
