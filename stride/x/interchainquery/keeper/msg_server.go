bc6e52f0495429bb2636629278a1ddc55c8d3376
Riley Edmunds <riley@stridelabs.co>
2022-05-28 19:03:17 -0400

    TEST50 clean up stride side of icq, just needs icq relayer; add initial icq dockerfile (not functional yet, needs configuration)

--- stride/x/interchainquery/keeper/msg_server.go
+++ stride/x/interchainquery/keeper/msg_server.go
@@ -5,6 +5,7 @@ import (
 
 	"github.com/Stride-Labs/stride/x/interchainquery/types"
 	sdk "github.com/cosmos/cosmos-sdk/types"
+	banktypes "github.com/cosmos/cosmos-sdk/x/bank/types"
 )
 
 type msgServer struct {
@@ -101,15 +102,32 @@ func (k msgServer) QueryBalance(goCtx context.Context, msg *types.MsgQueryBalanc
 	// 	callback interface{})
 
 	// TODO do we need to add a callback type for this to work?
-	// TODO
 	var cb Callback = func(k Keeper, ctx sdk.Context, args []byte, query types.Query) error {
+		panic(err)
+
 		k.Logger(ctx).Info("[TEMP] printing inside the querybalance callback")
+		// return k.SetAccountBalance(ctx, zone, query.QueryParameters["address"],
+		//  args)
+
+		// address := query.QueryParameters["address"]
+
+		queryResult := args
+		queryRes := banktypes.QueryAllBalancesResponse{}
+		err := k.cdc.UnmarshalJSON(queryResult, &queryRes)
+		if err != nil {
+			k.Logger(ctx).Error("Unable to unmarshal balances info for zone", "err", err)
+			return err
+		}
+		k.Logger(ctx).Info("[TEMP] printing result from query-balances:", queryRes.Balances)
+
+		ctx.EventManager().EmitEvents(sdk.Events{
+			sdk.NewEvent(
+				sdk.EventTypeMessage,
+				sdk.NewAttribute("balances", queryRes.Balances.String()),
+			),
+		})
 		return nil
 	}
-
-	// var cb Callback = func(k Keeper, ctx sdk.Context, args []byte, query types.Query) error {
-	// 	return k.SetAccountBalance(ctx, zone, query.QueryParameters["address"], args)
-	// }
 
 	k.Keeper.MakeRequest(
 		ctx,
@@ -120,89 +138,23 @@ func (k msgServer) QueryBalance(goCtx context.Context, msg *types.MsgQueryBalanc
 		// "cosmos.bank.v1beta1.Query/Balance",
 		"cosmos.bank.v1beta1.Query/AllBalances",
 		// pass in arguments to the query here
-		// map[string]string{"address": msg.Address},
-		// TODO revert from hardcode
-		map[string]string{"address": "cosmos1t2aqq3c6mt8fa6l5ady44manvhqf77sywjcldv"},
-		//TODO set this to something sensible
+		map[string]string{"address": msg.Address},
+		//TODO set this window to something sensible
 		sdk.NewInt(25),
 		types.ModuleName,
 		cb,
 	)
-
-	// var packet types.CreatePairPacketData
-
-	// packet.SourceDenom = msg.SourceDenom
-	// packet.TargetDenom = msg.TargetDenom
-
-	// // Transmit the packet
-	// err := k.TransmitCreatePairPacket(
-	// 	ctx,
-	// 	packet,
-	// 	msg.Port,
-	// 	msg.ChannelID,
-	// 	clienttypes.ZeroHeight(),
-	// 	msg.TimeoutTimestamp,
-	// )
-	// if err != nil {
-	// 	return nil, err
-	// }
-
 	// TODO how do we display the result here (from the target chain)
 	// 		=> for now, just use ctx logging:
 	k.Logger(ctx).Info("ICQ submitted; output = ") //, outputFromICQ)
 
+	ctx.EventManager().EmitEvents(sdk.Events{
+		sdk.NewEvent(
+			sdk.EventTypeMessage,
+			sdk.NewAttribute("balances", "TEST"),
+		),
+	})
+
 	// return; usually a response object or nil
 	return &types.MsgQueryBalanceResponse{}, nil
 }
-
-// func (k msgServer) LiquidStake(goCtx context.Context, msg *types.MsgLiquidStake) (*types.MsgLiquidStakeResponse, error) {
-// 	ctx := sdk.UnwrapSDKContext(goCtx)
-
-// 	// Init variables
-// 	// get the sender address
-// 	sender, err := sdk.AccAddressFromBech32(msg.Creator)
-// 	if err != nil {
-// 		k.Logger(ctx).Info("Invalid address")
-// 		return nil, sdkerrors.Wrap(sdkerrors.ErrInvalidAddress, "address invalid")
-// 	}
-// 	// get the coins to send, they need to be in the format {amount}{denom}
-// 	// NOTE: int is an int32 or int64 (depending on machine type) so converting from int32 -> int
-// 	// is safe. The converse is not true.
-// 	coinString := strconv.Itoa(int(msg.Amount)) + msg.Denom
-// 	coins, err := sdk.ParseCoinsNormalized(coinString)
-// 	if err != nil {
-// 		return nil, sdkerrors.Wrapf(sdkerrors.ErrInvalidType, "failed to parse %s coins", coins)
-// 	}
-
-// 	// Safety checks
-// 	// ensure Amount is non-negative, liquid staking 0 or less tokens is invalid
-// 	if msg.Amount < 1 {
-// 		k.Logger(ctx).Info("amount must be non-negative")
-// 		return nil, sdkerrors.Wrapf(sdkerrors.ErrInvalidCoins, "amount must be non-negative")
-// 	}
-// 	// check that the token is an IBC token
-// 	isIbcToken := types.IsIBCToken(msg.Denom)
-// 	if !isIbcToken {
-// 		k.Logger(ctx).Info("invalid token denom")
-// 		return nil, sdkerrors.Wrapf(types.ErrInvalidToken, "invalid token denom (%s)", msg.Denom)
-// 	}
-
-// 	// deposit `amount` of `denom` token to the stakeibc module
-// 	// NOTE: Should we add an additional check here? This is a pretty important line of code
-// 	// NOTE: If sender doesn't have enough coins, this panics (error is hard to interpret)
-// 	sdkerror := k.bankKeeper.SendCoinsFromAccountToModule(ctx, sender, types.ModuleName, coins)
-// 	if sdkerror != nil {
-// 		k.Logger(ctx).Error("failed to send tokens from Account to Module")
-// 		panic(sdkerror)
-// 	}
-
-// 	// mint user `amount` of the corresponding stAsset
-// 	// NOTE: We should ensure that denoms are unique - we don't want anyone spoofing denoms
-// 	err = k.MintStAsset(ctx, sender, msg.Amount, msg.Denom)
-// 	if err != nil {
-// 		k.Logger(ctx).Info("failed to send tokens from Account to Module")
-// 		return nil, sdkerrors.Wrap(sdkerrors.ErrInvalidCoins, "failed to mint stAssets to user")
-// 	}
-
-// 	return &types.MsgLiquidStakeResponse{}, nil
-// }
