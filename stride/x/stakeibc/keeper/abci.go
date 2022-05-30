3dbf22affbeb2b71f65bd9cb0385bbd1711f88b5
Aidan Salzmann <aidan96@gmail.com>
2022-05-31 01:15:25 +0200

    [TEST-12][3] Add DelegateOnHost, SubmitTxs functions

---
+++ stride/x/stakeibc/keeper/abci.go
@@ -0,0 +1,15 @@
+package keeper
+
+import (
+	"time"
+
+	"github.com/Stride-Labs/stride/x/stakeibc/types"
+	"github.com/cosmos/cosmos-sdk/telemetry"
+	sdk "github.com/cosmos/cosmos-sdk/types"
+)
+
+// BeginBlocker of stakeibc module
+func (k Keeper) BeginBlocker(ctx sdk.Context) {
+	defer telemetry.ModuleMeasureSince(types.ModuleName, time.Now(), telemetry.MetricKeyBeginBlocker)
+
+}
