e1a028a8d5dc0b4eb92fc4fffef0b2664e0a06be
Aidan Salzmann <aidan96@gmail.com>
2022-05-31 19:54:00 +0200

    add RegisterHostZone logic

--- stride/x/stakeibc/keeper/keeper.go
+++ stride/x/stakeibc/keeper/keeper.go
@@ -15,6 +15,7 @@ import (
 	paramtypes "github.com/cosmos/cosmos-sdk/x/params/types"
 	icacontrollerkeeper "github.com/cosmos/ibc-go/v3/modules/apps/27-interchain-accounts/controller/keeper"
 	ibckeeper "github.com/cosmos/ibc-go/v3/modules/core/keeper"
+	ibctmtypes "github.com/cosmos/ibc-go/v3/modules/light-clients/07-tendermint/types"
 )
 
 type (
@@ -78,3 +79,31 @@ func (k Keeper) Logger(ctx sdk.Context) log.Logger {
 func (k *Keeper) ClaimCapability(ctx sdk.Context, cap *capabilitytypes.Capability, name string) error {
 	return k.scopedKeeper.ClaimCapability(ctx, cap, name)
 }
+
+func (k Keeper) GetChainID(ctx sdk.Context, connectionID string) (string, error) {
+	conn, found := k.ibcKeeper.ConnectionKeeper.GetConnection(ctx, connectionID)
+	if !found {
+		return "", fmt.Errorf("invalid connection id, \"%s\" not found", connectionID)
+	}
+	clientState, found := k.ibcKeeper.ClientKeeper.GetClientState(ctx, conn.ClientId)
+	if !found {
+		return "", fmt.Errorf("client id \"%s\" not found for connection \"%s\"", conn.ClientId, connectionID)
+	}
+	client, ok := clientState.(*ibctmtypes.ClientState)
+	if !ok {
+		return "", fmt.Errorf("invalid client state for client \"%s\" on connection \"%s\"", conn.ClientId, connectionID)
+	}
+
+	return client.ChainId, nil
+}
+
+func (k Keeper) GetConnectionId(ctx sdk.Context, portId string) (string, error) {
+	icas := k.icaControllerKeeper.GetAllInterchainAccounts(ctx)
+	for _, ica := range icas {
+		if ica.PortId == portId {
+			return ica.ConnectionId, nil
+		}
+	}
+	return "", fmt.Errorf("portId %s has no associated connectionId", portId)
+}
+
---
+++ stride/x/stakeibc/keeper/msg_server_register_host_zone.go
@@ -0,0 +1,76 @@
+package keeper
+
+import (
+	"context"
+	"fmt"
+
+	"github.com/Stride-Labs/stride/x/stakeibc/types"
+
+	sdk "github.com/cosmos/cosmos-sdk/types"
+)
+
+// TODO(TEST-53): Remove this pre-launch (no need for clients to create / interact with ICAs)
+func (k Keeper) RegisterHostZone(goCtx context.Context, msg *types.MsgRegisterHostZone) (*types.MsgRegisterHostZoneResponse, error) {
+	ctx := sdk.UnwrapSDKContext(goCtx)
+
+	_ = ctx
+
+	// Get chain id from connection
+	chainId, err := k.GetChainID(ctx, msg.ConnectionId)
+	if err != nil {
+		return nil, fmt.Errorf("unable to obtain chain id: %w", err)
+	}
+
+	// get zone
+	_, found := k.GetHostZone(ctx, chainId)
+	if found {
+		return nil, fmt.Errorf("invalid chain id, zone for \"%s\" already registered", chainId)
+	}
+
+	// set the zone
+	zone := types.HostZone{
+		ChainId: chainId,
+		ConnectionId: msg.ConnectionId,
+		LocalDenom: msg.LocalDenom,
+		BaseDenom: msg.BaseDenom,
+	}
+
+	// write the zone back to the store
+	k.SetHostZone(ctx, zone)
+
+	// generate delegate account
+	// NOTE: in the future, if we implement proxy governance, we'll need many more delegate accounts
+	delegateAccount := chainId + ".delegate"
+	if err := k.icaControllerKeeper.RegisterInterchainAccount(ctx, zone.ConnectionId, delegateAccount); err != nil {
+		return nil, err
+	}
+
+	// generate fee account
+	feeAccount := chainId + ".fee"
+	if err := k.icaControllerKeeper.RegisterInterchainAccount(ctx, zone.ConnectionId, feeAccount); err != nil {
+		return nil, err
+	}
+
+	// generate withdrawal account
+	withdrawalAccount := chainId + ".withdrawal"
+	if err := k.icaControllerKeeper.RegisterInterchainAccount(ctx, zone.ConnectionId, withdrawalAccount); err != nil {
+		return nil, err
+	}
+
+	// TODO(TEST-XX): Set validators on the host zone, either using ICQ + intents or a WL 
+
+	// emit events
+	ctx.EventManager().EmitEvents(sdk.Events{
+		sdk.NewEvent(
+			sdk.EventTypeMessage,
+			sdk.NewAttribute(sdk.AttributeKeyModule, types.AttributeValueCategory),
+		),
+		sdk.NewEvent(
+			types.EventTypeRegisterZone,
+			sdk.NewAttribute(types.AttributeKeyConnectionId, msg.ConnectionId),
+			sdk.NewAttribute(types.AttributeKeyConnectionId, chainId),
+		),
+	})
+
+	return &types.MsgRegisterHostZoneResponse{}, nil
+}
