6e83ae5300f6be60b6a38582fd9ae2ccb62d06e2
Aidan Salzmann <aidan96@gmail.com>
2022-06-06 15:43:47 +0200

    [TEST-6] Add RedeemStake message

---
+++ x/stakeibc/client/cli/tx_redeem_stake.go
@@ -0,0 +1,49 @@
+package cli
+
+import (
+	"strconv"
+
+	"github.com/Stride-Labs/stride/x/stakeibc/types"
+	"github.com/cosmos/cosmos-sdk/client"
+	"github.com/cosmos/cosmos-sdk/client/flags"
+	"github.com/cosmos/cosmos-sdk/client/tx"
+	"github.com/spf13/cast"
+	"github.com/spf13/cobra"
+)
+
+var _ = strconv.Itoa(0)
+
+
+func CmdRedeemStake() *cobra.Command {
+	cmd := &cobra.Command{
+		Use:  "redeem-stake [amount] [denom]",
+		Short: "Broadcast message redeem-stake",
+		Args: cobra.ExactArgs(2),
+		RunE: func(cmd *cobra.Command, args []string) error {
+			argAmount, err := cast.ToInt32E(args[0])
+			if err != nil {
+				return err
+			}
+			argDenom := args[1]
+
+			clientCtx, err := client.GetClientTxContext(cmd)
+			if err != nil {
+				return err
+			}
+
+			msg := types.NewMsgRedeemStake(
+				clientCtx.GetFromAddress().String(),
+				argAmount,
+				argDenom,
+			)
+			if err := msg.ValidateBasic(); err != nil {
+				return err
+			}
+			return tx.GenerateOrBroadcastTxCLI(clientCtx, cmd.Flags(), msg)
+		},
+	}
+
+	flags.AddTxFlagsToCmd(cmd)
+
+	return cmd
+}
--- x/stakeibc/handler.go
+++ x/stakeibc/handler.go
@@ -29,6 +29,9 @@ func NewHandler(k keeper.Keeper) sdk.Handler {
 		case *types.MsgRegisterHostZone:
 			res, err := msgServer.RegisterHostZone(sdk.WrapSDKContext(ctx), msg)
 			return sdk.WrapServiceResult(ctx, res, err)
+		case *types.MsgRedeemStake:
+			res, err := msgServer.RedeemStake(sdk.WrapSDKContext(ctx), msg)
+			return sdk.WrapServiceResult(ctx, res, err)
 			// this line is used by starport scaffolding # 1
 		default:
 			errMsg := fmt.Sprintf("unrecognized %s message type: %T", types.ModuleName, msg)
