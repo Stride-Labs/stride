739fcfbd4c6068f665994bb5df3c73cfd5ff7c03
Riley Edmunds <riley@stridelabs.co>
2022-06-06 12:50:50 -0400

    codec path fixed

--- stride/x/interchainquery/types/codec.go
+++ stride/x/interchainquery/types/codec.go
@@ -14,7 +14,7 @@ var (
 )
 
 func RegisterLegacyAminoCodec(cdc *codec.LegacyAmino) {
-	cdc.RegisterConcrete(&MsgSubmitQueryResponse{}, "/stride.interchainquery.v1.MsgSubmitQueryResponse", nil)
+	cdc.RegisterConcrete(&MsgSubmitQueryResponse{}, "/stride.interchainquery.MsgSubmitQueryResponse", nil)
 }
 
 func RegisterInterfaces(registry types.InterfaceRegistry) {
