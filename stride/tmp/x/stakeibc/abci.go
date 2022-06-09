b75b3458a9893d3703088a14c4ec07fff6cb0a51
Aidan Salzmann <aidan96@gmail.com>
2022-06-09 16:35:52 +0200

    add lower stride

---
+++ stride/tmp/x/stakeibc/abci.go
@@ -0,0 +1,16 @@
+package stakeibc
+
+import (
+	"time"
+
+	"github.com/Stride-Labs/stride/x/stakeibc/keeper"
+	"github.com/Stride-Labs/stride/x/stakeibc/types"
+	"github.com/cosmos/cosmos-sdk/telemetry"
+	sdk "github.com/cosmos/cosmos-sdk/types"
+)
+
+// BeginBlocker of stakeibc module
+func BeginBlocker(ctx sdk.Context, k keeper.Keeper, bk types.BankKeeper) {
+	defer telemetry.ModuleMeasureSince(types.ModuleName, time.Now(), telemetry.MetricKeyBeginBlocker)
+
+}
---
+++ stride/tmp/x/stakeibc/client/cli/query.go
@@ -0,0 +1,41 @@
+package cli
+
+import (
+	"fmt"
+	// "strings"
+
+	"github.com/spf13/cobra"
+
+	"github.com/cosmos/cosmos-sdk/client"
+	// "github.com/cosmos/cosmos-sdk/client/flags"
+	// sdk "github.com/cosmos/cosmos-sdk/types"
+
+	"github.com/Stride-Labs/stride/x/stakeibc/types"
+)
+
+// GetQueryCmd returns the cli query commands for this module
+func GetQueryCmd(queryRoute string) *cobra.Command {
+	// Group stakeibc queries under a subcommand
+	cmd := &cobra.Command{
+		Use:                        types.ModuleName,
+		Short:                      fmt.Sprintf("Querying commands for the %s module", types.ModuleName),
+		DisableFlagParsing:         true,
+		SuggestionsMinimumDistance: 2,
+		RunE:                       client.ValidateCmd,
+	}
+
+	cmd.AddCommand(CmdQueryParams())
+	cmd.AddCommand(CmdShowValidator())
+	cmd.AddCommand(CmdShowDelegation())
+	cmd.AddCommand(CmdShowMinValidatorRequirements())
+	cmd.AddCommand(CmdShowICAAccount())
+	cmd.AddCommand(CmdListHostZone())
+	cmd.AddCommand(CmdShowHostZone())
+	cmd.AddCommand(CmdListDepositRecord())
+	cmd.AddCommand(CmdShowDepositRecord())
+	cmd.AddCommand(CmdModuleAddress())
+	cmd.AddCommand(CmdShowInterchainAccount())
+	// this line is used by starport scaffolding # 1
+
+	return cmd
+}
