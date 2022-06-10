22bf904d87c73d8fcbc199b7d8d73d3064c71dcf
Riley Edmunds <riley@stridelabs.co>
2022-06-10 17:27:51 -0400

    clean up ibctoken/hosttoken

--- x/stakeibc/keeper/hooks.go
+++ x/stakeibc/keeper/hooks.go
@@ -37,7 +37,7 @@ func (k Keeper) BeforeEpochStart(ctx sdk.Context, epochIdentifier string, epochN
 				delegateAddress := delegateAccount.Address
 				// TODO TEST-59 grab proper port name and channel name
 				timeoutHeight := clienttypes.NewHeight(0, 500)
-				transferCoin := sdk.NewCoin(depositRecord.Denom, sdk.NewInt(int64(depositRecord.Amount)))
+				transferCoin := sdk.NewCoin(hostZone.GetIBCDenom(), sdk.NewInt(int64(depositRecord.Amount)))
 				goCtx := sdk.WrapSDKContext(ctx)
 				msg := ibctypes.NewMsgTransfer("transfer", "channel-1", transferCoin, addr, delegateAddress, timeoutHeight, 0)
 				_, err := k.transferKeeper.Transfer(goCtx, msg)
