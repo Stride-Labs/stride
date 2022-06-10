087a314b30f50b4ae4f1b40bfb3934a65e2c682f
Riley Edmunds <riley@stridelabs.co>
2022-06-10 02:09:01 -0400

    init script fixed!

--- scripts/init.sh
+++ scripts/init.sh
@@ -141,17 +141,18 @@ $ICQ_RUN keys restore test "$ICQ_STRIDE_KEY" --chain stride-testnet
 $ICQ_RUN keys restore test "$ICQ_GAIA_KEY" --chain gaia-testnet
 
 echo "\nICQ addresses for Stride and Gaia:"
-ICQ_ADDRESS_STRIDE="stride12vfkpj7lpqg0n4j68rr5kyffc6wu55dzrjw3fe"
-echo $ICQ_ADDRESS_STRIDE
-ICQ_ADDRESS_GAIA="cosmos12vfkpj7lpqg0n4j68rr5kyffc6wu55dzrjw3fe"
-echo $ICQ_ADDRESS_GAIA
+ICQ_ADDRESS_STRIDE="stride12vfkpj7lpqg0n4j68rr5kyffc6wu55dzqewda4"
+# echo $ICQ_ADDRESS_STRIDE
+ICQ_ADDRESS_GAIA="cosmos1g6qdx6kdhpf000afvvpte7hp0vnpzapuyxp8uf"
+# echo $ICQ_ADDRESS_GAIA
 
-$STR1_EXEC tx bank send val1 $ICQ_ADDRESS_STRIDE 5000000ustrd --chain-id $main_chain -y --keyring-backend=test --home /stride/.strided
-$GAIA1_EXEC tx bank send gval1 $ICQ_ADDRESS_GAIA 5000000uatom --chain-id $main_gaia_chain -y --keyring-backend=test --home /gaia/.gaiad
+STR1_EXEC="docker-compose --ansi never exec -T stride1 strided --home /stride/.strided --chain-id STRIDE"
+$STR1_EXEC tx bank send val1 $ICQ_ADDRESS_STRIDE 5000000ustrd --chain-id $main_chain -y --keyring-backend test --home /stride/.strided
+GAIA1_EXEC="docker-compose --ansi never exec -T gaia1 gaiad --home /gaia/.gaiad"
+$GAIA1_EXEC tx bank send gval1 $ICQ_ADDRESS_GAIA 5000000uatom --chain-id $main_gaia_chain -y --keyring-backend test --home /gaia/.gaiad
 
 echo "\nLaunch interchainquery relayer"
 docker-compose up --force-recreate -d icq
-
 
 # Register host zone
 # ICA staking test
