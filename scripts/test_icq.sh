881ec426fe1c0bdce9c6cf1fd3c18f17663d1d5f
Riley Edmunds <riley@stridelabs.co>
2022-06-10 01:28:01 -0400

    add test_icq.sh tests!

--- scripts/test_icq.sh
+++ scripts/test_icq.sh
@@ -1,10 +1,29 @@
 #!/bin/bash
+STR1_EXEC="docker-compose --ansi never exec -T stride1 strided --home /stride/.strided --chain-id STRIDE"
+GAIA1_EXEC="docker-compose --ansi never exec -T gaia1 gaiad --home /gaia/.gaiad"
+ICQ_EXEC="docker-compose --ansi never exec -T icq interchain-queries"
 
-echo "Testing icq to read gaia address ${GAIA_ADDRESS_1} from stride"
-# strided tx interchainquery query-balance GAIA_1 cosmos1t2aqq3c6mt8fa6l5ady44manvhqf77sywjcldv uatom --connection-id connection-1  --home /stride/.strided --keyring-backend test --chain-id STRIDE_1 -y --from val1
-strided tx bank send val1 stride12vfkpj7lpqg0n4j68rr5kyffc6wu55dzqewda4 2500000ustrd --home /stride/.strided --keyring-backend test --chain-id STRIDE_1 -y
-sleep 5
-TXHASH=$(strided tx interchainquery query-balance GAIA_1 $GAIA_ADDRESS_1 uatom --connection-id connection-1  --home /stride/.strided --keyring-backend test --chain-id STRIDE_1 -y --from val1 | tail -c 65)
-strided q tx $TXHASH
-sleep 9
+# Set up: fund account on Stride
+$STR1_EXEC tx bank send val1 stride12vfkpj7lpqg0n4j68rr5kyffc6wu55dzqewda4 5000000ustrd \
+     --keyring-backend test -y
+sleep 10
 
+# # Test: query balance
+$STR1_EXEC tx interchainquery query-balance GAIA cosmos1t2aqq3c6mt8fa6l5ady44manvhqf77sywjcldv uatom \
+    --connection-id connection-1 --keyring-backend test -y --from val1
+sleep 15
+# $STR1_EXEC q txs --events message.module=interchainquery&limit=1
+$STR1_EXEC q txs --events message.action=/stride.interchainquery.MsgSubmitQueryResponse&limit=1 # | jq '.logs'
+
+
+# Test: query exchange rate
+$STR1_EXEC tx interchainquery query-exchangerate GAIA --keyring-backend test -y --from val1
+sleep 15
+# $STR1_EXEC q txs --events message.module=interchainquery&limit=1
+$STR1_EXEC q txs --events message.module=interchainquery&limit=1 # | jq '.logs'
+
+# # Test: query delegated balance | NOTE: need to instantiate delegationAccount before this test will work!
+# $STR1_EXEC tx interchainquery query-delegatedbalance GAIA --keyring-backend test -y --from val1
+# sleep 15
+# # $STR1_EXEC q txs --events message.module=interchainquery&limit=1
+# $STR1_EXEC q txs --events message.module=interchainquery&limit=1 # | jq '.logs'
