5c284022d1eac423153f5f65cce1df0060ba10f3
Riley Edmunds <riley@stridelabs.co>
2022-06-10 13:08:19 -0400

    fix register-host-zone denom

--- scripts/init.sh
+++ scripts/init.sh
@@ -158,7 +158,7 @@ docker-compose up --force-recreate -d icq
 # Register host zone
 # ICA staking test
 # first register host zone for ATOM chain
-IBCATOM='ibc/C4CFF46FD6DE35CA4CF4CE031E643C8FDC9BA4B99AE598E9B0ED98FE3A2319F9'
+IBCATOM='atom'
 docker-compose --ansi never exec -T $main_node strided tx stakeibc register-host-zone connection-0 $IBCATOM "st${IBCATOM}" --chain-id $main_chain --home /stride/.strided --keyring-backend test --from val1 --gas 500000 -y
 #############################################################################################################################
 # source ${SCRIPT_DIR}/ica_stake.sh
