0fe9ba95e4a4cdbf7199ad1498cc84fa3e4ff931
sampocs <sam.pochyly@gmail.com>
2022-06-15 13:19:07 -0500

    testing actions from new branch

--- scripts/testnet/droplet.tf
+++ scripts/testnet/droplet.tf
@@ -2,7 +2,7 @@
 terraform {
   required_providers {
     google = {
-      source = "hashicorp/google"
+      source  = "hashicorp/google"
       version = "4.24.0"
     }
   }
@@ -23,7 +23,7 @@ resource "google_compute_address" "node1" {
   region = "us-central1"
 }
 resource "google_compute_address" "node2" {
-  name = "node2"
+  name   = "node2"
   region = "europe-west6"
 }
 resource "google_compute_address" "node3" {
@@ -36,14 +36,14 @@ resource "google_compute_address" "seed" {
 }
 
 resource "google_compute_instance" "droplet-node1" {
-  name         = "droplet-node1"
-  machine_type = "e2-standard-4"
-  zone         = "us-central1-b"
-  tags         = ["ssh"]
+  name                      = "droplet-node1"
+  machine_type              = "e2-standard-4"
+  zone                      = "us-central1-b"
+  tags                      = ["ssh"]
   allow_stopping_for_update = true
 
   metadata = {
-    enable-oslogin = "TRUE"
+    enable-oslogin            = "TRUE"
     gce-container-declaration = "spec:\n  containers:\n    - name: node\n      image: 'gcr.io/stride-nodes/testnet:droplet_node1'\n      stdin: false\n      tty: false\n  restartPolicy: Always\n"
   }
   boot_disk {
@@ -60,24 +60,24 @@ resource "google_compute_instance" "droplet-node1" {
   }
 
   service_account {
-    scopes = [ "https://www.googleapis.com/auth/devstorage.read_only",
-        "https://www.googleapis.com/auth/logging.write",
-        "https://www.googleapis.com/auth/monitoring.write",
-        "https://www.googleapis.com/auth/servicecontrol",
-        "https://www.googleapis.com/auth/service.management.readonly",
-        "https://www.googleapis.com/auth/trace.append"]
+    scopes = ["https://www.googleapis.com/auth/devstorage.read_only",
+      "https://www.googleapis.com/auth/logging.write",
+      "https://www.googleapis.com/auth/monitoring.write",
+      "https://www.googleapis.com/auth/servicecontrol",
+      "https://www.googleapis.com/auth/service.management.readonly",
+    "https://www.googleapis.com/auth/trace.append"]
   }
 }
 
 resource "google_compute_instance" "droplet-node2" {
-  name         = "droplet-node2"
-  machine_type = "e2-standard-4"
-  zone         = "europe-west6-b"
-  tags         = ["ssh"]
+  name                      = "droplet-node2"
+  machine_type              = "e2-standard-4"
+  zone                      = "europe-west6-b"
+  tags                      = ["ssh"]
   allow_stopping_for_update = true
 
   metadata = {
-    enable-oslogin = "TRUE"
+    enable-oslogin            = "TRUE"
     gce-container-declaration = "spec:\n  containers:\n    - name: node\n      image: 'gcr.io/stride-nodes/testnet:droplet_node2'\n      stdin: false\n      tty: false\n  restartPolicy: Always\n"
   }
   boot_disk {
@@ -94,24 +94,24 @@ resource "google_compute_instance" "droplet-node2" {
   }
 
   service_account {
-    scopes = [ "https://www.googleapis.com/auth/devstorage.read_only",
-        "https://www.googleapis.com/auth/logging.write",
-        "https://www.googleapis.com/auth/monitoring.write",
-        "https://www.googleapis.com/auth/servicecontrol",
-        "https://www.googleapis.com/auth/service.management.readonly",
-        "https://www.googleapis.com/auth/trace.append"]
+    scopes = ["https://www.googleapis.com/auth/devstorage.read_only",
+      "https://www.googleapis.com/auth/logging.write",
+      "https://www.googleapis.com/auth/monitoring.write",
+      "https://www.googleapis.com/auth/servicecontrol",
+      "https://www.googleapis.com/auth/service.management.readonly",
+    "https://www.googleapis.com/auth/trace.append"]
   }
 }
 
 resource "google_compute_instance" "droplet-node3" {
-  name         = "droplet-node3"
-  machine_type = "e2-standard-4"
-  zone         = "us-east4-b"
-  tags         = ["ssh"]
+  name                      = "droplet-node3"
+  machine_type              = "e2-standard-4"
+  zone                      = "us-east4-b"
+  tags                      = ["ssh"]
   allow_stopping_for_update = true
 
   metadata = {
-    enable-oslogin = "TRUE"
+    enable-oslogin            = "TRUE"
     gce-container-declaration = "spec:\n  containers:\n    - name: node\n      image: 'gcr.io/stride-nodes/testnet:droplet_node3'\n      stdin: false\n      tty: false\n  restartPolicy: Always\n"
   }
   boot_disk {
@@ -128,24 +128,24 @@ resource "google_compute_instance" "droplet-node3" {
   }
 
   service_account {
-    scopes = [ "https://www.googleapis.com/auth/devstorage.read_only",
-        "https://www.googleapis.com/auth/logging.write",
-        "https://www.googleapis.com/auth/monitoring.write",
-        "https://www.googleapis.com/auth/servicecontrol",
-        "https://www.googleapis.com/auth/service.management.readonly",
-        "https://www.googleapis.com/auth/trace.append"]
+    scopes = ["https://www.googleapis.com/auth/devstorage.read_only",
+      "https://www.googleapis.com/auth/logging.write",
+      "https://www.googleapis.com/auth/monitoring.write",
+      "https://www.googleapis.com/auth/servicecontrol",
+      "https://www.googleapis.com/auth/service.management.readonly",
+    "https://www.googleapis.com/auth/trace.append"]
   }
 }
 
 resource "google_compute_instance" "droplet-seed" {
-  name         = "droplet-seed"
-  machine_type = "e2-standard-4"
-  zone         = "us-west1-b"
-  tags         = ["ssh"]
+  name                      = "droplet-seed"
+  machine_type              = "e2-standard-4"
+  zone                      = "us-west1-b"
+  tags                      = ["ssh"]
   allow_stopping_for_update = true
 
   metadata = {
-    enable-oslogin = "TRUE"
+    enable-oslogin            = "TRUE"
     gce-container-declaration = "spec:\n  containers:\n    - name: node\n      image: 'gcr.io/stride-nodes/testnet:droplet_seed'\n      stdin: false\n      tty: false\n  restartPolicy: Always\n"
   }
   boot_disk {
@@ -162,11 +162,47 @@ resource "google_compute_instance" "droplet-seed" {
   }
 
   service_account {
-    scopes = [ "https://www.googleapis.com/auth/devstorage.read_only",
-        "https://www.googleapis.com/auth/logging.write",
-        "https://www.googleapis.com/auth/monitoring.write",
-        "https://www.googleapis.com/auth/servicecontrol",
-        "https://www.googleapis.com/auth/service.management.readonly",
-        "https://www.googleapis.com/auth/trace.append"]
+    scopes = ["https://www.googleapis.com/auth/devstorage.read_only",
+      "https://www.googleapis.com/auth/logging.write",
+      "https://www.googleapis.com/auth/monitoring.write",
+      "https://www.googleapis.com/auth/servicecontrol",
+      "https://www.googleapis.com/auth/service.management.readonly",
+    "https://www.googleapis.com/auth/trace.append"]
   }
 }
+
+# resource "google_compute_instance" "test-droplet-sp" {
+#   name                      = "test-droplet-sp"
+#   machine_type              = "e2-standard-4"
+#   zone                      = "us-central1-a"
+#   tags                      = ["ssh"]
+#   allow_stopping_for_update = true
+
+#   metadata = {
+#     enable-oslogin            = "TRUE"
+#     gce-container-declaration = "spec:\n  containers:\n    - name: node\n      image: 'gcr.io/stride-nodes/testnet:droplet_node1'\n      stdin: false\n      tty: false\n  restartPolicy: Always\n"
+#   }
+#   boot_disk {
+#     initialize_params {
+#       image = "cos-cloud/cos-97-lts"
+#     }
+#   }
+
+#   network_interface {
+#     network = "default"
+#     access_config {
+#       nat_ip = google_compute_address.node1.address
+#     }
+#   }
+
+#   service_account {
+#     scopes = [
+#       "https://www.googleapis.com/auth/devstorage.read_only",
+#       "https://www.googleapis.com/auth/logging.write",
+#       "https://www.googleapis.com/auth/monitoring.write",
+#       "https://www.googleapis.com/auth/servicecontrol",
+#       "https://www.googleapis.com/auth/service.management.readonly",
+#       "https://www.googleapis.com/auth/trace.append"
+#     ]
+#   }
+# }
