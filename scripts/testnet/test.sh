033d1132445f8b819add381d10a29c431cbe6ba2
vish-stride <vishal@stridelabs.co>
2022-06-14 21:08:09 -0400

    attempt 8

---
+++ scripts/testnet/test.sh
@@ -0,0 +1,6 @@
+
+docker pull stridelabs/internal:droplet_node1
+
+docker tag stridelabs/internal:droplet_node1 gcr.io/stride-nodes/testnet:droplet_node1
+
+docker push gcr.io/stride-nodes/testnet:droplet_node1
