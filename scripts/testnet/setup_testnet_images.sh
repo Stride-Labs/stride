a54eea59aed1a1af69e0d7fb186c06c5fd70b235
vish-stride <vishal@stridelabs.co>
2022-06-14 22:51:01 -0400

    attempt 12

--- scripts/testnet/setup_testnet_images.sh
+++ scripts/testnet/setup_testnet_images.sh
@@ -28,22 +28,25 @@ for i in ${!STRIDE_DOCKER_NAMES[@]}; do
     moniker=${STRIDE_MONIKERS[i]}
     val_acct=${VAL_ACCTS[i]}
     st_cmd=${ST_CMDS[i]}
-    echo "\t$node_name"
     $st_cmd init $moniker --chain-id $STRIDE_CHAIN --overwrite 2> /dev/null
     sed -i -E 's|"stake"|"ustrd"|g' "${STATE}/${node_name}/config/genesis.json"
-    # add VALidator account
-    echo $vkey | $st_cmd keys add $val_acct --keyring-backend=test >> $STATE/keys.txt 2>&1
-    # get validator address
-    VAL_ADDR=$($st_cmd keys show $val_acct --keyring-backend test -a)
-    # add money for this validator account
-    $st_cmd add-genesis-account ${VAL_ADDR} $VAL_TOKENS
-    # actually set this account as a validator
-    yes | $st_cmd gentx $val_acct $STAKE_TOKENS --chain-id $STRIDE_CHAIN --keyring-backend test 2> /dev/null
+    if [ $i -ne $SEED_ID ]
+    then
+        # add VALidator account
+        echo $vkey | $st_cmd keys add $val_acct --keyring-backend=test >> $STATE/keys.txt 2>&1
+        # get validator address
+        VAL_ADDR=$($st_cmd keys show $val_acct --keyring-backend test -a)
+        # add money for this validator account
+        $st_cmd add-genesis-account ${VAL_ADDR} $VAL_TOKENS
+        # actually set this account as a validator
+        yes | $st_cmd gentx $val_acct $STAKE_TOKENS --chain-id $STRIDE_CHAIN --keyring-backend test 2> /dev/null
+    fi
     # now we grab the relevant node id
     end_name=${STRIDE_ENDPOINTS[i]}
     node_id=$($st_cmd tendermint show-node-id)@$end_name:$PORT_ID
+    echo $node_id
     STRIDE_NODES+=( $node_id )
-    if [ $i -ne $MAIN_ID ]
+    if [ $i -ne $MAIN_ID ] && [ $i -ne $SEED_ID ]
     then
         $main_cmd add-genesis-account ${VAL_ADDR} $VAL_TOKENS
         cp ${STATE}/${node_name}/config/gentx/*.json ${STATE}/${main_node}/config/gentx/
@@ -52,6 +55,10 @@ done
 
 # # now we process gentx txs 
 $main_cmd collect-gentxs 2> /dev/null
+
+# set seed node to actually be a seed node
+sed -i -E 's|seed_mode = false|seed_mode = true|g' "${STATE}/${seed_node}/config/config.toml"
+
 
 # add peers in config.toml so that nodes can find each other by constructing a fully connected
 # graph of nodes
@@ -75,13 +82,11 @@ for i in "${!STRIDE_CHAINS[@]}"; do
     fi
 done
 
-export DOCKER_BUILDKIT=0
-export DOCKER_DEFAULT_PLATFORM=linux/amd64
 
-docker tag stridezone:stride stridelabs/internal:droplet_base 
-docker push stridelabs/internal:droplet_base 
+# docker tag stridezone:stride stridelabs/internal:droplet_base 
+# docker push stridelabs/internal:droplet_base 
 
-docker pull stridelabs/internal:droplet_node1
+# docker pull stridelabs/internal:droplet_node1
 
 # docker buildx build --platform linux/amd64 -t stridelabs/internal:droplet_node1 -f dockerfiles/Dockerfile.node1 .
 
