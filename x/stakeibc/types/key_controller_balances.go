b4e5992a24d02f627c78943a389a8a7a81728d64
Riley Edmunds <riley@stridelabs.co>
2022-06-18 23:55:10 -0400

    draft logic to save controllerBalances before ICQ exch rate queries

--- x/stakeibc/types/key_controller_balances.go
+++ x/stakeibc/types/key_controller_balances.go
@@ -5,19 +5,19 @@ import "encoding/binary"
 var _ binary.ByteOrder
 
 const (
-    // ControllerBalancesKeyPrefix is the prefix to retrieve all ControllerBalances
+	// ControllerBalancesKeyPrefix is the prefix to retrieve all ControllerBalances
 	ControllerBalancesKeyPrefix = "ControllerBalances/value/"
 )
 
 // ControllerBalancesKey returns the store key to retrieve a ControllerBalances from the index fields
 func ControllerBalancesKey(
-index string,
+	index string,
 ) []byte {
 	var key []byte
-    
-    indexBytes := []byte(index)
-    key = append(key, indexBytes...)
-    key = append(key, []byte("/")...)
-    
+
+	indexBytes := []byte(index)
+	key = append(key, indexBytes...)
+	key = append(key, []byte("/")...)
+
 	return key
 }
