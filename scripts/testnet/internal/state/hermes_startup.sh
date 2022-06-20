d1330ce3a6c2c595f9aa86f66bbaeac501f3b344
vish-stride <vishal@stridelabs.co>
2022-06-20 00:18:25 -0400

    change trusting period

--- scripts/testnet/internal/state/hermes_startup.sh
+++ scripts/testnet/internal/state/hermes_startup.sh
@@ -3,8 +3,8 @@
 sleep 120
 
 echo "Restoring Hermes Accounts"
-hermes -c /tmp/hermes.toml keys restore --mnemonic "lift stairs armed gentle brisk asthma page palace input ice wet impulse thunder betray kangaroo adapt walnut grief twenty cluster large fan hole aisle" internal
-hermes -c /tmp/hermes.toml keys restore --mnemonic "surround speed ramp carpet noise suggest enjoy decline rally library potato tray cradle light ostrich announce neutral armed blast split effort sad off elite" GAIA_internal
+hermes -c /tmp/hermes.toml keys restore --mnemonic "drink turn model style increase suspect short dilemma funny senior van idle treat observe lamp pause melt female chief permit kick tower hold wrap" internal
+hermes -c /tmp/hermes.toml keys restore --mnemonic "jewel wedding now river obey cross roast team core subject assault blush check oxygen abuse reward estate alarm ski hub unique plastic attract liquid" GAIA_internal
 
 hermes -c /tmp/hermes.toml start &
 sleep 30
