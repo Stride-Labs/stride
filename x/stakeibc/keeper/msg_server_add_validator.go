64414f4e1a77e0d58a4731fe9816c97e523b5a63
vish-stride <vishal@stridelabs.co>
2022-06-21 19:47:16 -0400

    added ability to create validator

--- x/stakeibc/keeper/msg_server_add_validator.go
+++ x/stakeibc/keeper/msg_server_add_validator.go
@@ -2,6 +2,7 @@ package keeper
 
 import (
 	"context"
+	"fmt"
 
 	"github.com/Stride-Labs/stride/x/stakeibc/types"
 	sdk "github.com/cosmos/cosmos-sdk/types"
@@ -10,8 +11,30 @@ import (
 func (k msgServer) AddValidator(goCtx context.Context, msg *types.MsgAddValidator) (*types.MsgAddValidatorResponse, error) {
 	ctx := sdk.UnwrapSDKContext(goCtx)
 
-	// TODO: Handling the message
-	_ = ctx
+	hostZone, host_zone_found := k.GetHostZone(ctx, msg.HostZone)
+	if !host_zone_found {
+		k.Logger(ctx).Info(fmt.Sprintf("Host Zone not found %s", msg.HostZone))
+	}
+	validators := hostZone.Validators
+	// check that we don't already have this validator
+	for _, validator := range validators {
+		if validator.Address == msg.Address {
+			k.Logger(ctx).Info(fmt.Sprintf("Validator address %s already exists on Host Zone %s", msg.Address, msg.HostZone))
+			return nil, types.ErrValidatorAlreadyExists
+		}
+		if validator.Name == msg.Name {
+			k.Logger(ctx).Info(fmt.Sprintf("Validator name %s already exists on Host Zone %s", msg.Name, msg.HostZone))
+			return nil, types.ErrValidatorAlreadyExists
+		}
+	}
+	// add the validator
+	validators = append(validators, &types.Validator{
+		Name:           msg.Name,
+		Address:        msg.Address,
+		Status:         "active",
+		CommissionRate: int64(msg.Commission),
+		DelegationAmt:  0,
+	})
 
 	return &types.MsgAddValidatorResponse{}, nil
 }
