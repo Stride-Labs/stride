eeb4f567266423dfb783053e6bfa113218b3a371
vish-stride <vishal@stridelabs.co>
2022-06-21 23:44:09 -0400

    ignite scaffold message delete-validator hostZone name --module stakeibc

--- x/stakeibc/handler.go
+++ x/stakeibc/handler.go
@@ -41,6 +41,9 @@ func NewHandler(k keeper.Keeper) sdk.Handler {
 		case *types.MsgChangeValidatorWeight:
 			res, err := msgServer.ChangeValidatorWeight(sdk.WrapSDKContext(ctx), msg)
 			return sdk.WrapServiceResult(ctx, res, err)
+		case *types.MsgDeleteValidator:
+			res, err := msgServer.DeleteValidator(sdk.WrapSDKContext(ctx), msg)
+			return sdk.WrapServiceResult(ctx, res, err)
 			// this line is used by starport scaffolding # 1
 		default:
 			errMsg := fmt.Sprintf("unrecognized %s message type: %T", types.ModuleName, msg)
