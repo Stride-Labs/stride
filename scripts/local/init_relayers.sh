8ce362636c1cd9b41fad1a10d3bb9dfed7b155e7
sampocs <sam.pochyly@gmail.com>
2022-06-21 23:07:08 -0500

    removed irrelevant TODO

--- scripts/local/init_relayers.sh
+++ scripts/local/init_relayers.sh
@@ -10,6 +10,5 @@ echo $HERMES_STRIDE_MNEMONIC | $HERMES_CMD keys add -m /dev/stdin $STRIDE_CHAIN
 echo $HERMES_GAIA_MNEMONIC | $HERMES_CMD keys add -m /dev/stdin $GAIA_CHAIN
 
 echo "Adding ICQ keys"
-# TODO(TEST-82) redefine stride-testnet in lens' config to $STRIDE_CHAIN and gaia-testnet to $main-gaia-chain, then replace those below with $STRIDE_CHAIN and $GAIA_CHAIN
 echo $ICQ_STRIDE_MNEMONIC | $ICQ_CMD keys restore stridekey --chain stride-local --local
 echo $ICQ_GAIA_MNEMONIC | $ICQ_CMD keys restore gaiakey --chain gaia-local --local
