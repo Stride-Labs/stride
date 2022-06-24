7c26bf80f3c4301f61f76ebcd478bcfff3df7689
sampocs <sam.pochyly@gmail.com>
2022-06-24 11:03:11 -0500

    combined local mode init and start commands and separated out dependencies

--- scripts-local/build.sh
+++ scripts-local/build.sh
@@ -3,28 +3,8 @@
 set -eu 
 SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
 
-source ${SCRIPT_DIR}/vars.sh
-
 # First argument is build flags
 BUILDDIR="$2"
-CACHE="${3:-false}"
-
-# check and install dependencies
-if [ "$CACHE" != "true" ]; then
-    echo "\nChecking dependencies... "
-    DEPENDENCIES="jq"
-    deps=0
-    for name in $DEPENDENCIES
-    do
-        [[ $(type $name 2>/dev/null) ]] || { echo "\n    * $name is required to run this script;";deps=1; }
-    done
-    [[ $deps -ne 1 ]] && echo "OK\n" || { echo "\nInstall the missing dependencies and rerun this script...\n"; exit 1; }
-
-    # Clear existing state
-    rm -rf $STATE ~/.hermes/keys ~/.icq/keys
-else
-    echo "Rebuilding while restoring cached state..."
-fi
 
 while getopts sghi flag; do
     case "${flag}" in
@@ -49,9 +29,3 @@ while getopts sghi flag; do
     esac
 done
 
-# Initialize the state for stride/gaia and relayers
-if [ "$CACHE" != "true" ]; then
-    sh ${SCRIPT_DIR}/init_stride.sh
-    sh ${SCRIPT_DIR}/init_gaia.sh
-    sh ${SCRIPT_DIR}/init_relayers.sh
-fi
