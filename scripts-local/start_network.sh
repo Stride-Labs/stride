8637d1277db7b506f5eddb5cd198f6371723fe0a
sampocs <sam.pochyly@gmail.com>
2022-06-23 22:46:02 -0500

    fixed cache mode

--- scripts-local/start_network.sh
+++ scripts-local/start_network.sh
@@ -7,7 +7,7 @@ source $SCRIPT_DIR/vars.sh
 
 mkdir -p $SCRIPT_DIR/logs
 
-cache="${1:-false}"
+CACHE="${1:-false}"
 
 STRIDE_STATE=$SCRIPT_DIR/state/stride
 STRIDE_LOGS=$SCRIPT_DIR/logs/stride.log
@@ -20,10 +20,10 @@ ICQ_LOGS=$SCRIPT_DIR/logs/icq.log
 rm -rf $SCRIPT_DIR/logs/*.log
 
 # TODO (TEST-122) Fix "caching" option in local mode
-if [ "$cache" == "true" ]; then
+if [ "$CACHE" == "true" ]; then
     echo "Restoring from cache..."
     rm -rf $SCRIPT_DIR/state 
-    mv $SCRIPT_DIR/.state.backup $SCRIPT_DIR/state
+    cp -r $SCRIPT_DIR/.state.backup $SCRIPT_DIR/state
 fi
 
 # Starts Stride and Gaia in the background using nohup, pipes the logs to their corresponding log files,
@@ -35,7 +35,7 @@ nohup $GAIA_CMD start | sed -r -u "s/\x1B\[([0-9]{1,3}(;[0-9]{1,2})?)?[mGK]//g" 
 ( tail -f -n0 $GAIA_LOGS & ) | grep -q "finalizing commit of block"
 echo "Done"
 
-if [ "$cache" != "true" ]; then
+if [ "$CACHE" != "true" ]; then
     # If cache mode is disabled, create the hermes connection and channels, 
     # Logs are piped to the hermes log file and the script is halted until:
     #  1)  "Creating transfer channel" is printed (indicating the connection has been created)
@@ -46,27 +46,35 @@ if [ "$cache" != "true" ]; then
     echo "Done"
 
     printf '%s' "Creating Hermes Channel...    "
-    # contiuation of logs from above command
+    # continuation of logs from above command
     ( tail -f -n0 $HERMES_LOGS & ) | grep -q "Message ChanOpenInit"
     echo "Done"
 fi
 
 # Start hermes in the background and pause until the log message shows that it is up and running
+if [ "$CACHE" != "true" ]; then
+    # If we're starting for the first time, confirm with the connection handshake
+    hermes_start_msg_indicator="STRIDE->GAIA}: connection handshake step completed with events: OpenConfirmConnection"
+else
+    # If we're starting from cache, no need to wait for a handshake
+    hermes_start_msg_indicator="Hermes has started"
+fi
 printf '%s' "Starting Hermes...            "
 nohup $HERMES_CMD start >> $HERMES_LOGS 2>&1 &
-( tail -f -n0 $HERMES_LOGS & ) | grep -q -E "STRIDE->GAIA}: connection handshake step completed with events: OpenConfirmConnection"
+( tail -f -n0 $HERMES_LOGS & ) | grep -q -E "$hermes_start_msg_indicator"
 echo "Done"
 
 # Start ICQ in the background
 printf '%s' "Starting ICQ...               "
 nohup $ICQ_CMD run --local > $ICQ_LOGS 2>&1 &
+sleep 5
 echo "Done"
 
 # Create a copy of the state that can be used for the "cache" option
 echo "Network is ready for transactions.\n"
 cp -r $SCRIPT_DIR/state $SCRIPT_DIR/.state.backup
 
-if [ "$cache" != "true" ]; then
+if [ "$CACHE" != "true" ]; then
     # Submit a transaction on stride to register the gaia host zone
     echo "Creating host zone..."
     $STRIDE_CMD tx stakeibc register-host-zone \
