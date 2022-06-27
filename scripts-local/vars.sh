9526ba086e18fc73029d33e2401e135e401d607a
vish-stride <vishal@stridelabs.co>
2022-06-27 15:09:18 -0400

    added min validator rebalance threshold

--- scripts-local/vars.sh
+++ scripts-local/vars.sh
@@ -25,7 +25,15 @@ GAIA_VAL_ACCT=gval1
 GAIA_REV_ACCT=grev1
 GAIA_VAL_MNEMONIC="move next relief spatial resemble onion exhibit fitness major toss where square wrong exact infant skate dragon shift region over you gospel absorb double"
 GAIA_REV_MNEMONIC="tonight bonus finish chaos orchard plastic view nurse salad regret pause awake link bacon process core talent whale million hope luggage sauce card weasel"
+GAIA_VAL_ADDR="cosmos1pcag0cj4ttxg8l7pcg0q4ksuglswuuedcextl2"
 GAIA_CMD="$SCRIPT_DIR/../build/gaiad --home $STATE/gaia"
+
+GAIA_VAL_ACCT_2=gval2
+GAIA_VAL_MNEMONIC_2="guilt leader matrix lecture infant axis method grain diesel sting reflect brave estate surge october candy busy crash parade club practice sure gentle celery"
+GAIA_VAL_2_ADDR="cosmos133lfs9gcpxqj6er3kx605e3v9lqp2pg54sreu3"
+GAIA_VAL_ACCT_3=gval3
+GAIA_VAL_MNEMONIC_3="fire tape spread wing click winter awful ozone visa spray swear color table settle review rival meadow gauge speed tide timber disease float live"
+GAIA_VAL_3_ADDR="cosmos1fumal3j4lxzjp22fzffge8mw56qm33h9ez0xy2"
 
 HERMES_CMD="$SCRIPT_DIR/../build/hermes/release/hermes -c $SCRIPT_DIR/hermes/config.toml"
 
--- x/stakeibc/keeper/msg_server_rebalance_validators.go
+++ x/stakeibc/keeper/msg_server_rebalance_validators.go
@@ -18,23 +18,37 @@ func abs(n int64) int64 {
 	return n
 }
 
+func floatabs(n float64) float64 {
+	if n < 0 {
+		return -n
+	}
+	return n
+}
+
+func floatmax(a, b float64) float64 {
+	if a > b {
+		return a
+	}
+	return b
+}
+
 func (k msgServer) RebalanceValidators(goCtx context.Context, msg *types.MsgRebalanceValidators) (*types.MsgRebalanceValidatorsResponse, error) {
 	ctx := sdk.UnwrapSDKContext(goCtx)
 
 	hostZone, found := k.GetHostZone(ctx, msg.HostZone)
 	if !found {
-		k.Logger(ctx).Info(fmt.Sprintf("Host Zone not found %s", msg.HostZone))
+		k.Logger(ctx).Error(fmt.Sprintf("Host Zone not found %s", msg.HostZone))
 		return nil, types.ErrInvalidHostZone
 	}
 	maxNumRebalance := int(msg.NumRebalance)
 	if maxNumRebalance < 1 {
-		k.Logger(ctx).Info(fmt.Sprintf("Invalid number of validators to rebalance %d", maxNumRebalance))
+		k.Logger(ctx).Error(fmt.Sprintf("Invalid number of validators to rebalance %d", maxNumRebalance))
 		return nil, types.ErrNoValidatorWeights
 	}
 
 	validatorDeltas, err := k.GetValidatorDelegationAmtDifferences(ctx, hostZone)
 	if err != nil {
-		k.Logger(ctx).Info(fmt.Sprintf("Error getting validator deltas for Host Zone %s: %s", hostZone.ChainId, err))
+		k.Logger(ctx).Error(fmt.Sprintf("Error getting validator deltas for Host Zone %s: %s", hostZone.ChainId, err))
 		return nil, err
 	}
 
@@ -53,59 +67,64 @@ func (k msgServer) RebalanceValidators(goCtx context.Context, msg *types.MsgReba
 	}
 	sort.Slice(valDeltaList, lessFunc)
 	// now varDeltaList is sorted by deltaAmt
-	underWeightIndex := 0
-	overWeightIndex := len(valDeltaList) - 1
+	overWeightIndex := 0
+	underWeightIndex := len(valDeltaList) - 1
+
+	// check if there is a large enough rebalance, if not, just exit
+	total_delegation := float64(k.GetTotalValidatorDelegations(ctx, hostZone))
+	overweight_delta := floatabs(float64(valDeltaList[overWeightIndex].deltaAmt) / total_delegation)
+	underweight_delta := floatabs(float64(valDeltaList[underWeightIndex].deltaAmt) / total_delegation)
+	max_delta := floatmax(overweight_delta, underweight_delta)
+	rebalanceThreshold := float64(k.GetParam(ctx, types.KeyDepositInterval)) / float64(10000)
+	if max_delta < rebalanceThreshold {
+		k.Logger(ctx).Error("Not enough validator disruption to rebalance")
+		return nil, types.ErrNoValidatorWeights
+	}
 
 	var msgs []sdk.Msg
 	delegatorAddressStr := hostZone.DelegationAccount.Address
 	delegatorAddress := sdk.AccAddress(delegatorAddressStr)
 
-	// max_delta = utils.abs(valDeltaList[underWeightIndex].deltaAmt / )
-	// max_delta = max(max_delta, utils.abs(valDeltaList[overWeightIndex].deltaAmt))
-	// if max_delta < 0 {
-
-	// }
-
 	for i := 1; i < maxNumRebalance; i++ {
 		underWeightElem := valDeltaList[underWeightIndex]
 		overWeightElem := valDeltaList[overWeightIndex]
-		if underWeightElem.deltaAmt > 0 {
-			// if underWeightElem is positive, we're done rebalancing
+		if underWeightElem.deltaAmt < 0 {
+			// if underWeightElem is negative, we're done rebalancing
 			break
 		}
-		if overWeightElem.deltaAmt < 0 {
-			// if overWeightElem is negative, we're done rebalancing
+		if overWeightElem.deltaAmt > 0 {
+			// if overWeightElem is positive, we're done rebalancing
 			break
 		}
 		if abs(underWeightElem.deltaAmt) > abs(overWeightElem.deltaAmt) {
 			// if the underweight element is more overweight than the overweight element
 			// we transfer stake from the underweight element to the overweight element
-			underWeightElem.deltaAmt += overWeightElem.deltaAmt
-			overWeightIndex -= 1
+			underWeightElem.deltaAmt -= overWeightElem.deltaAmt
+			overWeightIndex += 1
 			// issue an ICA call to the host zone to rebalance the validator
 			redelagateMsg := stakingTypes.NewMsgBeginRedelegate(
 				delegatorAddress,
-				underWeightElem.valAddr,
 				overWeightElem.valAddr,
+				underWeightElem.valAddr,
 				sdk.NewInt64Coin(hostZone.HostDenom, abs(overWeightElem.deltaAmt)))
 			msgs = append(msgs, redelagateMsg)
 			overWeightElem.deltaAmt = 0
 		} else if abs(underWeightElem.deltaAmt) < abs(overWeightElem.deltaAmt) {
 			// if the overweight element is more overweight than the underweight element
-			overWeightElem.deltaAmt -= underWeightElem.deltaAmt
-			underWeightIndex += 1
+			overWeightElem.deltaAmt += underWeightElem.deltaAmt
+			underWeightIndex -= 1
 			// issue an ICA call to the host zone to rebalance the validator
 			redelagateMsg := stakingTypes.NewMsgBeginRedelegate(
 				delegatorAddress,
-				underWeightElem.valAddr,
 				overWeightElem.valAddr,
+				underWeightElem.valAddr,
 				sdk.NewInt64Coin(hostZone.HostDenom, abs(underWeightElem.deltaAmt)))
 			msgs = append(msgs, redelagateMsg)
 			underWeightElem.deltaAmt = 0
 		} else {
 			// if the two elements are equal, we increment both slices
-			underWeightIndex += 1
-			overWeightIndex -= 1
+			underWeightIndex -= 1
+			overWeightIndex += 1
 			// issue an ICA call to the host zone to rebalance the validator
 			redelagateMsg := stakingTypes.NewMsgBeginRedelegate(
 				delegatorAddress,
