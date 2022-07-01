638530d3f296ecf6838a5d6b5dbb45332ae32578
vish-stride <vishal@stridelabs.co>
2022-07-01 16:27:03 -0400

    add nginx cert

--- scripts/testnet/install_faucet.sh
+++ scripts/testnet/install_faucet.sh
@@ -4,7 +4,7 @@ git clone https://github.com/tendermint/faucet.git
 cd faucet 
 make build 
 make install
-/stride/go/bin/faucet --cli-name strided --denoms ustrd --account-name val2 --keyring-backend test --credit-amount 1000000000 --max-credit 3000000001 --home /stride/.stride
+/stride/go/bin/faucet --cli-name strided --denoms ustrd --account-name val2 --keyring-backend test --credit-amount 10000000 --max-credit 30000001 --home /stride/.stride --node https://stride-node2.internal.stridenet.co:26657
 # /stride/go/bin/faucet --cli-name strided --denoms ustrd --account-name val2 --keyring-backend test --home /stride/.stride
 # /stride/go/bin/faucet --cli-name /usr/local/bin/strided --denoms ustrd --account-name val2 --keyring-backend test --home /stride/.stride
 
