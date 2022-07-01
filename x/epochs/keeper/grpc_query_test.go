a8a3941152f74d7e7356cade5ab556fdc5856233
sampocs <sam.pochyly@gmail.com>
2022-06-30 19:50:58 -0500

    fixed epoch keeper tests

--- x/epochs/keeper/grpc_query_test.go
+++ x/epochs/keeper/grpc_query_test.go
@@ -5,6 +5,7 @@ import (
 	"time"
 
 	"github.com/Stride-Labs/stride/x/epochs/types"
+	_ "github.com/stretchr/testify/suite"
 )
 
 func (suite *KeeperTestSuite) TestQueryEpochInfos() {
@@ -16,7 +17,7 @@ func (suite *KeeperTestSuite) TestQueryEpochInfos() {
 	// Invalid param
 	epochInfosResponse, err := queryClient.EpochInfos(gocontext.Background(), &types.QueryEpochsInfoRequest{})
 	suite.Require().NoError(err)
-	suite.Require().Len(epochInfosResponse.Epochs, 2)
+	suite.Require().Len(epochInfosResponse.Epochs, 3)
 
 	// check if EpochInfos are correct
 	suite.Require().Equal(epochInfosResponse.Epochs[0].Identifier, "day")
@@ -25,10 +26,17 @@ func (suite *KeeperTestSuite) TestQueryEpochInfos() {
 	suite.Require().Equal(epochInfosResponse.Epochs[0].CurrentEpoch, int64(0))
 	suite.Require().Equal(epochInfosResponse.Epochs[0].CurrentEpochStartTime, chainStartTime)
 	suite.Require().Equal(epochInfosResponse.Epochs[0].EpochCountingStarted, false)
-	suite.Require().Equal(epochInfosResponse.Epochs[1].Identifier, "week")
+	suite.Require().Equal(epochInfosResponse.Epochs[1].Identifier, "stride_epoch")
 	suite.Require().Equal(epochInfosResponse.Epochs[1].StartTime, chainStartTime)
-	suite.Require().Equal(epochInfosResponse.Epochs[1].Duration, time.Hour*24*7)
+	suite.Require().Equal(epochInfosResponse.Epochs[1].Duration, time.Minute*5)
 	suite.Require().Equal(epochInfosResponse.Epochs[1].CurrentEpoch, int64(0))
 	suite.Require().Equal(epochInfosResponse.Epochs[1].CurrentEpochStartTime, chainStartTime)
 	suite.Require().Equal(epochInfosResponse.Epochs[1].EpochCountingStarted, false)
+	suite.Require().Equal(epochInfosResponse.Epochs[2].Identifier, "week")
+	suite.Require().Equal(epochInfosResponse.Epochs[2].StartTime, chainStartTime)
+	suite.Require().Equal(epochInfosResponse.Epochs[2].Duration, time.Hour*24*7)
+	suite.Require().Equal(epochInfosResponse.Epochs[2].CurrentEpoch, int64(0))
+	suite.Require().Equal(epochInfosResponse.Epochs[2].CurrentEpochStartTime, chainStartTime)
+	suite.Require().Equal(epochInfosResponse.Epochs[2].EpochCountingStarted, false)
+
 }
