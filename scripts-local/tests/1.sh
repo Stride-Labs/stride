95ee1b1d5a262ceadd2295dc7185c7c113a444af
Aidan Salzmann <aidan@stridelabs.co>
2022-07-01 23:43:22 -0400

    set block time to 5s

--- scripts-local/tests/1.sh
+++ scripts-local/tests/1.sh
@@ -13,12 +13,12 @@ $STRIDE_CMD q bank balances $STRIDE_ADDRESS
 
 # register our validators
 
-sleep 3
+sleep 5
 $GAIA_CMD tx bank send gval1 $GAIA_VAL_2_ADDR 10000uatom --chain-id $GAIA_CHAIN --keyring-backend test -y
-sleep 3
+sleep 5
 $GAIA_CMD tx bank send gval1 $GAIA_VAL_3_ADDR 10000uatom --chain-id $GAIA_CHAIN --keyring-backend test -y
 
-sleep 3
+sleep 5
 $STRIDE_CMD tx stakeibc add-validator GAIA gval1 $GAIA_DELEGATE_VAL 10 5 --chain-id $STRIDE_CHAIN --keyring-backend test --from $STRIDE_VAL_ACCT -y
-sleep 3
+sleep 5
 $STRIDE_CMD tx stakeibc add-validator GAIA gval2 $GAIA_DELEGATE_VAL_2 10 10 --chain-id $STRIDE_CHAIN --keyring-backend test --from $STRIDE_VAL_ACCT -y
