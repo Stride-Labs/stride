52275431ab1aba67871819f5f43c4c5ad4babd0e
Riley Edmunds <riley@stridelabs.co>
2022-07-02 13:07:54 -0400

    full flow working including claiming!

---
+++ scripts-local/tests/4.sh
@@ -0,0 +1,14 @@
+### LIQ STAKE + EXCH RATE TEST
+SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
+# import dependencies
+source ${SCRIPT_DIR}/../account_vars.sh
+
+#check balances before claiming redeemed stake
+$GAIA_CMD q bank balances cosmos1g6qdx6kdhpf000afvvpte7hp0vnpzapuyxp8uf
+
+#claim stake
+$STRIDE_CMD tx stakeibc claim-undelegated-tokens GAIA 3 --from val1 --keyring-backend test --chain-id STRIDE
+
+CSLEEP 30
+# #check balances after claiming redeemed stake
+$GAIA_CMD q bank balances cosmos1g6qdx6kdhpf000afvvpte7hp0vnpzapuyxp8uf
