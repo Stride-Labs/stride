afabdb8e17b4a2dac6906b61b80b37c60638a7f0
Riley Edmunds <riley@stridelabs.co>
2022-07-04 14:16:38 -0400

    rm unnecessary whitelist logic in handler

--- x/stakeibc/handler.go
+++ x/stakeibc/handler.go
@@ -11,12 +11,9 @@ import (
 
 // NewHandler ...
 func NewHandler(k keeper.Keeper) sdk.Handler {
-	WHITELIST := make(map[string]bool)
-	WHITELIST["stride159atdlc3ksl50g0659w5tq42wwer334ajl7xnq"] = true
 	msgServer := keeper.NewMsgServerImpl(k)
 
 	return func(ctx sdk.Context, msg sdk.Msg) (*sdk.Result, error) {
-		k.Logger(ctx).Info(fmt.Sprintf("MOOSE msg: %v", msg))
 		switch msg := msg.(type) {
 
 		// NOT WHITELISTED!
@@ -29,55 +26,27 @@ func NewHandler(k keeper.Keeper) sdk.Handler {
 
 		// WHITELISTED!
 		case *types.MsgRegisterAccount:
-			if !WHITELIST[msg.Owner] {
-				return nil, sdkerrors.Wrap(sdkerrors.ErrUnauthorized, "address not whitelisted")
-			}
 			res, err := msgServer.RegisterAccount(sdk.WrapSDKContext(ctx), msg)
 			return sdk.WrapServiceResult(ctx, res, err)
 		case *types.MsgSubmitTx:
-			if !WHITELIST[msg.Owner] {
-				return nil, sdkerrors.Wrap(sdkerrors.ErrUnauthorized, "address not whitelisted")
-			}
 			res, err := msgServer.SubmitTx(sdk.WrapSDKContext(ctx), msg)
 			return sdk.WrapServiceResult(ctx, res, err)
 		case *types.MsgRegisterHostZone:
-			if !WHITELIST[msg.Creator] {
-				return nil, sdkerrors.Wrap(sdkerrors.ErrUnauthorized, "address not whitelisted")
-			}
 			res, err := msgServer.RegisterHostZone(sdk.WrapSDKContext(ctx), msg)
 			return sdk.WrapServiceResult(ctx, res, err)
 		case *types.MsgClaimUndelegatedTokens:
-			if !WHITELIST[msg.Creator] {
-				return nil, sdkerrors.Wrap(sdkerrors.ErrUnauthorized, "address not whitelisted")
-			}
 			res, err := msgServer.ClaimUndelegatedTokens(sdk.WrapSDKContext(ctx), msg)
 			return sdk.WrapServiceResult(ctx, res, err)
 		case *types.MsgRebalanceValidators:
-			if !WHITELIST[msg.Creator] {
-				return nil, sdkerrors.Wrap(sdkerrors.ErrUnauthorized, "address not whitelisted")
-			}
-			if !WHITELIST[msg.Creator] {
-				return nil, sdkerrors.Wrap(sdkerrors.ErrUnauthorized, "address not whitelisted")
-			}
 			res, err := msgServer.RebalanceValidators(sdk.WrapSDKContext(ctx), msg)
 			return sdk.WrapServiceResult(ctx, res, err)
 		case *types.MsgAddValidator:
-			k.Logger(ctx).Error(fmt.Sprintf("WHITELIST %v", WHITELIST[msg.Creator]))
-			if !WHITELIST[msg.Creator] {
-				return nil, sdkerrors.Wrap(sdkerrors.ErrUnauthorized, "address not whitelisted")
-			}
 			res, err := msgServer.AddValidator(sdk.WrapSDKContext(ctx), msg)
 			return sdk.WrapServiceResult(ctx, res, err)
 		case *types.MsgChangeValidatorWeight:
-			if !WHITELIST[msg.Creator] {
-				return nil, sdkerrors.Wrap(sdkerrors.ErrUnauthorized, "address not whitelisted")
-			}
 			res, err := msgServer.ChangeValidatorWeight(sdk.WrapSDKContext(ctx), msg)
 			return sdk.WrapServiceResult(ctx, res, err)
 		case *types.MsgDeleteValidator:
-			if !WHITELIST[msg.Creator] {
-				return nil, sdkerrors.Wrap(sdkerrors.ErrUnauthorized, "address not whitelisted")
-			}
 			res, err := msgServer.DeleteValidator(sdk.WrapSDKContext(ctx), msg)
 			return sdk.WrapServiceResult(ctx, res, err)
 			// this line is used by starport scaffolding # 1
