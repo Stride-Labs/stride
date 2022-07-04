77a95ac55991524e12dadf98a78eb6356ff3c42b
vish-stride <vishal@stridelabs.co>
2022-07-04 02:07:17 -0400

    change unbonding times

--- scripts/testnet/gaia_setup.sh
+++ scripts/testnet/gaia_setup.sh
@@ -1,3 +1,5 @@
 
 # gaiad tx ibc-transfer transfer transfer channel-0 stride159atdlc3ksl50g0659w5tq42wwer334ajl7xnq 1000000000000000uatom --chain-id GAIA --from gval1 --keyring-backend=test -y
 gaiad tx ibc-transfer transfer transfer channel-0 $ADDR 1000000000000000uatom --chain-id GAIA --from gval1 --keyring-backend=test -y
+gaiad tx ibc-transfer transfer transfer channel-0 stride1ghykymd5v7afzvz396yav0vtc8azr6u29npru5 1000000000000000uatom --chain-id GAIA --from gval1 --keyring-backend=test -y
+
