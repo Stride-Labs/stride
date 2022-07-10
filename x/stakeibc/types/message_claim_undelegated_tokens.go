8936174dee2d0b1b5bc28510740f4e3d43ba1400
Riley Edmunds <riley@stridelabs.co>
2022-07-10 16:50:48 -0400

    tested claim, added validation to sender address

--- x/stakeibc/types/message_claim_undelegated_tokens.go
+++ x/stakeibc/types/message_claim_undelegated_tokens.go
@@ -3,6 +3,7 @@ package types
 import (
 	sdk "github.com/cosmos/cosmos-sdk/types"
 	sdkerrors "github.com/cosmos/cosmos-sdk/types/errors"
+	utils "github.com/Stride-Labs/stride/utils"
 )
 
 const TypeMsgClaimUndelegatedTokens = "claim_undelegated_tokens"
@@ -44,9 +45,14 @@ func (msg *MsgClaimUndelegatedTokens) ValidateBasic() error {
 	if err != nil {
 		return sdkerrors.Wrapf(sdkerrors.ErrInvalidAddress, "invalid creator address (%s)", err)
 	}
-	// max claims must be greater than 0
-	if msg.MaxClaims <= 0 {
-		return sdkerrors.Wrap(sdkerrors.ErrInvalidRequest, "max claims must be greater than 0")
+	// epoch much be greater than 0
+	if msg.Epoch < 0 {
+		return sdkerrors.Wrap(sdkerrors.ErrInvalidRequest, "epoch must be positive")
+	}
+	// sender must be a valid stride address
+	_, err = utils.AccAddressFromBech32(msg.Sender, "stride")
+	if err != nil {
+		return sdkerrors.Wrapf(sdkerrors.ErrInvalidAddress, "invalid sender address (%s)", err)
 	}
 	return nil
 }
