7b028225866351d2e369b2dad954b6991ba6ed30
Aidan Salzmann <aidan@stridelabs.co>
2022-07-11 10:01:20 -0400

    add remaining seconds call to epochs

--- scripts-local/account_vars.sh
+++ scripts-local/account_vars.sh
@@ -5,11 +5,13 @@ SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
 
 source ${SCRIPT_DIR}/vars.sh
 
+# MOVED TO vars.sh
 STRIDE_ADDRESS=$($STRIDE_CMD keys show $STRIDE_VAL_ACCT --keyring-backend test -a)
 GAIA_ADDRESS=$($GAIA_CMD keys show $GAIA_VAL_ACCT --keyring-backend test -a)
 
 HERMES_STRIDE_ADDRESS=$($STRIDE_CMD keys show $HERMES_STRIDE_ACCT --keyring-backend test -a)
 HERMES_GAIA_ADDRESS=$($GAIA_CMD keys show $HERMES_GAIA_ACCT --keyring-backend test -a)
 
+DELEGATION_ICA_ADDR='cosmos19l6d3d7k2pel8epgcpxc9np6fsvjpaaa06nm65vagwxap0e4jezq05mmvu'
 GAIA_DELEGATE_VAL='cosmosvaloper1pcag0cj4ttxg8l7pcg0q4ksuglswuuedadj7ne'
 GAIA_DELEGATE_VAL_2='cosmosvaloper133lfs9gcpxqj6er3kx605e3v9lqp2pg5syhvsz'
--- scripts-local/test-utils/run_any_cli_command.sh
+++ scripts-local/test-utils/run_any_cli_command.sh
@@ -4,4 +4,4 @@ SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
 source ${SCRIPT_DIR}/../account_vars.sh
 
 
-$STRIDE_CMD q stakeibc show-host-zone GAIA
+$STRIDE_CMD q epochs seconds-remaining day
