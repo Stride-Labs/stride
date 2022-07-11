8eb14a1084047cf9430b8959bc78d2aa75fff1f4
Aidan Salzmann <aidan@stridelabs.co>
2022-07-11 14:28:59 -0400

    add tokens on GAIA were staked test

--- scripts-local/test-utils/run_any_cli_command.sh
+++ scripts-local/test-utils/run_any_cli_command.sh
@@ -3,5 +3,9 @@ SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
 # import dependencies
 source ${SCRIPT_DIR}/../account_vars.sh
 
+$GAIA_CMD q staking delegation cosmos19l6d3d7k2pel8epgcpxc9np6fsvjpaaa06nm65vagwxap0e4jezq05mmvu cosmosvaloper1pcag0cj4ttxg8l7pcg0q4ksuglswuuedadj7ne
 
-$STRIDE_CMD q epochs seconds-remaining day
+# delegation $DELEGATION_ICA_ADDR cosmosvaloper1pcag0cj4ttxg8l7pcg0q4ksuglswuuedadj7ne
+
+
+# $GAIA_CMD q staking delegation $DELEGATION_ICA_ADDR cosmosvaloper1pcag0cj4ttxg8l7pcg0q4ksuglswuuedadj7ne
