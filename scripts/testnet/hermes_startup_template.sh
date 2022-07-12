3ff00ee351044a6dae7ff5b05b3d9689ab5cae19
sampocs <sam.pochyly@gmail.com>
2022-07-12 18:38:49 -0500

    removed channel-init arg

--- scripts/testnet/hermes_startup_template.sh
+++ scripts/testnet/hermes_startup_template.sh
@@ -12,20 +12,18 @@ done
 
 sleep 120
 
-if [ $CHANNEL_INIT = true ]; then 
-    echo "Creating hermes identifiers"
-    hermes tx raw create-client STRIDE GAIA
-    sleep 15 
+echo "Creating hermes identifiers"
+hermes tx raw create-client STRIDE GAIA
+sleep 15 
 
-    hermes tx raw conn-init STRIDE GAIA 07-tendermint-0 07-tendermint-0
-    sleep 15
+hermes tx raw conn-init STRIDE GAIA 07-tendermint-0 07-tendermint-0
+sleep 15
 
-    echo "Creating connection STRIDE <> GAIA"
-    hermes create connection STRIDE GAIA
-    sleep 15
+echo "Creating connection STRIDE <> GAIA"
+hermes create connection STRIDE GAIA
+sleep 15
 
-    echo "Creating transfer channel"
-    hermes create channel --port-a transfer --port-b transfer GAIA connection-0 
-fi
+echo "Creating transfer channel"
+hermes create channel --port-a transfer --port-b transfer GAIA connection-0 
 
 hermes start 
