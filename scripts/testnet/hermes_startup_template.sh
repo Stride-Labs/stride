c41f45371910de763ee048e6075ee416c5e5f7ac
sampocs <sam.pochyly@gmail.com>
2022-07-12 10:04:07 -0500

    removed key restoration to dockerfile and added flag to enable/disable channel init

--- scripts/testnet/hermes_startup_template.sh
+++ scripts/testnet/hermes_startup_template.sh
@@ -10,30 +10,22 @@ while true; do
     sleep 30
 done
 
-sleep 60
+sleep 120
 
-echo "Restoring Hermes Accounts"
-hermes keys restore -m "HERMES_STRIDE_MNEMONIC" STRIDE
-hermes keys restore -m "HERMES_GAIA_MNEMONIC" GAIA
-sleep 30
+if [ $CHANNEL_INIT = true ]; then 
+    echo "Creating hermes identifiers"
+    hermes tx raw create-client STRIDE GAIA
+    sleep 15 
 
-# hermes start &
-sleep 30
+    hermes tx raw conn-init STRIDE GAIA 07-tendermint-0 07-tendermint-0
+    sleep 15
 
-echo "Creating hermes identifiers"
-hermes tx raw create-client STRIDE GAIA
-sleep 15 
+    echo "Creating connection STRIDE <> GAIA"
+    hermes create connection STRIDE GAIA
+    sleep 15
 
-hermes tx raw conn-init STRIDE GAIA 07-tendermint-0 07-tendermint-0
-sleep 15
+    echo "Creating transfer channel"
+    hermes create channel --port-a transfer --port-b transfer GAIA connection-0 
+fi
 
-echo "Creating connection STRIDE <> GAIA"
-hermes create connection STRIDE GAIA
-sleep 15
-
-echo "Creating transfer channel"
-hermes create channel --port-a transfer --port-b transfer GAIA connection-0 
-# hermes tx raw chan-open-init STRIDE GAIA connection-0 transfer transfer
-
-hermes start &
-wait
+hermes start 
