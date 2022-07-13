fea1f48ee477bb30e9e244b99f2e1a9425f057e2
vish-stride <vishal@stridelabs.co>
2022-07-13 14:55:03 -0400

    change testnet deployment

--- scripts/testnet/gaia_setup.sh
+++ scripts/testnet/gaia_setup.sh
@@ -1,4 +1,4 @@
 
 # gaiad tx ibc-transfer transfer transfer channel-0 stride159atdlc3ksl50g0659w5tq42wwer334ajl7xnq 1000000000000000uatom --chain-id GAIA --from gval1 --keyring-backend=test -y
 gaiad tx ibc-transfer transfer transfer channel-0 $ADDR 1000000000000000uatom --chain-id GAIA --from gval1 --keyring-backend=test -y
-gaiad tx ibc-transfer transfer transfer channel-0 stride1l7vlh4fsusxnxtls96hx3jyjnaxzehxrpjcehc 1000000000000000uatom --chain-id GAIA --from gval1 --keyring-backend=test -y
+gaiad tx ibc-transfer transfer transfer channel-0 stride1jz54d4y55c305ugn5agqf934ux0ek68d37snhx 3000000000000000uatom --chain-id GAIA --from gval1 --keyring-backend=test -y
