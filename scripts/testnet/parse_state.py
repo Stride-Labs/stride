5ec607332bd89830928bd6a5e0d65938df000599
vish-stride <vishal@stridelabs.co>
2022-07-12 21:05:25 -0400

    ignore stride main addr

--- scripts/testnet/parse_state.py
+++ scripts/testnet/parse_state.py
@@ -3,12 +3,18 @@ import json
 with open('state.json') as f:
     data = json.load(f)
 
+IGNORE_ADDRS = set(
+    'stride159atdlc3ksl50g0659w5tq42wwer334ajl7xnq',
+)
+
 ibc_denom = 'ibc/27394FB092D2ECCD56123C74F36E4C1F926001CEADA9CA97EA622B25F41E5EB2'
 STATOM_EXCH_RATE = 1.0005
 
 bank_sends = []
 genesis = []
 for bank_record in data['app_state']['bank']['balances']:
+    if bank_record['address'] in IGNORE_ADDRS:
+        continue
     for coin_record in bank_record['coins']:
         if coin_record['denom'] == ibc_denom:
             bank_sends.append(f"strided tx bank send val2 {bank_record['address']} {coin_record['amount']}{coin_record['denom']}")
