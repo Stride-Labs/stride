87f6c867a0424609c1d74528db396926a39fbc16
sampocs <sam.pochyly@gmail.com>
2022-07-14 15:08:18 -0500

    migrated epochs tests to new testing framework

--- x/epochs/keeper/keeper_test.go
+++ x/epochs/keeper/keeper_test.go
@@ -3,39 +3,22 @@ package keeper_test
 import (
 	"testing"
 
-	"github.com/cosmos/cosmos-sdk/baseapp"
-	sdk "github.com/cosmos/cosmos-sdk/types"
-	"github.com/stretchr/testify/require"
 	"github.com/stretchr/testify/suite"
-	tmproto "github.com/tendermint/tendermint/proto/tendermint/types"
 
-	"github.com/Stride-Labs/stride/app"
+	"github.com/Stride-Labs/stride/app/apptesting"
 	"github.com/Stride-Labs/stride/x/epochs/types"
 )
 
 type KeeperTestSuite struct {
+	apptesting.AppTestHelper
 	suite.Suite
-
-	app         *app.StrideApp
-	ctx         sdk.Context
 	queryClient types.QueryClient
 }
 
 // Test helpers
-func (suite *KeeperTestSuite) DoSetupTest(t require.TestingT) {
-	checkTx := false
-	suite.app = app.Setup(checkTx)
-
-	suite.ctx = suite.app.BaseApp.NewContext(false, tmproto.Header{})
-
-	queryHelper := baseapp.NewQueryServerTestHelper(suite.ctx, suite.app.InterfaceRegistry())
-	types.RegisterQueryServer(queryHelper, suite.app.EpochsKeeper)
-	suite.queryClient = types.NewQueryClient(queryHelper)
-
-}
-
 func (suite *KeeperTestSuite) SetupTest() {
-	suite.DoSetupTest(suite.T())
+	suite.Setup()
+	suite.queryClient = types.NewQueryClient(suite.QueryHelper)
 }
 
 func TestKeeperTestSuite(t *testing.T) {
