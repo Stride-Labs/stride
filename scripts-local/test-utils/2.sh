ff90814f9c9867b722cc2f83b95f909177aa3ed6
Riley Edmunds <riley@stridelabs.co>
2022-07-19 21:49:54 -0400

    test

--- scripts-local/test-utils/2.sh
+++ scripts-local/test-utils/2.sh
@@ -7,3 +7,4 @@ $STRIDE_CMD tx stakeibc liquid-stake 10000 $ATOM_DENOM --keyring-backend test --
 echo "Waiting for liquid staked tokens to be delegated..."
 CSLEEP 60
 # $STRIDE_CMD tx stakeibc liquid-stake 10000 $ATOM_DENOM --keyring-backend test --from $STRIDE_VAL_ACCT -y --chain-id $STRIDE_CHAIN
+
