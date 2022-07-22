82c5f74983d9d69d78f7c209698fd1861f548dd5
sampocs <sam.pochyly@gmail.com>
2022-07-21 20:19:05 -0500

    added register interchain account logic

--- x/stakeibc/keeper/msg_server_register_interchain_account.go
+++ x/stakeibc/keeper/msg_server_register_interchain_account.go
@@ -2,6 +2,7 @@ package keeper
 
 import (
 	"context"
+	"fmt"
 
 	"github.com/Stride-Labs/stride/x/stakeibc/types"
 	sdk "github.com/cosmos/cosmos-sdk/types"
@@ -10,7 +11,17 @@ import (
 func (k msgServer) RegisterInterchainAccount(goCtx context.Context, msg *types.MsgRegisterInterchainAccount) (*types.MsgRegisterInterchainAccountResponse, error) {
 	ctx := sdk.UnwrapSDKContext(goCtx)
 
-	_ = ctx
+	hostZone, found := k.GetHostZone(ctx, msg.ChainId)
+	if !found {
+		k.Logger(ctx).Error(fmt.Sprintf("Host Zone not found: %s", msg.ChainId))
+		return nil, types.ErrInvalidHostZone
+	}
+
+	owner := types.FormatICAAccountOwner(msg.ChainId, msg.AccountType)
+	if err := k.ICAControllerKeeper.RegisterInterchainAccount(ctx, hostZone.ConnectionId, owner); err != nil {
+		k.Logger(ctx).Error(fmt.Sprintf("unable to register %s account : %s", msg.AccountType.String(), err))
+		return nil, err
+	}
 
 	return &types.MsgRegisterInterchainAccountResponse{}, nil
 }
