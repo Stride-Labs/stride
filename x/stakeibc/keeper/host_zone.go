1c085904f29a66c9082c85c233f7da376819c9d9
Riley Edmunds <riley@stridelabs.co>
2022-07-30 13:02:05 -0400

    merge fix

--- x/stakeibc/keeper/host_zone.go
+++ x/stakeibc/keeper/host_zone.go
@@ -8,10 +8,7 @@ import (
 	"github.com/cosmos/cosmos-sdk/store/prefix"
 	sdk "github.com/cosmos/cosmos-sdk/types"
 	sdkerrors "github.com/cosmos/cosmos-sdk/types/errors"
-<<<<<<< HEAD
-=======
 	"github.com/spf13/cast"
->>>>>>> main
 
 	"github.com/Stride-Labs/stride/x/stakeibc/types"
 )
