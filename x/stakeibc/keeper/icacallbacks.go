23f7dedb5a5434a8f22b3a0916ca6216aef02564
Aidan Salzmann <aidan@stridelabs.co>
2022-08-09 02:15:52 -0400

    message delegate callback further implementation

--- x/stakeibc/keeper/icacallbacks.go
+++ x/stakeibc/keeper/icacallbacks.go
@@ -3,14 +3,17 @@ package keeper
 import (
 	"fmt"
 
-	channeltypes "github.com/cosmos/ibc-go/v3/modules/core/04-channel/types"
+	"encoding/json"
 
-	sdk "github.com/cosmos/cosmos-sdk/types"
-
-	proto "github.com/gogo/protobuf/proto"
+	"github.com/spf13/cast"
 
 	icacallbackstypes "github.com/Stride-Labs/stride/x/icacallbacks/types"
 	"github.com/Stride-Labs/stride/x/stakeibc/types"
+
+	sdk "github.com/cosmos/cosmos-sdk/types"
+	sdkerrors "github.com/cosmos/cosmos-sdk/types/errors"
+	channeltypes "github.com/cosmos/ibc-go/v3/modules/core/04-channel/types"
+	"github.com/golang/protobuf/proto"
 )
 
 // ___________________________________________________________________________________________________
@@ -73,8 +76,76 @@ func (k Keeper) UnmarshalDelegateCallbackArgs(ctx sdk.Context, delegateCallback 
 func DelegateCallback(k Keeper, ctx sdk.Context, packet channeltypes.Packet, ack []byte, args []byte) error {
 	k.Logger(ctx).Info("DOGE DelegateCallback executing", "packet", packet, "ack", ack, "args", args)
 	// deserialize the ack
+	txMsgData, err := k.GetTxMsgData(ctx, ack)
+	if err != nil {
+		return err
+	}
+	// do we need txMsgData?
+	_ = txMsgData
+
 	// deserialize the args
 	delegateCallback := k.UnmarshalDelegateCallbackArgs(ctx, args)
 	k.Logger(ctx).Info(fmt.Sprintf("DelegateCallback %v", delegateCallback))
+	hostZone := delegateCallback.GetHostZoneId()
+	zone, found := k.GetHostZone(ctx, hostZone)
+	_ = zone
+	if !found {
+		return sdkerrors.Wrap(sdkerrors.ErrInvalidRequest, "host zone not found")
+	}
+	recordId := delegateCallback.GetDepositRecordId()
+
+
+	// k.Logger(ctx).Info(fmt.Sprintf("incrementing stakedBal %d", amount))
+	// if amount < 0 {
+	// 	errMsg := fmt.Sprintf("Balance to stake was negative: %d", amount)
+	// 	k.Logger(ctx).Error(errMsg)
+	// 	return sdkerrors.Wrapf(sdkerrors.ErrLogic, errMsg)
+	// } else {
+	// 	zone.StakedBal += amount
+	// 	success := k.AddDelegationToValidator(ctx, zone, delegateMsg.ValidatorAddress, amount)
+	// 	if !success {
+	// 		return sdkerrors.Wrapf(types.ErrValidatorDelegationChg, "Failed to add delegation to validator")
+	// 	}
+	// 	k.SetHostZone(ctx, zone)
+	// }
+
+	k.RecordsKeeper.RemoveDepositRecord(ctx, cast.ToUint64(recordId))
 	return nil
+}
+
+func (k Keeper) GetTxMsgData(ctx sdk.Context, acknowledgement []byte) (*sdk.TxMsgData, error) {
+	ack := channeltypes.Acknowledgement_Result{}
+	eventType := "callback"
+	ctx.EventManager().EmitEvent(
+		sdk.NewEvent(
+			eventType,
+			sdk.NewAttribute(sdk.AttributeKeyModule, types.ModuleName),
+			sdk.NewAttribute(types.AttributeKeyAck, fmt.Sprintf("%v", ack)),
+		),
+	)
+	err := json.Unmarshal(acknowledgement, &ack)
+	if err != nil {
+		ackErr := channeltypes.Acknowledgement_Error{}
+		err := json.Unmarshal(acknowledgement, &ackErr)
+		if err != nil {
+			ctx.EventManager().EmitEvent(
+				sdk.NewEvent(
+					eventType,
+					sdk.NewAttribute(types.AttributeKeyAckError, ackErr.Error),
+				),
+			)
+			k.Logger(ctx).Error("Unable to unmarshal acknowledgement error", "error", err, "data", acknowledgement)
+			return nil, err
+		}
+		k.Logger(ctx).Error("Unable to unmarshal acknowledgement result", "error", err, "remote_err", ackErr, "data", acknowledgement)
+		return nil, err
+	}
+
+	txMsgData := &sdk.TxMsgData{}
+	err = proto.Unmarshal(ack.Result, txMsgData)
+	if err != nil {
+		k.Logger(ctx).Error("Unable to unmarshal acknowledgement", "error", err, "ack", ack.Result)
+		return nil, err
+	}
+	return txMsgData, nil
 }
