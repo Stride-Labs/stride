54806faa0518af15cc16b6177fe722d8f619a495
Aidan Salzmann <aidan@stridelabs.co>
2022-08-08 23:25:31 -0400

    fix unit tests

--- testutil/keeper/icacallbacks.go
+++ testutil/keeper/icacallbacks.go
@@ -2,79 +2,20 @@ package keeper
 
 import (
 	"testing"
+	"time"
 
-	"github.com/cosmos/cosmos-sdk/codec"
-	codectypes "github.com/cosmos/cosmos-sdk/codec/types"
-	"github.com/cosmos/cosmos-sdk/store"
-	storetypes "github.com/cosmos/cosmos-sdk/store/types"
 	sdk "github.com/cosmos/cosmos-sdk/types"
-	capabilitykeeper "github.com/cosmos/cosmos-sdk/x/capability/keeper"
-	typesparams "github.com/cosmos/cosmos-sdk/x/params/types"
-	icacontrollerkeeper "github.com/cosmos/ibc-go/v3/modules/apps/27-interchain-accounts/controller/keeper"
-	ibckeeper "github.com/cosmos/ibc-go/v3/modules/core/keeper"
-	"github.com/stretchr/testify/require"
-	"github.com/tendermint/tendermint/libs/log"
 	tmproto "github.com/tendermint/tendermint/proto/tendermint/types"
-	tmdb "github.com/tendermint/tm-db"
 
+	strideapp "github.com/Stride-Labs/stride/app"
 	"github.com/Stride-Labs/stride/x/icacallbacks/keeper"
-	"github.com/Stride-Labs/stride/x/icacallbacks/types"
 )
 
 func IcacallbacksKeeper(t testing.TB) (*keeper.Keeper, sdk.Context) {
-	logger := log.NewNopLogger()
+	checkTx := false
+	app := strideapp.InitTestApp(checkTx)
+	icacallbackskeeper := app.IcacallbacksKeeper
+	ctx := app.BaseApp.NewContext(checkTx, tmproto.Header{Height: 1, ChainID: "stride-1", Time: time.Now().UTC()})
 
-	storeKey := sdk.NewKVStoreKey(types.StoreKey)
-	memStoreKey := storetypes.NewMemoryStoreKey(types.MemStoreKey)
-
-	db := tmdb.NewMemDB()
-	stateStore := store.NewCommitMultiStore(db)
-	stateStore.MountStoreWithDB(storeKey, sdk.StoreTypeIAVL, db)
-	stateStore.MountStoreWithDB(memStoreKey, sdk.StoreTypeMemory, nil)
-	require.NoError(t, stateStore.LoadLatestVersion())
-
-	registry := codectypes.NewInterfaceRegistry()
-	appCodec := codec.NewProtoCodec(registry)
-	capabilityKeeper := capabilitykeeper.NewKeeper(appCodec, storeKey, memStoreKey)
-
-	ss := typesparams.NewSubspace(appCodec,
-		types.Amino,
-		storeKey,
-		memStoreKey,
-		"IcacallbacksSubSpace",
-	)
-	IBCKeeper := ibckeeper.NewKeeper(
-		appCodec,
-		storeKey,
-		ss,
-		nil,
-		nil,
-		capabilityKeeper.ScopeToModule("IcacallbacksIBCKeeper"),
-	)
-
-	// IMPLEMENT ME
-	var ICAControllerKeeper icacontrollerkeeper.Keeper
-
-	paramsSubspace := typesparams.NewSubspace(appCodec,
-		types.Amino,
-		storeKey,
-		memStoreKey,
-		"IcacallbacksParams",
-	)
-	k := keeper.NewKeeper(
-		appCodec,
-		storeKey,
-		memStoreKey,
-		paramsSubspace,
-		capabilityKeeper.ScopeToModule("IcacallbacksScopedKeeper"),
-		*IBCKeeper,
-		ICAControllerKeeper,
-	)
-
-	ctx := sdk.NewContext(stateStore, tmproto.Header{}, false, logger)
-
-	// Initialize params
-	k.SetParams(ctx, types.DefaultParams())
-
-	return k, ctx
+	return &icacallbackskeeper, ctx
 }
