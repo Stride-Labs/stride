1799e0a212bdafa61991b10d65d53d6c454ce637
sampocs <sam.pochyly@gmail.com>
2022-08-09 17:51:01 -0400

    added sttoken amount to epoch unbonding record proto and added migration boilerplate

---
+++ x/records/migrations/v2/migrations.go
@@ -0,0 +1,16 @@
+package v2
+
+import (
+	sdk "github.com/cosmos/cosmos-sdk/types"
+	// v1 "github.com/Stride-Labs/stride/x/records/migrations/v1"
+)
+
+// TODO: Add migration logic to deserialize with other protos and re-serialize with new ones
+func UpdateUnbondingRecords(ctx sdk.Context) error {
+	var _ = ctx
+	return nil
+}
+func MigrateStore(ctx sdk.Context) error {
+	var _ = ctx
+	return nil
+}
