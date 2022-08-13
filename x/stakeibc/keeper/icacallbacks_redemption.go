9bd036567e6342a851d80270a92e073416d2aca1
sampocs <sam.pochyly@gmail.com>
2022-08-13 18:51:23 -0500

    added redemption callback and moved redemption logic from ibc_handlers to redemption callback

---
+++ x/stakeibc/keeper/icacallbacks_redemption.go
@@ -0,0 +1,90 @@
+package keeper
+
+import (
+	"fmt"
+
+	recordstypes "github.com/Stride-Labs/stride/x/records/types"
+	"github.com/Stride-Labs/stride/x/stakeibc/types"
+
+	sdk "github.com/cosmos/cosmos-sdk/types"
+	sdkerrors "github.com/cosmos/cosmos-sdk/types/errors"
+	channeltypes "github.com/cosmos/ibc-go/v3/modules/core/04-channel/types"
+	"github.com/golang/protobuf/proto"
+)
+
+func (k Keeper) MarshalRedemptionCallbackArgs(ctx sdk.Context, redemptionCallback types.RedemptionCallback) ([]byte, error) {
+	out, err := proto.Marshal(&redemptionCallback)
+	if err != nil {
+		k.Logger(ctx).Error(fmt.Sprintf("MarshalRedemptionCallbackArgs | %s", err.Error()))
+		return nil, err
+	}
+	return out, nil
+}
+
+func (k Keeper) UnmarshalRedemptionCallbackArgs(ctx sdk.Context, redemptionCallback []byte) (types.RedemptionCallback, error) {
+	unmarshalledRedemptionCallback := types.RedemptionCallback{}
+	if err := proto.Unmarshal(redemptionCallback, &unmarshalledRedemptionCallback); err != nil {
+		k.Logger(ctx).Error(fmt.Sprintf("UnmarshalUndelegateCallbackArgs | %s", err.Error()))
+		return unmarshalledRedemptionCallback, err
+	}
+	return unmarshalledRedemptionCallback, nil
+}
+
+func RedemptionCallback(k Keeper, ctx sdk.Context, packet channeltypes.Packet, ack *channeltypes.Acknowledgement_Result, args []byte) error {
+	logMsg := fmt.Sprintf("RedemptionCallback executing packet: %d, source: %s %s, dest: %s %s",
+		packet.Sequence, packet.SourceChannel, packet.SourcePort, packet.DestinationChannel, packet.DestinationPort)
+	k.Logger(ctx).Info(logMsg)
+
+	if ack == nil {
+		return sdkerrors.Wrap(sdkerrors.ErrInvalidRequest, "ack is nil")
+	}
+
+	// unmarshal the callback args and get the host zone
+	redemptionCallback, err := k.UnmarshalRedemptionCallbackArgs(ctx, args)
+	if err != nil {
+		errMsg := fmt.Sprintf("Unable to unmarshal redemption callback args | %s", err.Error())
+		k.Logger(ctx).Error(errMsg)
+		return sdkerrors.Wrapf(types.ErrUnmarshalFailure, errMsg)
+	}
+	k.Logger(ctx).Info(fmt.Sprintf("RedemptionCallback, HostZone: %s", redemptionCallback.HostZoneId))
+
+	hostZoneId := redemptionCallback.HostZoneId
+
+	dayEpochTracker, found := k.GetEpochTracker(ctx, "day")
+	if !found {
+		k.Logger(ctx).Error("failed to find epoch day")
+		return sdkerrors.Wrapf(types.ErrInvalidLengthEpochTracker, "no number for epoch (%s)", "day")
+	}
+	epochUnbondingRecords := k.RecordsKeeper.GetAllEpochUnbondingRecord(ctx)
+
+	for _, epochUnbondingRecord := range epochUnbondingRecords {
+		if epochUnbondingRecord.EpochNumber == dayEpochTracker.EpochNumber {
+			continue
+		}
+		// TODO: Update with nondeterministic loop
+		hostZoneUnbonding := epochUnbondingRecord.HostZoneUnbondings[hostZoneId]
+		// NOTE: at the beginning of the epoch we mark all PENDING_TRANSFER HostZoneUnbondingRecords as UNBONDED
+		// so that they're retried if the transfer fails
+		if hostZoneUnbonding.Status != recordstypes.HostZoneUnbonding_PENDING_TRANSFER {
+			k.Logger(ctx).Error("hostZoneUnbonding.Status != recordstypes.HostZoneUnbonding_PENDING_TRANSFER")
+			continue
+		}
+		hostZoneUnbonding.Status = recordstypes.HostZoneUnbonding_TRANSFERRED
+		userRedemptionRecords := hostZoneUnbonding.UserRedemptionRecords
+		for _, recordId := range userRedemptionRecords {
+			userRedemptionRecord, found := k.RecordsKeeper.GetUserRedemptionRecord(ctx, recordId)
+			if !found {
+				k.Logger(ctx).Error("failed to find user redemption record")
+				return sdkerrors.Wrapf(types.ErrRecordNotFound, "no user redemption record found for id (%s)", recordId)
+			}
+			if userRedemptionRecord.IsClaimable {
+				k.Logger(ctx).Info("user redemption record is already claimable")
+				continue
+			}
+			userRedemptionRecord.IsClaimable = true
+			k.RecordsKeeper.SetUserRedemptionRecord(ctx, userRedemptionRecord)
+		}
+		k.RecordsKeeper.SetEpochUnbondingRecord(ctx, epochUnbondingRecord)
+	}
+	return nil
+}
