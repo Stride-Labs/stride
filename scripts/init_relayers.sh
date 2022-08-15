3391e3ecda91f9840f44f7e5482716fb7995239e
sampocs <sam.pochyly@gmail.com>
2022-08-15 13:56:00 -0500

    continuing to debug relayer

--- scripts/init_relayers.sh
+++ scripts/init_relayers.sh
@@ -7,13 +7,20 @@ source ${SCRIPT_DIR}/vars.sh
 
 mkdir -p $STATE/hermes
 mkdir -p $STATE/icq
+mkdir -p $STATE/relayer/config
 
 cp ${SCRIPT_DIR}/config/icq_config.yaml $STATE/icq/config.yaml
 cp ${SCRIPT_DIR}/config/hermes_config.toml $STATE/hermes/config.toml
+cp ${SCRIPT_DIR}/config/relayer_config.yaml $STATE/relayer/config/config.yaml
 
 echo "Adding Hermes keys"
 $HERMES_CMD keys restore --name rly1 --mnemonic "$HERMES_STRIDE_MNEMONIC" $STRIDE_CHAIN_ID 
 $HERMES_CMD keys restore --name rly2 --mnemonic "$HERMES_GAIA_MNEMONIC" $GAIA_CHAIN_ID
+
+# echo "Adding Relayer keys"
+# $RELAYER_CMD keys restore stride rly1 "$RELAYER_STRIDE_MNEMONIC"
+# $RELAYER_CMD keys restore gaia rly2 "$RELAYER_GAIA_MNEMONIC" 
+# $RELAYER_CMD paths new $STRIDE_CHAIN_ID $GAIA_CHAIN_ID stride-gaia
 
 echo "Adding ICQ keys"
 echo $ICQ_STRIDE_MNEMONIC | $ICQ_CMD keys restore icq1 --chain stride 
