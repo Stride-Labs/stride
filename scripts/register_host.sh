599a4b1e6e4474bc911efe28fe4767e8bf71cd8a
sampocs <sam.pochyly@gmail.com>
2022-08-15 23:50:57 -0500

    docker mode working!

--- scripts/register_host.sh
+++ scripts/register_host.sh
@@ -4,24 +4,38 @@ set -eu
 SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
 source $SCRIPT_DIR/vars.sh
 
-# Submit a transaction on stride to register the gaia host zone
-printf "\nCreating host zone...\n"
-$MAIN_STRIDE_CMD tx stakeibc register-host-zone \
-    connection-0 $ATOM_DENOM cosmos $IBC_ATOM_DENOM channel-0 1 \
-    --from $STRIDE_ADMIN_ACCT --gas 1000000 --home $SCRIPT_DIR/state/stride1 -y
-CSLEEP 10
+CHAIN_ID="$1"
+CONNECTION="$2"
+CHANNEL="$3"
 
-printf "\nRegistering validators...\n"
-$MAIN_STRIDE_CMD tx stakeibc add-validator $GAIA_CHAIN_ID gval1 $GAIA_DELEGATE_VAL_1 10 5 --from $STRIDE_ADMIN_ACCT -y
-CSLEEP 10
-$MAIN_STRIDE_CMD tx stakeibc add-validator $GAIA_CHAIN_ID gval2 $GAIA_DELEGATE_VAL_2 10 10 --from $STRIDE_ADMIN_ACCT -y
-CSLEEP 10
+MAIN_CMD=$(GET_VAR_VALUE       ${CHAIN_ID}_MAIN_CMD)
+VAL_PREFIX=$(GET_VAR_VALUE     ${CHAIN_ID}_VAL_PREFIX)
+IBC_DENOM=$(GET_VAR_VALUE      ${CHAIN_ID}_IBC_DENOM)
+HOST_DENOM=$(GET_VAR_VALUE     ${CHAIN_ID}_DENOM)
+ADDRESS_PREFIX=$(GET_VAR_VALUE ${CHAIN_ID}_ADDRESS_PREFIX)
 
-# sleep a while longer to wait for ICA accounts to set up
-printf "\nWaiting for ICA accounts on host..."
+# Get validator addresses
+DELEGATE_VAL_1="$($MAIN_CMD q staking validators | grep ${CHAIN_ID}_1 -A 5 | grep operator | awk '{print $2}')"
+DELEGATE_VAL_2="$($MAIN_CMD q staking validators | grep ${CHAIN_ID}_2 -A 5 | grep operator | awk '{print $2}')"
+
+echo "$CHAIN_ID - Registering host zone..."
+$STRIDE_MAIN_CMD tx stakeibc register-host-zone \
+    $CONNECTION $HOST_DENOM $ADDRESS_PREFIX $IBC_DENOM $CHANNEL 1 \
+    --gas 1000000 --from $STRIDE_ADMIN_ACCT --home $SCRIPT_DIR/state/stride1 -y | grep -E "code:|txhash:" | sed 's/^/  /'
+sleep 4
+
+echo "$CHAIN_ID - Registering validators..."
+$STRIDE_MAIN_CMD tx stakeibc add-validator $CHAIN_ID ${VAL_PREFIX}1 $DELEGATE_VAL_1 10 5 \
+    --from $STRIDE_ADMIN_ACCT -y | grep -E "code:|txhash:" | sed 's/^/  /'
+sleep 4
+
+$STRIDE_MAIN_CMD tx stakeibc add-validator $CHAIN_ID ${VAL_PREFIX}2 $DELEGATE_VAL_2 10 10 \
+    --from $STRIDE_ADMIN_ACCT -y | grep -E "code:|txhash:" | sed 's/^/  /'
+sleep 4
+
 while true; do
-    if ! $MAIN_STRIDE_CMD q stakeibc list-host-zone | grep Account | grep -q null; then
-        sleep 5
+    if ! $STRIDE_MAIN_CMD q stakeibc list-host-zone | grep Account | grep -q null; then
+        sleep 1
         break
     fi
 done
