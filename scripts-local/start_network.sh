3e481bbf2b3b3bffe099092b0009bbbf3a03c8e5
vish-stride <vishal@stridelabs.co>
2022-08-15 23:25:19 -0400

    working commit

--- scripts-local/start_network.sh
+++ scripts-local/start_network.sh
@@ -22,7 +22,7 @@ make stop 2>/dev/null || true
 rm -rf $SCRIPT_DIR/state $SCRIPT_DIR/logs/*.log $SCRIPT_DIR/logs/temp
 
 # Recreate each log file
-for log in $STRIDE_LOGS $GAIA_LOGS $GAIA_LOGS_2 $HERMES_LOGS $ICQ_LOGS; do
+for log in $STRIDE_LOGS $STRIDE_LOGS_2 $STRIDE_LOGS_3 $STRIDE_LOGS_4 $STRIDE_LOGS_5 $GAIA_LOGS $GAIA_LOGS_2 $HERMES_LOGS $ICQ_LOGS $JUNO_LOGS $TX_LOGS $KEYS_LOGS $OSMO_LOGS; do
     touch $log
 done
 
@@ -32,6 +32,8 @@ if [ "$CACHE" != "true" ]; then
     sh ${SCRIPT_DIR}/init_stride.sh
     sh ${SCRIPT_DIR}/init_gaia.sh
     sh ${SCRIPT_DIR}/init_relayers.sh
+    sh ${SCRIPT_DIR}/init_juno.sh
+    sh ${SCRIPT_DIR}/init_osmo.sh
 else
     # Otherwise, restore from the backup file
     echo "Restoring state from cache..."
@@ -40,15 +42,27 @@ fi
 
 # Starts Stride and Gaia in the background using nohup, pipes the logs to their corresponding log files,
 #   and halts the script until Stride/Gaia have each finalized a block
-printf '\n%s' "Starting Stride and Gaia...   "
+printf '\n%s' "Starting Stride, Gaia, Osmo, and Juno...   "
 nohup $STRIDE_CMD start | sed -r -u "s/\x1B\[([0-9]{1,3}(;[0-9]{1,2})?)?[mGK]//g" > $STRIDE_LOGS 2>&1 &
+nohup $STRIDE_CMD_2 start | sed -r -u "s/\x1B\[([0-9]{1,3}(;[0-9]{1,2})?)?[mGK]//g" > $STRIDE_LOGS_2 2>&1 &
+nohup $STRIDE_CMD_3 start | sed -r -u "s/\x1B\[([0-9]{1,3}(;[0-9]{1,2})?)?[mGK]//g" > $STRIDE_LOGS_3 2>&1 &
+nohup $STRIDE_CMD_4 start | sed -r -u "s/\x1B\[([0-9]{1,3}(;[0-9]{1,2})?)?[mGK]//g" > $STRIDE_LOGS_4 2>&1 &
+nohup $STRIDE_CMD_5 start | sed -r -u "s/\x1B\[([0-9]{1,3}(;[0-9]{1,2})?)?[mGK]//g" > $STRIDE_LOGS_5 2>&1 &
 nohup $GAIA_CMD start | sed -r -u "s/\x1B\[([0-9]{1,3}(;[0-9]{1,2})?)?[mGK]//g" > $GAIA_LOGS 2>&1 &
 nohup $GAIA_CMD_2 start | sed -r -u "s/\x1B\[([0-9]{1,3}(;[0-9]{1,2})?)?[mGK]//g" > $GAIA_LOGS_2 2>&1 &
-# nohup $GAIA_CMD_3 start | sed -r -u "s/\x1B\[([0-9]{1,3}(;[0-9]{1,2})?)?[mGK]//g" > $GAIA_LOGS_3 2>&1 &
+nohup $JUNO_CMD start | sed -r -u "s/\x1B\[([0-9]{1,3}(;[0-9]{1,2})?)?[mGK]//g" > $JUNO_LOGS 2>&1 &
+nohup $OSMO_CMD start | sed -r -u "s/\x1B\[([0-9]{1,3}(;[0-9]{1,2})?)?[mGK]//g" > $OSMO_LOGS 2>&1 &
 
 ( tail -f -n0 $STRIDE_LOGS & ) | grep -q "finalizing commit of block"
+( tail -f -n0 $STRIDE_LOGS_2 & ) | grep -q "finalizing commit of block"
+( tail -f -n0 $STRIDE_LOGS_3 & ) | grep -q "finalizing commit of block"
+( tail -f -n0 $STRIDE_LOGS_4 & ) | grep -q "finalizing commit of block"
+( tail -f -n0 $STRIDE_LOGS_5 & ) | grep -q "finalizing commit of block"
 ( tail -f -n0 $GAIA_LOGS & ) | grep -q "finalizing commit of block"
-sleep 5
+( tail -f -n0 $GAIA_LOGS_2 & ) | grep -q "finalizing commit of block"
+( tail -f -n0 $JUNO_LOGS & ) | grep -q "finalizing commit of block"
+( tail -f -n0 $OSMO_LOGS & ) | grep -q "finalizing commit of block"
+sleep 2
 echo "Done"
 
 if [ "$CACHE" != "true" ]; then
@@ -56,15 +70,18 @@ if [ "$CACHE" != "true" ]; then
     # Logs are piped to the hermes log file and the script is halted until:
     #  1)  "Creating transfer channel" is printed (indicating the connection has been created)
     #  2)  "Message ChanOpenInit" is printed (indicating the channnel has been created)
-    printf '%s' "Creating Hermes Connection... "
     bash $SCRIPT_DIR/init_channel.sh >> $HERMES_LOGS 2>&1 &
-    ( tail -f -n0 $HERMES_LOGS & ) | grep -q "Creating transfer channel"
-    echo "Done"
+    for i in {1..3}
+    do
+        printf '%s' "Creating Hermes Connection $i... "
+        ( tail -f -n0 $HERMES_LOGS & ) | grep -q "Creating transfer channel"
+        echo "Done"
 
-    printf '%s' "Creating Hermes Channel...    "
-    # continuation of logs from above command
-    ( tail -f -n0 $HERMES_LOGS & ) | grep -q "Success: Channel"
-    echo "Done"
+        printf '%s' "Creating Hermes Channel $i...    "
+        # continuation of logs from above command
+        ( tail -f -n0 $HERMES_LOGS & ) | grep -q "Success: Channel"
+        echo "Done"
+    done
 fi
 
 # Start hermes in the background and pause until the log message shows that it is up and running
@@ -83,6 +100,7 @@ echo "Done"
 # Create a copy of the state that can be used for the "cache" option
 echo "Network is ready for transactions.\n"
 rm -rf $SCRIPT_DIR/.state.backup
+sleep 1
 cp -r $SCRIPT_DIR/state $SCRIPT_DIR/.state.backup
 
 if [ "$CACHE" != "true" ]; then
@@ -91,3 +109,5 @@ fi
 
 # Add more detailed log files
 $SCRIPT_DIR/create_logs.sh &
+
+echo "Done! Go get em."
