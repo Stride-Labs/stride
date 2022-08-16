57c95a394a565212a74931ca91da00a98db3e04e
Aidan Salzmann <aidan@stridelabs.co>
2022-08-16 14:14:08 -0400

    add ClearBalance msg

---
+++ x/stakeibc/keeper/msg_server_clear_balance.go
@@ -0,0 +1,61 @@
+package keeper
+
+import (
+	"context"
+	"fmt"
+
+	sdk "github.com/cosmos/cosmos-sdk/types"
+	sdkerrors "github.com/cosmos/cosmos-sdk/types/errors"
+	ibctransfertypes "github.com/cosmos/ibc-go/v3/modules/apps/transfer/types"
+	"github.com/spf13/cast"
+
+	"github.com/Stride-Labs/stride/x/stakeibc/types"
+)
+
+func (k msgServer) ClearBalance(goCtx context.Context, msg *types.MsgClearBalance) (*types.MsgClearBalanceResponse, error) {
+	ctx := sdk.UnwrapSDKContext(goCtx)
+
+	zone, found := k.GetHostZone(ctx, msg.ChainId)
+	if !found {
+		return nil, sdkerrors.Wrapf(types.ErrInvalidHostZone, "chainId: %s", msg.ChainId)
+	}
+	feeAccount := zone.GetFeeAccount()
+	if feeAccount == nil {
+		return nil, sdkerrors.Wrapf(types.ErrFeeAccountNotRegistered, "chainId: %s", msg.ChainId)
+	}
+
+	// should this be a param? the transfer port _should_ always be the same across zones
+	sourcePort := "transfer"
+	// Should this be a param?
+	// I think as long as we have a timeout on this, it should be hard to attack (even if someone send a tx on a bad channel, it would be reverted relatively quickly)
+	sourceChannel := "???"
+	coinString := cast.ToString(msg.Amount) + zone.GetHostDenom()
+	tokens, err := sdk.ParseCoinNormalized(coinString)
+	if err != nil {
+		k.Logger(ctx).Error(fmt.Sprintf("failed to parse coin (%s)", coinString))
+		return nil, sdkerrors.Wrapf(err, "failed to parse coin (%s)", coinString)
+	}
+	sender := feeAccount.GetAddress()
+	// where to store this?
+	receiver := "stride19uvw0azm9u0k6vqe4e22cga6kteskdqq3ulj6q"
+	timeoutTimestamp := cast.ToUint64(ctx.BlockTime().UnixNano()) + icaTimeoutNanos
+	msgs := []sdk.Msg{
+		&ibctransfertypes.MsgTransfer{
+			SourcePort: sourcePort,
+			SourceChannel: sourceChannel,
+			Token: tokens,
+			Sender: sender,
+			Receiver: receiver,
+			TimeoutTimestamp: timeoutTimestamp,
+		},
+	}
+
+	connectionId := zone.GetConnectionId()
+	
+	icaTimeoutNanos := k.GetParam(ctx, types.KeyICATimeoutNanos)
+	icaTimeoutNanos = cast.ToUint64(ctx.BlockTime().UnixNano()) + icaTimeoutNanos
+
+	k.SubmitTxs(ctx, connectionId, msgs, *feeAccount, icaTimeoutNanos, "", nil)
+	return &types.MsgClearBalanceResponse{}, nil
+}
+
---
+++ x/stakeibc/types/message_clear_balance.go
@@ -0,0 +1,52 @@
+package types
+
+import (
+	sdk "github.com/cosmos/cosmos-sdk/types"
+	sdkerrors "github.com/cosmos/cosmos-sdk/types/errors"
+)
+
+const TypeMsgClearBalance = "clear_balance"
+
+var _ sdk.Msg = &MsgClearBalance{}
+
+func NewMsgClearBalance(creator string, chainId string, amount uint64) *MsgClearBalance {
+	return &MsgClearBalance{
+		Creator:   creator,
+		ChainId:    chainId,
+		Amount: amount,
+	}
+}
+
+func (msg *MsgClearBalance) Route() string {
+	return RouterKey
+}
+
+func (msg *MsgClearBalance) Type() string {
+	return TypeMsgClearBalance
+}
+
+func (msg *MsgClearBalance) GetSigners() []sdk.AccAddress {
+	creator, err := sdk.AccAddressFromBech32(msg.Creator)
+	if err != nil {
+		panic(err)
+	}
+	return []sdk.AccAddress{creator}
+}
+
+func (msg *MsgClearBalance) GetSignBytes() []byte {
+	bz := ModuleCdc.MustMarshalJSON(msg)
+	return sdk.MustSortJSON(bz)
+}
+
+func (msg *MsgClearBalance) ValidateBasic() error {
+	_, err := sdk.AccAddressFromBech32(msg.Creator)
+	if err != nil {
+		return sdkerrors.Wrapf(sdkerrors.ErrInvalidAddress, "invalid creator address (%s)", err)
+	}
+	// TODO: add validation
+	// Should we let anyone call this?
+	// if err := utils.ValidateAdminAddress(msg.Creator); err != nil {
+	// 	return err
+	// }
+	return nil
+}
