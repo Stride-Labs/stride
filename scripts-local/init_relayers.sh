0e58744dda497739ce798918084007b00d76f2ad
sampocs <sam.pochyly@gmail.com>
2022-08-16 16:37:58 -0500

    added icq config

--- scripts-local/init_relayers.sh
+++ scripts-local/init_relayers.sh
@@ -10,5 +10,7 @@ $HERMES_CMD keys restore --mnemonic "$HERMES_STRIDE_MNEMONIC" $STRIDE_CHAIN
 $HERMES_CMD keys restore --mnemonic "$HERMES_GAIA_MNEMONIC" $GAIA_CHAIN
 
 echo "Adding ICQ keys"
-echo $ICQ_STRIDE_MNEMONIC | $ICQ_CMD keys restore stridekey --chain stride-local --local
-echo $ICQ_GAIA_MNEMONIC | $ICQ_CMD keys restore gaiakey --chain gaia-local --local
+mkdir -p $SCRIPT_DIR/state/icq
+cp $SCRIPT_DIR/icq/config.yaml $SCRIPT_DIR/state/icq/config.yaml
+echo $ICQ_STRIDE_MNEMONIC | $ICQ_CMD keys restore stridekey --chain stride-local
+echo $ICQ_GAIA_MNEMONIC | $ICQ_CMD keys restore gaiakey --chain gaia-local
