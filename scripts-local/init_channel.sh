539c6f6560ff3998d19a2cb33695e93db41b3d39
vish-stride <vishal@stridelabs.co>
2022-08-17 01:27:18 -0400

    add osmo host zone to localnet

--- scripts-local/init_channel.sh
+++ scripts-local/init_channel.sh
@@ -11,16 +11,17 @@ $HERMES_CMD create connection $STRIDE_CHAIN $GAIA_CHAIN
 echo "Creating transfer channel for Gaia"
 $HERMES_CMD create channel --port-a transfer --port-b transfer $GAIA_CHAIN connection-0 
 
-sleep 5
-# start go relayer 
+sleep 3
 nohup rly start gaia_path -p events --home $SCRIPT_DIR/go-rly >> $RLY_GAIA_LOGS 2>&1 &
 
-# echo "Creating connection $STRIDE_CHAIN <> $OSMO_CHAIN"
-# $HERMES_CMD create connection $STRIDE_CHAIN $OSMO_CHAIN 
+echo "Creating connection $STRIDE_CHAIN <> $OSMO_CHAIN"
+$HERMES_CMD create connection $STRIDE_CHAIN $OSMO_CHAIN 
 
-# echo "Creating transfer channel for Osmo"
-# $HERMES_CMD create channel --port-a transfer --port-b transfer $OSMO_CHAIN connection-0
+echo "Creating transfer channel for Osmo"
+$HERMES_CMD create channel --port-a transfer --port-b transfer $OSMO_CHAIN connection-0
 
+sleep 3
+nohup rly start osmo_path -p events --home $SCRIPT_DIR/go-rly --debug-addr localhost:7598 >> $RLY_OSMO_LOGS 2>&1 &
 # nohup rly start osmo_path -p events --home $SCRIPT_DIR/go-rly >> $RLY_OSMO_LOGS 2>&1 &
 # echo "Creating connection $STRIDE_CHAIN <> $JUNO_CHAIN"
 # $HERMES_CMD create connection $STRIDE_CHAIN $JUNO_CHAIN 
