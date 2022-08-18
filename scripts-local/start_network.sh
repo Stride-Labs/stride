765fce65c25bdf75c73300af8b5bfcf6e64764db
Riley Edmunds <riley@stridelabs.co>
2022-08-18 01:54:01 -0400

    kill networks upon re-init'ing & auto-restart icq upon icq errors (untested)

--- scripts-local/start_network.sh
+++ scripts-local/start_network.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 
+# kill previous networks
+make stop &> /dev/null
+
 set -eu
 SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
 source $SCRIPT_DIR/vars.sh
@@ -92,7 +95,7 @@ echo "Done"
 
 # Start ICQ in the background
 printf '%s' "Starting ICQ...     "
-nohup $ICQ_CMD run --local >> $ICQ_LOGS 2>&1 &
+source $SCRIPT_DIR/icq_startup.sh &
 sleep 5
 echo "Done"
 
