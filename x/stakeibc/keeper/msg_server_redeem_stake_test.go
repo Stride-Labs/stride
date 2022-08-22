8a85eedc77fd712ceeb8350a5105ee926f1279c4
sampocs <sam.pochyly@gmail.com>
2022-08-22 17:42:25 -0500

    renamed suite -> s to keep naming consistent across tests

--- x/stakeibc/keeper/msg_server_redeem_stake_test.go
+++ x/stakeibc/keeper/msg_server_redeem_stake_test.go
@@ -26,15 +26,15 @@ type RedeemStakeTestCase struct {
 	validMsg     stakeibc.MsgRedeemStake
 }
 
-func (suite *KeeperTestSuite) SetupRedeemStake() RedeemStakeTestCase {
+func (s *KeeperTestSuite) SetupRedeemStake() RedeemStakeTestCase {
 	redeemAmount := uint64(1_000_000)
 	user := Account{
-		acc:           suite.TestAccs[0],
+		acc:           s.TestAccs[0],
 		atomBalance:   sdk.NewInt64Coin("ibc/uatom", 10_000_000),
 		stAtomBalance: sdk.NewInt64Coin("stuatom", 10_000_000),
 	}
-	suite.FundAccount(user.acc, user.atomBalance)
-	suite.FundAccount(user.acc, user.stAtomBalance)
+	s.FundAccount(user.acc, user.atomBalance)
+	s.FundAccount(user.acc, user.stAtomBalance)
 
 	zoneAddress := types.NewZoneAddress(chainId)
 
@@ -43,8 +43,8 @@ func (suite *KeeperTestSuite) SetupRedeemStake() RedeemStakeTestCase {
 		atomBalance:   sdk.NewInt64Coin("ibc/uatom", 10_000_000),
 		stAtomBalance: sdk.NewInt64Coin("stuatom", 10_000_000),
 	}
-	suite.FundAccount(zoneAccount.acc, zoneAccount.atomBalance)
-	suite.FundAccount(zoneAccount.acc, zoneAccount.stAtomBalance)
+	s.FundAccount(zoneAccount.acc, zoneAccount.atomBalance)
+	s.FundAccount(zoneAccount.acc, zoneAccount.stAtomBalance)
 
 	// TODO define the host zone with stakedBal and validators with staked amounts
 	hostZone := stakeibc.HostZone{
@@ -53,7 +53,7 @@ func (suite *KeeperTestSuite) SetupRedeemStake() RedeemStakeTestCase {
 		Bech32Prefix:   "cosmos",
 		RedemptionRate: sdk.NewDec(1.0),
 		StakedBal:      1234567890,
-		Address: 		zoneAddress.String(),
+		Address:        zoneAddress.String(),
 	}
 
 	epochTrackerDay := stakeibc.EpochTracker{
@@ -74,13 +74,13 @@ func (suite *KeeperTestSuite) SetupRedeemStake() RedeemStakeTestCase {
 	}
 	epochUnbondingRecord.HostZoneUnbondings = append(epochUnbondingRecord.HostZoneUnbondings, hostZoneUnbonding)
 
-	suite.App.StakeibcKeeper.SetHostZone(suite.Ctx, hostZone)
-	suite.App.StakeibcKeeper.SetEpochTracker(suite.Ctx, epochTrackerDay)
-	suite.App.RecordsKeeper.SetEpochUnbondingRecord(suite.Ctx, epochUnbondingRecord)
+	s.App.StakeibcKeeper.SetHostZone(s.Ctx, hostZone)
+	s.App.StakeibcKeeper.SetEpochTracker(s.Ctx, epochTrackerDay)
+	s.App.RecordsKeeper.SetEpochUnbondingRecord(s.Ctx, epochUnbondingRecord)
 
 	return RedeemStakeTestCase{
-		user:     user,
-		hostZone: hostZone,
+		user:        user,
+		hostZone:    hostZone,
 		zoneAccount: zoneAccount,
 		initialState: RedeemStakeState{
 			epochNumber:                        epochTrackerDay.EpochNumber,
@@ -97,8 +97,8 @@ func (suite *KeeperTestSuite) SetupRedeemStake() RedeemStakeTestCase {
 	}
 }
 
-func (suite *KeeperTestSuite) TestRedeemStakeSuccessful() {
-	tc := suite.SetupRedeemStake()
+func (s *KeeperTestSuite) TestRedeemStakeSuccessful() {
+	tc := s.SetupRedeemStake()
 	initialState := tc.initialState
 
 	msg := tc.validMsg
@@ -110,24 +110,25 @@ func (suite *KeeperTestSuite) TestRedeemStakeSuccessful() {
 	redeemAmount := sdk.NewInt(amt)
 
 	// get the initial unbonding amount *before* calling liquid stake, so we can use it to calc expected vs actual in diff space
-	_, err = suite.msgServer.RedeemStake(sdk.WrapSDKContext(suite.Ctx), &msg)
-	suite.Require().NoError(err)
+	_, err = s.msgServer.RedeemStake(sdk.WrapSDKContext(s.Ctx), &msg)
+	s.Require().NoError(err)
 
 	// User STUATOM balance should have DECREASED by the amount to be redeemed
 	expectedUserStAtomBalance := user.stAtomBalance.SubAmount(redeemAmount)
-	actualUserStAtomBalance := suite.App.BankKeeper.GetBalance(suite.Ctx, user.acc, "stuatom")
-	suite.CompareCoins(expectedUserStAtomBalance, actualUserStAtomBalance, "user stuatom balance")
+	actualUserStAtomBalance := s.App.BankKeeper.GetBalance(s.Ctx, user.acc, "stuatom")
+	s.CompareCoins(expectedUserStAtomBalance, actualUserStAtomBalance, "user stuatom balance")
 
 	// Gaia's hostZoneUnbonding NATIVE TOKEN amount should have INCREASED from 0 to the amount redeemed multiplied by the redemption rate
 	// Gaia's hostZoneUnbonding STTOKEN amount should have INCREASED from 0 to be amount redeemed
-	epochTracker, found := suite.App.StakeibcKeeper.GetEpochTracker(suite.Ctx, "day")
-	suite.Require().True(found)
-	epochUnbondingRecord, found := suite.App.RecordsKeeper.GetEpochUnbondingRecord(suite.Ctx, epochTracker.EpochNumber)
-	suite.Require().True(found)
-	hostZoneUnbonding, found := suite.App.RecordsKeeper.GetHostZoneUnbondingByChainId(suite.Ctx, epochUnbondingRecord.EpochNumber, chainId)
-	suite.Require().True(found)
+	epochTracker, found := s.App.StakeibcKeeper.GetEpochTracker(s.Ctx, "day")
+	s.Require().True(found, "epoch tracker")
+	epochUnbondingRecord, found := s.App.RecordsKeeper.GetEpochUnbondingRecord(s.Ctx, epochTracker.EpochNumber)
+	s.Require().True(found, "epoch unbonding record")
+	hostZoneUnbonding, found := s.App.RecordsKeeper.GetHostZoneUnbondingByChainId(s.Ctx, epochUnbondingRecord.EpochNumber, chainId)
+	s.Require().True(found, "host zone unbondings by chain ID")
+	hostZone, found := s.App.StakeibcKeeper.GetHostZone(s.Ctx, msg.HostZone)
+	s.Require().True(found, "host zone")
 
-	hostZone, _ := suite.App.StakeibcKeeper.GetHostZone(suite.Ctx, msg.HostZone)
 	nativeRedemptionAmount := (redeemAmount.Int64() * hostZone.RedemptionRate.TruncateInt().Int64())
 	stTokenBurnAmount := redeemAmount.Int64()
 
@@ -136,131 +137,131 @@ func (suite *KeeperTestSuite) TestRedeemStakeSuccessful() {
 	expectedHostZoneUnbondingNativeAmount := int64(initialState.initialNativeEpochUnbondingAmount) + nativeRedemptionAmount
 	expectedHostZoneUnbondingStTokenAmount := int64(initialState.initialStTokenEpochUnbondingAmount) + stTokenBurnAmount
 
-	suite.Require().Equal(expectedHostZoneUnbondingNativeAmount, actualHostZoneUnbondingNativeAmount, "host zone native unbonding amount")
-	suite.Require().Equal(expectedHostZoneUnbondingStTokenAmount, actualHostZoneUnbondingStTokenAmount, "host zone stToken burn amount")
+	s.Require().Equal(expectedHostZoneUnbondingNativeAmount, actualHostZoneUnbondingNativeAmount, "host zone native unbonding amount")
+	s.Require().Equal(expectedHostZoneUnbondingStTokenAmount, actualHostZoneUnbondingStTokenAmount, "host zone stToken burn amount")
 
 	// UserRedemptionRecord should have been created with correct amount, sender, receiver, host zone, isClaimable
 	userRedemptionRecords := hostZoneUnbonding.UserRedemptionRecords
-	suite.Require().Equal(len(userRedemptionRecords), 1)
+	s.Require().Equal(len(userRedemptionRecords), 1)
 	userRedemptionRecordId := userRedemptionRecords[0]
-	userRedemptionRecord, found := suite.App.RecordsKeeper.GetUserRedemptionRecord(suite.Ctx, userRedemptionRecordId)
-	suite.Require().True(found)
+	userRedemptionRecord, found := s.App.RecordsKeeper.GetUserRedemptionRecord(s.Ctx, userRedemptionRecordId)
+	s.Require().True(found)
 	// check amount
-	suite.Require().Equal(int64(userRedemptionRecord.Amount), expectedHostZoneUnbondingNativeAmount)
+	s.Require().Equal(expectedHostZoneUnbondingNativeAmount, int64(userRedemptionRecord.Amount), "redemption record amount")
 	// check sender
-	suite.Require().Equal(userRedemptionRecord.Sender, msg.Creator)
+	s.Require().Equal(msg.Creator, userRedemptionRecord.Sender, "redemption record sender")
 	// check receiver
-	suite.Require().Equal(userRedemptionRecord.Receiver, msg.Receiver)
+	s.Require().Equal(msg.Receiver, userRedemptionRecord.Receiver, "redemption record receiver")
 	// check host zone
-	suite.Require().Equal(userRedemptionRecord.HostZoneId, msg.HostZone)
+	s.Require().Equal(msg.HostZone, userRedemptionRecord.HostZoneId, "redemption record host zone")
 	// check isClaimable
-	suite.Require().Equal(userRedemptionRecord.IsClaimable, false)
+	s.Require().False(userRedemptionRecord.IsClaimable, "redemption record should be marked not claimable")
 }
 
-func (suite *KeeperTestSuite) TestInvalidCreatorAddress() {
-	tc := suite.SetupRedeemStake()
+func (s *KeeperTestSuite) TestInvalidCreatorAddress() {
+	tc := s.SetupRedeemStake()
 	invalidMsg := tc.validMsg
 
 	// cosmos instead of stride address
 	invalidMsg.Creator = "cosmos1g6qdx6kdhpf000afvvpte7hp0vnpzapuyxp8uf"
-	_, err := suite.msgServer.RedeemStake(sdk.WrapSDKContext(suite.Ctx), &invalidMsg)
-	suite.Require().EqualError(err, fmt.Sprintf("creator address is invalid: %s. err: invalid Bech32 prefix; expected stride, got cosmos: invalid address", invalidMsg.Creator))
+	_, err := s.msgServer.RedeemStake(sdk.WrapSDKContext(s.Ctx), &invalidMsg)
+	s.Require().EqualError(err, fmt.Sprintf("creator address is invalid: %s. err: invalid Bech32 prefix; expected stride, got cosmos: invalid address", invalidMsg.Creator))
 
 	// invalid stride address
 	invalidMsg.Creator = "stride1g6qdx6kdhpf000afvvpte7hp0vnpzapuyxp8uf"
-	_, err = suite.msgServer.RedeemStake(sdk.WrapSDKContext(suite.Ctx), &invalidMsg)
-	suite.Require().EqualError(err, fmt.Sprintf("creator address is invalid: %s. err: decoding bech32 failed: invalid checksum (expected 8dpmg9 got yxp8uf): invalid address", invalidMsg.Creator))
+	_, err = s.msgServer.RedeemStake(sdk.WrapSDKContext(s.Ctx), &invalidMsg)
+	s.Require().EqualError(err, fmt.Sprintf("creator address is invalid: %s. err: decoding bech32 failed: invalid checksum (expected 8dpmg9 got yxp8uf): invalid address", invalidMsg.Creator))
 
 	// empty address
 	invalidMsg.Creator = ""
-	_, err = suite.msgServer.RedeemStake(sdk.WrapSDKContext(suite.Ctx), &invalidMsg)
-	suite.Require().EqualError(err, fmt.Sprintf("creator address is invalid: %s. err: empty address string is not allowed: invalid address", invalidMsg.Creator))
+	_, err = s.msgServer.RedeemStake(sdk.WrapSDKContext(s.Ctx), &invalidMsg)
+	s.Require().EqualError(err, fmt.Sprintf("creator address is invalid: %s. err: empty address string is not allowed: invalid address", invalidMsg.Creator))
 
 	// wrong len address
 	invalidMsg.Creator = "stride1g6qdx6kdhpf000afvvpte7hp0vnpzapuyxp8ufabc"
-	_, err = suite.msgServer.RedeemStake(sdk.WrapSDKContext(suite.Ctx), &invalidMsg)
-	suite.Require().EqualError(err, fmt.Sprintf("creator address is invalid: %s. err: decoding bech32 failed: invalid character not part of charset: 98: invalid address", invalidMsg.Creator))
+	_, err = s.msgServer.RedeemStake(sdk.WrapSDKContext(s.Ctx), &invalidMsg)
+	s.Require().EqualError(err, fmt.Sprintf("creator address is invalid: %s. err: decoding bech32 failed: invalid character not part of charset: 98: invalid address", invalidMsg.Creator))
 }
 
-func (suite *KeeperTestSuite) TestRedeemStakeHostZoneNotFound() {
-	tc := suite.SetupRedeemStake()
+func (s *KeeperTestSuite) TestRedeemStakeHostZoneNotFound() {
+	tc := s.SetupRedeemStake()
 
 	invalidMsg := tc.validMsg
 	invalidMsg.HostZone = "fake_host_zone"
-	_, err := suite.msgServer.RedeemStake(sdk.WrapSDKContext(suite.Ctx), &invalidMsg)
+	_, err := s.msgServer.RedeemStake(sdk.WrapSDKContext(s.Ctx), &invalidMsg)
 
-	suite.Require().EqualError(err, "host zone is invalid: fake_host_zone: host zone not registered")
+	s.Require().EqualError(err, "host zone is invalid: fake_host_zone: host zone not registered")
 }
 
-func (suite *KeeperTestSuite) TestRedeemStakeRateAboveMaxThreshold() {
-	tc := suite.SetupRedeemStake()
+func (s *KeeperTestSuite) TestRedeemStakeRateAboveMaxThreshold() {
+	tc := s.SetupRedeemStake()
 
 	hz := tc.hostZone
 	hz.RedemptionRate = sdk.NewDec(100)
-	suite.App.StakeibcKeeper.SetHostZone(suite.Ctx, hz)
+	s.App.StakeibcKeeper.SetHostZone(s.Ctx, hz)
 
-	_, err := suite.msgServer.RedeemStake(sdk.WrapSDKContext(suite.Ctx), &tc.validMsg)
-	suite.Require().Error(err)
+	_, err := s.msgServer.RedeemStake(sdk.WrapSDKContext(s.Ctx), &tc.validMsg)
+	s.Require().Error(err)
 }
 
-func (suite *KeeperTestSuite) TestInvalidReceiverAddress() {
-	tc := suite.SetupRedeemStake()
+func (s *KeeperTestSuite) TestInvalidReceiverAddress() {
+	tc := s.SetupRedeemStake()
 
 	invalidMsg := tc.validMsg
 
 	// stride instead of cosmos address
 	invalidMsg.Receiver = "stride159atdlc3ksl50g0659w5tq42wwer334ajl7xnq"
-	_, err := suite.msgServer.RedeemStake(sdk.WrapSDKContext(suite.Ctx), &invalidMsg)
-	suite.Require().EqualError(err, "invalid receiver address (invalid Bech32 prefix; expected cosmos, got stride): invalid address")
+	_, err := s.msgServer.RedeemStake(sdk.WrapSDKContext(s.Ctx), &invalidMsg)
+	s.Require().EqualError(err, "invalid receiver address (invalid Bech32 prefix; expected cosmos, got stride): invalid address")
 
 	// invalid cosmos address
 	invalidMsg.Receiver = "cosmos1g6qdx6kdhpf000afvvpte7hp0vnpzapuyxp8ua"
-	_, err = suite.msgServer.RedeemStake(sdk.WrapSDKContext(suite.Ctx), &invalidMsg)
-	suite.Require().EqualError(err, "invalid receiver address (decoding bech32 failed: invalid checksum (expected yxp8uf got yxp8ua)): invalid address")
+	_, err = s.msgServer.RedeemStake(sdk.WrapSDKContext(s.Ctx), &invalidMsg)
+	s.Require().EqualError(err, "invalid receiver address (decoding bech32 failed: invalid checksum (expected yxp8uf got yxp8ua)): invalid address")
 
 	// empty address
 	invalidMsg.Receiver = ""
-	_, err = suite.msgServer.RedeemStake(sdk.WrapSDKContext(suite.Ctx), &invalidMsg)
-	suite.Require().EqualError(err, "invalid receiver address (empty address string is not allowed): invalid address")
+	_, err = s.msgServer.RedeemStake(sdk.WrapSDKContext(s.Ctx), &invalidMsg)
+	s.Require().EqualError(err, "invalid receiver address (empty address string is not allowed): invalid address")
 
 	// wrong len address
 	invalidMsg.Receiver = "cosmos1g6qdx6kdhpf000afvvpte7hp0vnpzapuyxp8ufa"
-	_, err = suite.msgServer.RedeemStake(sdk.WrapSDKContext(suite.Ctx), &invalidMsg)
-	suite.Require().EqualError(err, "invalid receiver address (decoding bech32 failed: invalid checksum (expected xp8ugp got xp8ufa)): invalid address")
+	_, err = s.msgServer.RedeemStake(sdk.WrapSDKContext(s.Ctx), &invalidMsg)
+	s.Require().EqualError(err, "invalid receiver address (decoding bech32 failed: invalid checksum (expected xp8ugp got xp8ufa)): invalid address")
 }
 
-func (suite *KeeperTestSuite) TestRedeemStakeRedeemMoreThanStaked() {
-	tc := suite.SetupRedeemStake()
+func (s *KeeperTestSuite) TestRedeemStakeRedeemMoreThanStaked() {
+	tc := s.SetupRedeemStake()
 
 	invalidMsg := tc.validMsg
 	invalidMsg.Amount = uint64(1_000_000_000_000_000)
-	_, err := suite.msgServer.RedeemStake(sdk.WrapSDKContext(suite.Ctx), &invalidMsg)
+	_, err := s.msgServer.RedeemStake(sdk.WrapSDKContext(s.Ctx), &invalidMsg)
 
-	suite.Require().EqualError(err, fmt.Sprintf("cannot unstake an amount g.t. staked balance on host zone: %d: invalid amount", invalidMsg.Amount))
+	s.Require().EqualError(err, fmt.Sprintf("cannot unstake an amount g.t. staked balance on host zone: %d: invalid amount", invalidMsg.Amount))
 }
 
-func (suite *KeeperTestSuite) TestRedeemStakeNoEpochTrackerDay() {
-	tc := suite.SetupRedeemStake()
+func (s *KeeperTestSuite) TestRedeemStakeNoEpochTrackerDay() {
+	tc := s.SetupRedeemStake()
 
 	invalidMsg := tc.validMsg
-	suite.App.RecordsKeeper.RemoveEpochUnbondingRecord(suite.Ctx, tc.initialState.epochNumber)
-	_, err := suite.msgServer.RedeemStake(sdk.WrapSDKContext(suite.Ctx), &invalidMsg)
+	s.App.RecordsKeeper.RemoveEpochUnbondingRecord(s.Ctx, tc.initialState.epochNumber)
+	_, err := s.msgServer.RedeemStake(sdk.WrapSDKContext(s.Ctx), &invalidMsg)
 
-	suite.Require().EqualError(err, "latest epoch unbonding record not found: epoch unbonding record not found")
+	s.Require().EqualError(err, "latest epoch unbonding record not found: epoch unbonding record not found")
 }
 
-func (suite *KeeperTestSuite) TestRedeemStakeUserAlreadyRedeemedThisEpoch() {
-	tc := suite.SetupRedeemStake()
+func (s *KeeperTestSuite) TestRedeemStakeUserAlreadyRedeemedThisEpoch() {
+	tc := s.SetupRedeemStake()
 
 	invalidMsg := tc.validMsg
-	_, err := suite.msgServer.RedeemStake(sdk.WrapSDKContext(suite.Ctx), &invalidMsg)
-	suite.Require().NoError(err)
-	_, err = suite.msgServer.RedeemStake(sdk.WrapSDKContext(suite.Ctx), &invalidMsg)
-	suite.Require().EqualError(err, fmt.Sprintf("user already redeemed this epoch: GAIA.1.%s: redemption record already exists", suite.TestAccs[0]))
+	_, err := s.msgServer.RedeemStake(sdk.WrapSDKContext(s.Ctx), &invalidMsg)
+	s.Require().NoError(err)
+	_, err = s.msgServer.RedeemStake(sdk.WrapSDKContext(s.Ctx), &invalidMsg)
+	s.Require().EqualError(err, fmt.Sprintf("user already redeemed this epoch: GAIA.1.%s: redemption record already exists", s.TestAccs[0]))
 }
 
-func (suite *KeeperTestSuite) TestRedeemStakeHostZoneNoUnbondings() {
-	tc := suite.SetupRedeemStake()
+func (s *KeeperTestSuite) TestRedeemStakeHostZoneNoUnbondings() {
+	tc := s.SetupRedeemStake()
 
 	invalidMsg := tc.validMsg
 	epochUnbondingRecord := recordtypes.EpochUnbondingRecord{
@@ -274,10 +275,10 @@ func (suite *KeeperTestSuite) TestRedeemStakeHostZoneNoUnbondings() {
 	}
 	epochUnbondingRecord.HostZoneUnbondings = append(epochUnbondingRecord.HostZoneUnbondings, hostZoneUnbonding)
 
-	suite.App.RecordsKeeper.SetEpochUnbondingRecord(suite.Ctx, epochUnbondingRecord)
-	_, err := suite.msgServer.RedeemStake(sdk.WrapSDKContext(suite.Ctx), &invalidMsg)
+	s.App.RecordsKeeper.SetEpochUnbondingRecord(s.Ctx, epochUnbondingRecord)
+	_, err := s.msgServer.RedeemStake(sdk.WrapSDKContext(s.Ctx), &invalidMsg)
 
-	suite.Require().EqualError(err, "host zone not found in unbondings: GAIA: host zone not registered")
+	s.Require().EqualError(err, "host zone not found in unbondings: GAIA: host zone not registered")
 }
 
 func (s *KeeperTestSuite) TestRedeemStake_InvalidHostAddress() {
