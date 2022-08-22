c4a789c7a53567ec816762b62294addeebd551fd
Riley Edmunds <riley@stridelabs.co>
2022-08-21 20:22:59 -0400

    add 0.9 rr invariant to stakeibc

---
+++ x/stakeibc/keeper/invariants.go
@@ -0,0 +1,45 @@
+package keeper
+
+// DONTCOVER
+
+import (
+	"fmt"
+
+	sdk "github.com/cosmos/cosmos-sdk/types"
+
+	"github.com/Stride-Labs/stride/x/stakeibc/types"
+)
+
+const redemptionRateInvariantName = "redemption-rate-above-0.9"
+
+// RegisterInvariants registers all governance invariants.
+func RegisterInvariants(ir sdk.InvariantRegistry, k Keeper) {
+	ir.RegisterRoute(types.ModuleName, redemptionRateInvariantName, RedemptionRateInvariant(k))
+}
+
+// AllInvariants runs all invariants of the gamm module
+func AllInvariants(k Keeper) sdk.Invariant {
+	return func(ctx sdk.Context) (string, bool) {
+		msg, broke := RedemptionRateInvariant(k)(ctx)
+		// note: once we have >1 invariant here, follow the pattern from staking module invariants here: https://github.com/cosmos/cosmos-sdk/blob/v0.46.0/x/staking/keeper/invariants.go
+		return msg, broke
+	}
+}
+
+// RedemptionRateInvariant checks that all hostZone redemption rates are above a fixed threshold
+func RedemptionRateInvariant(k Keeper) sdk.Invariant {
+
+	// threshold is 0.9
+	THRESHOLD := sdk.NewDec(9).Quo(sdk.NewDec(10))
+
+	return func(ctx sdk.Context) (string, bool) {
+		for _, hz := range k.GetAllHostZone(ctx) {
+			if hz.RedemptionRate.LT(THRESHOLD) {
+				return sdk.FormatInvariant(types.ModuleName, redemptionRateInvariantName,
+					fmt.Sprintf("[INVARIANT BROKEN!!!] %s's RR is %s", hz.GetChainId(), hz.RedemptionRate.String())), true
+			}
+		}
+		return sdk.FormatInvariant(types.ModuleName, redemptionRateInvariantName,
+			fmt.Sprintf("All RR's are GT %s", THRESHOLD.String())), false
+	}
+}
