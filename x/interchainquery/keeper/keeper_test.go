9795099e7aa9b98eb7adc4fc53bb926d345f5315
sampocs <sam@stridelabs.co>
2022-08-26 13:16:33 -0500

    [UNIT-TESTS] IBC Testing (#226)

--- x/interchainquery/keeper/keeper_test.go
+++ x/interchainquery/keeper/keeper_test.go
@@ -6,18 +6,14 @@ import (
 	"github.com/stretchr/testify/suite"
 
 	"github.com/Stride-Labs/stride/app/apptesting"
-	"github.com/Stride-Labs/stride/x/interchainquery/keeper"
-	"github.com/Stride-Labs/stride/x/interchainquery/types"
 )
 
 type KeeperTestSuite struct {
 	apptesting.AppTestHelper
-	msgServer types.MsgServer
 }
 
 func (s *KeeperTestSuite) SetupTest() {
 	s.Setup()
-	s.msgServer = keeper.NewMsgServerImpl(s.App.InterchainqueryKeeper)
 }
 
 func TestKeeperTestSuite(t *testing.T) {
--- x/stakeibc/keeper/keeper_test.go
+++ x/stakeibc/keeper/keeper_test.go
@@ -3,20 +3,23 @@ package keeper_test
 import (
 	"testing"
 
+	"github.com/stretchr/testify/suite"
+
 	"github.com/Stride-Labs/stride/app/apptesting"
 	"github.com/Stride-Labs/stride/x/stakeibc/keeper"
 	"github.com/Stride-Labs/stride/x/stakeibc/types"
-	"github.com/stretchr/testify/suite"
 )
 
 type KeeperTestSuite struct {
 	apptesting.AppTestHelper
-	msgServer types.MsgServer
 }
 
 func (s *KeeperTestSuite) SetupTest() {
 	s.Setup()
-	s.msgServer = keeper.NewMsgServerImpl(s.App.StakeibcKeeper)
+}
+
+func (s *KeeperTestSuite) GetMsgServer() types.MsgServer {
+	return keeper.NewMsgServerImpl(s.App.StakeibcKeeper)
 }
 
 func TestKeeperTestSuite(t *testing.T) {
