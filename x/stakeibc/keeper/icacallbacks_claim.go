97cbbe48b248620b049a26c2a3e1c4fa488884c9
Aidan Salzmann <aidan@stridelabs.co>
2022-08-28 11:18:20 -0400

    add claim cb unittest

--- x/stakeibc/keeper/icacallbacks_claim.go
+++ x/stakeibc/keeper/icacallbacks_claim.go
@@ -32,20 +32,6 @@ func (k Keeper) UnmarshalClaimCallbackArgs(ctx sdk.Context, claimCallback []byte
 }
 
 func ClaimCallback(k Keeper, ctx sdk.Context, packet channeltypes.Packet, ack *channeltypes.Acknowledgement, args []byte) error {
-	if ack == nil {
-		// handle timeout
-		k.Logger(ctx).Error(fmt.Sprintf("ClaimCallback timeout, ack is nil, packet %v", packet))
-		return nil
-	}
-	
-	txMsgData, err := icacallbacks.GetTxMsgData(ctx, *ack, k.Logger(ctx))
-	if err != nil {
-		k.Logger(ctx).Error(fmt.Sprintf("failed to fetch txMsgData, packet %v", packet))
-		return sdkerrors.Wrap(icacallbackstypes.ErrTxMsgData, err.Error())
-	}
-	k.Logger(ctx).Info("ClaimCallback executing", "packet", packet, "txMsgData", txMsgData, "args", args)
-
-	
 	// deserialize the args
 	claimCallback, err := k.UnmarshalClaimCallbackArgs(ctx, args)
 	if err != nil {
@@ -57,10 +43,24 @@ func ClaimCallback(k Keeper, ctx sdk.Context, packet channeltypes.Packet, ack *c
 		return sdkerrors.Wrapf(types.ErrRecordNotFound, "user redemption record not found %s", claimCallback.GetUserRedemptionRecordId())
 	}
 
+	// handle timeout
+	if ack == nil {
+		k.Logger(ctx).Error(fmt.Sprintf("ClaimCallback timeout, ack is nil, packet %v", packet))
+		userClaimRecord.IsClaimable = true
+		k.RecordsKeeper.SetUserRedemptionRecord(ctx, userClaimRecord)
+		return nil
+	}
+
+	txMsgData, err := icacallbacks.GetTxMsgData(ctx, *ack, k.Logger(ctx))
+	if err != nil {
+		k.Logger(ctx).Error(fmt.Sprintf("failed to unmarshal txMsgData, packet %v", packet))
+		return sdkerrors.Wrap(icacallbackstypes.ErrTxMsgData, err.Error())
+	}
+
+	k.Logger(ctx).Info("ClaimCallback executing", "packet", packet, "txMsgData", txMsgData, "args", args)
+	// handle failed tx on host chain
 	if len(txMsgData.Data) == 0 {
 		k.Logger(ctx).Error(fmt.Sprintf("ClaimCallback failed, packet %v", packet))
-		// transaction on the host chain failed
-		// set UserClaimRecord as claimable
 		userClaimRecord.IsClaimable = true
 		k.RecordsKeeper.SetUserRedemptionRecord(ctx, userClaimRecord)
 		return nil
---
+++ x/stakeibc/keeper/icacallbacks_claim_test.go
@@ -0,0 +1,120 @@
+package keeper_test
+
+import (
+	"fmt"
+
+	sdk "github.com/cosmos/cosmos-sdk/types"
+	banktypes "github.com/cosmos/cosmos-sdk/x/bank/types"
+	channeltypes "github.com/cosmos/ibc-go/v3/modules/core/04-channel/types"
+	_ "github.com/stretchr/testify/suite"
+
+	recordtypes "github.com/Stride-Labs/stride/x/records/types"
+	stakeibckeeper "github.com/Stride-Labs/stride/x/stakeibc/keeper"
+	"github.com/Stride-Labs/stride/x/stakeibc/types"
+)
+
+type ClaimCallbackState struct {
+	callbackArgs types.ClaimCallback
+}
+
+type ClaimCallbackArgs struct {
+	packet channeltypes.Packet
+	ack    *channeltypes.Acknowledgement
+	args   []byte
+}
+
+type ClaimCallbackTestCase struct {
+	initialState ClaimCallbackState
+	validArgs    ClaimCallbackArgs
+}
+
+func (s *KeeperTestSuite) SetupClaimCallback() ClaimCallbackTestCase {
+	epochNumber := uint64(1)
+	recordId := recordtypes.UserRedemptionRecordKeyFormatter(chainId, epochNumber, "sender")
+	userRedemptionRecord := recordtypes.UserRedemptionRecord{
+		Id: recordId,
+		// after a user calls ClaimUndelegatedTokens, the record is set to isClaimable = false
+		// to prevent double claims
+		IsClaimable: false,
+	}
+	s.App.RecordsKeeper.SetUserRedemptionRecord(s.Ctx(), userRedemptionRecord)
+
+	packet := channeltypes.Packet{}
+	var msgs []sdk.Msg
+	msgs = append(msgs, &banktypes.MsgSend{})
+	ack := s.ICAPacketAcknowledgement(msgs)
+	callbackArgs := types.ClaimCallback{
+		UserRedemptionRecordId: recordId,
+	}
+	args, err := s.App.StakeibcKeeper.MarshalClaimCallbackArgs(s.Ctx(), callbackArgs)
+	s.Require().NoError(err)
+
+	return ClaimCallbackTestCase{
+		initialState: ClaimCallbackState{
+			callbackArgs: callbackArgs,
+		},
+		validArgs: ClaimCallbackArgs{
+			packet: packet,
+			ack:    &ack,
+			args:   args,
+		},
+	}
+}
+
+func (s *KeeperTestSuite) TestClaimCallback_Successful() {
+	tc := s.SetupClaimCallback()
+	initialState := tc.initialState
+	validArgs := tc.validArgs
+
+	err := stakeibckeeper.ClaimCallback(s.App.StakeibcKeeper, s.Ctx(), validArgs.packet, validArgs.ack, validArgs.args)
+	s.Require().NoError(err)
+
+	// Confirm record has been deleted
+	_, found := s.App.RecordsKeeper.GetUserRedemptionRecord(s.Ctx(), initialState.callbackArgs.UserRedemptionRecordId)
+	s.Require().False(found)
+}
+
+func (s *KeeperTestSuite) checkClaimStateIfCallbackFailed(tc ClaimCallbackTestCase) {
+	// Confirm record is set to isClaimable = true (if the callback failed, it should be reset to true so that users can retry the claim)
+	record, found := s.App.RecordsKeeper.GetUserRedemptionRecord(s.Ctx(), tc.initialState.callbackArgs.UserRedemptionRecordId)
+	s.Require().True(found)
+	s.Require().True(record.IsClaimable)
+}
+
+func (s *KeeperTestSuite) TestClaimCallback_ClaimCallbackTimeout() {
+	tc := s.SetupClaimCallback()
+	invalidArgs := tc.validArgs
+	// a nil ack means the request timed out
+	invalidArgs.ack = nil
+	err := stakeibckeeper.ClaimCallback(s.App.StakeibcKeeper, s.Ctx(), invalidArgs.packet, invalidArgs.ack, invalidArgs.args)
+	s.Require().NoError(err)
+	s.checkClaimStateIfCallbackFailed(tc)
+}
+
+func (s *KeeperTestSuite) TestClaimCallback_ClaimCallbackErrorOnHost() {
+	tc := s.SetupClaimCallback()
+	invalidArgs := tc.validArgs
+	// an error ack means the tx failed on the host
+	fullAck := channeltypes.Acknowledgement{Response: &channeltypes.Acknowledgement_Error{Error: "error"}}
+	invalidArgs.ack = &fullAck
+
+	err := stakeibckeeper.ClaimCallback(s.App.StakeibcKeeper, s.Ctx(), invalidArgs.packet, invalidArgs.ack, invalidArgs.args)
+	s.Require().NoError(err)
+	s.checkClaimStateIfCallbackFailed(tc)
+}
+
+func (s *KeeperTestSuite) TestClaimCallback_WrongCallbackArgs() {
+	tc := s.SetupClaimCallback()
+	invalidArgs := tc.validArgs
+
+	err := stakeibckeeper.ClaimCallback(s.App.StakeibcKeeper, s.Ctx(), invalidArgs.packet, invalidArgs.ack, []byte("random bytes"))
+	s.Require().EqualError(err, "unexpected EOF")
+}
+
+func (s *KeeperTestSuite) TestClaimCallback_RecordNotFound() {
+	tc := s.SetupClaimCallback()
+	validArgs := tc.validArgs
+	s.App.RecordsKeeper.RemoveUserRedemptionRecord(s.Ctx(), tc.initialState.callbackArgs.UserRedemptionRecordId)
+	err := stakeibckeeper.ClaimCallback(s.App.StakeibcKeeper, s.Ctx(), validArgs.packet, validArgs.ack, validArgs.args)
+	s.Require().EqualError(err, fmt.Sprintf("user redemption record not found %s: record not found", tc.initialState.callbackArgs.UserRedemptionRecordId))
+}
