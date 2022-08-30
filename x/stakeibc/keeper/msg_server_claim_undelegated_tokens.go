81937c3af321361966086f638ad6c32d27a92074
Aidan Salzmann <aidan@stridelabs.co>
2022-08-30 03:06:33 -0400

    lint

--- x/stakeibc/keeper/msg_server_claim_undelegated_tokens.go
+++ x/stakeibc/keeper/msg_server_claim_undelegated_tokens.go
@@ -81,7 +81,7 @@ func (k Keeper) GetClaimableRedemptionRecord(ctx sdk.Context, msg *types.MsgClai
 		return nil, sdkerrors.Wrapf(types.ErrInvalidUserRedemptionRecord, "user redemption record is not claimable: %s, host zone unbonding has status: %s, requires status TRANSFERRED", userRedemptionRecordKey, hostZoneUnbonding.Status)
 	}
 	// records that have isClaimable set to False have already been claimed (and are pending an ack)
-	if userRedemptionRecord.IsClaimable == false {
+	if !userRedemptionRecord.IsClaimable {
 		errMsg := fmt.Sprintf("User redemption record %s is not claimable, pending ack", userRedemptionRecord.Id)
 		k.Logger(ctx).Error(errMsg)
 		return nil, sdkerrors.Wrapf(types.ErrInvalidUserRedemptionRecord, "user redemption record is not claimable: %s, pending ack", userRedemptionRecordKey, hostZoneUnbonding.Status)
