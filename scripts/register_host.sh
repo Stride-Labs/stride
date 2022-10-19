ceff377d4dc3f4e8e5193c0eeb3b3ab94b74d91a
sampocs <sam@stridelabs.co>
2022-10-19 18:54:29 -0500

    Added Upgrade Scripts to Dockernet (#307)

--- scripts/register_host.sh
+++ scripts/register_host.sh
@@ -10,7 +10,6 @@ HOST_ZONE_NUM="$2"
 CONNECTION="connection-$HOST_ZONE_NUM"
 CHANNEL="channel-$HOST_ZONE_NUM"
 
-MAIN_CMD=$(GET_VAR_VALUE       ${CHAIN_ID}_MAIN_CMD)
 VAL_PREFIX=$(GET_VAR_VALUE     ${CHAIN_ID}_VAL_PREFIX)
 IBC_DENOM=$(GET_VAR_VALUE      IBC_${CHAIN_ID}_CHANNEL_${HOST_ZONE_NUM}_DENOM)
 HOST_DENOM=$(GET_VAR_VALUE     ${CHAIN_ID}_DENOM)
@@ -20,7 +19,7 @@ NUM_VALS=$(GET_VAR_VALUE       ${CHAIN_ID}_NUM_NODES)
 echo "$CHAIN_ID - Registering host zone..."
 $STRIDE_MAIN_CMD tx stakeibc register-host-zone \
     $CONNECTION $HOST_DENOM $ADDRESS_PREFIX $IBC_DENOM $CHANNEL 1 \
-    --gas 1000000 --from $STRIDE_ADMIN_ACCT --home $SCRIPT_DIR/state/stride1 -y | grep -E "code:|txhash:" | sed 's/^/  /'
+    --gas 1000000 --from $STRIDE_ADMIN_ACCT --home $SCRIPT_DIR/state/stride1 -y | TRIM_TX
 sleep 4
 
 echo "$CHAIN_ID - Registering validators..."
@@ -30,7 +29,7 @@ for (( i=1; i <= $NUM_VALS; i++ )); do
     weight=${weights[$i]}
 
     $STRIDE_MAIN_CMD tx stakeibc add-validator $CHAIN_ID ${VAL_PREFIX}${i} $delegate_val 10 $weight \
-        --from $STRIDE_ADMIN_ACCT -y | grep -E "code:|txhash:" | sed 's/^/  /'
+        --from $STRIDE_ADMIN_ACCT -y | TRIM_TX
     sleep 4
 done
 
