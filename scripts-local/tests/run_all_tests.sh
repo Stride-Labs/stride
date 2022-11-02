5dd23818ceb1a355cd082fa2bedfdc056bf27b51
sampocs <sam.pochyly@gmail.com>
2022-11-02 15:32:55 -0500

    removed scripts-local

--- scripts-local/tests/run_all_tests.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-BASE_SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
-
-# run test files
-bats $BASE_SCRIPT_DIR/gaia_tests.bats
-bats $BASE_SCRIPT_DIR/osmo_tests.bats
-bats $BASE_SCRIPT_DIR/juno_tests.bats
