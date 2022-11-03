6014736daa13fc282bf92f8eddfce7d228738222
sampocs <sam@stridelabs.co>
2022-11-03 10:43:57 -0500

    Removed Localnet (#326)

--- scripts-local/test-utils/2.sh
+++
@@ -1,9 +0,0 @@
-### LIQ STAKE + EXCH RATE TEST
-SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
-# import dependencies
-source ${SCRIPT_DIR}/../account_vars.sh
-
-$STRIDE_CMD tx stakeibc liquid-stake 10000 $ATOM_DENOM --keyring-backend test --from $STRIDE_VAL_ACCT -y --chain-id $STRIDE_CHAIN -y
-echo "Waiting for liquid staked tokens to be delegated..."
-CSLEEP 60
-# $STRIDE_CMD tx stakeibc liquid-stake 10000 $ATOM_DENOM --keyring-backend test --from $STRIDE_VAL_ACCT -y --chain-id $STRIDE_CHAIN
--- scripts-local/upgrades/view_proposal_status.sh
+++
@@ -1,8 +0,0 @@
-STRIDED=scripts-local/upgrades/binaries/strided1
-
-$STRIDED query gov proposal 1 | grep "voting_end_time"
-
-while true; do
-  $STRIDED query gov proposal 1 | grep "status"
-  sleep 5
-done
