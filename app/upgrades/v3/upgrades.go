366751d71fc87edb3689e505f6f34570ec9b526e
antstalepresh <stalepresh121@outlook.com>
2022-11-08 07:52:20 -0500

    disable auto airdrop creation

--- app/upgrades/v3/upgrades.go
+++ app/upgrades/v3/upgrades.go
@@ -1,22 +1,20 @@
 package v3
 
 import (
-	"time"
-
 	sdk "github.com/cosmos/cosmos-sdk/types"
 	"github.com/cosmos/cosmos-sdk/types/module"
 	upgradetypes "github.com/cosmos/cosmos-sdk/x/upgrade/types"
 
 	claimKeeper "github.com/Stride-Labs/stride/x/claim/keeper"
-	claimTypes "github.com/Stride-Labs/stride/x/claim/types"
+	// claimTypes "github.com/Stride-Labs/stride/x/claim/types"
 )
 
 // Note: ensure these values are properly set before running upgrade
 const (
-	UpgradeName        = "v3"
-	airdropDistributor = ""
-	airdropIdentifier  = ""
-	airdropDuration    = time.Hour * 24 * 30 * 12 * 3 // 3 years
+	UpgradeName = "v3"
+	// airdropDistributor = "stride1htlzesdgm6pvunstrgk3p7aj6s66j08f9v3rgl"
+	// airdropIdentifier  = "osmosis"
+	// airdropDuration    = time.Hour * 24 * 30 * 12 * 3 // 3 years
 )
 
 // CreateUpgradeHandler creates an SDK upgrade handler for v3
@@ -27,10 +25,10 @@ func CreateUpgradeHandler(
 ) upgradetypes.UpgradeHandler {
 	return func(ctx sdk.Context, _ upgradetypes.Plan, vm module.VersionMap) (module.VersionMap, error) {
 		newVm, err := mm.RunMigrations(ctx, configurator, vm)
-		err1 := ck.CreateAirdropAndEpoch(ctx, airdropDistributor, claimTypes.DefaultClaimDenom, uint64(ctx.BlockTime().Unix()), uint64(airdropDuration.Seconds()), airdropIdentifier)
-		if err1 != nil {
-			panic(err1)
-		}
+		// err1 := ck.CreateAirdropAndEpoch(ctx, airdropDistributor, claimTypes.DefaultClaimDenom, uint64(ctx.BlockTime().Unix()), uint64(airdropDuration.Seconds()), airdropIdentifier)
+		// if err1 != nil {
+		// 	panic(err1)
+		// }
 		ck.LoadAllocationData(ctx, allocations)
 		return newVm, err
 	}
