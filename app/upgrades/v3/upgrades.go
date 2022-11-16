545bbe0db9e34ab536852a0f8986a559dc60954c
antstalepresh <stalepresh121@outlook.com>
2022-11-16 18:21:46 -0500

    update upgrade handler

--- app/upgrades/v3/upgrades.go
+++ app/upgrades/v3/upgrades.go
@@ -12,10 +12,16 @@ import (
 )
 
 // Note: ensure these values are properly set before running upgrade
-const (
-	UpgradeName        = "v3"
-	airdropDistributor = "stride1htlzesdgm6pvunstrgk3p7aj6s66j08f9v3rgl"
-	airdropIdentifier  = "osmosis"
+var (
+	UpgradeName         = "v3"
+	airdropDistributors = []string{
+		"stride1thl8e7smew8q7jrz8at4f64wrjjl8mwan3nc4l",
+		"stride104az7rd5yh3p8qn4ary8n3xcwquuwgee4vnvvc",
+		"stride17kvetgthwt6caku5qjs2rx2njgh26vmg448r5u",
+		"stride1swvv9kpp75e60pvlv5x6mcw5f54qgpph239e5s",
+		"stride1ywrhas3ae7z3ljqxmgdzjx8wyaf3djwuh4hdlj",
+	}
+	airdropIdentifiers = []string{"stride", "gaia", "osmosis", "juno", "stars"}
 	airdropDuration    = time.Hour * 24 * 30 * 12 * 3 // 3 years
 )
 
@@ -30,9 +36,15 @@ func CreateUpgradeHandler(
 		if err != nil {
 			return newVm, err
 		}
-		err = ck.CreateAirdropAndEpoch(ctx, airdropDistributor, claimtypes.DefaultClaimDenom, uint64(ctx.BlockTime().Unix()), uint64(airdropDuration.Seconds()), airdropIdentifier)
-		if err != nil {
-			return newVm, err
+
+		// total number of airdrop distributors must be equal to identifiers
+		if len(airdropDistributors) == len(airdropIdentifiers) {
+			for idx, airdropDistributor := range airdropDistributors {
+				err = ck.CreateAirdropAndEpoch(ctx, airdropDistributor, claimtypes.DefaultClaimDenom, uint64(ctx.BlockTime().Unix()), uint64(airdropDuration.Seconds()), airdropIdentifiers[idx])
+				if err != nil {
+					return newVm, err
+				}
+			}
 		}
 		ck.LoadAllocationData(ctx, allocations)
 		return newVm, nil
