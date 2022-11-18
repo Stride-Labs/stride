124582eb90c14c4aea32148febd6ff3cd6bbf6ec
Aidan Salzmann <aidan@stridelabs.co>
2022-11-18 15:18:52 -0500

    rm scratch

--- scripts/scratch.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-set -eu 
-SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
-source ${SCRIPT_DIR}/vars.sh
-
-$STRIDE_MAIN_CMD tx stakeibc liquid-stake 1000 uatom --keyring-backend test --from val1 -y --chain-id $STRIDE_CHAIN_ID
