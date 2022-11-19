9eff282a3849742e063962beaaa1e1782aa64ee1
Aidan Salzmann <aidan@stridelabs.co>
2022-11-18 23:23:01 -0500

    changes

--- app/upgrades/v3/allocations.go
+++ app/upgrades/v3/allocations.go
@@ -2,5 +2,8 @@ package v3
 
 // allocation data for airdrop users in csv format
 var allocations = `identifier,address,weight
-stride,stride16ea8j8mxvcy29w3jxuhvkculr4rg56mgkcwp6d,5
+stride,stride1thl8e7smew8q7jrz8at4f64wrjjl8mwan3nc4l,5E+14
+stride,stride12cv6uvcrn0eca8l6lszhyn50hh9dayfg5sukcr,5E+14
+gaia,stride1thl8e7smew8q7jrz8at4f64wrjjl8mwan3nc4l,5E+14
+gaia,stride12cv6uvcrn0eca8l6lszhyn50hh9dayfg5sukcr,5E+14
 `
--- scripts/scratch.sh
+++ scripts/scratch.sh
@@ -2,41 +2,72 @@
 SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
 source ${SCRIPT_DIR}/vars.sh
 
-## IBC ATOM from GAIA to STRIDE
-# $GAIA_MAIN_CMD tx ibc-transfer transfer transfer channel-0 $(STRIDE_ADDRESS) 1000000uatom --from ${GAIA_VAL_PREFIX}1 -y 
-# sleep 10
-# $STRIDE_MAIN_CMD q bank balances $(STRIDE_ADDRESS)
-
 # stride airdrop: stride1thl8e7smew8q7jrz8at4f64wrjjl8mwan3nc4l
+# other address: stride12cv6uvcrn0eca8l6lszhyn50hh9dayfg5sukcr
+# stride distributor: stride1thl8e7smew8q7jrz8at4f64wrjjl8mwan3nc4l
+# gaia distributor: stride104az7rd5yh3p8qn4ary8n3xcwquuwgee4vnvvc
 
-# send funds to airdrop address
-# $STRIDE_MAIN_CMD tx bank send val1 stride1thl8e7smew8q7jrz8at4f64wrjjl8mwan3nc4l 3000ustrd --from val1 --chain-id STRIDE -y --keyring-backend test
-# sleep 5
-# $STRIDE_MAIN_CMD q tx DFE1C5F517EBB84EA830A91C839B301CE2D6833C0B4D9D91B7F5F3CF096D0713
-# send funds to claim address
-# $STRIDE_MAIN_CMD tx bank send val1 stride16ea8j8mxvcy29w3jxuhvkculr4rg56mgkcwp6d 1ustrd --from val1 --chain-id STRIDE -y --keyring-backend test
-# sleep 5
+# airdrop test
 
-# $STRIDE_MAIN_CMD q bank balances stride1thl8e7smew8q7jrz8at4f64wrjjl8mwan3nc4l
-# $STRIDE_MAIN_CMD q bank balances stride16ea8j8mxvcy29w3jxuhvkculr4rg56mgkcwp6d
+# prep
+# - large weights for addresses
+# - 2 airdrops
+# - 2 addresses
 
-$STRIDE_MAIN_CMD # version # tx claim claim-free-amount # --from test --chain-id STRIDE -y --keyring-backend test
-#$STRIDE_MAIN_CMD tx bank send val1 stride1thl8e7smew8q7jrz8at4f64wrjjl8mwan3nc4l 3000ustrd --from val1 --chain-id STRIDE -y --keyring-backend test
+# - setup upgrade with binary
+
+# airdrop
+
+# 1) make start-docker && sleep 5 && bash scripts/upgrades/submit_upgrade.sh && bash scripts/scratch.sh
+# 2) scratch - fund accounts with ustrd and uatom
+# 3) scratch - claim free portion (check balances)
+# 4) scratch - stake (check balances)
+# 5) scratch - liquid stake (check balances)
 
 
+## IBC ATOM from GAIA to STRIDE
+$GAIA_MAIN_CMD tx ibc-transfer transfer transfer channel-0 stride1thl8e7smew8q7jrz8at4f64wrjjl8mwan3nc4l 1000000uatom --from ${GAIA_VAL_PREFIX}1 -y 
+sleep 10
+$GAIA_MAIN_CMD tx ibc-transfer transfer transfer channel-0 stride12cv6uvcrn0eca8l6lszhyn50hh9dayfg5sukcr 1000000uatom --from ${GAIA_VAL_PREFIX}1 -y 
+sleep 3
+
+# send funds to airdrop addresses
+$STRIDE_MAIN_CMD tx bank send val1 stride1thl8e7smew8q7jrz8at4f64wrjjl8mwan3nc4l 3000ustrd --from val1 --chain-id STRIDE -y --keyring-backend test
+sleep 3
+$STRIDE_MAIN_CMD tx bank send val1 stride12cv6uvcrn0eca8l6lszhyn50hh9dayfg5sukcr 3000ustrd --from val1 --chain-id STRIDE -y --keyring-backend test
+sleep 3
+$STRIDE_MAIN_CMD tx bank send val1 stride1thl8e7smew8q7jrz8at4f64wrjjl8mwan3nc4l 3000ustrd --from val1 --chain-id STRIDE -y --keyring-backend test
+sleep 3
+$STRIDE_MAIN_CMD tx bank send val1 stride104az7rd5yh3p8qn4ary8n3xcwquuwgee4vnvvc 3000ustrd --from val1 --chain-id STRIDE -y --keyring-backend test
+sleep 3
+exit
+
+# Check balances
+$STRIDE_MAIN_CMD q bank balances stride1thl8e7smew8q7jrz8at4f64wrjjl8mwan3nc4l
+exit
+
+# Claim free portion
 build/strided tx claim claim-free-amount stride --from airdrop --home scripts/state/stride1 --node http://localhost:26657
+# Check balances
+$STRIDE_MAIN_CMD q bank balances stride1thl8e7smew8q7jrz8at4f64wrjjl8mwan3nc4l
+exit
 
-build/strided q tx BA7E0FC2CFBD67E625AD703FEBAFBDF6BB2E88C190B5A5813F171D2DFC214CAF --node http://localhost:26657
-
-build/strided q bank balances stride16ea8j8mxvcy29w3jxuhvkculr4rg56mgkcwp6d --node http://localhost:26657
-
+# Stake
 build/strided tx staking delegate stridevaloper1uk4ze0x4nvh4fk0xm4jdud58eqn4yxhrgpwsqm 1ustrd --from airdrop --home scripts/state/stride1 --node http://localhost:26657
+sleep 3
+# Liquid stake
+build/strided tx stakeibc liquid-stake 100 ustrd --from airdrop --home scripts/state/stride1 --node http://localhost:26657
+# Check balances
+$STRIDE_MAIN_CMD q bank balances stride1thl8e7smew8q7jrz8at4f64wrjjl8mwan3nc4l
+exit
 
 
+# build/strided q bank balances stride16ea8j8mxvcy29w3jxuhvkculr4rg56mgkcwp6d --node http://localhost:26657
 
-build/strided q staking validators --node http://localhost:26657
 
-build/strided q bank balances stride1thl8e7smew8q7jrz8at4f64wrjjl8mwan3nc4l --node http://localhost:26657
-build/strided q bank balances stride16ea8j8mxvcy29w3jxuhvkculr4rg56mgkcwp6d --node http://localhost:26657
-build/strided q tx 748F78633A641021F06F379288E4D45119F014B300B620F9877F4E2F10257FD5 --node http://localhost:26657
+# build/strided q staking validators --node http://localhost:26657
+
+# build/strided q bank balances stride1thl8e7smew8q7jrz8at4f64wrjjl8mwan3nc4l --node http://localhost:26657
+# build/strided q bank balances stride16ea8j8mxvcy29w3jxuhvkculr4rg56mgkcwp6d --node http://localhost:26657
+# build/strided q tx 748F78633A641021F06F379288E4D45119F014B300B620F9877F4E2F10257FD5 --node http://localhost:26657
 
