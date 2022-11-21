2e6c6ecdbfa3a432193c3ebe0739a99b9ce4cd26
ThanhNhann <thanhnhan98qh@gmail.com>
2022-11-21 21:17:43 +0700

    remove sdk error: epochs module

--- x/epochs/handler.go
+++ x/epochs/handler.go
@@ -3,17 +3,15 @@ package epochs
 import (
 	"fmt"
 
-	sdk "github.com/cosmos/cosmos-sdk/types"
-	sdkerrors "github.com/cosmos/cosmos-sdk/types/errors"
-
 	"github.com/Stride-Labs/stride/v3/x/epochs/keeper"
 	"github.com/Stride-Labs/stride/v3/x/epochs/types"
+	sdk "github.com/cosmos/cosmos-sdk/types"
 )
 
 // NewHandler returns a handler for epochs module messages
 func NewHandler(k keeper.Keeper) sdk.Handler {
 	return func(_ sdk.Context, msg sdk.Msg) (*sdk.Result, error) {
 		errMsg := fmt.Sprintf("unrecognized %s message type: %T", types.ModuleName, msg)
-		return nil, sdkerrors.Wrap(sdkerrors.ErrUnknownRequest, errMsg)
+		return nil, fmt.Errorf("Unknown request: %s", errMsg)
 	}
 }
