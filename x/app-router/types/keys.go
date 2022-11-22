151e1d403f1833064af5341e1f165201d98722e0
jstr1121 <jun@stridelabs.co>
2022-11-23 01:53:56 +0800

    resolve node not starting issue

--- x/app-router/types/keys.go
+++ x/app-router/types/keys.go
@@ -2,7 +2,7 @@ package types
 
 const (
 	// ModuleName defines the module name
-	ModuleName = "app_router"
+	ModuleName = "approuter"
 
 	// StoreKey defines the primary module store key
 	StoreKey = ModuleName
