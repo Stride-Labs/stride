7f40e43498aef8f231f5a5ced31534305c35edf1
ThanhNhann <thanhnhan98qh@gmail.com>
2022-11-22 11:54:34 +0700

    update show error icacallbacks module

--- x/icacallbacks/types/errors.go
+++ x/icacallbacks/types/errors.go
@@ -15,54 +15,54 @@ type ErrorInterface interface {
 type ErrSample struct{}
 
 func (e ErrSample) Error() error {
-	return errors.Unwrap(fmt.Errorf("Sample error"))
+	return fmt.Errorf("Sample error")
 }
 
 // ErrInvalidPacketTimeout
 type ErrInvalidPacketTimeout struct{}
 
 func (e ErrInvalidPacketTimeout) Error() error {
-	return errors.Unwrap(fmt.Errorf("Invalid packet timeout"))
+	return fmt.Errorf("Invalid packet timeout")
 }
 
 // ErrInvalidVersion
 type ErrInvalidVersion struct{}
 
 func (e ErrInvalidVersion) Error() error {
-	return errors.Unwrap(fmt.Errorf("Invalid version"))
+	return fmt.Errorf("Invalid version")
 }
 
 // ErrCallbackHandlerNotFound
 type ErrCallbackHandlerNotFound struct{}
 
 func (e ErrCallbackHandlerNotFound) Error() error {
-	return errors.Unwrap(fmt.Errorf("Icacallback handler not found"))
+	return fmt.Errorf("Icacallback handler not found")
 }
 
 // ErrCallbackIdNotFound
 type ErrCallbackIdNotFound struct{}
 
 func (e ErrCallbackIdNotFound) Error() error {
-	return errors.Unwrap(fmt.Errorf("Icacallback ID not found"))
+	return fmt.Errorf("Icacallback ID not found")
 }
 
 // ErrCallbackFailed
 type ErrCallbackFailed struct{}
 
 func (e ErrCallbackFailed) Error() error {
-	return errors.Unwrap(fmt.Errorf("Icacallback failed"))
+	return fmt.Errorf("Icacallback failed")
 }
 
 // ErrCallbackDataNotFound
 type ErrCallbackDataNotFound struct{}
 
 func (e ErrCallbackDataNotFound) Error() error {
-	return errors.Unwrap(fmt.Errorf("Icacallback data not found"))
+	return fmt.Errorf("Icacallback data not found")
 }
 
 // ErrTxMsgData
 type ErrTxMsgData struct{}
 
 func (e ErrTxMsgData) Error() error {
-	return errors.Unwrap(fmt.Errorf("TxMsgData fetch failed"))
+	return fmt.Errorf("TxMsgData fetch failed")
 }
