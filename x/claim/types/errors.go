1981237203bb650c6a78759146b9b9bec636acc5
ThanhNhann <thanhnhan98qh@gmail.com>
2022-11-22 11:51:36 +0700

    fix fail test claim module

--- x/claim/types/errors.go
+++ x/claim/types/errors.go
@@ -3,7 +3,6 @@ package types
 // DONTCOVER
 
 import (
-	"errors"
 	"fmt"
 )
 
@@ -15,47 +14,47 @@ type ErrorInterface interface {
 type ErrTotalWeightNotSet struct{}
 
 func (e ErrTotalWeightNotSet) Error() error {
-	return errors.Unwrap(fmt.Errorf("total weight not set"))
+	return fmt.Errorf("total weight not set")
 }
 
 // ErrTotalWeightParse
 type ErrTotalWeightParse struct{}
 
 func (e ErrTotalWeightParse) Error() error {
-	return errors.Unwrap(fmt.Errorf("total weight parse error"))
+	return fmt.Errorf("total weight parse error")
 }
 
 // ErrFailedToGetTotalWeight
 type ErrFailedToGetTotalWeight struct{}
 
 func (e ErrFailedToGetTotalWeight) Error() error {
-	return errors.Unwrap(fmt.Errorf("failed to get total weight"))
+	return fmt.Errorf("failed to get total weight")
 }
 
 // ErrFailedToParseDec
 type ErrFailedToParseDec struct{}
 
 func (e ErrFailedToParseDec) Error() error {
-	return errors.Unwrap(fmt.Errorf("failed to parse dec from str"))
+	return fmt.Errorf("failed to parse dec from str")
 }
 
 // ErrAirdropAlreadyExists
 type ErrAirdropAlreadyExists struct{}
 
 func (e ErrAirdropAlreadyExists) Error() error {
-	return errors.Unwrap(fmt.Errorf("airdrop with same identifier already exists"))
+	return fmt.Errorf("airdrop with same identifier already exists")
 }
 
 // ErrDistributorAlreadyExists
 type ErrDistributorAlreadyExists struct{}
 
 func (e ErrDistributorAlreadyExists) Error() error {
-	return errors.Unwrap(fmt.Errorf("airdrop with same distributor already exists"))
+	return fmt.Errorf("airdrop with same distributor already exists")
 }
 
 // ErrInvalidAmount
 type ErrInvalidAmount struct{}
 
 func (e ErrInvalidAmount) Error() error {
-	return errors.Unwrap(fmt.Errorf("cannot claim negative tokens"))
+	return fmt.Errorf("cannot claim negative tokens")
 }
