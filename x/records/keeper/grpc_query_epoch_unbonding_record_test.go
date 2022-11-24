5fa71eb4f15bf6d1b5fa767e7a23a7741b1b4ee1
ThanhNhann <thanhnhan98qh@gmail.com>
2022-11-24 16:41:16 +0700

    fix test for records module

--- x/records/keeper/grpc_query_epoch_unbonding_record_test.go
+++ x/records/keeper/grpc_query_epoch_unbonding_record_test.go
@@ -1,7 +1,6 @@
 package keeper_test
 
 import (
-	"fmt"
 	"testing"
 
 	sdk "github.com/cosmos/cosmos-sdk/types"
@@ -38,7 +37,7 @@ func TestEpochUnbondingRecordQuerySingle(t *testing.T) {
 		{
 			desc:    "KeyNotFound",
 			request: &types.QueryGetEpochUnbondingRecordRequest{EpochNumber: uint64(len(msgs))},
-			err:     fmt.Errorf("Key not found"),
+			err:     status.Error(codes.NotFound, "Key not found"),
 		},
 		{
 			desc: "InvalidRequest",
