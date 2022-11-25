5e7bbdb4c2d53d0495d827aa137d223449cd052f
DongLieu <duongduydongtq@gmail.com>
2022-11-25 17:13:42 +0700

    jjjjjjjjjjjjjjjjjjjjjj

--- x/records/types/errors.go
+++ x/records/types/errors.go
@@ -7,52 +7,51 @@ import (
 )
 
 // x/records module sentinel errors
-// var (
-//
-//	ErrInvalidVersion               = sdkerrors.Register(ModuleName, 1501, "invalid version")
-//	ErrRedemptionAlreadyExists      = sdkerrors.Register(ModuleName, 1502, "redemption record already exists")
-//	ErrEpochUnbondingRecordNotFound = sdkerrors.Register(ModuleName, 1503, "epoch unbonding record not found")
-//	ErrUnknownDepositRecord         = sdkerrors.Register(ModuleName, 1504, "unknown deposit record")
-//	ErrUnmarshalFailure             = sdkerrors.Register(ModuleName, 1505, "cannot unmarshal")
-//	ErrAddingHostZone               = sdkerrors.Register(ModuleName, 1506, "could not add hzu to epoch unbonding record")
-//
-// )
-type ErrorInterface interface {
-	Error() error
-}
+var (
+	ErrInvalidVersion               = fmt.Errorf("invalid version")
+	ErrRedemptionAlreadyExists      = fmt.Errorf("redemption record already exists")
+	ErrEpochUnbondingRecordNotFound = fmt.Errorf(ModuleName, 1503, "epoch unbonding record not found")
+	ErrUnknownDepositRecord         = fmt.Errorf(ModuleName, 1504, "unknown deposit record")
+	ErrUnmarshalFailure             = fmt.Errorf(ModuleName, 1505, "cannot unmarshal")
+	ErrAddingHostZone               = fmt.Errorf(ModuleName, 1506, "could not add hzu to epoch unbonding record")
+)
 
-type ErrInvalidVersion struct{}
+// type ErrorInterface interface {
+// 	Error() error
+// }
 
-func (e ErrInvalidVersion) Error() error {
-	return fmt.Errorf("Invalid version")
-}
+// type ErrInvalidVersion struct{}
 
-type ErrRedemptionAlreadyExists struct{}
+// func (e ErrInvalidVersion) Error() error {
+// 	return fmt.Errorf("invalid version")
+// }
 
-func (e ErrRedemptionAlreadyExists) Error() error {
-	return fmt.Errorf("Redemption already exists")
-}
+// type ErrRedemptionAlreadyExists struct{}
 
-type ErrEpochUnbondingRecordNotFound struct{}
+// func (e ErrRedemptionAlreadyExists) Error() error {
+// 	return fmt.Errorf("redemption record already exists")
+// }
 
-func (e ErrEpochUnbondingRecordNotFound) Error() error {
-	return fmt.Errorf("Epoch unbonding record not found")
-}
+// type ErrEpochUnbondingRecordNotFound struct{}
 
-type ErrUnknownDepositRecord struct{}
+// func (e ErrEpochUnbondingRecordNotFound) Error() error {
+// 	return fmt.Errorf("epoch unbonding record not found")
+// }
 
-func (e ErrUnknownDepositRecord) Error() error {
-	return fmt.Errorf("Unknown deposit record")
-}
+// type ErrUnknownDepositRecord struct{}
 
-type ErrUnmarshalFailure struct{}
+// func (e ErrUnknownDepositRecord) Error() error {
+// 	return fmt.Errorf("unknown deposit record")
+// }
 
-func (e ErrUnmarshalFailure) Error() error {
-	return fmt.Errorf("Unmarshal failure")
-}
+// type ErrUnmarshalFailure struct{}
 
-type ErrAddingHostZone struct{}
+// func (e ErrUnmarshalFailure) Error() error {
+// 	return fmt.Errorf("cannot unmarshal")
+// }
 
-func (e ErrAddingHostZone) Error() error {
-	return fmt.Errorf("Error adding host zone")
-}
+// type ErrAddingHostZone struct{}
+
+// func (e ErrAddingHostZone) Error() error {
+// 	return fmt.Errorf("could not add hzu to epoch unbonding record")
+// }
