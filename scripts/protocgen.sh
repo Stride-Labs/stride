67ef76914a4b0d305c65e4ba3a1838432fc27368
Jacob Gadikian <jacobgadikian@gmail.com>
2022-12-04 18:43:23 +0700

    shellcheck fixes

--- scripts/protocgen.sh
+++ scripts/protocgen.sh
@@ -6,15 +6,15 @@ set -eo pipefail
 go get github.com/regen-network/cosmos-proto/protoc-gen-gocosmos 2>/dev/null
 
 # get cosmos sdk from github
-go get github.com/cosmos/cosmos-sdk@v0.45.11 2>/dev/null
+# go get github.com/cosmos/cosmos-sdk@v0.45.11 2>/dev/null
 
 echo "Generating gogo proto code"
 cd proto
 proto_dirs=$(find ./stride -path -prune -o -name '*.proto' -print0 | xargs -0 -n1 dirname | sort | uniq)
 for dir in $proto_dirs; do
   for file in $(find "${dir}" -maxdepth 1 -name '*.proto'); do
-    if grep go_package $file &>/dev/null; then
-      buf generate --template buf.gen.gogo.yaml $file
+    if grep go_package "$file" &>/dev/null; then
+      buf generate --template buf.gen.gogo.yaml "$file"
     fi
   done
 done
