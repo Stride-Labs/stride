f4f1f7258fc6b4785c88115ec0df4e69fdaa85be
sampocs <sam.pochyly@gmail.com>
2022-12-07 12:32:28 -0600

    removed stale ignite comments

--- x/ratelimit/types/codec.go
+++ x/ratelimit/types/codec.go
@@ -4,17 +4,12 @@ import (
 	"github.com/cosmos/cosmos-sdk/codec"
 	cdctypes "github.com/cosmos/cosmos-sdk/codec/types"
 
-	// this line is used by starport scaffolding # 1
 	"github.com/cosmos/cosmos-sdk/types/msgservice"
 )
 
-func RegisterCodec(cdc *codec.LegacyAmino) {
-	// this line is used by starport scaffolding # 2
-}
+func RegisterCodec(cdc *codec.LegacyAmino) {}
 
 func RegisterInterfaces(registry cdctypes.InterfaceRegistry) {
-	// this line is used by starport scaffolding # 3
-
 	msgservice.RegisterMsgServiceDesc(registry, &_Msg_serviceDesc)
 }
 
