5aebd75bc93991b8a7db3b4ec6e3a18cc9a0e437
antstalepresh <stalepresh121@outlook.com>
2022-12-06 20:26:28 -0500

    proto rebuild, some comment updates

--- x/ratelimit/handler.go
+++ x/ratelimit/handler.go
@@ -10,7 +10,7 @@ import (
 	"github.com/Stride-Labs/stride/v3/x/ratelimit/types"
 )
 
-// NewHandler ...
+// NewHandler returns ratelimit module messages
 func NewHandler(k keeper.Keeper) sdk.Handler {
 	// this line is used by starport scaffolding # handler/msgServer
 
@@ -18,7 +18,7 @@ func NewHandler(k keeper.Keeper) sdk.Handler {
 		ctx = ctx.WithEventManager(sdk.NewEventManager())
 
 		switch msg := msg.(type) {
-		// this line is used by starport scaffolding # 1
+		// TODO: need to define msg router here
 		default:
 			errMsg := fmt.Sprintf("unrecognized %s message type: %T", types.ModuleName, msg)
 			return nil, sdkerrors.Wrap(sdkerrors.ErrUnknownRequest, errMsg)
