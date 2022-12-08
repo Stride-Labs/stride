758c688af6c5d6a43d809fef4b3f853ec4a60999
antstalepresh <stalepresh121@outlook.com>
2022-12-07 21:54:26 -0500

    add  msg types and routes, add creator field to tx msgs

--- x/ratelimit/handler.go
+++ x/ratelimit/handler.go
@@ -12,11 +12,26 @@ import (
 
 // NewHandler returns ratelimit module messages
 func NewHandler(k keeper.Keeper) sdk.Handler {
+	msgServer := keeper.NewMsgServerImpl(k)
 	return func(ctx sdk.Context, msg sdk.Msg) (*sdk.Result, error) {
 		ctx = ctx.WithEventManager(sdk.NewEventManager())
 
 		switch msg := msg.(type) {
-		// TODO: need to define msg router here
+		case *types.MsgAddQuota:
+			res, err := msgServer.AddQuota(sdk.WrapSDKContext(ctx), msg)
+			return sdk.WrapServiceResult(ctx, res, err)
+		case *types.MsgAddRateLimit:
+			res, err := msgServer.AddRateLimit(sdk.WrapSDKContext(ctx), msg)
+			return sdk.WrapServiceResult(ctx, res, err)
+		case *types.MsgRemoveQuota:
+			res, err := msgServer.RemoveQuota(sdk.WrapSDKContext(ctx), msg)
+			return sdk.WrapServiceResult(ctx, res, err)
+		case *types.MsgRemoveRateLimit:
+			res, err := msgServer.RemoveRateLimit(sdk.WrapSDKContext(ctx), msg)
+			return sdk.WrapServiceResult(ctx, res, err)
+		case *types.MsgResetRateLimit:
+			res, err := msgServer.ResetRateLimit(sdk.WrapSDKContext(ctx), msg)
+			return sdk.WrapServiceResult(ctx, res, err)
 		default:
 			errMsg := fmt.Sprintf("unrecognized %s message type: %T", types.ModuleName, msg)
 			return nil, sdkerrors.Wrap(sdkerrors.ErrUnknownRequest, errMsg)
