c36081c721f9ce1b96ea1faba5df9b832e7c8d3f
antstalepresh <stalepresh121@outlook.com>
2022-12-09 00:35:33 -0500

    add unit tests for quota keeper functions

---
+++ x/ratelimit/keeper/quota_test.go
@@ -0,0 +1,48 @@
+package keeper_test
+
+import (
+	"fmt"
+
+	"github.com/Stride-Labs/stride/v4/x/ratelimit/types"
+)
+
+func (s *KeeperTestSuite) createQuotas() []types.Quota {
+	quotas := []types.Quota{}
+	for i := 1; i <= 5; i++ {
+		quota := types.Quota{
+			Name:            fmt.Sprintf("name-%d", i),
+			MaxPercentRecv:  uint64(i),
+			MaxPercentSend:  uint64(i),
+			DurationMinutes: uint64(i),
+		}
+
+		quotas = append(quotas, quota)
+		s.App.RatelimitKeeper.SetQuota(s.Ctx, quota)
+	}
+	return quotas
+}
+
+func (s *KeeperTestSuite) TestGetQuota() {
+	quotas := s.createQuotas()
+	expectedQuota := quotas[0]
+
+	actualQuota, found := s.App.RatelimitKeeper.GetQuota(s.Ctx, "name-1")
+	s.Require().True(found, "element not found")
+	s.Require().Equal(expectedQuota, actualQuota)
+}
+
+func (s *KeeperTestSuite) TestRemoveQuota() {
+	quotas := s.createQuotas()
+	idToRemove := quotas[0].Name
+
+	s.App.RatelimitKeeper.RemoveQuota(s.Ctx, idToRemove)
+	_, found := s.App.RatelimitKeeper.GetQuota(s.Ctx, idToRemove)
+	s.Require().False(found, "removed element found")
+}
+
+func (s *KeeperTestSuite) TestGetAllQuotas() {
+	expectedQuotas := s.createQuotas()
+	actualQuotas := s.App.RatelimitKeeper.GetAllQuotas(s.Ctx)
+	s.Require().Len(actualQuotas, len(expectedQuotas))
+	s.Require().ElementsMatch(expectedQuotas, actualQuotas, "all quotas")
+}
