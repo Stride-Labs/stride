bec9830699f7c2aedc79aa4a3a8e793049ca2439
Riley Edmunds <riley@stridelabs.co>
2022-12-08 20:59:16 -0500

    docs: x/mint readme cleanup

--- readme-docs/gather-docs.sh
+++ readme-docs/gather-docs.sh
@@ -1,9 +1,11 @@
-for f in $(find "./x" -type f -name "*.md"  \ # -not -path "deps/*"
-            -not -path "scripts/*" \
-            -not -path "readme-docs/*"
-        )
+#!/bin/bash
+for f in $(find "./x" \
+                -type f \
+                -name "*.md" )
+                # -not -path "scripts/*" \
+                # -not -path "readme-docs/*"\
+        # )
 do
         echo $f
-        cp $f readme-docs/md
-        
+        # cp $f readme-docs/md
 done
