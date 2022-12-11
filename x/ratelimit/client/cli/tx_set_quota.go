6d92d0fd524a2462cf7845c3ec6f78d317bc1821
antstalepresh <stalepresh121@outlook.com>
2022-12-11 10:13:14 -0500

    implement cli & tests

---
+++ x/ratelimit/client/cli/tx_set_quota.go
@@ -0,0 +1,59 @@
+package cli
+
+import (
+	"strconv"
+
+	"github.com/cosmos/cosmos-sdk/client"
+	"github.com/cosmos/cosmos-sdk/client/flags"
+	"github.com/cosmos/cosmos-sdk/client/tx"
+	"github.com/spf13/cobra"
+
+	"github.com/Stride-Labs/stride/v4/x/ratelimit/types"
+)
+
+func CmdSetQuota() *cobra.Command {
+	cmd := &cobra.Command{
+		Use:   "set-quota [name] [max-percent-send] [max-percent-recv] [duration-minutes]",
+		Short: "Broadcast message set-quota",
+		Args:  cobra.ExactArgs(4),
+		RunE: func(cmd *cobra.Command, args []string) (err error) {
+			argName := args[0]
+			argMaxPercentSend, err := strconv.Atoi(args[1])
+			if err != nil {
+				return err
+			}
+
+			argMaxPercentRecv, err := strconv.Atoi(args[2])
+			if err != nil {
+				return err
+			}
+
+			argDurationMinutes, err := strconv.Atoi(args[3])
+			if err != nil {
+				return err
+			}
+
+			clientCtx, err := client.GetClientTxContext(cmd)
+			if err != nil {
+				return err
+			}
+			msg := types.NewMsgSetQuota(
+				clientCtx.GetFromAddress().String(),
+				argName,
+				uint64(argMaxPercentSend),
+				uint64(argMaxPercentRecv),
+				uint64(argDurationMinutes),
+			)
+
+			if err := msg.ValidateBasic(); err != nil {
+				return err
+			}
+
+			return tx.GenerateOrBroadcastTxCLI(clientCtx, cmd.Flags(), msg)
+		},
+	}
+
+	flags.AddTxFlagsToCmd(cmd)
+
+	return cmd
+}
