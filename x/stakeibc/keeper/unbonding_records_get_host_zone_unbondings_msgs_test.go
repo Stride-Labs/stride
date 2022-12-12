13f02089a7423dfba9fab517722c38a86655ff26
hoangdv2429 <hoangdv2429@gmail.com>
2022-12-12 14:07:02 +0700

    adding testcases for GetValidatorDelegationAmtDifferences

--- x/stakeibc/keeper/unbonding_records_get_host_zone_unbondings_msgs_test.go
+++ x/stakeibc/keeper/unbonding_records_get_host_zone_unbondings_msgs_test.go
@@ -220,3 +220,28 @@ func (s *KeeperTestSuite) TestGetTargetValAmtsForHostZone_Success() {
 	s.Require().Equal(tc.valNames[1], tc.hostZone.Validators[2].Address)
 	s.Require().Equal(tc.valNames[2], tc.hostZone.Validators[1].Address)
 }
+
+func (s *KeeperTestSuite) TestGetTargetValAmtsForHostZone_InvalidAmountOfDelegation() {
+	tc := s.SetupGetHostZoneUnbondingMsgs()
+
+	// if unbond/ finalDelegation is zero then return err
+	unbond := uint64(0)
+	_, err := s.App.StakeibcKeeper.GetTargetValAmtsForHostZone(s.Ctx, tc.hostZone, unbond)
+	s.Require().EqualError(err, stakeibc.ErrNoValidatorWeights.Error(), "Delegate zero amount should fail")
+
+}
+func (s *KeeperTestSuite) TestGetTargetValAmtsForHostZone_ErrNoValidatorsWeight() {
+	tc := s.SetupGetHostZoneUnbondingMsgs()
+
+	unbond := uint64(1_000_000)
+
+	// assign zero amount to all validators's weights
+	validators := tc.hostZone.GetValidators()
+	for _, validator := range validators {
+		validator.Weight = 0
+	}
+
+	// if weight is zero then return err
+	_, err := s.App.StakeibcKeeper.GetTargetValAmtsForHostZone(s.Ctx, tc.hostZone, unbond)
+	s.Require().EqualError(err, stakeibc.ErrNoValidatorWeights.Error(), "Delegate zero amount should fail")
+}
