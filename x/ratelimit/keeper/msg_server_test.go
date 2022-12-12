f0f4b4cde3f1d3514c8f8fff8675bcdef2bd16d1
sampocs <sam.pochyly@gmail.com>
2022-12-12 15:04:22 -0600

    removed quota integration tests

--- x/ratelimit/keeper/msg_server_test.go
+++ x/ratelimit/keeper/msg_server_test.go
@@ -8,18 +8,18 @@ import (
 )
 
 var addRateLimitMsg = &types.MsgAddRateLimit{
-	Denom:           "denom",
-	ChannelId:       "channel-0",
-	MaxPercentRecv:  10,
-	MaxPercentSend:  20,
-	DurationMinutes: 30,
+	Denom:          "denom",
+	ChannelId:      "channel-0",
+	MaxPercentRecv: 10,
+	MaxPercentSend: 20,
+	DurationHours:  30,
 }
 
 var updateLimitMsg = &types.MsgUpdateRateLimit{
-	PathId:          "denom/channel-0",
-	MaxPercentRecv:  10,
-	MaxPercentSend:  20,
-	DurationMinutes: 30,
+	PathId:         "denom/channel-0",
+	MaxPercentRecv: 10,
+	MaxPercentSend: 20,
+	DurationHours:  30,
 }
 
 var removeRateLimitMsg = &types.MsgRemoveRateLimit{
