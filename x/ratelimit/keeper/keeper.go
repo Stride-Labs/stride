b47726927409517ee1ccc7789c999cc0ba76ae38
sampocs <sam.pochyly@gmail.com>
2022-12-13 13:40:28 -0600

    added addInflow and addOutflow and unit tests

--- x/ratelimit/keeper/keeper.go
+++ x/ratelimit/keeper/keeper.go
@@ -4,6 +4,7 @@ import (
 	"github.com/cosmos/cosmos-sdk/codec"
 	storetypes "github.com/cosmos/cosmos-sdk/store/types"
 	sdk "github.com/cosmos/cosmos-sdk/types"
+	bankkeeper "github.com/cosmos/cosmos-sdk/x/bank/keeper"
 	capabilitytypes "github.com/cosmos/cosmos-sdk/x/capability/types"
 	porttypes "github.com/cosmos/ibc-go/v3/modules/core/05-port/types"
 	"github.com/cosmos/ibc-go/v3/modules/core/exported"
@@ -15,17 +16,21 @@ type (
 		cdc      codec.BinaryCodec
 
 		ics4Wrapper porttypes.ICS4Wrapper
+		bankKeeper  bankkeeper.Keeper
 	}
 )
 
 func NewKeeper(
-	cdc codec.BinaryCodec, key storetypes.StoreKey,
+	cdc codec.BinaryCodec,
+	key storetypes.StoreKey,
 	ics4Wrapper porttypes.ICS4Wrapper,
+	bankKeeper bankkeeper.Keeper,
 ) *Keeper {
 	return &Keeper{
 		cdc:         cdc,
 		storeKey:    key,
 		ics4Wrapper: ics4Wrapper,
+		bankKeeper:  bankKeeper,
 	}
 }
 
