24eca013b6ff7b8fc3a6001ca9192b8031ff9715
antstalepresh <stalepresh121@outlook.com>
2022-12-14 11:37:44 -0500

    implementing ratelimit msgserver & tests

--- x/ratelimit/keeper/msg_server.go
+++ x/ratelimit/keeper/msg_server.go
@@ -3,6 +3,8 @@ package keeper
 import (
 	"context"
 
+	sdk "github.com/cosmos/cosmos-sdk/types"
+
 	"github.com/Stride-Labs/stride/v4/x/ratelimit/types"
 )
 
@@ -19,21 +21,90 @@ func NewMsgServerImpl(keeper Keeper) types.MsgServer {
 var _ types.MsgServer = msgServer{}
 
 func (server msgServer) AddRateLimit(goCtx context.Context, msg *types.MsgAddRateLimit) (*types.MsgAddRateLimitResponse, error) {
-	// TODO:
+	ctx := sdk.UnwrapSDKContext(goCtx)
+	path := types.Path{
+		Denom:     msg.Denom,
+		ChannelId: msg.ChannelId,
+	}
+	quota := types.Quota{
+		MaxPercentSend: msg.MaxPercentSend,
+		MaxPercentRecv: msg.MaxPercentRecv,
+		DurationHours:  msg.DurationHours,
+	}
+	flow := types.Flow{
+		Inflow:       0,
+		Outflow:      0,
+		ChannelValue: server.Keeper.GetChannelValue(ctx, msg.Denom).Uint64(),
+	}
+
+	_, found := server.Keeper.GetRateLimit(ctx, msg.Denom, msg.ChannelId)
+	if found {
+		return nil, types.ErrRateLimitKeyDuplicated
+	}
+
+	server.Keeper.SetRateLimit(ctx, types.RateLimit{
+		Path:  &path,
+		Quota: &quota,
+		Flow:  &flow,
+	})
+
 	return &types.MsgAddRateLimitResponse{}, nil
 }
 
 func (server msgServer) UpdateRateLimit(goCtx context.Context, msg *types.MsgUpdateRateLimit) (*types.MsgUpdateRateLimitResponse, error) {
-	// TODO:
+	ctx := sdk.UnwrapSDKContext(goCtx)
+	path := types.Path{
+		Denom:     msg.Denom,
+		ChannelId: msg.ChannelId,
+	}
+	quota := types.Quota{
+		MaxPercentSend: msg.MaxPercentSend,
+		MaxPercentRecv: msg.MaxPercentRecv,
+		DurationHours:  msg.DurationHours,
+	}
+	flow := types.Flow{
+		Inflow:       0,
+		Outflow:      0,
+		ChannelValue: server.Keeper.GetChannelValue(ctx, msg.Denom).Uint64(),
+	}
+
+	_, found := server.Keeper.GetRateLimit(ctx, msg.Denom, msg.ChannelId)
+	if !found {
+		return nil, types.ErrRateLimitKeyNotFound
+	}
+
+	server.Keeper.SetRateLimit(ctx, types.RateLimit{
+		Path:  &path,
+		Quota: &quota,
+		Flow:  &flow,
+	})
+
 	return &types.MsgUpdateRateLimitResponse{}, nil
 }
 
 func (server msgServer) RemoveRateLimit(goCtx context.Context, msg *types.MsgRemoveRateLimit) (*types.MsgRemoveRateLimitResponse, error) {
-	// TODO:
+	ctx := sdk.UnwrapSDKContext(goCtx)
+
+	_, found := server.Keeper.GetRateLimit(ctx, msg.Denom, msg.ChannelId)
+	if !found {
+		return nil, types.ErrRateLimitKeyNotFound
+	}
+
+	server.Keeper.RemoveRateLimit(ctx, msg.Denom, msg.ChannelId)
 	return &types.MsgRemoveRateLimitResponse{}, nil
 }
 
 func (server msgServer) ResetRateLimit(goCtx context.Context, msg *types.MsgResetRateLimit) (*types.MsgResetRateLimitResponse, error) {
-	// TODO:
+	ctx := sdk.UnwrapSDKContext(goCtx)
+
+	rateLimit, found := server.Keeper.GetRateLimit(ctx, msg.Denom, msg.ChannelId)
+	if !found {
+		return nil, types.ErrRateLimitKeyNotFound
+	}
+
+	rateLimit.Flow.Inflow = 0
+	rateLimit.Flow.Outflow = 0
+
+	server.Keeper.SetRateLimit(ctx, rateLimit)
 	return &types.MsgResetRateLimitResponse{}, nil
 }
--- x/ratelimit/keeper/msg_server_test.go
+++ x/ratelimit/keeper/msg_server_test.go
@@ -1,7 +1,7 @@
 package keeper_test
 
 import (
-	// sdk "github.com/cosmos/cosmos-sdk/types"
+	sdk "github.com/cosmos/cosmos-sdk/types"
 
 	"github.com/Stride-Labs/stride/v4/app/apptesting"
 	"github.com/Stride-Labs/stride/v4/x/ratelimit/types"
@@ -15,12 +15,12 @@ var addRateLimitMsg = &types.MsgAddRateLimit{
 	DurationHours:  30,
 }
 
-var updateLimitMsg = &types.MsgUpdateRateLimit{
+var updateRateLimitMsg = &types.MsgUpdateRateLimit{
 	Denom:          "denom",
 	ChannelId:      "channel-0",
-	MaxPercentRecv: 10,
-	MaxPercentSend: 20,
-	DurationHours:  30,
+	MaxPercentRecv: 20,
+	MaxPercentSend: 30,
+	DurationHours:  40,
 }
 
 var removeRateLimitMsg = &types.MsgRemoveRateLimit{
@@ -41,19 +41,50 @@ func (s *KeeperTestSuite) TestMsgServer_AddRateLimit() {
 	denom := addRateLimitMsg.Denom
 	channelId := addRateLimitMsg.ChannelId
 
-	// TODO: Uncomment once function is implemented
-	_ = denom
-	_ = channelId
-	// // Add a rate limit successfully
-	// _, err := s.GetMsgServer().AddRateLimit(sdk.WrapSDKContext(s.Ctx), addRateLimitMsg)
-	// s.Require().NoError(err)
+	// Add a rate limit successfully
+	_, err := s.GetMsgServer().AddRateLimit(sdk.WrapSDKContext(s.Ctx), addRateLimitMsg)
+	s.Require().NoError(err)
 
-	// _, found := s.App.RatelimitKeeper.GetRateLimit(s.Ctx, denom, channelId)
-	// s.Require().True(found)
+	_, found := s.App.RatelimitKeeper.GetRateLimit(s.Ctx, denom, channelId)
+	s.Require().True(found)
 
-	// // check for duplicate rate limit
-	// _, err = s.GetMsgServer().AddRateLimit(sdk.WrapSDKContext(s.Ctx), addRateLimitMsg)
-	// s.Require().Error(err)
+	// Check for duplicate rate limit
+	_, err = s.GetMsgServer().AddRateLimit(sdk.WrapSDKContext(s.Ctx), addRateLimitMsg)
+	s.Require().Error(err)
+}
+
+func (s *KeeperTestSuite) TestMsgServer_UpdateRateLimit() {
+	s.SetupTest()
+	validAddr, _ := apptesting.GenerateTestAddrs()
+	addRateLimitMsg.Creator = validAddr
+	updateRateLimitMsg.Creator = validAddr
+
+	denom := updateRateLimitMsg.Denom
+	channelId := updateRateLimitMsg.ChannelId
+
+	// Attempt to update a rate limit that does not exist
+	_, err := s.GetMsgServer().UpdateRateLimit(sdk.WrapSDKContext(s.Ctx), updateRateLimitMsg)
+	s.Require().Error(err)
+
+	// Add a rate limit successfully
+	_, err = s.GetMsgServer().AddRateLimit(sdk.WrapSDKContext(s.Ctx), addRateLimitMsg)
+	s.Require().NoError(err)
+
+	_, found := s.App.RatelimitKeeper.GetRateLimit(s.Ctx, denom, channelId)
+	s.Require().True(found)
+
+	// Update the rate limit successfully
+	_, err = s.GetMsgServer().UpdateRateLimit(sdk.WrapSDKContext(s.Ctx), updateRateLimitMsg)
+	s.Require().NoError(err)
+
+	// Check ratelimit quota is updated correctly
+	updatedRateLimit, found := s.App.RatelimitKeeper.GetRateLimit(s.Ctx, denom, channelId)
+	s.Require().True(found)
+	s.Require().Equal(updatedRateLimit.Quota, &types.Quota{
+		MaxPercentSend: updateRateLimitMsg.MaxPercentSend,
+		MaxPercentRecv: updateRateLimitMsg.MaxPercentRecv,
+		DurationHours:  updateRateLimitMsg.DurationHours,
+	})
 }
 
 func (s *KeeperTestSuite) TestMsgServer_RemoveRateLimit() {
@@ -65,24 +96,52 @@ func (s *KeeperTestSuite) TestMsgServer_RemoveRateLimit() {
 	denom := removeRateLimitMsg.Denom
 	channelId := removeRateLimitMsg.ChannelId
 
-	// TODO: Uncomment once function is implemented
-	_ = denom
-	_ = channelId
-	// // Attempt to remove a rate limit that does not exist
-	// _, err := s.GetMsgServer().RemoveRateLimit(sdk.WrapSDKContext(s.Ctx), removeRateLimitMsg)
-	// s.Require().Error(err)
+	// Attempt to remove a rate limit that does not exist
+	_, err := s.GetMsgServer().RemoveRateLimit(sdk.WrapSDKContext(s.Ctx), removeRateLimitMsg)
+	s.Require().Error(err)
 
-	// // Add a rate limit successfully
-	// _, err = s.GetMsgServer().AddRateLimit(sdk.WrapSDKContext(s.Ctx), addRateLimitMsg)
-	// s.Require().NoError(err)
+	// Add a rate limit successfully
+	_, err = s.GetMsgServer().AddRateLimit(sdk.WrapSDKContext(s.Ctx), addRateLimitMsg)
+	s.Require().NoError(err)
 
-	// _, found := s.App.RatelimitKeeper.GetRateLimit(s.Ctx, denom, channelId)
-	// s.Require().True(found)
+	_, found := s.App.RatelimitKeeper.GetRateLimit(s.Ctx, denom, channelId)
+	s.Require().True(found)
 
-	// // Remove the rate limit
-	// _, err = s.GetMsgServer().RemoveRateLimit(sdk.WrapSDKContext(s.Ctx), removeRateLimitMsg)
-	// s.Require().NoError(err)
+	// Remove the rate limit
+	_, err = s.GetMsgServer().RemoveRateLimit(sdk.WrapSDKContext(s.Ctx), removeRateLimitMsg)
+	s.Require().NoError(err)
 
-	// _, found = s.App.RatelimitKeeper.GetRateLimit(s.Ctx, denom, channelId)
-	// s.Require().False(found)
+	_, found = s.App.RatelimitKeeper.GetRateLimit(s.Ctx, denom, channelId)
+	s.Require().False(found)
+}
+
+func (s *KeeperTestSuite) TestMsgServer_ResetRateLimit() {
+	s.SetupTest()
+	validAddr, _ := apptesting.GenerateTestAddrs()
+	addRateLimitMsg.Creator = validAddr
+	resetRateLimitMsg.Creator = validAddr
+
+	denom := resetRateLimitMsg.Denom
+	channelId := resetRateLimitMsg.ChannelId
+
+	// Attempt to reset a rate limit that does not exist
+	_, err := s.GetMsgServer().ResetRateLimit(sdk.WrapSDKContext(s.Ctx), resetRateLimitMsg)
+	s.Require().Error(err)
+
+	// Add a rate limit successfully
+	_, err = s.GetMsgServer().AddRateLimit(sdk.WrapSDKContext(s.Ctx), addRateLimitMsg)
+	s.Require().NoError(err)
+
+	_, found := s.App.RatelimitKeeper.GetRateLimit(s.Ctx, denom, channelId)
+	s.Require().True(found)
+
+	// Reset the rate limit successfully
+	_, err = s.GetMsgServer().ResetRateLimit(sdk.WrapSDKContext(s.Ctx), resetRateLimitMsg)
+	s.Require().NoError(err)
+
+	// Check ratelimit quota is reset correctly
+	resetRateLimit, found := s.App.RatelimitKeeper.GetRateLimit(s.Ctx, denom, channelId)
+	s.Require().True(found)
+	s.Require().Equal(resetRateLimit.Flow.Inflow, uint64(0))
+	s.Require().Equal(resetRateLimit.Flow.Outflow, uint64(0))
 }
