0aaf5e7cc669c214427955a8c64d05862e734f4b
ThanhNhann <thanhnhan98qh@gmail.com>
2022-12-16 15:34:35 +0700

    update test for liquid stake

--- x/stakeibc/keeper/msg_server_liquid_stake_test.go
+++ x/stakeibc/keeper/msg_server_liquid_stake_test.go
@@ -8,7 +8,6 @@ import (
 
 	epochtypes "github.com/Stride-Labs/stride/v4/x/epochs/types"
 	recordtypes "github.com/Stride-Labs/stride/v4/x/records/types"
-	"github.com/Stride-Labs/stride/v4/x/stakeibc/types"
 	stakeibctypes "github.com/Stride-Labs/stride/v4/x/stakeibc/types"
 )
 
@@ -20,7 +19,7 @@ type Account struct {
 
 type LiquidStakeState struct {
 	depositRecordAmount int64
-	hostZone            types.HostZone
+	hostZone            stakeibctypes.HostZone
 }
 
 type LiquidStakeTestCase struct {
@@ -40,7 +39,7 @@ func (s *KeeperTestSuite) SetupLiquidStake() LiquidStakeTestCase {
 	}
 	s.FundAccount(user.acc, user.atomBalance)
 
-	zoneAddress := types.NewZoneAddress(HostChainId)
+	zoneAddress := stakeibctypes.NewZoneAddress(HostChainId)
 
 	zoneAccount := Account{
 		acc:           zoneAddress,
@@ -246,4 +245,22 @@ func (s *KeeperTestSuite) TestLiquidStake_InvalidHostAddress() {
 
 	_, err := s.GetMsgServer().LiquidStake(sdk.WrapSDKContext(s.Ctx), &tc.validMsg)
 	s.Require().EqualError(err, "could not bech32 decode address cosmosXXX of zone with id: GAIA")
+}
+
+func (s *KeeperTestSuite) TestLiquidStake_IntOverflowAmount() {
+	tc := s.SetupLiquidStake()
+
+	tc.user.atomBalance = sdk.NewCoin(IbcAtom, sdk.NewIntFromUint64(18000000000000000000))
+
+	s.FundAccount(tc.user.acc, tc.user.atomBalance)
+
+	balance := tc.user.atomBalance.Amount.Uint64()
+
+	invalidMsg := tc.validMsg
+	invalidMsg.Amount = uint64(balance - 1000)
+
+	_, err := s.GetMsgServer().LiquidStake(sdk.WrapSDKContext(s.Ctx), &invalidMsg)
+
+	expectedErr := fmt.Sprintf("failed to mint %s stAssets to user: failed to convert amount to int64: overflow: unable to cast %d of type uint64 to int64", invalidMsg.HostDenom, invalidMsg.Amount)
+	s.Require().EqualError(err, expectedErr)
 }
