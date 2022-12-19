c94bc8258f9133b5fe9c2346a1eb4751301c9582
sampocs <sam.pochyly@gmail.com>
2022-12-19 16:01:07 -0600

    added docstrings

--- x/ratelimit/keeper/msg_server.go
+++ x/ratelimit/keeper/msg_server.go
@@ -20,6 +20,7 @@ func NewMsgServerImpl(keeper Keeper) types.MsgServer {
 
 var _ types.MsgServer = msgServer{}
 
+// Adds a new rate limit. Fails if the rate limit already exists or the channel value is 0
 func (server msgServer) AddRateLimit(goCtx context.Context, msg *types.MsgAddRateLimit) (*types.MsgAddRateLimitResponse, error) {
 	ctx := sdk.UnwrapSDKContext(goCtx)
 	path := types.Path{
@@ -54,6 +55,7 @@ func (server msgServer) AddRateLimit(goCtx context.Context, msg *types.MsgAddRat
 	return &types.MsgAddRateLimitResponse{}, nil
 }
 
+// Updates an existing rate limit. Fails if the rate limit doesn't exist
 func (server msgServer) UpdateRateLimit(goCtx context.Context, msg *types.MsgUpdateRateLimit) (*types.MsgUpdateRateLimitResponse, error) {
 	ctx := sdk.UnwrapSDKContext(goCtx)
 	path := types.Path{
@@ -85,6 +87,7 @@ func (server msgServer) UpdateRateLimit(goCtx context.Context, msg *types.MsgUpd
 	return &types.MsgUpdateRateLimitResponse{}, nil
 }
 
+// Removes a rate limit. Fails if the rate limit doesn't exist
 func (server msgServer) RemoveRateLimit(goCtx context.Context, msg *types.MsgRemoveRateLimit) (*types.MsgRemoveRateLimitResponse, error) {
 	ctx := sdk.UnwrapSDKContext(goCtx)
 
@@ -97,6 +100,7 @@ func (server msgServer) RemoveRateLimit(goCtx context.Context, msg *types.MsgRem
 	return &types.MsgRemoveRateLimitResponse{}, nil
 }
 
+// Resets the flow on a rate limit. Fails if the rate limit doesn't exist
 func (server msgServer) ResetRateLimit(goCtx context.Context, msg *types.MsgResetRateLimit) (*types.MsgResetRateLimitResponse, error) {
 	ctx := sdk.UnwrapSDKContext(goCtx)
 	err := server.Keeper.ResetRateLimit(ctx, msg.Denom, msg.ChannelId)
