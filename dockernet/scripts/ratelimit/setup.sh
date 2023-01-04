f59df9a1dd40ab2e37f167441934597e1a203eea
sampocs <sam.pochyly@gmail.com>
2023-01-04 12:50:46 -0600

    updated chain-id query in integration test

--- dockernet/scripts/ratelimit/setup.sh
+++ dockernet/scripts/ratelimit/setup.sh
@@ -113,7 +113,7 @@ setup_rate_limits() {
     fi
 
     # Confirm there are 4 rate limits on osmo (this is to test out the rate-limits-by-chain query)
-    num_rate_limits=$($STRIDE_MAIN_CMD q ratelimit show-rate-limits OSMO | grep path | wc -l | xargs)
+    num_rate_limits=$($STRIDE_MAIN_CMD q ratelimit rate-limits-by-chain OSMO | grep path | wc -l | xargs)
     if [[ "$num_rate_limits" != "4" ]]; then 
         echo "ERROR: OSMO should have 4 rate limits (it had: $num_rate_limits)"
         exit 1
