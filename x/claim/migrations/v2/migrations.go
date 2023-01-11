ff1d4c5f69c9c4110c5886ec2270d1cf4a90b2cc
sampocs <sam.pochyly@gmail.com>
2023-01-11 17:17:42 -0600

    moved over convert functions

--- x/claim/migrations/v2/migrations.go
+++ x/claim/migrations/v2/migrations.go
@@ -4,7 +4,26 @@ import (
 	"github.com/cosmos/cosmos-sdk/codec"
 	storetypes "github.com/cosmos/cosmos-sdk/store/types"
 	sdk "github.com/cosmos/cosmos-sdk/types"
+
+	oldclaimtypes "github.com/Stride-Labs/stride/v4/x/claim/migrations/v2/types"
+	claimtypes "github.com/Stride-Labs/stride/v4/x/claim/types"
 )
+
+func convertToNewClaimParams(oldParams oldclaimtypes.Params) claimtypes.Params {
+	var newParams claimtypes.Params
+	for _, airDrop := range oldParams.Airdrops {
+		newAirDrop := claimtypes.Airdrop{
+			AirdropIdentifier:  airDrop.AirdropIdentifier,
+			AirdropStartTime:   airDrop.AirdropStartTime,
+			AirdropDuration:    airDrop.AirdropDuration,
+			ClaimDenom:         airDrop.ClaimDenom,
+			DistributorAddress: airDrop.DistributorAddress,
+			ClaimedSoFar:       sdk.NewInt(airDrop.ClaimedSoFar),
+		}
+		newParams.Airdrops = append(newParams.Airdrops, &newAirDrop)
+	}
+	return newParams
+}
 
 func MigrateStore(ctx sdk.Context, storeKey storetypes.StoreKey, cdc codec.BinaryCodec) error {
 	return nil
--- x/stakeibc/migrations/v2/migrations.go
+++ x/stakeibc/migrations/v2/migrations.go
@@ -4,7 +4,57 @@ import (
 	"github.com/cosmos/cosmos-sdk/codec"
 	storetypes "github.com/cosmos/cosmos-sdk/store/types"
 	sdk "github.com/cosmos/cosmos-sdk/types"
+
+	oldstakeibctypes "github.com/Stride-Labs/stride/v4/x/stakeibc/migrations/v2/types"
+	stakeibctypes "github.com/Stride-Labs/stride/v4/x/stakeibc/types"
 )
+
+func convertToNewValidator(oldValidator oldstakeibctypes.Validator) stakeibctypes.Validator {
+	return stakeibctypes.Validator{
+		Name:                 oldValidator.Name,
+		Address:              oldValidator.Address,
+		Status:               stakeibctypes.Validator_ValidatorStatus(oldValidator.Status),
+		CommissionRate:       oldValidator.CommissionRate,
+		DelegationAmt:        sdk.NewIntFromUint64(oldValidator.DelegationAmt),
+		Weight:               oldValidator.Weight,
+		InternalExchangeRate: (*stakeibctypes.ValidatorExchangeRate)(oldValidator.InternalExchangeRate),
+	}
+}
+
+func convertToNewHostZone(oldHostZone oldstakeibctypes.HostZone) stakeibctypes.HostZone {
+	var validators []*stakeibctypes.Validator
+	var blacklistValidator []*stakeibctypes.Validator
+
+	for _, oldValidator := range oldHostZone.Validators {
+		newValidator := convertToNewValidator(*oldValidator)
+		validators = append(validators, &newValidator)
+	}
+
+	for _, oldValidator := range oldHostZone.BlacklistedValidators {
+		newValidator := convertToNewValidator(*oldValidator)
+		blacklistValidator = append(blacklistValidator, &newValidator)
+	}
+
+	return stakeibctypes.HostZone{
+		ChainId:               oldHostZone.ChainId,
+		ConnectionId:          oldHostZone.ConnectionId,
+		Bech32Prefix:          oldHostZone.Bech32Prefix,
+		TransferChannelId:     oldHostZone.TransferChannelId,
+		Validators:            validators,
+		BlacklistedValidators: blacklistValidator,
+		WithdrawalAccount:     oldHostZone.WithdrawalAccount,
+		FeeAccount:            oldHostZone.FeeAccount,
+		DelegationAccount:     oldHostZone.DelegationAccount,
+		RedemptionAccount:     oldHostZone.RedemptionAccount,
+		IbcDenom:              oldHostZone.IbcDenom,
+		HostDenom:             oldHostZone.HostDenom,
+		LastRedemptionRate:    oldHostZone.LastRedemptionRate,
+		RedemptionRate:        oldHostZone.RedemptionRate,
+		UnbondingFrequency:    oldHostZone.UnbondingFrequency,
+		StakedBal:             sdk.NewIntFromUint64(oldHostZone.StakedBal),
+		Address:               oldHostZone.Address,
+	}
+}
 
 func MigrateStore(ctx sdk.Context, storeKey storetypes.StoreKey, cdc codec.BinaryCodec) error {
 	return nil
