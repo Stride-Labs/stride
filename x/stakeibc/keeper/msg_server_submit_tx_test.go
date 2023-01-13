0562e469cba44c366eb4e6b2a3af3254d642e3b0
hoangdv2429 <hoangdv2429@gmail.com>
2023-01-13 17:23:22 +0700

    merge all PRs into one file

--- x/stakeibc/keeper/msg_server_submit_tx_test.go
+++ x/stakeibc/keeper/msg_server_submit_tx_test.go
@@ -5,6 +5,7 @@ import (
 	"time"
 
 	sdk "github.com/cosmos/cosmos-sdk/types"
+	distributiontypes "github.com/cosmos/cosmos-sdk/x/distribution/types"
 	ibctesting "github.com/cosmos/ibc-go/v3/testing"
 	_ "github.com/stretchr/testify/suite"
 
@@ -16,19 +17,25 @@ import (
 	_ "github.com/stretchr/testify/suite"
 )
 
-type DelegateOnHostTestcase struct {
+type SubmitTxTestcase struct {
 	hostZone      stakeibctypes.HostZone
 	amt           sdk.Coin
 	depositRecord []recordstypes.DepositRecord
+	msgs          []sdk.Msg
+	epochTracker  stakeibctypes.EpochTracker
 }
 
-func (s *KeeperTestSuite) SetupDelegationOnHost() DelegateOnHostTestcase {
+func (s *KeeperTestSuite) SetupSubmitTx_emptyStrideEpoch() SubmitTxTestcase {
 	//Set Deposit Records of type Delegate
 	stakedBal := sdk.NewInt(5_000)
 	AddressDelegateAccount := "1"
 	DelegateAccount := &types.ICAAccount{Address: AddressDelegateAccount, Target: types.ICAAccountType_DELEGATION}
+	AddressWithdrawalAccount := "stride_ADDRESS"
+	WithdrawalAccount := &types.ICAAccount{Address: AddressWithdrawalAccount, Target: types.ICAAccountType_WITHDRAWAL}
 	delegationAccountOwner := fmt.Sprintf("%s.%s", HostChainId, "DELEGATION")
+	withdrawalAccountOwner := fmt.Sprintf("%s.%s", HostChainId, "WITHDRAWAL")
 	s.CreateICAChannel(delegationAccountOwner)
+	s.CreateICAChannel(withdrawalAccountOwner)
 	DepositRecordDelegate := []recordstypes.DepositRecord{
 		{
 			Id:         1,
@@ -77,11 +84,31 @@ func (s *KeeperTestSuite) SetupDelegationOnHost() DelegateOnHostTestcase {
 		StakedBal:         stakedBal,
 		Validators:        validators,
 		DelegationAccount: DelegateAccount,
+		WithdrawalAccount: WithdrawalAccount,
 		ConnectionId:      ibctesting.FirstConnectionID,
+	}
+
+	delegationIca := hostZone.DelegationAccount
+	withdrawalIcaAddr := hostZone.WithdrawalAccount.Address
+	msgs := []sdk.Msg{
+		&distributiontypes.MsgSetWithdrawAddress{
+			DelegatorAddress: delegationIca.Address,
+			WithdrawAddress:  withdrawalIcaAddr,
+		},
 	}
 
 	s.App.StakeibcKeeper.SetHostZone(s.Ctx, hostZone)
 
+	return SubmitTxTestcase{
+		hostZone:      hostZone,
+		amt:           stakeCoin,
+		depositRecord: DepositRecordDelegate,
+		msgs:          msgs,
+	}
+}
+
+func (s *KeeperTestSuite) SetupSubmitTx() SubmitTxTestcase {
+	tc := s.SetupSubmitTx_emptyStrideEpoch()
 	//set Stride epoch
 	epochIdentifier := epochtypes.STRIDE_EPOCH
 	epochTracker := stakeibctypes.EpochTracker{
@@ -93,15 +120,17 @@ func (s *KeeperTestSuite) SetupDelegationOnHost() DelegateOnHostTestcase {
 
 	s.App.StakeibcKeeper.SetEpochTracker(s.Ctx, epochTracker)
 
-	return DelegateOnHostTestcase{
-		hostZone:      hostZone,
-		amt:           stakeCoin,
-		depositRecord: DepositRecordDelegate,
+	return SubmitTxTestcase{
+		hostZone:      tc.hostZone,
+		amt:           tc.amt,
+		depositRecord: tc.depositRecord,
+		msgs:          tc.msgs,
+		epochTracker:  epochTracker,
 	}
 }
 
 func (s *KeeperTestSuite) TestDelegateOnHost_Successful() {
-	tc := s.SetupDelegationOnHost()
+	tc := s.SetupSubmitTx()
 	err := s.App.StakeibcKeeper.DelegateOnHost(s.Ctx, tc.hostZone, tc.amt, tc.depositRecord[0])
 	DelegateDepositRecord, found := s.App.RecordsKeeper.GetDepositRecord(s.Ctx, tc.depositRecord[0].Id)
 	s.Require().NoError(err)
@@ -110,7 +139,7 @@ func (s *KeeperTestSuite) TestDelegateOnHost_Successful() {
 }
 
 func (s *KeeperTestSuite) TestDelegateOnHost_ConnectionIDNotFound() {
-	tc := s.SetupDelegationOnHost()
+	tc := s.SetupSubmitTx()
 	//change Hostzone's chainId so no connection should be found
 	tc.hostZone.ChainId = "superGAIA"
 	s.App.StakeibcKeeper.SetHostZone(s.Ctx, tc.hostZone)
@@ -122,7 +151,7 @@ func (s *KeeperTestSuite) TestDelegateOnHost_ConnectionIDNotFound() {
 }
 
 func (s *KeeperTestSuite) TestDelegateOnHost_InvalidDelegationAccount() {
-	tc := s.SetupDelegationOnHost()
+	tc := s.SetupSubmitTx()
 	//change Hostzone's Delegation accounts so no accounts should be found
 	AddressDelegateAccount := ""
 	DelegateAccount := &types.ICAAccount{Address: AddressDelegateAccount, Target: types.ICAAccountType_DELEGATION}
@@ -136,7 +165,7 @@ func (s *KeeperTestSuite) TestDelegateOnHost_InvalidDelegationAccount() {
 }
 
 func (s *KeeperTestSuite) TestDelegationOnHost_ErrorGettingTargetDelegateAmtOnValidators() {
-	tc := s.SetupDelegationOnHost()
+	tc := s.SetupSubmitTx()
 	//change Hostzone's Validators to empty so error should be returnd
 	validators := []*stakeibctypes.Validator{}
 	tc.hostZone.Validators = validators
@@ -145,4 +174,162 @@ func (s *KeeperTestSuite) TestDelegationOnHost_ErrorGettingTargetDelegateAmtOnVa
 
 	error := `no non-zero validator weights`
 	s.EqualError(err, error, "Hostzone's Validators has been cleared so this should fail")
+}
+
+func (s *KeeperTestSuite) TestUpdateWithdrawalBalance_successful() {
+	tc := s.SetupSubmitTx()
+	err := s.App.StakeibcKeeper.UpdateWithdrawalBalance(s.Ctx, tc.hostZone)
+	s.Require().NoError(err)
+}
+
+func (s *KeeperTestSuite) TestUpdateWithdrawalBalance_FailedToGetICATimeoutNanos() {
+	tc := s.SetupSubmitTx_emptyStrideEpoch()
+
+	err := s.App.StakeibcKeeper.UpdateWithdrawalBalance(s.Ctx, tc.hostZone)
+	expectedErr := "Failed to get ICA timeout nanos for epochType stride_epoch using param, error: "
+	expectedErr += "Failed to get epoch tracker for stride_epoch: invalid request: invalid request"
+	s.EqualError(err, expectedErr, "Hostzone is set without Stride Epoch so it should fail")
+}
+
+func (s *KeeperTestSuite) TestUpdateWithdrawalBalance_EmptyConnectionId() {
+	tc := s.SetupSubmitTx()
+	//change hostzone's connectionId
+	tc.hostZone.ConnectionId = ""
+	s.App.StakeibcKeeper.SetHostZone(s.Ctx, tc.hostZone)
+
+	err := s.App.StakeibcKeeper.UpdateWithdrawalBalance(s.Ctx, tc.hostZone)
+	expectedErr := "[ICQ Validation Check] Failed! connection id cannot be empty: "
+	expectedErr += "invalid request"
+	s.EqualError(err, expectedErr, "Hostzone is set without Stride Epoch so it should fail")
+}
+
+func (s *KeeperTestSuite) TestSetWithdrawalAddressOnHost_successful() {
+	tc := s.SetupSubmitTx()
+	err := s.App.StakeibcKeeper.SetWithdrawalAddressOnHost(s.Ctx, tc.hostZone)
+	s.Require().NoError(err)
+}
+
+func (s *KeeperTestSuite) TestSetWithdrawalAddressOnHost_FailedToGetICATimeoutNanos() {
+	tc := s.SetupSubmitTx_emptyStrideEpoch()
+
+	err := s.App.StakeibcKeeper.SetWithdrawalAddressOnHost(s.Ctx, tc.hostZone)
+	expectedErr := "Failed to SubmitTxs for connection-0, GAIA, [delegator_address:\"1\" withdraw_address:\"stride_ADDRESS\" ]: "
+	expectedErr += "invalid request"
+	s.EqualError(err, expectedErr, "Hostzone is set without Stride Epoch so it should fail")
+}
+
+func (s *KeeperTestSuite) TestGetStartTimeNextEpoch_Success() {
+	// SetEpochTracker
+	epochIdentifier := epochtypes.STRIDE_EPOCH
+	epochTracker := stakeibctypes.EpochTracker{
+		EpochIdentifier:    epochIdentifier,
+		EpochNumber:        uint64(2),
+		NextEpochStartTime: uint64(time.Now().Unix()),
+		Duration:           uint64(2),
+	}
+	s.App.StakeibcKeeper.SetEpochTracker(s.Ctx, epochTracker)
+
+	epochReturn, err := s.App.StakeibcKeeper.GetStartTimeNextEpoch(s.Ctx, epochIdentifier)
+
+	s.Require().NoError(err)
+
+	s.Require().Equal(epochReturn, epochTracker.NextEpochStartTime)
+
+}
+func (s *KeeperTestSuite) TestGetStartTimeNextEpoch_FailedToGetEpoch() {
+	// SetEpochTracker
+	epochIdentifier := epochtypes.STRIDE_EPOCH
+	epochTracker := stakeibctypes.EpochTracker{
+		EpochIdentifier:    epochIdentifier,
+		EpochNumber:        uint64(2),
+		NextEpochStartTime: uint64(time.Now().Unix()),
+		Duration:           uint64(2),
+	}
+	s.App.StakeibcKeeper.SetEpochTracker(s.Ctx, epochTracker)
+
+	_, err := s.App.StakeibcKeeper.GetStartTimeNextEpoch(s.Ctx, "epoch_stride")
+
+	s.Require().EqualError(err, fmt.Sprintf("Failed to get epoch tracker for %s: %s", "epoch_stride", "invalid request"))
+}
+
+func (s *KeeperTestSuite) TestSubmitsTx_Successful() {
+	tc := s.SetupSubmitTx()
+	hostZone := tc.hostZone
+	msgs := tc.msgs
+	account := hostZone.DelegationAccount
+
+	_, err := s.App.StakeibcKeeper.SubmitTxs(s.Ctx, hostZone.ConnectionId, msgs, *account, tc.epochTracker.NextEpochStartTime, "", nil)
+	s.Require().NoError(err)
+
+}
+func (s *KeeperTestSuite) TestSubmitsTx_FailedCallBackSendTx() {
+	tc := s.SetupSubmitTx()
+	hostZone := tc.hostZone
+	msgs := tc.msgs
+	account := hostZone.DelegationAccount
+	//test case
+	tc.epochTracker.NextEpochStartTime = 0
+
+	_, err := s.App.StakeibcKeeper.SubmitTxs(s.Ctx, hostZone.ConnectionId, msgs, *account, tc.epochTracker.NextEpochStartTime, "", nil)
+	s.Require().EqualError(err, "timeout timestamp must be in the future")
+}
+
+func (s *KeeperTestSuite) TestSubmitsTx_FailedToRetrieveActiveChannel() {
+	tc := s.SetupSubmitTx()
+	hostZone := tc.hostZone
+	msgs := tc.msgs
+	account := hostZone.DelegationAccount
+	//test case
+	account.Target = stakeibctypes.ICAAccountType_FEE
+
+	_, err := s.App.StakeibcKeeper.SubmitTxs(s.Ctx, hostZone.ConnectionId, msgs, *account, tc.epochTracker.NextEpochStartTime, "", nil)
+	s.Require().EqualError(err, "failed to retrieve active channel for port icacontroller-GAIA.FEE: no active channel for this owner")
+
+}
+
+func (s *KeeperTestSuite) TestSubmitsTx_InvalidConnectionIdNotFound() {
+	tc := s.SetupSubmitTx()
+	hostZone := tc.hostZone
+	msgs := tc.msgs
+	account := hostZone.DelegationAccount
+	//test case
+	ConnectionID_test := "connection_test"
+
+	_, err := s.App.StakeibcKeeper.SubmitTxs(s.Ctx, ConnectionID_test, msgs, *account, tc.epochTracker.NextEpochStartTime, "", nil)
+	s.Require().EqualError(err, fmt.Sprintf("invalid connection id, %s not found", ConnectionID_test))
+}
+func (s *KeeperTestSuite) TestSubmitTxsEpoch_Successful() {
+	tc := s.SetupSubmitTx()
+	hostZone := tc.hostZone
+	msgs := tc.msgs
+	account := hostZone.DelegationAccount
+	connectionID := ibctesting.FirstConnectionID
+	epochIdentifier := epochtypes.STRIDE_EPOCH
+
+	_, err := s.App.StakeibcKeeper.SubmitTxsEpoch(s.Ctx, connectionID, msgs, *account, epochIdentifier, "", nil)
+	s.Require().NoError(err)
+}
+func (s *KeeperTestSuite) TestSubmitTxsEpoch_FailedCallBackSubmitTxs() {
+	tc := s.SetupSubmitTx()
+	hostZone := tc.hostZone
+	msgs := tc.msgs
+	account := hostZone.DelegationAccount
+	//test case
+	connectionID := "connectionID_test"
+	epochIdentifier := epochtypes.STRIDE_EPOCH
+
+	_, err := s.App.StakeibcKeeper.SubmitTxsEpoch(s.Ctx, connectionID, msgs, *account, epochIdentifier, "", nil)
+	s.Require().EqualError(err, fmt.Sprintf("invalid connection id, %s not found", connectionID))
+}
+func (s *KeeperTestSuite) TestSubmitTxsEpoch_FailedToGetICA() {
+	tc := s.SetupSubmitTx()
+	hostZone := tc.hostZone
+	msgs := tc.msgs
+	account := hostZone.DelegationAccount
+	connectionID := ibctesting.FirstConnectionID
+	//test case
+	epochIdentifier := "epochtypes_test"
+
+	_, err := s.App.StakeibcKeeper.SubmitTxsEpoch(s.Ctx, connectionID, msgs, *account, epochIdentifier, "", nil)
+	s.Require().EqualError(err, fmt.Sprintf("Failed to convert timeoutNanos to uint64, error: Failed to get epoch tracker for %s: invalid request: invalid request", epochIdentifier))
 }
