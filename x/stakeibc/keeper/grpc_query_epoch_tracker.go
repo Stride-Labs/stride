fd3802dfedb4a0891c257b3772e89c4f8e0b7208
sampocs <sam.pochyly@gmail.com>
2023-01-19 18:01:28 -0600

    removed pagination from epoch tracker query

--- x/stakeibc/keeper/grpc_query_epoch_tracker.go
+++ x/stakeibc/keeper/grpc_query_epoch_tracker.go
@@ -3,9 +3,7 @@ package keeper
 import (
 	"context"
 
-	"github.com/cosmos/cosmos-sdk/store/prefix"
 	sdk "github.com/cosmos/cosmos-sdk/types"
-	"github.com/cosmos/cosmos-sdk/types/query"
 	"google.golang.org/grpc/codes"
 	"google.golang.org/grpc/status"
 
@@ -17,26 +15,10 @@ func (k Keeper) EpochTrackerAll(c context.Context, req *types.QueryAllEpochTrack
 		return nil, status.Error(codes.InvalidArgument, "invalid request")
 	}
 
-	var epochTrackers []types.EpochTracker
 	ctx := sdk.UnwrapSDKContext(c)
+	epochTrackers := k.GetAllEpochTracker(ctx)
 
-	store := ctx.KVStore(k.storeKey)
-	epochTrackerStore := prefix.NewStore(store, types.KeyPrefix(types.EpochTrackerKeyPrefix))
-
-	pageRes, err := query.Paginate(epochTrackerStore, req.Pagination, func(key []byte, value []byte) error {
-		var epochTracker types.EpochTracker
-		if err := k.cdc.Unmarshal(value, &epochTracker); err != nil {
-			return err
-		}
-
-		epochTrackers = append(epochTrackers, epochTracker)
-		return nil
-	})
-	if err != nil {
-		return nil, status.Error(codes.Internal, err.Error())
-	}
-
-	return &types.QueryAllEpochTrackerResponse{EpochTracker: epochTrackers, Pagination: pageRes}, nil
+	return &types.QueryAllEpochTrackerResponse{EpochTracker: epochTrackers}, nil
 }
 
 func (k Keeper) EpochTracker(c context.Context, req *types.QueryGetEpochTrackerRequest) (*types.QueryGetEpochTrackerResponse, error) {
--- x/stakeibc/types/query.pb.gw.go
+++ x/stakeibc/types/query.pb.gw.go
@@ -303,20 +303,9 @@ func local_request_Query_EpochTracker_0(ctx context.Context, marshaler runtime.M
 
 }
 
-var (
-	filter_Query_EpochTrackerAll_0 = &utilities.DoubleArray{Encoding: map[string]int{}, Base: []int(nil), Check: []int(nil)}
-)
-
 func request_Query_EpochTrackerAll_0(ctx context.Context, marshaler runtime.Marshaler, client QueryClient, req *http.Request, pathParams map[string]string) (proto.Message, runtime.ServerMetadata, error) {
 	var protoReq QueryAllEpochTrackerRequest
 	var metadata runtime.ServerMetadata
-
-	if err := req.ParseForm(); err != nil {
-		return nil, metadata, status.Errorf(codes.InvalidArgument, "%v", err)
-	}
-	if err := runtime.PopulateQueryParameters(&protoReq, req.Form, filter_Query_EpochTrackerAll_0); err != nil {
-		return nil, metadata, status.Errorf(codes.InvalidArgument, "%v", err)
-	}
 
 	msg, err := client.EpochTrackerAll(ctx, &protoReq, grpc.Header(&metadata.HeaderMD), grpc.Trailer(&metadata.TrailerMD))
 	return msg, metadata, err
@@ -326,13 +315,6 @@ func request_Query_EpochTrackerAll_0(ctx context.Context, marshaler runtime.Mars
 func local_request_Query_EpochTrackerAll_0(ctx context.Context, marshaler runtime.Marshaler, server QueryServer, req *http.Request, pathParams map[string]string) (proto.Message, runtime.ServerMetadata, error) {
 	var protoReq QueryAllEpochTrackerRequest
 	var metadata runtime.ServerMetadata
-
-	if err := req.ParseForm(); err != nil {
-		return nil, metadata, status.Errorf(codes.InvalidArgument, "%v", err)
-	}
-	if err := runtime.PopulateQueryParameters(&protoReq, req.Form, filter_Query_EpochTrackerAll_0); err != nil {
-		return nil, metadata, status.Errorf(codes.InvalidArgument, "%v", err)
-	}
 
 	msg, err := server.EpochTrackerAll(ctx, &protoReq)
 	return msg, metadata, err
