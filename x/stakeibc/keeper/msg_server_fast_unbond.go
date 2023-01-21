2a9307a86158ccbbd456fa9a17308e046454cec8
Kevin DeGraaf <kevin@sifchain.finance>
2023-01-20 17:24:24 -0800

    Work in progress.

--- x/stakeibc/keeper/msg_server_fast_unbond.go
+++ x/stakeibc/keeper/msg_server_fast_unbond.go
@@ -1,1 +1,101 @@
 package keeper
+
+import (
+	"context"
+	"fmt"
+	epochtypes "github.com/Stride-Labs/stride/v4/x/epochs/types"
+
+	sdk "github.com/cosmos/cosmos-sdk/types"
+	sdkerrors "github.com/cosmos/cosmos-sdk/types/errors"
+
+	"github.com/Stride-Labs/stride/v4/x/stakeibc/types"
+)
+
+func (k msgServer) FastUnbond(goCtx context.Context, msg *types.MsgFastUnbond) (*types.MsgFastUnbondResponse, error) {
+	ctx := sdk.UnwrapSDKContext(goCtx)
+	k.Logger(ctx).Info(fmt.Sprintf("fast unbond: %s", msg.String()))
+
+	// get our addresses, make sure they're valid
+	sender, err := sdk.AccAddressFromBech32(msg.Creator)
+	if err != nil {
+		return nil, sdkerrors.Wrapf(sdkerrors.ErrInvalidAddress, "creator address is invalid: %s. err: %s", msg.Creator, err.Error())
+	}
+
+	// then make sure host zone is valid
+	hostZone, found := k.GetHostZone(ctx, msg.HostZone)
+	if !found {
+		return nil, sdkerrors.Wrapf(types.ErrInvalidHostZone, "host zone is invalid: %s", msg.HostZone)
+	}
+
+	// construct desired unstaking amount from host zone
+	stDenom := types.StAssetDenomFromHostZoneDenom(hostZone.HostDenom)
+	nativeAmount := sdk.NewDecFromInt(msg.Amount).Mul(hostZone.RedemptionRate).RoundInt()
+
+	if nativeAmount.GT(hostZone.StakedBal) {
+		return nil, sdkerrors.Wrapf(types.ErrInvalidAmount, "cannot unstake an amount g.t. staked balance on host zone: %v", msg.Amount)
+	}
+
+	// safety check: redemption rate must be within safety bounds
+	rateIsSafe, err := k.IsRedemptionRateWithinSafetyBounds(ctx, hostZone)
+	if !rateIsSafe || (err != nil) {
+		errMsg := fmt.Sprintf("IsRedemptionRateWithinSafetyBounds check failed. hostZone: %s, err: %s", hostZone.String(), err.Error())
+		return nil, sdkerrors.Wrapf(types.ErrRedemptionRateOutsideSafetyBounds, errMsg)
+	}
+
+	// TODO(TEST-112) bigint safety
+	coinString := nativeAmount.String() + stDenom
+	inCoin, err := sdk.ParseCoinNormalized(coinString)
+	if err != nil {
+		return nil, sdkerrors.Wrapf(sdkerrors.ErrInvalidCoins, "could not parse inCoin: %s. err: %s", coinString, err.Error())
+	}
+	// safety checks on the coin
+	// 	- Redemption amount must be positive
+	if !nativeAmount.IsPositive() {
+		return nil, sdkerrors.Wrapf(sdkerrors.ErrInvalidCoins, "amount must be greater than 0. found: %v", msg.Amount)
+	}
+	// 	- Creator owns at least "amount" stAssets
+	balance := k.bankKeeper.GetBalance(ctx, sender, stDenom)
+	k.Logger(ctx).Info(fmt.Sprintf("Redemption issuer IBCDenom balance: %v%s", balance.Amount, balance.Denom))
+	k.Logger(ctx).Info(fmt.Sprintf("Redemption requested redemotion amount: %v%s", inCoin.Amount, inCoin.Denom))
+	if balance.Amount.LT(msg.Amount) {
+		return nil, sdkerrors.Wrapf(sdkerrors.ErrInvalidCoins, "balance is lower than redemption amount. redemption amount: %v, balance %v: ", msg.Amount, balance.Amount)
+	}
+
+	// Find and subtract this amount from a deposit record if it is big enough
+	epochTracker, found := k.GetEpochTracker(ctx, epochtypes.STRIDE_EPOCH)
+	if !found {
+		k.Logger(ctx).Error("failed to find stride epoch")
+		return nil, sdkerrors.Wrapf(sdkerrors.ErrNotFound, "no epoch number for epoch (%s)", epochtypes.STRIDE_EPOCH)
+	}
+	// Does this use too much gas?
+	depositRecord, found := k.RecordsKeeper.GetDepositRecordByEpochAndChain(ctx, epochTracker.EpochNumber, hostZone.ChainId)
+	if !found {
+		k.Logger(ctx).Error("failed to find deposit record")
+		return nil, sdkerrors.Wrapf(sdkerrors.ErrNotFound, fmt.Sprintf("no deposit record for epoch (%d)", epochTracker.EpochNumber))
+	}
+	if nativeAmount.GT(depositRecord.Amount) {
+		return nil, sdkerrors.Wrapf(types.ErrInvalidAmount, "cannot fast unbond an amount %v g.t. pending deposit balance on host zone: %v", nativeAmount, msg.Amount)
+	}
+	depositRecord.Amount = depositRecord.Amount.Sub(nativeAmount)
+	bech32ZoneAddress, err := sdk.AccAddressFromBech32(hostZone.Address)
+	if err != nil {
+		return nil, fmt.Errorf("could not bech32 decode address %s of zone with id: %s", hostZone.Address, hostZone.ChainId)
+	}
+	// Send that amount back to user
+	err = k.bankKeeper.SendCoins(ctx, bech32ZoneAddress, sender, sdk.NewCoins(inCoin))
+	if err != nil {
+		k.Logger(ctx).Error("Failed to send sdk.NewCoins(inCoins) from account to module")
+		return nil, sdkerrors.Wrapf(types.ErrInsufficientFunds, "couldn't send %v derivative %s tokens from module account. err: %s", inCoin.Amount, inCoin.Denom, err.Error())
+	}
+
+	// Escrow user's balance
+	redeemCoin := sdk.NewCoins(sdk.NewCoin(stDenom, msg.Amount))
+	err = k.bankKeeper.SendCoins(ctx, sender, bech32ZoneAddress, redeemCoin)
+	if err != nil {
+		k.Logger(ctx).Error("Failed to send sdk.NewCoins(inCoins) from account to module")
+		return nil, sdkerrors.Wrapf(types.ErrInsufficientFunds, "couldn't send %v derivative %s tokens to module account. err: %s", msg.Amount, hostZone.HostDenom, err.Error())
+	}
+
+	k.Logger(ctx).Info(fmt.Sprintf("executed fast unbond: %s", msg.String()))
+	return &types.MsgFastUnbondResponse{}, nil
+}
---
+++ x/stakeibc/keeper/msg_server_fast_unbond_test.go
@@ -0,0 +1,175 @@
+package keeper_test
+
+import (
+	sdkmath "cosmossdk.io/math"
+	"fmt"
+	epochtypes "github.com/Stride-Labs/stride/v4/x/epochs/types"
+	recordtypes "github.com/Stride-Labs/stride/v4/x/records/types"
+	stakeibctypes "github.com/Stride-Labs/stride/v4/x/stakeibc/types"
+	sdk "github.com/cosmos/cosmos-sdk/types"
+	_ "github.com/stretchr/testify/suite"
+)
+
+type FastUnbondState struct {
+	epochNumber         uint64
+	depositRecordAmount sdkmath.Int
+	hostZone            stakeibctypes.HostZone
+}
+
+type FastUnbondTestCase struct {
+	user         Account
+	zoneAccount  Account
+	initialState FastUnbondState
+	validMsg     stakeibctypes.MsgFastUnbond
+}
+
+func (s *KeeperTestSuite) SetupFastUnbond() FastUnbondTestCase {
+	stakeAmount := sdkmath.NewInt(1_000_000)
+	initialDepositAmount := sdkmath.NewInt(1_000_000)
+	user := Account{
+		acc:           s.TestAccs[0],
+		atomBalance:   sdk.NewInt64Coin(IbcAtom, 1_000_000),
+		stAtomBalance: sdk.NewInt64Coin(StAtom, 1_000_000),
+	}
+	s.FundAccount(user.acc, user.atomBalance)
+	s.FundAccount(user.acc, user.stAtomBalance)
+
+	zoneAddress := stakeibctypes.NewZoneAddress(HostChainId)
+
+	zoneAccount := Account{
+		acc:           zoneAddress,
+		atomBalance:   sdk.NewInt64Coin(IbcAtom, 10_000_000),
+		stAtomBalance: sdk.NewInt64Coin(StAtom, 10_000_000),
+	}
+	s.FundAccount(zoneAccount.acc, zoneAccount.atomBalance)
+	s.FundAccount(zoneAccount.acc, zoneAccount.stAtomBalance)
+
+	hostZone := stakeibctypes.HostZone{
+		ChainId:        HostChainId,
+		HostDenom:      Atom,
+		IbcDenom:       IbcAtom,
+		RedemptionRate: sdk.NewDec(1.0),
+		Address:        zoneAddress.String(),
+		StakedBal:      stakeAmount,
+	}
+
+	epochTracker := stakeibctypes.EpochTracker{
+		EpochIdentifier: epochtypes.STRIDE_EPOCH,
+		EpochNumber:     1,
+	}
+
+	initialDepositRecord := recordtypes.DepositRecord{
+		Id:                 1,
+		DepositEpochNumber: 1,
+		HostZoneId:         "GAIA",
+		Amount:             initialDepositAmount,
+	}
+
+	s.App.StakeibcKeeper.SetHostZone(s.Ctx, hostZone)
+	s.App.StakeibcKeeper.SetEpochTracker(s.Ctx, epochTracker)
+	s.App.RecordsKeeper.SetDepositRecord(s.Ctx, initialDepositRecord)
+
+	return FastUnbondTestCase{
+		user:        user,
+		zoneAccount: zoneAccount,
+		initialState: FastUnbondState{
+			epochNumber:         epochTracker.EpochNumber,
+			depositRecordAmount: initialDepositAmount,
+			hostZone:            hostZone,
+		},
+		validMsg: stakeibctypes.MsgFastUnbond{
+			Creator:  user.acc.String(),
+			HostZone: HostChainId,
+			Amount:   stakeAmount,
+		},
+	}
+}
+
+func (s *KeeperTestSuite) TestFastUnbond_Successful() {
+	tc := s.SetupFastUnbond()
+	msg := tc.validMsg
+
+	// Validate Fast Unbonding
+	_, err := s.GetMsgServer().FastUnbond(sdk.WrapSDKContext(s.Ctx), &msg)
+	s.Require().NoError(err)
+
+	// TODO: Check math
+}
+
+func (s *KeeperTestSuite) TestFastUnbond_InvalidCreatorAddress() {
+	tc := s.SetupFastUnbond()
+	invalidMsg := tc.validMsg
+
+	// cosmos instead of stride address
+	invalidMsg.Creator = "cosmos1g6qdx6kdhpf000afvvpte7hp0vnpzapuyxp8uf"
+	_, err := s.GetMsgServer().FastUnbond(sdk.WrapSDKContext(s.Ctx), &invalidMsg)
+	s.Require().EqualError(err, fmt.Sprintf("creator address is invalid: %s. err: invalid Bech32 prefix; expected stride, got cosmos: invalid address", invalidMsg.Creator))
+
+	// invalid stride address
+	invalidMsg.Creator = "stride1g6qdx6kdhpf000afvvpte7hp0vnpzapuyxp8uf"
+	_, err = s.GetMsgServer().FastUnbond(sdk.WrapSDKContext(s.Ctx), &invalidMsg)
+	s.Require().EqualError(err, fmt.Sprintf("creator address is invalid: %s. err: decoding bech32 failed: invalid checksum (expected 8dpmg9 got yxp8uf): invalid address", invalidMsg.Creator))
+
+	// empty address
+	invalidMsg.Creator = ""
+	_, err = s.GetMsgServer().FastUnbond(sdk.WrapSDKContext(s.Ctx), &invalidMsg)
+	s.Require().EqualError(err, fmt.Sprintf("creator address is invalid: %s. err: empty address string is not allowed: invalid address", invalidMsg.Creator))
+
+	// wrong len address
+	invalidMsg.Creator = "stride1g6qdx6kdhpf000afvvpte7hp0vnpzapuyxp8ufabc"
+	_, err = s.GetMsgServer().FastUnbond(sdk.WrapSDKContext(s.Ctx), &invalidMsg)
+	s.Require().EqualError(err, fmt.Sprintf("creator address is invalid: %s. err: decoding bech32 failed: invalid character not part of charset: 98: invalid address", invalidMsg.Creator))
+}
+
+func (s *KeeperTestSuite) TestFastUnbond_HostZoneNotFound() {
+	tc := s.SetupFastUnbond()
+
+	invalidMsg := tc.validMsg
+	invalidMsg.HostZone = "fake_host_zone"
+	_, err := s.GetMsgServer().FastUnbond(sdk.WrapSDKContext(s.Ctx), &invalidMsg)
+
+	s.Require().EqualError(err, "host zone is invalid: fake_host_zone: host zone not registered")
+}
+
+func (s *KeeperTestSuite) TestFastUnbond_RateAboveMaxThreshold() {
+	tc := s.SetupFastUnbond()
+
+	hz := tc.initialState.hostZone
+	hz.RedemptionRate = sdk.NewDec(100)
+	s.App.StakeibcKeeper.SetHostZone(s.Ctx, hz)
+
+	_, err := s.GetMsgServer().FastUnbond(sdk.WrapSDKContext(s.Ctx), &tc.validMsg)
+	s.Require().Error(err)
+}
+
+func (s *KeeperTestSuite) TestFastUnbond_RedeemMoreThanStaked() {
+	tc := s.SetupFastUnbond()
+
+	invalidMsg := tc.validMsg
+	invalidMsg.Amount = sdkmath.NewInt(1_000_000_000_000_000)
+	_, err := s.GetMsgServer().FastUnbond(sdk.WrapSDKContext(s.Ctx), &invalidMsg)
+
+	s.Require().EqualError(err, fmt.Sprintf("cannot unstake an amount g.t. staked balance on host zone: %v: invalid amount", invalidMsg.Amount))
+}
+
+func (s *KeeperTestSuite) TestFastUnbond_NoEpochTrackerDay() {
+	tc := s.SetupFastUnbond()
+
+	invalidMsg := tc.validMsg
+	s.App.StakeibcKeeper.RemoveEpochTracker(s.Ctx, epochtypes.STRIDE_EPOCH)
+	_, err := s.GetMsgServer().FastUnbond(sdk.WrapSDKContext(s.Ctx), &invalidMsg)
+
+	s.Require().EqualError(err, fmt.Sprintf("no epoch number for epoch (%s): not found", epochtypes.STRIDE_EPOCH))
+}
+
+func (s *KeeperTestSuite) TestFastUnbond_InvalidHostAddress() {
+	tc := s.SetupFastUnbond()
+
+	// Update hostzone with invalid address
+	badHostZone, _ := s.App.StakeibcKeeper.GetHostZone(s.Ctx, tc.validMsg.HostZone)
+	badHostZone.Address = "cosmosXXX"
+	s.App.StakeibcKeeper.SetHostZone(s.Ctx, badHostZone)
+
+	_, err := s.GetMsgServer().FastUnbond(sdk.WrapSDKContext(s.Ctx), &tc.validMsg)
+	s.Require().EqualError(err, "could not bech32 decode address cosmosXXX of zone with id: GAIA")
+}
