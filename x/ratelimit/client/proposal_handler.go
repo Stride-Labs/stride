3a69e4520a56bbfab181e84c84f062b0b1dd67a8
sampocs <sam.pochyly@gmail.com>
2023-01-23 11:37:50 -0600

    removed rest client

--- x/ratelimit/client/proposal_handler.go
+++ x/ratelimit/client/proposal_handler.go
@@ -2,14 +2,13 @@ package client
 
 import (
 	"github.com/Stride-Labs/stride/v5/x/ratelimit/client/cli"
-	"github.com/Stride-Labs/stride/v5/x/ratelimit/client/rest"
 
 	govclient "github.com/cosmos/cosmos-sdk/x/gov/client"
 )
 
 var (
-	AddRateLimitProposalHandler    = govclient.NewProposalHandler(cli.CmdAddRateLimitProposal, rest.ProposalAddRateLimitRESTHandler)
-	UpdateRateLimitProposalHandler = govclient.NewProposalHandler(cli.CmdUpdateRateLimitProposal, rest.ProposalUpdateRateLimitRESTHandler)
-	RemoveRateLimitProposalHandler = govclient.NewProposalHandler(cli.CmdRemoveRateLimitProposal, rest.ProposalRemoveRateLimitRESTHandler)
-	ResetRateLimitProposalHandler  = govclient.NewProposalHandler(cli.CmdResetRateLimitProposal, rest.ProposalResetRateLimitRESTHandler)
+	AddRateLimitProposalHandler    = govclient.NewProposalHandler(cli.CmdAddRateLimitProposal)
+	UpdateRateLimitProposalHandler = govclient.NewProposalHandler(cli.CmdUpdateRateLimitProposal)
+	RemoveRateLimitProposalHandler = govclient.NewProposalHandler(cli.CmdRemoveRateLimitProposal)
+	ResetRateLimitProposalHandler  = govclient.NewProposalHandler(cli.CmdResetRateLimitProposal)
 )
