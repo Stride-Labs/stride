3d590dc8f51bfebb8d64ed9a3e3389e69c293b1c
sampocs <sam.pochyly@gmail.com>
2023-01-26 18:25:24 -0600

    fixed bug with log

--- x/records/keeper/epoch_unbonding_record.go
+++ x/records/keeper/epoch_unbonding_record.go
@@ -161,8 +161,8 @@ func (k Keeper) ResetEpochUnbondingRecordEpochNumbers(ctx sdk.Context) error {
 		epochNumber := binary.BigEndian.Uint64(epochNumberBz)
 		epochUnbondingRecord.EpochNumber = epochNumber
 
-		ctx.Logger().Info("Updating EpochUnbondingRecord EpochNumber - Old Key: %v, New Key: %v, Epoch Number: %d",
-			epochNumberBz, GetEpochUnbondingRecordIDBytes(epochUnbondingRecord.EpochNumber), epochNumber)
+		ctx.Logger().Info(fmt.Sprintf("Updating EpochUnbondingRecord EpochNumber - Old Key: %v, New Key: %v, Epoch Number: %d",
+			epochNumberBz, GetEpochUnbondingRecordIDBytes(epochUnbondingRecord.EpochNumber), epochNumber))
 
 		k.SetEpochUnbondingRecord(ctx, epochUnbondingRecord)
 	}
