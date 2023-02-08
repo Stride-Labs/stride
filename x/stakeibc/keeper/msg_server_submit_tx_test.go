78dd6c5dc175bb59783dc6bbfacae013299a339c
ThanhNhann <thanhnhan98qh@gmail.com>
2023-02-08 14:17:47 +0700

    add more test cases for UpdateWithdrawalBalance function

--- x/stakeibc/keeper/msg_server_submit_tx_test.go
+++ x/stakeibc/keeper/msg_server_submit_tx_test.go
@@ -188,6 +188,25 @@ func (s *KeeperTestSuite) TestDelegateOnHost_FailedToGetICATimeoutNanos() {
 	s.Require().Error(err)
 }
 
+func (s *KeeperTestSuite) TestDelegateOnHost_NoWithdrawalAccountFound() {
+	tc := s.SetupSubmitTx_emptyStrideEpoch()
+	tc.hostZone.WithdrawalAccount.Address = ""
+	s.App.StakeibcKeeper.SetHostZone(s.Ctx, tc.hostZone)
+	err := s.App.StakeibcKeeper.UpdateWithdrawalBalance(s.Ctx, tc.hostZone)
+	expectedErr := fmt.Sprintf("no withdrawal account found for %s: ICA acccount not found on host zone", tc.hostZone.ChainId)
+	s.EqualError(err, expectedErr)
+}
+
+func (s *KeeperTestSuite) TestDelegateOnHost_InvalidWithdrawalAccountAddress() {
+	tc := s.SetupSubmitTx_emptyStrideEpoch()
+	tc.hostZone.WithdrawalAccount.Address = "sTrIdE"
+	s.App.StakeibcKeeper.SetHostZone(s.Ctx, tc.hostZone)
+	err := s.App.StakeibcKeeper.UpdateWithdrawalBalance(s.Ctx, tc.hostZone)
+	expectedErr := fmt.Sprintf("invalid withdrawal account address, could not decode (decoding bech32 failed: invalid bech32 string length %s): invalid request", fmt.Sprint(len(tc.hostZone.WithdrawalAccount.Address)))
+	s.Require().Error(err)
+	s.EqualError(err, expectedErr)
+}
+
 func (s *KeeperTestSuite) TestUpdateWithdrawalBalance_successful() {
 	tc := s.SetupSubmitTx()
 	err := s.App.StakeibcKeeper.UpdateWithdrawalBalance(s.Ctx, tc.hostZone)
