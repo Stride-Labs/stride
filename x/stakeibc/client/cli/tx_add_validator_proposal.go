0d506c32c1152d1ae7ff89bf3383e7de5ab59d12
sampocs <sam.pochyly@gmail.com>
2023-02-09 11:34:11 -0600

    sdkerrors -> errorsmod

--- x/stakeibc/client/cli/tx_add_validator_proposal.go
+++ x/stakeibc/client/cli/tx_add_validator_proposal.go
@@ -10,6 +10,8 @@ import (
 	"github.com/cosmos/cosmos-sdk/client"
 	"github.com/cosmos/cosmos-sdk/client/tx"
 	sdk "github.com/cosmos/cosmos-sdk/types"
+
+	errorsmod "cosmossdk.io/errors"
 	sdkerrors "github.com/cosmos/cosmos-sdk/types/errors"
 	"github.com/cosmos/cosmos-sdk/version"
 	govcli "github.com/cosmos/cosmos-sdk/x/gov/client/cli"
@@ -94,7 +96,7 @@ Where proposal.json contains:
 			}
 
 			if len(deposit) != 1 || deposit.GetDenomByIndex(0) != strideDenom {
-				return sdkerrors.Wrapf(sdkerrors.ErrInvalidCoins, "Deposit token denom must be %s", strideDenom)
+				return errorsmod.Wrapf(sdkerrors.ErrInvalidCoins, "Deposit token denom must be %s", strideDenom)
 			}
 
 			msg, err := govtypes.NewMsgSubmitProposal(&proposal, deposit, from)
