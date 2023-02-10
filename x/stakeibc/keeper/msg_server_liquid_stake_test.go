afb2d50fb361eca191e847d7b368ed1bf09d7d31
jstride <jun@stridelabs.co>
2023-02-10 15:38:22 +0800

    add test for liquid stake and redeem stake for halted zone

--- x/stakeibc/keeper/msg_server_liquid_stake_test.go
+++ x/stakeibc/keeper/msg_server_liquid_stake_test.go
@@ -246,3 +246,15 @@ func (s *KeeperTestSuite) TestLiquidStake_InvalidHostAddress() {
 	_, err := s.GetMsgServer().LiquidStake(sdk.WrapSDKContext(s.Ctx), &tc.validMsg)
 	s.Require().EqualError(err, "could not bech32 decode address cosmosXXX of zone with id: GAIA")
 }
+
+func (s *KeeperTestSuite) TestLiquidStake_HaltedZone() {
+	tc := s.SetupLiquidStake()
+	haltedHostZone := tc.initialState.hostZone
+	haltedHostZone.Halted = true
+	s.App.StakeibcKeeper.SetHostZone(s.Ctx, haltedHostZone)
+	// Fund the user with the non-ibc denom
+	s.FundAccount(tc.user.acc, sdk.NewInt64Coin(haltedHostZone.IbcDenom, 1000000000))
+	_, err := s.GetMsgServer().LiquidStake(sdk.WrapSDKContext(s.Ctx), &tc.validMsg)
+
+	s.Require().EqualError(err, fmt.Sprintf("halted host zone found for denom (%s): Halted host zone found", haltedHostZone.HostDenom))
+}
