6c5c463e069ffaafe1f4f863ba13eaa690c022be
jstride <jun@stridelabs.co>
2023-02-13 21:25:40 +0800

    add test for IsRedemptionRateWithinSafetyBounds

--- x/stakeibc/keeper/keeper.go
+++ x/stakeibc/keeper/keeper.go
@@ -260,11 +260,20 @@ func (k Keeper) GetICATimeoutNanos(ctx sdk.Context, epochType string) (uint64, e
 
 // safety check: ensure the redemption rate is NOT below our min safety threshold && NOT above our max safety threshold on host zone
 func (k Keeper) IsRedemptionRateWithinSafetyBounds(ctx sdk.Context, zone types.HostZone) (bool, error) {
+	params := k.GetParams(ctx)
 	minSafetyThresholdInt := k.GetParam(ctx, types.KeySafetyMinRedemptionRateThreshold)
 	minSafetyThreshold := sdk.NewDec(int64(minSafetyThresholdInt)).Quo(sdk.NewDec(100))
 
+	if rate, ok := params.MinRedemptionRates[zone.ChainId]; ok {
+		minSafetyThreshold = sdk.MustNewDecFromStr(rate)
+	}
+
 	maxSafetyThresholdInt := k.GetParam(ctx, types.KeySafetyMaxRedemptionRateThreshold)
 	maxSafetyThreshold := sdk.NewDec(int64(maxSafetyThresholdInt)).Quo(sdk.NewDec(100))
+
+	if rate, ok := params.MaxRedemptionRates[zone.ChainId]; ok {
+		maxSafetyThreshold = sdk.MustNewDecFromStr(rate)
+	}
 
 	redemptionRate := zone.RedemptionRate
 
--- x/stakeibc/keeper/keeper_test.go
+++ x/stakeibc/keeper/keeper_test.go
@@ -3,6 +3,7 @@ package keeper_test
 import (
 	"testing"
 
+	sdk "github.com/cosmos/cosmos-sdk/types"
 	"github.com/stretchr/testify/suite"
 
 	"github.com/Stride-Labs/stride/v5/app/apptesting"
@@ -43,4 +44,29 @@ func (s *KeeperTestSuite) GetMsgServer() types.MsgServer {
 
 func TestKeeperTestSuite(t *testing.T) {
 	suite.Run(t, new(KeeperTestSuite))
+}
+
+func (s *KeeperTestSuite) TestIsRedemptionRateWithinSafetyBounds() {
+	params := s.App.StakeibcKeeper.GetParams(s.Ctx)
+	params.MinRedemptionRates["osmosis-1"] = "0.3"
+	params.MaxRedemptionRates["osmosis-1"] = "2.0"
+	s.App.StakeibcKeeper.SetParams(s.Ctx, params)
+
+	zone := types.HostZone{
+		ChainId:        "osmosis-1",
+		RedemptionRate: sdk.NewDec(1),
+	}
+	rrSafe, err := s.App.StakeibcKeeper.IsRedemptionRateWithinSafetyBounds(s.Ctx, zone)
+	s.Require().NoError(err)
+	s.Require().True(rrSafe)
+
+	zone.RedemptionRate = sdk.NewDecWithPrec(16, 1) // 1.6
+	rrSafe, err = s.App.StakeibcKeeper.IsRedemptionRateWithinSafetyBounds(s.Ctx, zone)
+	s.Require().NoError(err)
+	s.Require().True(rrSafe)
+
+	zone.RedemptionRate = sdk.NewDecWithPrec(21, 1) // 2.1
+	rrSafe, err = s.App.StakeibcKeeper.IsRedemptionRateWithinSafetyBounds(s.Ctx, zone)
+	s.Require().Error(err)
+	s.Require().False(rrSafe)
 }
