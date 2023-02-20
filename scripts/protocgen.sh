be3363590b32ecc5fe20b283d9ed790c3cff2838
sampocs <sam.pochyly@gmail.com>
2023-02-20 09:58:04 -0600

    updated protos and moved cosmwasm protos to stride

--- scripts/protocgen.sh
+++ scripts/protocgen.sh
@@ -10,19 +10,7 @@ go get github.com/regen-network/cosmos-proto/protoc-gen-gocosmos 2>/dev/null
 
 echo "Generating gogo proto code"
 cd proto
-
-# Generate stride protos
 proto_dirs=$(find ./stride -path -prune -o -name '*.proto' -print0 | xargs -0 -n1 dirname | sort | uniq)
-for dir in $proto_dirs; do
-  for file in $(find "${dir}" -maxdepth 1 -name '*.proto'); do
-    if grep go_package "$file" &>/dev/null; then
-      buf generate --template buf.gen.gogo.yaml "$file"
-    fi
-  done
-done
-
-# Generate cosmwasm protos
-proto_dirs=$(find ./cosmwasm -path -prune -o -name '*.proto' -print0 | xargs -0 -n1 dirname | sort | uniq)
 for dir in $proto_dirs; do
   for file in $(find "${dir}" -maxdepth 1 -name '*.proto'); do
     if grep go_package "$file" &>/dev/null; then
