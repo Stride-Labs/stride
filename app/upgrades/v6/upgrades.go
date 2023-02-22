d64ff9c7a77f05020e4c8562c7900a9a9b14dd6e
vish-stride <vishal@stridelabs.co>
2023-02-21 19:45:48 -0500

    update upgrade to catch errors

--- app/upgrades/v6/upgrades.go
+++ app/upgrades/v6/upgrades.go
@@ -3,6 +3,7 @@ package v6
 import (
 	"github.com/cosmos/cosmos-sdk/codec"
 	sdk "github.com/cosmos/cosmos-sdk/types"
+	sdkerrors "github.com/cosmos/cosmos-sdk/types/errors"
 	"github.com/cosmos/cosmos-sdk/types/module"
 	upgradetypes "github.com/cosmos/cosmos-sdk/x/upgrade/types"
 
@@ -23,12 +24,21 @@ func CreateUpgradeHandler(
 ) upgradetypes.UpgradeHandler {
 	return func(ctx sdk.Context, _ upgradetypes.Plan, vm module.VersionMap) (module.VersionMap, error) {
 		// Reset Claims
-		claimKeeper.ResetClaimStatus(ctx, "gaia")
-		claimKeeper.ResetClaimStatus(ctx, "stride")
-		claimKeeper.ResetClaimStatus(ctx, "osmo")
-		claimKeeper.ResetClaimStatus(ctx, "juno")
-		claimKeeper.ResetClaimStatus(ctx, "stars")
-
+		if err := claimKeeper.ResetClaimStatus(ctx, "stride"); err != nil {
+			return vm, sdkerrors.Wrapf(err, "unable to reset stride claim status")
+		}
+		if err := claimKeeper.ResetClaimStatus(ctx, "gaia"); err != nil {
+			return vm, sdkerrors.Wrapf(err, "unable to reset gaia claim status")
+		}
+		if err := claimKeeper.ResetClaimStatus(ctx, "osmo"); err != nil {
+			return vm, sdkerrors.Wrapf(err, "unable to reset osmo claim status")
+		}
+		if err := claimKeeper.ResetClaimStatus(ctx, "juno"); err != nil {
+			return vm, sdkerrors.Wrapf(err, "unable to reset juno claim status")
+		}
+		if err := claimKeeper.ResetClaimStatus(ctx, "stars"); err != nil {
+			return vm, sdkerrors.Wrapf(err, "unable to reset stars claim status")
+		}
 		return mm.RunMigrations(ctx, configurator, vm)
 	}
 }
