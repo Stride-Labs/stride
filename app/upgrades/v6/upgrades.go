69380fbee1c21ce049f62e32cbb54a5f3f92de1b
sampocs <sam.pochyly@gmail.com>
2023-02-28 15:12:02 -0700

    fixed linting error

--- app/upgrades/v6/upgrades.go
+++ app/upgrades/v6/upgrades.go
@@ -3,9 +3,9 @@ package v6
 import (
 	"fmt"
 
+	errorsmod "cosmossdk.io/errors"
 	"github.com/cosmos/cosmos-sdk/codec"
 	sdk "github.com/cosmos/cosmos-sdk/types"
-	sdkerrors "github.com/cosmos/cosmos-sdk/types/errors"
 	"github.com/cosmos/cosmos-sdk/types/module"
 	upgradetypes "github.com/cosmos/cosmos-sdk/x/upgrade/types"
 
@@ -29,7 +29,7 @@ func CreateUpgradeHandler(
 		airdropClaimTypes := []string{"stride", "gaia", "osmosis", "juno", "stars"}
 		for _, claimType := range airdropClaimTypes {
 			if err := claimKeeper.ResetClaimStatus(ctx, claimType); err != nil {
-				return vm, sdkerrors.Wrapf(err, fmt.Sprintf("unable to reset %s claim status", claimType))
+				return vm, errorsmod.Wrapf(err, fmt.Sprintf("unable to reset %s claim status", claimType))
 			}
 		}
 		return mm.RunMigrations(ctx, configurator, vm)
