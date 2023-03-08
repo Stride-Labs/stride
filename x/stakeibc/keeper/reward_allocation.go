98f7eb91a75484e06452aa0273c4ac0cacf849ea
Riley Edmunds <riley@stridelabs.co>
2023-03-08 12:21:22 -0500

    mv helpers to own file

---
+++ x/stakeibc/keeper/reward_allocation.go
@@ -0,0 +1,81 @@
+package keeper
+
+import (
+	"fmt"
+
+	errorsmod "cosmossdk.io/errors"
+	sdk "github.com/cosmos/cosmos-sdk/types"
+
+	authtypes "github.com/cosmos/cosmos-sdk/x/auth/types"
+
+	"github.com/Stride-Labs/stride/v6/x/stakeibc/types"
+)
+
+// Liquid Stake Reward Collector Balance
+func (k Keeper) LiquidStakeRewardCollectorBalance(ctx sdk.Context, msgSvr types.MsgServer) bool {
+	k.Logger(ctx).Info("Liquid Staking reward collector balance")
+	rewardCollectorAddress := k.accountKeeper.GetModuleAccount(ctx, types.RewardCollectorName).GetAddress()
+	rewardedTokens := k.bankKeeper.GetAllBalances(ctx, rewardCollectorAddress)
+	if rewardedTokens.IsEqual(sdk.Coins{}) {
+		k.Logger(ctx).Info("No reward to allocate from RewardCollector")
+		return false
+	}
+
+	rewardsAccrued := false
+	for _, token := range rewardedTokens {
+		// get hostzone by reward token (in ibc denom format)
+		hz, err := k.GetHostZoneFromIBCDenom(ctx, token.Denom)
+		if err != nil {
+			k.Logger(ctx).Info("Token denom %s in module account is not from a supported host zone", token.Denom)
+			fmt.Println("nodenom")
+			continue
+		}
+
+		// liquid stake all tokens
+		msg := types.NewMsgLiquidStake(rewardCollectorAddress.String(), token.Amount, hz.HostDenom)
+		_, err = msgSvr.LiquidStake(ctx, msg)
+		if err != nil {
+			fmt.Println("noLS")
+			fmt.Println(err.Error())
+			k.Logger(ctx).Error("Can't liquid stake %s for hostzone %s", token.String(), hz.ChainId)
+			continue
+		}
+		k.Logger(ctx).Info(fmt.Sprintf("Liquid staked %s for hostzone %s's accrued rewards", token.String(), hz.ChainId))
+		rewardsAccrued = true
+	}
+	return rewardsAccrued
+}
+
+// Sweep stTokens from Reward Collector to Fee Collector
+func (k Keeper) SweepStTokensFromRewardCollToFeeColl(ctx sdk.Context) error {
+	// Send all stTokens to fee collector to distribute to delegator later
+	rewardCollectorAddress := k.accountKeeper.GetModuleAccount(ctx, types.RewardCollectorName).GetAddress()
+
+	rewardCollCoins := k.bankKeeper.GetAllBalances(ctx, rewardCollectorAddress)
+	k.Logger(ctx).Info(fmt.Sprintf("Reward collector has %s", rewardCollCoins.String()))
+	stTokens := sdk.NewCoins()
+	for _, token := range rewardCollCoins {
+		// get hostzone by reward token (in ibc denom format)
+		isStToken := k.CheckIsStToken(ctx, token.Denom)
+		k.Logger(ctx).Info(fmt.Sprintf("%s is stToken: %t", token.String(), isStToken))
+		if isStToken {
+			stTokens = append(stTokens, token)
+		}
+	}
+	k.Logger(ctx).Info(fmt.Sprintf("Sending %s stTokens from %s to %s", stTokens.String(), types.RewardCollectorName, authtypes.FeeCollectorName))
+
+	err := k.bankKeeper.SendCoinsFromModuleToModule(ctx, types.RewardCollectorName, authtypes.FeeCollectorName, stTokens)
+	if err != nil {
+		return errorsmod.Wrapf(err, fmt.Sprintf("Can't send coins from module %s to module %s, err %s", types.RewardCollectorName, authtypes.FeeCollectorName, err.Error()))
+	}
+	return nil
+}
+
+// (1) liquid stake reward collector balance, then (2) sweet stTokens from reward collector to fee collector
+func (k Keeper) AllocateHostZoneReward(ctx sdk.Context) error {
+	msgSvr := NewMsgServerImpl(k)
+	if rewardsFound := k.LiquidStakeRewardCollectorBalance(ctx, msgSvr); !rewardsFound {
+		return nil
+	}
+	return k.SweepStTokensFromRewardCollToFeeColl(ctx)
+}
