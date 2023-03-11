d87022fa3d39dd868c7746b0499742a258b05c90
sampocs <sam@stridelabs.co>
2023-03-10 18:23:56 -0600

    Code Cleanup: MsgLiquidStake (#651)
    Closes: #XXX
    
    ## Context and purpose of the change
    Cleaned up MsgLiquidStake
    
    ## Brief Changelog
    * Cleaned up variable naming, commenting, logging, and errors
    * Moved validation checks as upstream as possible so that they occur before any state changes (relevant to protocol directed liquid stakes)
    * Added filter to GetDepositRecordsByEpochAndChain to only grab transfer records
    * Added event emission after LS
    * Added check to error if the LS would result in 0 stTokens
    
    
    ## Author's Checklist
    
    I have...
    
    - [ ] Run and PASSED locally all GAIA integration tests
    - [ ] If the change is contentful, I either:
        - [ ] Added a new unit test OR 
        - [ ] Added test cases to existing unit tests
    - [ ] OR this change is a trivial rework / code cleanup without any test coverage
    
    If skipped any of the tests above, explain.
    
    
    ## Reviewers Checklist
    
    *All items are required. Please add a note if the item is not applicable and please add
    your handle next to the items reviewed if you only reviewed selected items.*
    
    I have...
    
    - [ ] reviewed state machine logic
    - [ ] reviewed API design and naming
    - [ ] manually tested (if applicable)
    - [ ] confirmed the author wrote unit tests for new logic
    - [ ] reviewed documentation exists and is accurate
    
    
    ## Documentation and Release Note
    
      - [ ] Does this pull request introduce a new feature or user-facing behavior changes? 
      - [ ] Is a relevant changelog entry added to the `Unreleased` section in `CHANGELOG.md`?
      - [ ] This pull request updates existing proto field values (and require a backend and frontend migration)? 
      - [ ] Does this pull request change existing proto field names (and require a frontend migration)?
      How is the feature or change documented? 
          - [ ] not applicable
          - [ ] jira ticket `XXX` 
          - [ ] specification (`x/<module>/spec/`) 
          - [ ] README.md 
          - [ ] not documented

--- x/stakeibc/types/events.go
+++ x/stakeibc/types/events.go
@@ -1,6 +1,6 @@
 package types
 
-// IBC events
+// Events
 const (
 	EventTypeTimeout = "timeout"
 	// this line is used by starport scaffolding # ibc/packet/event
@@ -16,14 +16,21 @@ const (
 	EventTypeLiquidStakeRequest = "liquid_stake"
 	EventTypeHostZoneHalt       = "halt_zone"
 
+	AttributeKeyHostZone         = "host_zone"
 	AttributeKeyConnectionId     = "connection_id"
 	AttributeKeyRecipientChain   = "chain_id"
 	AttributeKeyRecipientAddress = "recipient"
 	AttributeKeyBurnAmount       = "burn_amount"
 	AttributeKeyRedeemAmount     = "redeem_amount"
 	AttributeKeySourceAddress    = "source"
-	AttributeKeyHostZone         = "host_zone"
-	AttributeKeyRedemptionRate   = "redemption_rate"
+
+	AttributeKeyRedemptionRate = "redemption_rate"
+
+	AttributeKeyLiquidStaker    = "liquid_staker"
+	AttributeKeyNativeBaseDenom = "native_base_denom"
+	AttributeKeyNativeIBCDenom  = "native_ibc_denom"
+	AttributeKeyNativeAmount    = "native_amount"
+	AttributeKeyStTokenAmount   = "sttoken_amount"
 
 	AttributeValueCategory = ModuleName
 )
