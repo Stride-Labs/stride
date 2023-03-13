e0313ad1f51f46e8df2869dfd5b90a0809d3cfa8
Riley Edmunds <riley@stridelabs.co>
2023-03-13 19:45:19 -0400

    add sei test case

--- dockernet/scripts/test_sei_case.sh
+++ dockernet/scripts/test_sei_case.sh
@@ -6,7 +6,7 @@ source ${SCRIPT_DIR}/../config.sh
 $OSMO_MAIN_CMD tx ibc-transfer transfer transfer channel-0 $(STRIDE_ADDRESS) 1000000uosmo --from ${OSMO_VAL_PREFIX}1 -y 
 sleep 3
 ## LIQUID STAKE IT
-$STRIDE_MAIN_CMD tx stakeibc liquid-stake 10000 $OSMO_DENOM --from sval1_test -y --keyring-backend os
+$STRIDE_MAIN_CMD tx stakeibc liquid-stake 50000 $OSMO_DENOM --from sval1_test -y --keyring-backend os
 sleep 3
 ## CHECK WE GOT stOSMO
 $STRIDE_MAIN_CMD q bank balances stride1uk4ze0x4nvh4fk0xm4jdud58eqn4yxhrt52vv7
