10dea809e6fe8195e81821b6f96ead0884c3f71a
vish-stride <vishal@stridelabs.co>
2023-03-23 16:33:30 -0400

    sample evmos data

--- app/upgrades/v8/allocations.go
+++ app/upgrades/v8/allocations.go
@@ -1,3 +1,6 @@
 package v8
 
-var allocations = `TODO`
+var allocations = `identifier,address,weight
+evmos,stride1a784enjjr2lgwe3uqzj857l4f8xh9fh6hl4r8d,32828
+evmos,stride14egt7yngswlq640pxqkuk40rwxkfqneyzhlaxh,31630'
+`
