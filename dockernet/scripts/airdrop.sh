9406c2a97cbb2550065fb72dd2490e10b07cf38f
Riley Edmunds <riley@stridelabs.co>
2023-03-24 15:15:59 -0400

    airdrop testing script docs

--- dockernet/scripts/airdrop.sh
+++ dockernet/scripts/airdrop.sh
@@ -2,6 +2,10 @@
 SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
 source ${SCRIPT_DIR}/../config.sh
 
+# CLEANUP if running tests twice, clear out and re-fund accounts
+# $STRIDE_MAIN_CMD keys delete distributor-test -y
+# $STRIDE_MAIN_CMD keys delete airdrop-test -y
+
 # First, start the network with `make start-docker`
 # Then, run this script with `bash dockernet/scripts/airdrop.sh`
 
@@ -41,19 +45,72 @@ $STRIDE_MAIN_CMD query bank balances stride1nf6v2paty9m22l3ecm7dpakq2c92ueyununa
 # NOTE: You can claim here using the CLI, or from the frontend!
 # Claim 20% of the free tokens
 echo "Claiming fee amount..."
-$STRIDE_MAIN_CMD tx claim claim-free-amount --from airdrop-test --gas 400000
+$STRIDE_MAIN_CMD tx claim claim-free-amount --from airdrop-test --gas 400000 -y
 sleep 5
 echo "Balance after claim:" 
 $STRIDE_MAIN_CMD query bank balances stride1nf6v2paty9m22l3ecm7dpakq2c92ueyununayr
 # Stake, to claim another 20%
 echo "Staking..."
-$STRIDE_MAIN_CMD tx staking delegate stridevaloper1nnurja9zt97huqvsfuartetyjx63tc5zrj5x9f 100ustrd --from airdrop-test --gas 400000
+$STRIDE_MAIN_CMD tx staking delegate stridevaloper1nnurja9zt97huqvsfuartetyjx63tc5zrj5x9f 100ustrd --from airdrop-test --gas 400000 -y
 sleep 5
 echo "Balance after stake:" 
 $STRIDE_MAIN_CMD query bank balances stride1nf6v2paty9m22l3ecm7dpakq2c92ueyununayr
 # Liquid stake, to claim the final 60% of tokens
 echo "Liquid staking..."
-$STRIDE_MAIN_CMD tx stakeibc liquid-stake 1000 uatom --from airdrop-test --gas 400000
+$STRIDE_MAIN_CMD tx stakeibc liquid-stake 1000 uatom --from airdrop-test --gas 400000 -y
 sleep 5
 echo "Balance after liquid stake:" 
 $STRIDE_MAIN_CMD query bank balances stride1nf6v2paty9m22l3ecm7dpakq2c92ueyununayr
+
+
+
+### Test airdrop flow for chains who have non-standard coin types (not type 118). 
+    #   For example Evmos is using coin type 60, while Stride uses 118. Therefore, we can't map Evmos <> Stride addresses, because the one-way mapping works like this
+    #       seed phrase  ----> Evmos address (e.g. evmos123z469cfejeusvk87ufrs5520wmdxmmlc7qzuw)
+    #                    ----> Stride address (e.g. stride19uvw0azm9u0k6vqe4e22cga6kteskdqq3ulj6q)
+    #       and there is no function that can map between the two addresses.
+
+
+    # 1. Overwrite incorrectly-derived stride address associated with an airdrop account with the proper Stride address (e.g. stride1abc...xyz)
+    #     a. query the claims module to get the airdrop-eligible address
+    #     b. ibc-transfer from Evmos to Stride to change the airdrop account
+    #          Memo: {
+    #            "autopilot": {
+    #                 "stakeibc": {
+    #                   "stride_address": "stride1abc...xyz",
+    #                   },
+    #                     "claim": {
+    #                     }
+    #                },
+    #            }
+    #           Receiver: "xxx"
+    #     c. query the claims module to get the updated airdrop-eligible address
+    #     d. claim the airdrop from this address
+    #     e. verify the vesting account is created 
+
+
+### Test airdrop reset and multiple claims flow
+    #   The Stride airdrop occurs in batches. We need to test three batches. 
+
+    # SETUP
+    # 1. Create a new airdrop that rolls into its next batch in just 30 seconds
+    #    - include the add'l param that makes each batch 30 seconds long (after the first batch) 
+    # 2. Set the airdrop allocations
+
+    # BATCH 1
+    # 3. Claim the airdrop
+    # 4. Check that the claim worked
+    # 5. Query to check airdrop vesting account was created (w/ correct amount)
+
+    # BATCH 2
+    # 6. Wait 30 seconds
+    # 7. Claim the airdrop
+    # 8. Check that the claim worked
+    # 9. Query to check airdrop vesting account was created (w/ correct amount)
+
+    # BATCH 3
+    # 6. Wait 30 seconds
+    # 7. Claim the airdrop
+    # 8. Check that the claim worked
+    # 9. Query to check airdrop vesting account was created (w/ correct amount)
+
