dde70902f05e291571bcb7597320c71c8cd0348f
sampocs <sam.pochyly@gmail.com>
2023-04-06 10:28:44 -0500

    fixed typo in lsm token deposit keeper functions

--- x/stakeibc/keeper/lsm_token_deposit.go
+++ x/stakeibc/keeper/lsm_token_deposit.go
@@ -24,28 +24,28 @@ func (k Keeper) GetAllLSMTokenDeposit(ctx sdk.Context) []types.LSMTokenDeposit {
 	return []types.LSMTokenDeposit{}
 }
 
-func (k Keeper) AddLSMTokenDeposit(ctx sdk.Context, deposit types.LSMDepositStatus) {
+func (k Keeper) AddLSMTokenDeposit(ctx sdk.Context, deposit types.LSMTokenDeposit) {
 	// TODO [LSM]
 	// See if a deposit already exists for this denom
 	// If so, increment the amount
 	// otherwise, create a new deposit
 }
 
-func (k Keeper) UpdateLSMTokenDepositStatus(ctx sdk.Context, deposit types.LSMDepositStatus, status types.LSMDepositStatus) {
+func (k Keeper) UpdateLSMTokenDepositStatus(ctx sdk.Context, deposit types.LSMTokenDeposit, status types.LSMDepositStatus) {
 	// TODO [LSM]
 }
 
-func (k Keeper) GetLSMDepositsForHostZone(ctx sdk.Context, deposit types.LSMDepositStatus, chainId string) []types.LSMDepositStatus {
+func (k Keeper) GetLSMDepositsForHostZone(ctx sdk.Context, deposit types.LSMTokenDeposit, chainId string) []types.LSMTokenDeposit {
 	// TODO [LSM]
-	return []types.LSMDepositStatus{}
+	return []types.LSMTokenDeposit{}
 }
 
 func (k Keeper) GetLSMDepositsForHostZoneWithStatus(
 	ctx sdk.Context,
-	deposit types.LSMDepositStatus,
+	deposit types.LSMTokenDeposit,
 	chainId string,
 	status types.LSMDepositStatus,
-) []types.LSMDepositStatus {
+) []types.LSMTokenDeposit {
 	// TODO [LSM]
-	return []types.LSMDepositStatus{}
+	return []types.LSMTokenDeposit{}
 }
