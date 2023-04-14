298962e8764a94689a44e607325c4a56a1c24916
sampocs <sam@stridelabs.co>
2023-04-13 20:56:16 -0500

    [LSM] Implement RefundLSMToken (#714)

--- x/stakeibc/keeper/lsm.go
+++ x/stakeibc/keeper/lsm.go
@@ -109,3 +109,22 @@ func (k Keeper) ShouldQueryValidatorExchangeRate(ctx sdk.Context, validator type
 	//     => OldProgress/Interval: 0, NewProgress/Interval: 1
 	return oldProgress.Quo(queryInterval).LT(newProgress.Quo(queryInterval))
 }
+
+// Helper function to Refund an LSM Token to the user if both:
+//  a) the LSMLiquidStake transaction is interrupted with a validator exchange rate query, and
+//  b) the handling of the query callback fails
+func (k Keeper) RefundLSMToken(ctx sdk.Context, lsmLiquidStake types.LSMLiquidStake) error {
+	liquidStakerAddress := lsmLiquidStake.Staker
+	hostZoneAddress, err := sdk.AccAddressFromBech32(lsmLiquidStake.HostZone.Address)
+	if err != nil {
+		return errorsmod.Wrapf(err, "host zone address is invalid")
+	}
+	refundToken := lsmLiquidStake.LSMIBCToken
+
+	if err := k.bankKeeper.SendCoins(ctx, hostZoneAddress, liquidStakerAddress, sdk.NewCoins(refundToken)); err != nil {
+		return errorsmod.Wrapf(err,
+			"failed to send tokens from Module Account (%s) to Staker (%s)", hostZoneAddress.String(), liquidStakerAddress.String())
+	}
+
+	return nil
+}
