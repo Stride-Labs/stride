7c04d3641faaef07d29763c4766fe15238e2be45
ethan <ethan@stridelabs.co>
2023-04-18 09:58:21 -0600

    removing basic type getters for direct attributes

--- x/stakeibc/keeper/grpc_query_lsm_deposits.go
+++ x/stakeibc/keeper/grpc_query_lsm_deposits.go
@@ -12,13 +12,13 @@ import (
 )
 
 func (k Keeper) LSMDeposit(c context.Context, req *types.QueryLSMDepositRequest) (*types.QueryLSMDepositResponse, error) {
-	if req == nil || req.GetChainId() == "" || req.GetDenom() == "" {
+	if req == nil || req.ChainId == "" || req.Denom == "" {
 		return nil, status.Error(codes.InvalidArgument, "invalid request")
 	}
 
 	ctx := sdk.UnwrapSDKContext(c)
 
-	deposit, found := k.GetLSMTokenDeposit(ctx, req.GetChainId(), req.GetDenom())
+	deposit, found := k.GetLSMTokenDeposit(ctx, req.ChainId, req.Denom)
 	if !found {
 		return nil, status.Error(codes.NotFound, "LSM deposit not found")
 	}
@@ -35,22 +35,22 @@ func (k Keeper) LSMDeposits(c context.Context, req *types.QueryLSMDepositsReques
 	ctx := sdk.UnwrapSDKContext(c)
 
 	// Case 1: no chain_id was given, so we should load all deposits across all chains
-	if req.GetChainId() == "" {
+	if req.ChainId == "" {
 		deposits = k.GetAllLSMTokenDeposit(ctx)
 	}
 
 	// Case 2: chain_id is given, load all for that chain
-	if req.GetChainId() != "" {
-		deposits = k.GetLSMDepositsForHostZone(ctx, req.GetChainId())
+	if req.ChainId != "" {
+		deposits = k.GetLSMDepositsForHostZone(ctx, req.ChainId)
 	}
 
 	// Filter for matches by hand if validator_address or status optional filters are given
 	filtered := []types.LSMTokenDeposit{}
-	filterByValidator := req.GetValidatorAddress() != ""
-	filterByStatus := req.GetStatus() != ""
+	filterByValidator := req.ValidatorAddress != ""
+	filterByStatus := req.Status != ""
 	for _, deposit := range deposits {
-		validatorMatch := !filterByValidator || (deposit.ValidatorAddress == req.GetValidatorAddress())
-		statusMatch := !filterByStatus || (deposit.Status.String() == req.GetStatus())
+		validatorMatch := !filterByValidator || (deposit.ValidatorAddress == req.ValidatorAddress)
+		statusMatch := !filterByStatus || (deposit.Status.String() == req.Status)
 		if validatorMatch && statusMatch {
 			filtered = append(filtered, deposit)
 		}
