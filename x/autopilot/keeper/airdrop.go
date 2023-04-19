e00a771a70e53f9bf9b0c9822d787494c074e7de
sampocs <sam.pochyly@gmail.com>
2023-04-19 01:13:43 -0500

    added additional safety check in airdrop autopilot

--- x/autopilot/keeper/airdrop.go
+++ x/autopilot/keeper/airdrop.go
@@ -9,19 +9,31 @@ import (
 	sdk "github.com/cosmos/cosmos-sdk/types"
 	sdkerrors "github.com/cosmos/cosmos-sdk/types/errors"
 	transfertypes "github.com/cosmos/ibc-go/v5/modules/apps/transfer/types"
+	channeltypes "github.com/cosmos/ibc-go/v5/modules/core/04-channel/types"
 
 	"github.com/Stride-Labs/stride/v8/utils"
 	"github.com/Stride-Labs/stride/v8/x/autopilot/types"
+	stakeibctypes "github.com/Stride-Labs/stride/v8/x/stakeibc/types"
 )
 
 func (k Keeper) TryUpdateAirdropClaim(
 	ctx sdk.Context,
+	packet channeltypes.Packet,
 	data transfertypes.FungibleTokenPacketData,
 	packetMetadata types.ClaimPacketMetadata,
 ) error {
 	params := k.GetParams(ctx)
 	if !params.ClaimActive {
 		return errors.New("packet forwarding param is not active")
+	}
+
+	// verify packet originated on a registered host zone
+	if packet.GetDestPort() != transfertypes.PortID {
+		return errors.New("packet should ")
+	}
+	if _, found := k.stakeibcKeeper.GetHostZoneFromTransferChannelID(ctx, packet.GetDestChannel()); !found {
+		return errorsmod.Wrapf(stakeibctypes.ErrHostZoneNotFound,
+			"host zone not found for transfer channel %s", packet.GetDestChannel())
 	}
 
 	// grab relevant addresses
