62f5746762b5ee6fca0f2424198ecf7914780d85
sampocs <sam.pochyly@gmail.com>
2023-04-21 17:43:53 -0500

    v8 -> v9

--- x/autopilot/types/genesis_test.go
+++ x/autopilot/types/genesis_test.go
@@ -5,7 +5,7 @@ import (
 
 	"github.com/stretchr/testify/require"
 
-	"github.com/Stride-Labs/stride/v8/x/autopilot/types"
+	"github.com/Stride-Labs/stride/v9/x/autopilot/types"
 )
 
 func TestGenesisState_Validate(t *testing.T) {
--- x/stakeibc/keeper/msg_server_redeem_stake.go
+++ x/stakeibc/keeper/msg_server_redeem_stake.go
@@ -4,14 +4,14 @@ import (
 	"context"
 	"fmt"
 
-	recordstypes "github.com/Stride-Labs/stride/v8/x/records/types"
-	"github.com/Stride-Labs/stride/v8/x/stakeibc/types"
+	recordstypes "github.com/Stride-Labs/stride/v9/x/records/types"
+	"github.com/Stride-Labs/stride/v9/x/stakeibc/types"
 
 	errorsmod "cosmossdk.io/errors"
 	sdk "github.com/cosmos/cosmos-sdk/types"
 	sdkerrors "github.com/cosmos/cosmos-sdk/types/errors"
 
-	"github.com/Stride-Labs/stride/v8/utils"
+	"github.com/Stride-Labs/stride/v9/utils"
 )
 
 func (k msgServer) RedeemStake(goCtx context.Context, msg *types.MsgRedeemStake) (*types.MsgRedeemStakeResponse, error) {
