9b0c27ac8a2958aea0bad0af94d3ff90116e4b75
sampocs <sam.pochyly@gmail.com>
2023-04-23 15:17:26 -0500

    removed msg server from records

--- x/records/keeper/keeper_test.go
+++ x/records/keeper/keeper_test.go
@@ -6,8 +6,6 @@ import (
 	"github.com/stretchr/testify/suite"
 
 	"github.com/Stride-Labs/stride/v9/app/apptesting"
-	"github.com/Stride-Labs/stride/v9/x/records/keeper"
-	"github.com/Stride-Labs/stride/v9/x/records/types"
 )
 
 type KeeperTestSuite struct {
@@ -16,14 +14,6 @@ type KeeperTestSuite struct {
 
 func (s *KeeperTestSuite) SetupTest() {
 	s.Setup()
-}
-
-// Dynamically gets the MsgServer for this module's keeper
-// this function must be used so that the MsgServer is always created with the most updated App context
-//	which can change depending on the type of test
-//	(e.g. tests with only one Stride chain vs tests with multiple chains and IBC support)
-func (s *KeeperTestSuite) GetMsgServer() types.MsgServer {
-	return keeper.NewMsgServerImpl(s.App.RecordsKeeper)
 }
 
 func TestKeeperTestSuite(t *testing.T) {
