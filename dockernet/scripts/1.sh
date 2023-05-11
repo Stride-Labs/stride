89d7b2ad276a10e31d675e819b3313117046529b
Robert Zaremba <robert@zaremba.ch>
2023-05-11 13:38:37 +0200

    Update umee 1.sh dockernet test script
    Umee block time is 6s. So 5s sleep time may not be enough.

--- dockernet/scripts/1.sh
+++ dockernet/scripts/1.sh
@@ -4,5 +4,5 @@ source ${SCRIPT_DIR}/../config.sh
 
 ## IBC ATOM from GAIA to STRIDE
 $UMEE_MAIN_CMD tx ibc-transfer transfer transfer channel-0 $(STRIDE_ADDRESS) 1000000uumee --from ${UMEE_VAL_PREFIX}1 -y 
-sleep 5
+sleep 7
 $STRIDE_MAIN_CMD q bank balances $(STRIDE_ADDRESS)
