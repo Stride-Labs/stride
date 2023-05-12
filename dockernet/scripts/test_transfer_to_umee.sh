c944e3116117729a59e8840f8a5001ae691817ed
sampocs <sam.pochyly@gmail.com>
2023-05-12 13:30:08 -0500

    updated umee version

--- dockernet/scripts/test_transfer_to_umee.sh
+++ dockernet/scripts/test_transfer_to_umee.sh
@@ -4,7 +4,7 @@ source ${SCRIPT_DIR}/../config.sh
 
 $UMEE_MAIN_CMD q bank balances umee1uk4ze0x4nvh4fk0xm4jdud58eqn4yxhr6fh0uq
 $STRIDE_MAIN_CMD q bank balances stride1uk4ze0x4nvh4fk0xm4jdud58eqn4yxhrt52vv7
-$STRIDE_MAIN_CMD tx ibc-transfer transfer transfer channel-0 umee1uk4ze0x4nvh4fk0xm4jdud58eqn4yxhr6fh0uq 1ustrd --from val1 -y 
-sleep 10
+$STRIDE_MAIN_CMD tx ibc-transfer transfer transfer channel-0 umee1uk4ze0x4nvh4fk0xm4jdud58eqn4yxhr6fh0uq 1ustrd --from val1 -y | TRIM_TX
+sleep 15
 $UMEE_MAIN_CMD q bank balances umee1uk4ze0x4nvh4fk0xm4jdud58eqn4yxhr6fh0uq
 $STRIDE_MAIN_CMD q bank balances stride1uk4ze0x4nvh4fk0xm4jdud58eqn4yxhrt52vv7
