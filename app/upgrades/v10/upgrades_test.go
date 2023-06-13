c0875154f98fd1c1858537d9dcc7d0d843fb6be6
sampocs <sam.pochyly@gmail.com>
2023-06-12 19:06:37 -0500

    added unit tests for rate limits and bad kids

--- app/upgrades/v10/upgrades_test.go
+++ app/upgrades/v10/upgrades_test.go
@@ -2,17 +2,24 @@ package v10_test
 
 import (
 	"fmt"
+	"strings"
 	"testing"
 
 	sdkmath "cosmossdk.io/math"
 	"github.com/stretchr/testify/suite"
 
 	sdk "github.com/cosmos/cosmos-sdk/types"
+	minttypes "github.com/cosmos/cosmos-sdk/x/mint/types"
+
+	transfertypes "github.com/cosmos/ibc-go/v7/modules/apps/transfer/types"
+	channeltypes "github.com/cosmos/ibc-go/v7/modules/core/04-channel/types"
 
 	"github.com/Stride-Labs/stride/v9/app/apptesting"
 	v10 "github.com/Stride-Labs/stride/v9/app/upgrades/v10"
+	"github.com/Stride-Labs/stride/v9/utils"
 
 	icacallbackstypes "github.com/Stride-Labs/stride/v9/x/icacallbacks/types"
+	ratelimittypes "github.com/Stride-Labs/stride/v9/x/ratelimit/types"
 	recordskeeper "github.com/Stride-Labs/stride/v9/x/records/keeper"
 	recordstypes "github.com/Stride-Labs/stride/v9/x/records/types"
 	stakeibckeeper "github.com/Stride-Labs/stride/v9/x/stakeibc/keeper"
@@ -22,6 +29,8 @@ import (
 	cosmosproto "github.com/cosmos/gogoproto/proto"
 	deprecatedproto "github.com/golang/protobuf/proto" //nolint:staticcheck
 )
+
+var initialRateLimitChannelValue = sdk.NewInt(1_000_000)
 
 type UpgradeTestSuite struct {
 	apptesting.AppTestHelper
@@ -43,6 +52,17 @@ func (s *UpgradeTestSuite) TestUpgrade() {
 	clientParams.AllowedClients = []string{}
 	s.App.IBCKeeper.ClientKeeper.SetParams(s.Ctx, clientParams)
 
+	// Fund the community pool growth address
+	communityGrowthAddress := sdk.MustAccAddressFromBech32(v10.CommunityPoolGrowthAddress)
+	s.FundAccount(communityGrowthAddress, sdk.NewCoin(v10.Ustrd, v10.BadKidsTransferAmount))
+
+	// Add cosmoshub to check that a rate limit is added
+	atom := "uatom"
+	stAtom := "st" + atom
+	transferChannelId := "channel-0"
+	s.setupRateLimitedHostZone("cosmoshub-4", stAtom, transferChannelId)
+
+	// Submit upgrade
 	s.ConfirmUpgradeSucceededs("v10", dummyUpgradeHeight)
 
 	// Check mint parameters after upgrade
@@ -67,6 +87,19 @@ func (s *UpgradeTestSuite) TestUpgrade() {
 	// Check localhost client was added
 	clientParams = s.App.IBCKeeper.ClientKeeper.GetParams(s.Ctx)
 	s.Require().Contains(clientParams.AllowedClients, "09-localhost")
+
+	// Check the transfer was successful
+	communityGrowthBalance := s.App.BankKeeper.GetBalance(s.Ctx, communityGrowthAddress, v10.Ustrd)
+	s.Require().Zero(communityGrowthBalance.Amount.Int64(), "community growth balance after transfer")
+
+	badKidsCustodianAddress := sdk.MustAccAddressFromBech32(v10.BadKidsCustodian)
+	badKidsCustodianBalance := s.App.BankKeeper.GetBalance(s.Ctx, badKidsCustodianAddress, v10.Ustrd)
+	s.Require().Equal(v10.BadKidsTransferAmount.Int64(), badKidsCustodianBalance.Amount.Int64(), "bad kids balance")
+
+	// Check that the rate limit was added
+	rateLimits := s.App.RatelimitKeeper.GetAllRateLimits(s.Ctx)
+	s.Require().Len(rateLimits, 1, "one rate limit should have been added")
+	s.validateRateLimit(rateLimits[0], "cosmoshub-4", stAtom, transferChannelId)
 }
 
 func (s *UpgradeTestSuite) createCallbackData(id string, callback deprecatedproto.Message) icacallbackstypes.CallbackData {
@@ -201,5 +234,73 @@ func (s *UpgradeTestSuite) TestMigrateCallbackData() {
 			s.mustUnmarshalCallback(finalCallback.CallbackArgs, &finalCallbackArgs)
 			s.Require().Equal(initialTransferCallbackArgs, finalCallbackArgs, "transfer callback")
 		}
+	}
+}
+
+func (s *UpgradeTestSuite) setupRateLimitedHostZone(chainId, stDenom, channelId string) {
+	// Store host zone in stakeibc
+	s.App.StakeibcKeeper.SetHostZone(s.Ctx, stakeibctypes.HostZone{
+		ChainId:           chainId,
+		HostDenom:         strings.ReplaceAll(stDenom, "st", ""),
+		TransferChannelId: channelId,
+	})
+
+	// Create the transfer channel
+	s.App.IBCKeeper.ChannelKeeper.SetChannel(s.Ctx, transfertypes.PortID, channelId, channeltypes.Channel{})
+
+	// Mint the st denom so there's an initial channel value
+	initialStAtom := sdk.NewCoin(stDenom, initialRateLimitChannelValue)
+	err := s.App.BankKeeper.MintCoins(s.Ctx, minttypes.ModuleName, sdk.NewCoins(initialStAtom))
+	s.Require().NoError(err)
+}
+
+func (s *UpgradeTestSuite) validateRateLimit(rateLimit ratelimittypes.RateLimit, chainId, denom, channelId string) {
+	s.Require().Equal(denom, rateLimit.Path.Denom, "rate limit denom")
+	s.Require().Equal(channelId, rateLimit.Path.ChannelId, "rate limit channel")
+	description := fmt.Sprintf("%s - %s - %s", chainId, denom, channelId)
+
+	expectedThreshold := v10.NewRateLimits[chainId].Int64()
+	quota := rateLimit.Quota
+	s.Require().Equal(expectedThreshold, quota.MaxPercentSend.Int64(), "%s - rate limit send threshold", description)
+	s.Require().Equal(expectedThreshold, quota.MaxPercentRecv.Int64(), "%s - rate limit recv threshold", description)
+	s.Require().Equal(v10.RateLimitDurationHours, quota.DurationHours, "%s - rate limit duration", description)
+
+	flow := rateLimit.Flow
+	s.Require().Zero(flow.Inflow.Int64(), "%s - rate limit inflow", description)
+	s.Require().Zero(flow.Outflow.Int64(), "%s - rate limit outflow", description)
+	s.Require().Equal(initialRateLimitChannelValue.Int64(), flow.ChannelValue.Int64(),
+		"%s - rate limit channel value", description)
+}
+
+func (s *UpgradeTestSuite) TestEnableRateLimits() {
+	// Create a host zone for each new rate limited host
+	rateLimitedHosts := utils.StringMapKeys(v10.NewRateLimits)
+	for i, chainId := range rateLimitedHosts {
+		denom := fmt.Sprintf("stdenom-%d", i)
+		channelId := fmt.Sprintf("channel-%d", i)
+		s.setupRateLimitedHostZone(chainId, denom, channelId)
+	}
+
+	// Add a host zone that's not in the rate limit map
+	s.App.StakeibcKeeper.SetHostZone(s.Ctx, stakeibctypes.HostZone{
+		ChainId: "differnet-host-zone",
+	})
+
+	// Enable the rate limits
+	err := v10.EnableRateLimits(s.Ctx, s.App.RatelimitKeeper, s.App.IBCKeeper.ChannelKeeper, s.App.StakeibcKeeper)
+	s.Require().NoError(err, "no error expected when enabling new rate limits")
+
+	// Confirm correct number of new rate limits
+	allRateLimits := s.App.RatelimitKeeper.GetAllRateLimits(s.Ctx)
+	s.Require().Equal(len(v10.NewRateLimits), len(allRateLimits), "number of new rate limits")
+
+	// Verify each rate limit
+	for i, chainId := range rateLimitedHosts {
+		denom := fmt.Sprintf("stdenom-%d", i)
+		channelId := fmt.Sprintf("channel-%d", i)
+
+		rateLimit, found := s.App.RatelimitKeeper.GetRateLimit(s.Ctx, denom, channelId)
+		s.Require().True(found, "rate limit for %s and %s should have been found", denom, channelId)
+		s.validateRateLimit(rateLimit, chainId, denom, channelId)
 	}
 }
