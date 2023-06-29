7c998d5290c3149c1af29358fb3e9fa97516139f
Hieu Vu <72878483+hieuvubk@users.noreply.github.com>
2023-06-29 14:37:31 +0700

    StakeIbcPostDecorator

---
+++ x/stakeibc/keeper/posthandler.go
@@ -0,0 +1,76 @@
+package keeper
+
+import (
+	"fmt"
+	sdk "github.com/cosmos/cosmos-sdk/types"
+	"github.com/Stride-Labs/stride/v11/x/stakeibc/types"
+)
+
+type StakeIbcPostDecorator struct {
+	StakeIbcKeeper Keeper
+}
+
+func NewStakeIbcPostDecorator(stakeIbcDecorator Keeper) StakeIbcPostDecorator {
+	return StakeIbcPostDecorator{
+		StakeIbcKeeper: stakeIbcDecorator,
+	}
+}
+
+// This posthandler will re-compute the redemption rate and check again the rate already saved in host zone
+func (stakeIbcDec StakeIbcPostDecorator) PostHandle(ctx sdk.Context, tx sdk.Tx, simulate bool, success bool,  next sdk.PostHandler) (sdk.Context, error) {
+	if ctx.IsCheckTx() {
+		return next(ctx, tx, simulate, success)
+	}
+
+	// Create a cache context to execute the posthandler such that
+	// 1. If there is an error, then the cache context is discarded
+	// 2. If there is no error, then the cache context is written to the main context with no gas consumed
+	cacheCtx, write := ctx.CacheContext()
+	// CacheCtx's by default _share_ their gas meter with the parent.
+	// In our case, the cache ctx is given a new gas meter instance entirely,
+	// so gas usage is not counted towards tx gas usage.
+	//
+	// 50M is chosen as a large enough number to ensure that the posthandler will not run out of gas,
+	// but will eventually terminate in event of an accidental infinite loop with some gas usage.
+	cacheCtx = cacheCtx.WithGasMeter(sdk.NewGasMeter(sdk.Gas(50_000_000)))
+
+	hostzones := stakeIbcDec.StakeIbcKeeper.GetAllHostZone(cacheCtx)
+	for _, hz := range hostzones {
+		reComputeRedemptionRate := stakeIbcDec.StakeIbcKeeper.ComputeRedemptionRate(cacheCtx, hz)
+		stakeIbcDec.StakeIbcKeeper.CompareRedemptionRate(cacheCtx, reComputeRedemptionRate, hz)
+	}
+	write()
+
+	return next(ctx, tx, simulate, success)
+}
+
+func(k Keeper) ComputeRedemptionRate(ctx sdk.Context, hz types.HostZone) sdk.Dec {
+	stDenomSupply := k.bankKeeper.GetSupply(ctx, types.StAssetDenomFromHostZoneDenom(hz.HostDenom))
+	depositRecords := k.RecordsKeeper.GetAllDepositRecord(ctx)
+	undelegatedBalance := k.GetUndelegatedBalance(hz, depositRecords)
+	stakedBalance := hz.StakedBal
+	moduleAcctBalance := k.GetModuleAccountBalance(hz, depositRecords)
+	redemptionRate := (sdk.NewDecFromInt(undelegatedBalance).Add(sdk.NewDecFromInt(stakedBalance)).Add(sdk.NewDecFromInt(moduleAcctBalance))).Quo(sdk.NewDecFromInt(stDenomSupply.Amount))
+	return redemptionRate
+}
+
+func(k Keeper) CompareRedemptionRate(ctx sdk.Context, recomputeRedemptionRate sdk.Dec, hz types.HostZone) {
+	// If reComputeRedemptionRate not equal to current rate, hz should be halted
+	if !recomputeRedemptionRate.Equal(hz.RedemptionRate) {
+		hz.Halted = true
+		k.SetHostZone(ctx, hz)
+
+		// set rate limit on stAsset
+		stDenom := types.StAssetDenomFromHostZoneDenom(hz.HostDenom)
+		k.RatelimitKeeper.AddDenomToBlacklist(ctx, stDenom)
+
+		k.Logger(ctx).Error(fmt.Sprintf("[INVARIANT BROKEN!!!] %s's recomputeRR is %s different from zone RR is %s.", hz.GetChainId(), recomputeRedemptionRate.String(), hz.RedemptionRate.String()))
+		ctx.EventManager().EmitEvent(
+			sdk.NewEvent(
+				types.EventTypeHostZoneHalt,
+				sdk.NewAttribute(types.AttributeKeyHostZone, hz.ChainId),
+				sdk.NewAttribute(types.AttributeKeyRedemptionRate, hz.RedemptionRate.String()),
+			),
+		)
+	}
+}
