011318726bcf8bb3365b6a8c5906ea9f5795675e
Hieu Vu <72878483+hieuvubk@users.noreply.github.com>
2023-07-04 20:41:20 +0700

    calculate diff between before & after tx

--- x/stakeibc/keeper/posthandler.go
+++ x/stakeibc/keeper/posthandler.go
@@ -4,6 +4,7 @@ import (
 	"fmt"
 	sdk "github.com/cosmos/cosmos-sdk/types"
 	"github.com/Stride-Labs/stride/v11/x/stakeibc/types"
+	stakeibctypes "github.com/Stride-Labs/stride/v11/x/stakeibc/types"
 )
 
 type StakeIbcPostDecorator struct {
@@ -34,10 +35,56 @@ func (stakeIbcDec StakeIbcPostDecorator) PostHandle(ctx sdk.Context, tx sdk.Tx, 
 	// but will eventually terminate in event of an accidental infinite loop with some gas usage.
 	cacheCtx = cacheCtx.WithGasMeter(sdk.NewGasMeter(sdk.Gas(50_000_000)))
 
+	moduleAddr := stakeIbcDec.StakeIbcKeeper.AccountKeeper.GetModuleAccount(ctx, stakeibctypes.ModuleName).GetAddress()
 	hostzones := stakeIbcDec.StakeIbcKeeper.GetAllHostZone(cacheCtx)
 	for _, hz := range hostzones {
-		reComputeRedemptionRate := stakeIbcDec.StakeIbcKeeper.ComputeRedemptionRate(cacheCtx, hz)
-		stakeIbcDec.StakeIbcKeeper.CompareRedemptionRate(cacheCtx, reComputeRedemptionRate, hz)
+		stSupplyAfter := stakeIbcDec.StakeIbcKeeper.bankKeeper.GetSupply(ctx, hz.HostDenom)
+		stSupplyBefore, err := stakeIbcDec.StakeIbcKeeper.GetStSupply(ctx, hz)
+		if err != nil {
+			hz.Halted = true
+			stakeIbcDec.StakeIbcKeeper.SetHostZone(ctx, hz)
+			ctx.EventManager().EmitEvent(
+				sdk.NewEvent(
+					types.EventTypeHostZoneHalt,
+					sdk.NewAttribute(types.AttributeKeyHostZone, hz.ChainId),
+					sdk.NewAttribute(types.AttributeKeyRedemptionRate, hz.RedemptionRate.String()),
+				),
+			)
+			continue
+		}
+
+		ibcDenomModuleAccountBalanceAfter := stakeIbcDec.StakeIbcKeeper.bankKeeper.GetBalance(ctx, moduleAddr, hz.IbcDenom)
+		ibcDenomModuleAccountBalanceBefore, err := stakeIbcDec.StakeIbcKeeper.GetModuleAccountIbcBalance(ctx, hz)
+		if err != nil {
+			hz.Halted = true
+			stakeIbcDec.StakeIbcKeeper.SetHostZone(ctx, hz)
+			ctx.EventManager().EmitEvent(
+				sdk.NewEvent(
+					types.EventTypeHostZoneHalt,
+					sdk.NewAttribute(types.AttributeKeyHostZone, hz.ChainId),
+					sdk.NewAttribute(types.AttributeKeyRedemptionRate, hz.RedemptionRate.String()),
+				),
+			)
+			continue
+		}
+
+		stSupplyDiff := stSupplyAfter.Amount.Sub(stSupplyBefore.Amount)
+		ibcDenomBalanceDiff := ibcDenomModuleAccountBalanceAfter.Amount.Sub(ibcDenomModuleAccountBalanceBefore.Amount)
+
+		expectedRedemptionRate := sdk.NewDecFromInt(ibcDenomBalanceDiff).Quo(sdk.NewDecFromInt(stSupplyDiff))
+		if !expectedRedemptionRate.Equal(hz.RedemptionRate) {
+			hz.Halted = true
+			stakeIbcDec.StakeIbcKeeper.SetHostZone(ctx, hz)
+
+			stakeIbcDec.StakeIbcKeeper.Logger(ctx).Error(fmt.Sprintf("[INVARIANT BROKEN!!!] %s's recomputeRR is %s different from zone RR is %s.", hz.GetChainId(), expectedRedemptionRate.String(), hz.RedemptionRate.String()))
+			ctx.EventManager().EmitEvent(
+				sdk.NewEvent(
+					types.EventTypeHostZoneHalt,
+					sdk.NewAttribute(types.AttributeKeyHostZone, hz.ChainId),
+					sdk.NewAttribute(types.AttributeKeyRedemptionRate, hz.RedemptionRate.String()),
+				),
+			)
+		}
 	}
 	write()
 
@@ -46,15 +93,20 @@ func (stakeIbcDec StakeIbcPostDecorator) PostHandle(ctx sdk.Context, tx sdk.Tx, 
 
 func(k Keeper) ComputeRedemptionRate(ctx sdk.Context, hz types.HostZone) sdk.Dec {
 	stDenomSupply := k.bankKeeper.GetSupply(ctx, types.StAssetDenomFromHostZoneDenom(hz.HostDenom))
+	fmt.Println("stDenomSupply", stDenomSupply)
 	depositRecords := k.RecordsKeeper.GetAllDepositRecord(ctx)
 	undelegatedBalance := k.GetUndelegatedBalance(hz, depositRecords)
+	fmt.Println("undelegatedBalance", undelegatedBalance)
 	stakedBalance := hz.StakedBal
+	fmt.Println("stakedBalance", stakedBalance)
 	moduleAcctBalance := k.GetModuleAccountBalance(hz, depositRecords)
+	fmt.Println("moduleAcctBalance", moduleAcctBalance)
 	redemptionRate := (sdk.NewDecFromInt(undelegatedBalance).Add(sdk.NewDecFromInt(stakedBalance)).Add(sdk.NewDecFromInt(moduleAcctBalance))).Quo(sdk.NewDecFromInt(stDenomSupply.Amount))
 	return redemptionRate
 }
 
 func(k Keeper) CompareRedemptionRate(ctx sdk.Context, recomputeRedemptionRate sdk.Dec, hz types.HostZone) {
+	fmt.Println("hz rate", hz.RedemptionRate)
 	// If reComputeRedemptionRate not equal to current rate, hz should be halted
 	if !recomputeRedemptionRate.Equal(hz.RedemptionRate) {
 		hz.Halted = true
