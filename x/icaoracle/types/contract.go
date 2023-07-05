2a9ead570a02cb0ad34040fe6949814bbfcaa6ec
sampocs <sam.pochyly@gmail.com>
2023-07-05 03:11:53 -0500

    misc changes when integration testing

--- x/icaoracle/types/contract.go
+++ x/icaoracle/types/contract.go
@@ -4,26 +4,10 @@ import (
 	"encoding/base64"
 )
 
-type MsgInstantiateOracleContract struct {
-	AdminAddress string `json:"admin_address"`
-}
-
-type postMetric struct {
-	Key         string `json:"key"`
-	Value       string `json:"value"`
-	MetricType  string `json:"metric_type"`
-	UpdateTime  int64  `json:"update_time"`
-	BlockHeight int64  `json:"block_height"`
-	Attributes  string `json:"attributes"`
-}
-
-type MsgExecuteContractPostMetric struct {
-	PostMetric postMetric `json:"post_metric"`
-}
-
+// Creates a new PostMetric contract message from a Metric
 func NewMsgExecuteContractPostMetric(metric Metric) MsgExecuteContractPostMetric {
 	return MsgExecuteContractPostMetric{
-		PostMetric: postMetric{
+		PostMetric: &MsgPostMetric{
 			Key:         metric.Key,
 			Value:       metric.Value,
 			MetricType:  metric.MetricType,
