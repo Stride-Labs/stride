025bd9f28365742c415439081b892f45439d828c
strbrian <brian@stridelabs.co>
2023-08-09 11:01:25 -0400

    register reward denoms

--- app/upgrades/v13/upgrades.go
+++ app/upgrades/v13/upgrades.go
@@ -4,6 +4,9 @@ import (
 	sdk "github.com/cosmos/cosmos-sdk/types"
 	"github.com/cosmos/cosmos-sdk/types/module"
 	upgradetypes "github.com/cosmos/cosmos-sdk/x/upgrade/types"
+
+	stakeibckeeper "github.com/Stride-Labs/stride/v12/x/stakeibc/keeper"
+	stakeibctypes "github.com/Stride-Labs/stride/v12/x/stakeibc/types"
 )
 
 var (
@@ -14,9 +17,19 @@ var (
 func CreateUpgradeHandler(
 	mm *module.Manager,
 	configurator module.Configurator,
+	stakeibcKeeper stakeibckeeper.Keeper,
 ) upgradetypes.UpgradeHandler {
 	return func(ctx sdk.Context, _ upgradetypes.Plan, vm module.VersionMap) (module.VersionMap, error) {
 		ctx.Logger().Info("Starting upgrade v13...")
+		hostZones := stakeibcKeeper.GetAllHostZone(ctx)
+		allDenoms := []string{}
+
+		// get all stToken denoms
+		for _, zone := range hostZones {
+			allDenoms = append(allDenoms, stakeibctypes.StAssetDenomFromHostZoneDenom(zone.HostDenom))
+		}
+
+		stakeibcKeeper.RegisterStTokenDenomsToWhitelist(ctx, allDenoms)
 		return mm.RunMigrations(ctx, configurator, vm)
 	}
 }
