3fdb5289c32d45de9dfb0bcd98577640d2dadf5a
sampocs <sam.pochyly@gmail.com>
2023-08-10 15:39:17 -0500

    added consumer params query

--- x/stakeibc/client/cli/query.go
+++ x/stakeibc/client/cli/query.go
@@ -33,6 +33,7 @@ func GetQueryCmd(queryRoute string) *cobra.Command {
 	cmd.AddCommand(CmdListEpochTracker())
 	cmd.AddCommand(CmdShowEpochTracker())
 	cmd.AddCommand(CmdNextPacketSequence())
+	cmd.AddCommand(CmdQueryConsumerParams())
 	// this line is used by starport scaffolding # 1
 
 	return cmd
