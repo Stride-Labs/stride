38b7709637496ad4b31610d60b79f967437161f8
sampocs <sam.pochyly@gmail.com>
2023-08-10 16:03:08 -0500

    added consumer params query

---
+++ x/stakeibc/client/cli/query_consumer_params.go
@@ -0,0 +1,35 @@
+package cli
+
+import (
+	"context"
+
+	"github.com/cosmos/cosmos-sdk/client"
+	"github.com/cosmos/cosmos-sdk/client/flags"
+	"github.com/spf13/cobra"
+
+	"github.com/Stride-Labs/stride/v13/x/stakeibc/types"
+)
+
+func CmdQueryConsumerParams() *cobra.Command {
+	cmd := &cobra.Command{
+		Use:   "consumer-params",
+		Short: "consumer params",
+		Args:  cobra.ExactArgs(0),
+		RunE: func(cmd *cobra.Command, args []string) error {
+			clientCtx := client.GetClientContextFromCmd(cmd)
+
+			queryClient := types.NewQueryClient(clientCtx)
+
+			res, err := queryClient.ConsumerParams(context.Background(), &types.QueryConsumerParams{})
+			if err != nil {
+				return err
+			}
+
+			return clientCtx.PrintProto(res)
+		},
+	}
+
+	flags.AddQueryFlagsToCmd(cmd)
+
+	return cmd
+}
---
+++ x/stakeibc/keeper/grpc_query_consumer_params.go
@@ -0,0 +1,22 @@
+package keeper
+
+import (
+	"context"
+
+	sdk "github.com/cosmos/cosmos-sdk/types"
+	"google.golang.org/grpc/codes"
+	"google.golang.org/grpc/status"
+
+	"github.com/Stride-Labs/stride/v13/x/stakeibc/types"
+)
+
+func (k Keeper) ConsumerParams(c context.Context, req *types.QueryConsumerParams) (*types.QueryConsumerParamsResponse, error) {
+	if req == nil {
+		return nil, status.Error(codes.InvalidArgument, "invalid request")
+	}
+	ctx := sdk.UnwrapSDKContext(c)
+
+	params := types.ConsumerParams(k.ConsumerKeeper.GetConsumerParams(ctx))
+
+	return &types.QueryConsumerParamsResponse{Params: params}, nil
+}
