b999335b07531445d339ec7f1a4417ec3dd4cb86
Riley Edmunds <riley@stridelabs.co>
2023-08-10 19:56:03 -0700

    test: add extra tokens and validators to the lsm frontend setup

--- dockernet/scripts/lsm/setup.sh
+++ dockernet/scripts/lsm/setup.sh
@@ -3,7 +3,7 @@ set -eu
 SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
 source ${SCRIPT_DIR}/../../config.sh
 
-## Create Accounts
+# Create Accounts
 echo ">>> Registering Accounts:"
 mnemonic="match blade slide sort seven width degree february garden hospital valve odor scan exhaust bird chuckle age ozone timber claim office hurdle dance roast"
 echo $mnemonic | $STRIDE_MAIN_CMD keys add staker --recover --keyring-backend test
@@ -11,24 +11,34 @@ sleep 5
 echo $mnemonic | $GAIA_MAIN_CMD keys add staker --recover --keyring-backend test
 sleep 5
 
+# echo $GAIA_MAIN_CMD q validators
+
 # Staker Address on Stride: stride1x92tnm6pfkl3gsfy0rfaez5myq5zh99a6a2w0p
 # Staker Address on GAIA:   cosmos1x92tnm6pfkl3gsfy0rfaez5myq5zh99aek2jmd
 # Validator Address:        cosmosvaloper1uk4ze0x4nvh4fk0xm4jdud58eqn4yxhrdt795p
 staker_lsm_address=$($GAIA_MAIN_CMD keys show staker -a) 
 staker_stride_address=$($STRIDE_MAIN_CMD keys show staker -a) 
 validator_address=$(GET_VAL_ADDR GAIA 2)
+validator_address_2="cosmosvaloper1nnurja9zt97huqvsfuartetyjx63tc5zxcyn3n"
+validator_address_4="cosmosvaloper1py0fvhdtq4au3d9l88rec6vyda3e0wttr0ks75"
 
 ## Fund accounts
 echo ">>> Fund staking account:"
-$GAIA_MAIN_CMD tx bank send $($GAIA_MAIN_CMD keys show gval1 -a) $staker_lsm_address 10000000uatom --from rly8 -y | TRIM_TX 
+$GAIA_MAIN_CMD tx bank send $($GAIA_MAIN_CMD keys show gval1 -a) $staker_lsm_address 2000000000000uatom --from rly8 -y | TRIM_TX 
 sleep 5 && echo ""
 
 echo "Bank balance:"
 $GAIA_MAIN_CMD q bank balances $staker_lsm_address 
 
 ## Delegate, Tokenize and Transfer
-echo ">>> Delegate:"
-$GAIA_MAIN_CMD tx staking delegate $validator_address 10000000uatom --from staker -y | TRIM_TX && echo ""
+echo ">>> Delegate to Coinbase:"
+$GAIA_MAIN_CMD tx staking delegate $validator_address 1500000000000uatom --from staker -y | TRIM_TX && echo ""
+sleep 5
+echo ">>> Delegate to XXX:"
+$GAIA_MAIN_CMD tx staking delegate $validator_address_2 10000000uatom --from staker -y | TRIM_TX && echo ""
+sleep 5
+echo ">>> Delegate to XXX:"
+$GAIA_MAIN_CMD tx staking delegate $validator_address_4 10000000uatom --from staker -y | TRIM_TX && echo ""
 sleep 5
 
 echo "Delegations:"
