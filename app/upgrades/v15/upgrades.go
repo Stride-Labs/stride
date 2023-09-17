ea2750c3af96b8aaccfe68abb435bc89047e16ea
sampocs <sam.pochyly@gmail.com>
2023-09-17 18:09:21 -0500

    added v15 upgrade handler

---
+++ app/upgrades/v15/upgrades.go
@@ -0,0 +1,25 @@
+package v15
+
+import (
+	sdk "github.com/cosmos/cosmos-sdk/types"
+	"github.com/cosmos/cosmos-sdk/types/module"
+	upgradetypes "github.com/cosmos/cosmos-sdk/x/upgrade/types"
+
+	stakeibckeeper "github.com/Stride-Labs/stride/v14/x/stakeibc/keeper"
+)
+
+var (
+	UpgradeName = "v15"
+)
+
+// CreateUpgradeHandler creates an SDK upgrade handler for v15
+func CreateUpgradeHandler(
+	mm *module.Manager,
+	configurator module.Configurator,
+	stakeibcKeeper stakeibckeeper.Keeper,
+) upgradetypes.UpgradeHandler {
+	return func(ctx sdk.Context, _ upgradetypes.Plan, vm module.VersionMap) (module.VersionMap, error) {
+		ctx.Logger().Info("Starting upgrade v15...")
+		return mm.RunMigrations(ctx, configurator, vm)
+	}
+}
