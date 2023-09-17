6ba11aff80ced2ba6782afa2b1f8cfc2845e65e8
sampocs <sam.pochyly@gmail.com>
2023-09-16 23:48:10 -0500

    admin gated tx

--- x/stakeibc/types/message_calibrate_delegation.go
+++ x/stakeibc/types/message_calibrate_delegation.go
@@ -6,6 +6,8 @@ import (
 	errorsmod "cosmossdk.io/errors"
 	sdk "github.com/cosmos/cosmos-sdk/types"
 	sdkerrors "github.com/cosmos/cosmos-sdk/types/errors"
+
+	"github.com/Stride-Labs/stride/v14/utils"
 )
 
 const TypeMsgCalibrateDelegation = "calibrate_delegation"
@@ -45,6 +47,9 @@ func (msg *MsgCalibrateDelegation) ValidateBasic() error {
 	_, err := sdk.AccAddressFromBech32(msg.Creator)
 	if err != nil {
 		return errorsmod.Wrapf(sdkerrors.ErrInvalidAddress, "invalid creator address (%s)", err)
+	}
+	if err := utils.ValidateAdminAddress(msg.Creator); err != nil {
+		return err
 	}
 
 	// basic checks on host denom
