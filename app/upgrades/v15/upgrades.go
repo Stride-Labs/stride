e04a4cbc5bf271022f08d291850a7ee4f233eaff
sampocs <sam.pochyly@gmail.com>
2023-09-17 20:17:09 -0500

    set RR bounds in upgrade handler

--- app/upgrades/v15/upgrades.go
+++ app/upgrades/v15/upgrades.go
@@ -10,6 +10,15 @@ import (
 
 var (
 	UpgradeName = "v15"
+
+	EvmosChainId                = "evmos_9001-2"
+	EvmosOuterMinRedemptionRate = sdk.MustNewDecFromStr("1.34")
+	EvmosInnerMinRedemptionRate = sdk.MustNewDecFromStr("1.36")
+
+	RedemptionRateOuterMinAdjustment = sdk.MustNewDecFromStr("0.05")
+	RedemptionRateInnerMinAdjustment = sdk.MustNewDecFromStr("0.03")
+	RedemptionRateInnerMaxAdjustment = sdk.MustNewDecFromStr("0.05")
+	RedemptionRateOuterMaxAdjustment = sdk.MustNewDecFromStr("0.10")
 )
 
 // CreateUpgradeHandler creates an SDK upgrade handler for v15
@@ -20,6 +29,35 @@ func CreateUpgradeHandler(
 ) upgradetypes.UpgradeHandler {
 	return func(ctx sdk.Context, _ upgradetypes.Plan, vm module.VersionMap) (module.VersionMap, error) {
 		ctx.Logger().Info("Starting upgrade v15...")
+
+		// Set host zone redemption rate bounds based on a percentage of their current rate
+		for _, hostZone := range stakeibcKeeper.GetAllHostZone(ctx) {
+			if hostZone.ChainId == EvmosChainId {
+				hostZone.MinRedemptionRate = EvmosOuterMinRedemptionRate
+				hostZone.MinInnerRedemptionRate = EvmosInnerMinRedemptionRate
+				hostZone.MaxInnerRedemptionRate = hostZone.MaxRedemptionRate
+
+				stakeibcKeeper.SetHostZone(ctx, hostZone)
+			} else {
+				outerMinDelta := hostZone.RedemptionRate.Mul(RedemptionRateOuterMinAdjustment)
+				innerMinDelta := hostZone.RedemptionRate.Mul(RedemptionRateInnerMinAdjustment)
+				innerMaxDelta := hostZone.RedemptionRate.Mul(RedemptionRateInnerMaxAdjustment)
+				outerMaxDelta := hostZone.RedemptionRate.Mul(RedemptionRateOuterMaxAdjustment)
+
+				outerMin := hostZone.RedemptionRate.Sub(outerMinDelta)
+				innerMin := hostZone.RedemptionRate.Sub(innerMinDelta)
+				innerMax := hostZone.RedemptionRate.Add(innerMaxDelta)
+				outerMax := hostZone.RedemptionRate.Add(outerMaxDelta)
+
+				hostZone.MinRedemptionRate = outerMin
+				hostZone.MinInnerRedemptionRate = innerMin
+				hostZone.MaxInnerRedemptionRate = innerMax
+				hostZone.MaxRedemptionRate = outerMax
+
+				stakeibcKeeper.SetHostZone(ctx, hostZone)
+			}
+		}
+
 		return mm.RunMigrations(ctx, configurator, vm)
 	}
 }
--- app/upgrades/v15/upgrades_test.go
+++ app/upgrades/v15/upgrades_test.go
@@ -1,11 +1,16 @@
 package v15_test
 
 import (
+	"fmt"
 	"testing"
 
 	"github.com/stretchr/testify/suite"
 
+	sdk "github.com/cosmos/cosmos-sdk/types"
+
 	"github.com/Stride-Labs/stride/v14/app/apptesting"
+	v15 "github.com/Stride-Labs/stride/v14/app/upgrades/v15"
+	stakeibctypes "github.com/Stride-Labs/stride/v14/x/stakeibc/types"
 )
 
 type UpgradeTestSuite struct {
@@ -20,7 +25,82 @@ func TestKeeperTestSuite(t *testing.T) {
 	suite.Run(t, new(UpgradeTestSuite))
 }
 
+type UpdateRedemptionRateBounds struct {
+	CurrentRedemptionRate          sdk.Dec
+	ExpectedMinOuterRedemptionRate sdk.Dec
+	ExpectedMinInnerRedemptionRate sdk.Dec
+	ExpectedMaxInnerRedemptionRate sdk.Dec
+	ExpectedMaxOuterRedemptionRate sdk.Dec
+}
+
 func (s *UpgradeTestSuite) TestUpgrade() {
 	dummyUpgradeHeight := int64(5)
+
+	// Define test cases consisting of an initial redemption rate and expected bounds
+	testCases := []UpdateRedemptionRateBounds{
+		{
+			CurrentRedemptionRate:          sdk.MustNewDecFromStr("1.0"),
+			ExpectedMinOuterRedemptionRate: sdk.MustNewDecFromStr("0.95"), // 1 - 5% = 0.95
+			ExpectedMinInnerRedemptionRate: sdk.MustNewDecFromStr("0.97"), // 1 - 3% = 0.97
+			ExpectedMaxInnerRedemptionRate: sdk.MustNewDecFromStr("1.05"), // 1 + 5% = 1.05
+			ExpectedMaxOuterRedemptionRate: sdk.MustNewDecFromStr("1.10"), // 1 + 10% = 1.1
+		},
+		{
+			CurrentRedemptionRate:          sdk.MustNewDecFromStr("1.1"),
+			ExpectedMinOuterRedemptionRate: sdk.MustNewDecFromStr("1.045"), // 1.1 - 5% = 1.045
+			ExpectedMinInnerRedemptionRate: sdk.MustNewDecFromStr("1.067"), // 1.1 - 3% = 1.067
+			ExpectedMaxInnerRedemptionRate: sdk.MustNewDecFromStr("1.155"), // 1.1 + 5% = 1.155
+			ExpectedMaxOuterRedemptionRate: sdk.MustNewDecFromStr("1.210"), // 1.1 + 10% = 1.21
+		},
+		{
+			CurrentRedemptionRate:          sdk.MustNewDecFromStr("1.25"),
+			ExpectedMinOuterRedemptionRate: sdk.MustNewDecFromStr("1.1875"), // 1.25 - 5% = 1.1875
+			ExpectedMinInnerRedemptionRate: sdk.MustNewDecFromStr("1.2125"), // 1.25 - 3% = 1.2125
+			ExpectedMaxInnerRedemptionRate: sdk.MustNewDecFromStr("1.3125"), // 1.25 + 5% = 1.3125
+			ExpectedMaxOuterRedemptionRate: sdk.MustNewDecFromStr("1.3750"), // 1.25 + 10% = 1.375
+		},
+	}
+
+	// Create a host zone for each test case
+	for i, tc := range testCases {
+		chainId := fmt.Sprintf("chain-%d", i)
+
+		hostZone := stakeibctypes.HostZone{
+			ChainId:        chainId,
+			RedemptionRate: tc.CurrentRedemptionRate,
+		}
+		s.App.StakeibcKeeper.SetHostZone(s.Ctx, hostZone)
+	}
+
+	// Create an evmos host zone
+	initialEvmosMaxOuter := sdk.MustNewDecFromStr("1.50")
+	s.App.StakeibcKeeper.SetHostZone(s.Ctx, stakeibctypes.HostZone{
+		ChainId:           v15.EvmosChainId,
+		MaxRedemptionRate: initialEvmosMaxOuter,
+	})
+
+	// Run the upgrade to set the bounds
 	s.ConfirmUpgradeSucceededs("v15", dummyUpgradeHeight)
+
+	// Confirm the correct bounds were set
+	for i, tc := range testCases {
+		chainId := fmt.Sprintf("chain-%d", i)
+
+		hostZone, found := s.App.StakeibcKeeper.GetHostZone(s.Ctx, chainId)
+		s.Require().True(found)
+
+		s.Require().Equal(tc.ExpectedMinOuterRedemptionRate, hostZone.MinRedemptionRate, "min outer")
+		s.Require().Equal(tc.ExpectedMinInnerRedemptionRate, hostZone.MinInnerRedemptionRate, "min inner")
+		s.Require().Equal(tc.ExpectedMaxInnerRedemptionRate, hostZone.MinInnerRedemptionRate, "max inner")
+		s.Require().Equal(tc.ExpectedMaxOuterRedemptionRate, hostZone.MaxRedemptionRate, "max outer")
+	}
+
+	// Confirm evmos' custom bounds were set
+	evmosHostZone, found := s.App.StakeibcKeeper.GetHostZone(s.Ctx, v15.EvmosChainId)
+	s.Require().True(found)
+
+	s.Require().Equal(v15.EvmosOuterMinRedemptionRate, evmosHostZone.MinRedemptionRate, "min outer")
+	s.Require().Equal(v15.EvmosInnerMinRedemptionRate, evmosHostZone.MinInnerRedemptionRate, "min inner")
+	s.Require().Equal(initialEvmosMaxOuter, evmosHostZone.MinInnerRedemptionRate, "max inner")
+	s.Require().Equal(initialEvmosMaxOuter, evmosHostZone.MaxRedemptionRate, "max outer")
 }
