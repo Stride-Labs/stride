714c1399eb73715268beb30b43068a456842670b
sampocs <sam.pochyly@gmail.com>
2023-12-05 13:12:46 -0600

    slimmed down query setup

--- x/stakeibc/keeper/icqcallbacks_callibrate_delegation_test.go
+++ x/stakeibc/keeper/icqcallbacks_callibrate_delegation_test.go
@@ -1,23 +1,16 @@
 package keeper_test
 
 import (
-	"time"
-
 	sdkmath "cosmossdk.io/math"
 
 	sdk "github.com/cosmos/cosmos-sdk/types"
-	"github.com/cosmos/gogoproto/proto"
-	ibctesting "github.com/cosmos/ibc-go/v7/testing"
 
 	icqtypes "github.com/Stride-Labs/stride/v16/x/interchainquery/types"
 	"github.com/Stride-Labs/stride/v16/x/stakeibc/keeper"
 	"github.com/Stride-Labs/stride/v16/x/stakeibc/types"
 )
 
-func (s *KeeperTestSuite) SetupDelegatorSharesICQCallbackLargeSlash() DelegatorSharesICQCallbackTestCase {
-	// Setting this up to initialize the coordinator for the block time
-	s.CreateTransferChannel(HostChainId)
-
+func (s *KeeperTestSuite) TestCalibrateDelegation() {
 	valIndexQueried := 1
 	tokensBeforeSlash := sdkmath.NewInt(10000)
 	sharesToTokensRate := sdk.NewDec(1).Quo(sdk.NewDec(2)) // 0.5
@@ -28,14 +21,10 @@ func (s *KeeperTestSuite) SetupDelegatorSharesICQCallbackLargeSlash() DelegatorS
 	// 7500 slash tokens / 10000 initial tokens = 75% slash
 	expectedTokensAfterSlash := sdkmath.NewInt(2500)
 	expectedSlashAmount := tokensBeforeSlash.Sub(expectedTokensAfterSlash)
-	slashPercentage := sdk.MustNewDecFromStr("0.75")
 	weightBeforeSlash := uint64(20)
-	expectedWeightAfterSlash := uint64(5)
 	totalDelegation := sdkmath.NewInt(100000)
 
 	s.Require().Equal(numShares, sdk.NewDecFromInt(expectedTokensAfterSlash.Mul(sdkmath.NewInt(2))), "tokens, shares, and sharesToTokens rate aligned")
-	s.Require().Equal(slashPercentage, sdk.NewDecFromInt(expectedSlashAmount).Quo(sdk.NewDecFromInt(tokensBeforeSlash)), "expected slash percentage")
-	s.Require().Equal(slashPercentage, sdk.NewDec(int64(weightBeforeSlash-expectedWeightAfterSlash)).Quo(sdk.NewDec(int64(weightBeforeSlash))), "weight reduction")
 
 	hostZone := types.HostZone{
 		ChainId:          HostChainId,
@@ -45,89 +34,39 @@ func (s *KeeperTestSuite) SetupDelegatorSharesICQCallbackLargeSlash() DelegatorS
 			{
 				Name:       "val1",
 				Address:    "valoper1",
-				Weight:     1,
 				Delegation: sdkmath.ZeroInt(),
 			},
-			// This is the validator in question
+			// This is the validator being queried
 			{
-				Name:                        "val2",
-				Address:                     ValAddress,
-				SharesToTokensRate:          sharesToTokensRate,
-				Delegation:                  tokensBeforeSlash,
-				Weight:                      weightBeforeSlash,
-				SlashQueryInProgress:        true,
-				DelegationChangesInProgress: 0,
+				Name:               "val2",
+				Address:            ValAddress,
+				SharesToTokensRate: sharesToTokensRate,
+				Delegation:         tokensBeforeSlash,
 			},
 		},
 	}
 	s.App.StakeibcKeeper.SetHostZone(s.Ctx, hostZone)
 
+	// Mock out the query and query response
 	queryResponse := s.CreateDelegatorSharesQueryResponse(ValAddress, numShares)
+	query := icqtypes.Query{ChainId: HostChainId}
 
-	// Create callback data
-	callbackDataBz, err := proto.Marshal(&types.DelegatorSharesQueryCallback{
-		InitialValidatorDelegation: tokensBeforeSlash,
-	})
-	s.Require().NoError(err, "no error expected when marshalling callback data")
+	delegationAmountBeforeSlash := hostZone.Validators[valIndexQueried].Delegation
 
-	// Set the timeout timestamp to be 1 minute after the block time, and
-	// the timeout duration to be 5 minutes
-	blockTime := time.Date(2023, 1, 1, 0, 0, 0, 0, time.UTC)
-	s.Ctx = s.Ctx.WithBlockTime(blockTime)
-	timeoutDuration := time.Minute
-	timeoutTimestamp := uint64(blockTime.Add(timeoutDuration).UnixNano())
+	//////////
 
-	// Create the query that represents the ICQ in flight
-	query := icqtypes.Query{
-		Id:               "query-1",
-		ChainId:          HostChainId,
-		ConnectionId:     ibctesting.FirstConnectionID,
-		QueryType:        icqtypes.STAKING_STORE_QUERY_WITH_PROOF,
-		CallbackData:     callbackDataBz,
-		CallbackId:       keeper.ICQCallbackID_Delegation,
-		CallbackModule:   types.ModuleName,
-		TimeoutDuration:  timeoutDuration,
-		TimeoutTimestamp: timeoutTimestamp,
-		RequestSent:      true,
-		TimeoutPolicy:    icqtypes.TimeoutPolicy_RETRY_QUERY_REQUEST,
-	}
-	s.App.InterchainqueryKeeper.SetQuery(s.Ctx, query)
-
-	return DelegatorSharesICQCallbackTestCase{
-		valIndexQueried: valIndexQueried,
-		validArgs: DelegatorSharesICQCallbackArgs{
-			query:        query,
-			callbackArgs: queryResponse,
-		},
-		hostZone:                 hostZone,
-		numShares:                numShares,
-		slashPercentage:          slashPercentage,
-		expectedDelegationAmount: expectedTokensAfterSlash,
-		expectedSlashAmount:      expectedSlashAmount,
-		expectedWeight:           expectedWeightAfterSlash,
-		sharesToTokensRate:       sharesToTokensRate,
-		retryTimeoutDuration:     timeoutDuration,
-	}
-}
-
-func (s *KeeperTestSuite) TestCalibrateDelegationCallback_SlashBeyondThreshold() {
-	tc := s.SetupDelegatorSharesICQCallbackLargeSlash()
-	weightBeforeSlash := tc.hostZone.Validators[tc.valIndexQueried].Weight
-	delegationAmountBeforeSlash := tc.hostZone.Validators[tc.valIndexQueried].Delegation
 	// Callback
-	err := keeper.CalibrateDelegationCallback(s.App.StakeibcKeeper, s.Ctx, tc.validArgs.callbackArgs, tc.validArgs.query)
+	err := keeper.CalibrateDelegationCallback(s.App.StakeibcKeeper, s.Ctx, queryResponse, query)
 	// nil return indicates that the callback was not successful
 	s.Require().Nil(err)
 
 	// Confirm the staked balance was not decreased on the host
 	hostZone, found := s.App.StakeibcKeeper.GetHostZone(s.Ctx, HostChainId)
 	s.Require().True(found, "host zone found")
-	// println(tc.hostZone.TotalDelegations.Sub(hostZone.TotalDelegations).Int64())
-	// println(tc.hostZone.TotalDelegations.Int64())
-	s.Require().Equal(sdk.ZeroInt().Int64(), tc.hostZone.TotalDelegations.Sub(hostZone.TotalDelegations).Int64(), "staked bal not slashed")
+	s.Require().Equal(sdk.ZeroInt().Int64(), hostZone.TotalDelegations.Sub(hostZone.TotalDelegations).Int64(), "staked bal not slashed")
 
 	// Confirm the validator's weight and delegation amount were not reduced
-	validator := hostZone.Validators[tc.valIndexQueried]
+	validator := hostZone.Validators[valIndexQueried]
 	s.Require().Equal(weightBeforeSlash, validator.Weight, "validator weight unchanged")
 	s.Require().Equal(delegationAmountBeforeSlash.Int64(), validator.Delegation.Int64(), "validator delegation amount")
 
