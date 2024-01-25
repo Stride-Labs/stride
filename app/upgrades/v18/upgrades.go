2759211fa2a59e38a9261e20291d83421c1f9a4e
sampocs <sam.pochyly@gmail.com>
2024-01-25 17:57:30 -0600

    bugs

--- app/upgrades/v18/upgrades.go
+++ app/upgrades/v18/upgrades.go
@@ -2,6 +2,7 @@ package v18
 
 import (
 	errorsmod "cosmossdk.io/errors"
+	sdkmath "cosmossdk.io/math"
 	sdk "github.com/cosmos/cosmos-sdk/types"
 	"github.com/cosmos/cosmos-sdk/types/module"
 	upgradetypes "github.com/cosmos/cosmos-sdk/x/upgrade/types"
@@ -68,6 +69,8 @@ func UpdateUnbondingRecords(
 	// loop over host zone unbonding records
 	for _, epochUnbondingRecord := range rk.GetAllEpochUnbondingRecord(ctx) {
 		for _, hostZoneUnbonding := range epochUnbondingRecord.HostZoneUnbondings {
+			epochNumber := epochUnbondingRecord.EpochNumber
+			chainId := hostZoneUnbonding.HostZoneId
 
 			// we can ignore any record that's not currently unbonding
 			if hostZoneUnbonding.Status != recordtypes.HostZoneUnbonding_EXIT_TRANSFER_QUEUE {
@@ -90,7 +93,8 @@ func UpdateUnbondingRecords(
 			if !recordUnbondedBeforeProp {
 				hostZone, found := sk.GetHostZone(ctx, hostZoneUnbonding.HostZoneId)
 				if !found {
-					return errorsmod.Wrapf(stakeibctypes.ErrHostZoneNotFound, "unable to find host zone with chain-id %s", hostZoneUnbonding.HostZoneId)
+					return errorsmod.Wrapf(stakeibctypes.ErrHostZoneNotFound,
+						"unable to find host zone with chain-id %s", hostZoneUnbonding.HostZoneId)
 				}
 
 				redemptionRateDuringProp := redemptionRatesDuringProp[hostZoneUnbonding.HostZoneId]
@@ -99,17 +103,27 @@ func UpdateUnbondingRecords(
 			}
 
 			// now update all userRedemptionRecords by using the redemption rate to set the native token amount
+			totalNativeAmount := sdkmath.ZeroInt()
 			for _, userRedemptionRecordId := range hostZoneUnbonding.UserRedemptionRecords {
 				userRedemptionRecord, found := rk.GetUserRedemptionRecord(ctx, userRedemptionRecordId)
 				if !found {
-					return errorsmod.Wrapf(recordtypes.ErrHostUnbondingRecordNotFound, "unable to find user redemption record with id %s", userRedemptionRecordId)
+					return errorsmod.Wrapf(recordtypes.ErrHostUnbondingRecordNotFound,
+						"unable to find user redemption record with id %s", userRedemptionRecordId)
 				}
-				userRedemptionRecord.NativeTokenAmount = sdk.NewDecFromInt(userRedemptionRecord.StTokenAmount).Mul(recordRedemptionRate).TruncateInt()
+
+				userNativeAmount := sdk.NewDecFromInt(userRedemptionRecord.StTokenAmount).Mul(recordRedemptionRate).TruncateInt()
+				totalNativeAmount = totalNativeAmount.Add(userNativeAmount)
+
+				userRedemptionRecord.NativeTokenAmount = userNativeAmount
 				rk.SetUserRedemptionRecord(ctx, userRedemptionRecord)
 			}
 
 			// finally, update the hostZoneUnbonding record
-			return rk.SetHostZoneUnbondingRecord(ctx, epochUnbondingRecord.EpochNumber, hostZoneUnbonding.HostZoneId, *hostZoneUnbonding)
+			hostZoneUnbonding.NativeTokenAmount = totalNativeAmount
+			if err := rk.SetHostZoneUnbondingRecord(ctx, epochNumber, chainId, *hostZoneUnbonding); err != nil {
+				return errorsmod.Wrapf(err, "unable to set host zone unbonding for %s and epoch %d",
+					hostZoneUnbonding.HostZoneId, epochUnbondingRecord.EpochNumber)
+			}
 		}
 	}
 	return nil
