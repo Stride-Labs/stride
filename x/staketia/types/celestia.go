92bc9fb0440adb6f6f5091f59bd310f28c0e0745
sampocs <sam.pochyly@gmail.com>
2024-02-29 16:46:46 -0600

    debugging gosec error

--- x/staketia/types/celestia.go
+++ x/staketia/types/celestia.go
@@ -5,7 +5,7 @@ const (
 	CelestiaChainId                   = "celestia"
 	StrideToCelestiaTransferChannelId = "channel-162"
 	CelestiaNativeTokenDenom          = "utia"
-	CelestiaNativeTokenIBCDenom       = "ibc/BF3B4F53F3694B66E13C23107C84B6485BD2B96296BB7EC680EA77BBA75B4801"
+	CelestiaNativeTokenIBCDenom       = "ibc/BF3B4F53F3694B66E13C23107C84B6485BD2B96296BB7EC680EA77BBA75B4801" // #nosec G101
 
 	DelegationAddressOnCelestia = "celestia1d6ntc7s8gs86tpdyn422vsqc6uaz9cejnxz5p5" // C0
 	RewardAddressOnCelestia     = "celestia15up3hegy8zuqhy0p9m8luh0c984ptu2g5p4xpf" // C1
