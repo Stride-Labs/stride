fb6871974f5554b50b9ea83d3e9de4ede4c1434f
sampocs <sam.pochyly@gmail.com>
2024-03-01 11:08:11 -0600

    added ICS migration

--- app/upgrades/v19/upgrades.go
+++ app/upgrades/v19/upgrades.go
@@ -1,12 +1,14 @@
 package v19
 
 import (
+	errorsmod "cosmossdk.io/errors"
 	wasmkeeper "github.com/CosmWasm/wasmd/x/wasm/keeper"
 	wasmtypes "github.com/CosmWasm/wasmd/x/wasm/types"
 	"github.com/cosmos/cosmos-sdk/codec"
 	sdk "github.com/cosmos/cosmos-sdk/types"
 	"github.com/cosmos/cosmos-sdk/types/module"
 	upgradetypes "github.com/cosmos/cosmos-sdk/x/upgrade/types"
+	ccvconsumerkeeper "github.com/cosmos/interchain-security/v4/x/ccv/consumer/keeper"
 )
 
 const (
@@ -18,6 +20,7 @@ func CreateUpgradeHandler(
 	mm *module.Manager,
 	configurator module.Configurator,
 	cdc codec.Codec,
+	consumerKeeper ccvconsumerkeeper.Keeper,
 	wasmKeeper wasmkeeper.Keeper,
 ) upgradetypes.UpgradeHandler {
 	return func(ctx sdk.Context, _ upgradetypes.Plan, vm module.VersionMap) (module.VersionMap, error) {
@@ -27,7 +30,12 @@ func CreateUpgradeHandler(
 		ctx.Logger().Info("Running module migrations...")
 		newVm, err := mm.RunMigrations(ctx, configurator, vm)
 		if err != nil {
-			return newVm, err
+			return newVm, errorsmod.Wrapf(err, "unable to run module migrations")
+		}
+
+		// Migrate ICS to v4
+		if err := MigrateICSOutstandingDowntime(ctx, consumerKeeper); err != nil {
+			return newVm, errorsmod.Wrapf(err, "unable to migrate ICS to v14")
 		}
 
 		// Update wasm params so that contracts can only be uploaded through governance
@@ -43,4 +51,23 @@ func CreateUpgradeHandler(
 
 		return newVm, nil
 	}
+}
+
+// Migrates Outstanding Downtime for upgrade to ICS v4
+// https://github.com/cosmos/interchain-security/blob/release/v4.0.x/UPGRADING.md#v40x
+func MigrateICSOutstandingDowntime(ctx sdk.Context, ck ccvconsumerkeeper.Keeper) error {
+	ctx.Logger().Info("Migrating ICS outstanding downtime...")
+
+	downtimes := ck.GetAllOutstandingDowntimes(ctx)
+	for _, od := range downtimes {
+		consAddr, err := sdk.ConsAddressFromBech32(od.ValidatorConsensusAddress)
+		if err != nil {
+			return err
+		}
+		ck.DeleteOutstandingDowntime(ctx, consAddr)
+	}
+
+	ctx.Logger().Info("Finished ICS outstanding downtime")
+
+	return nil
 }
