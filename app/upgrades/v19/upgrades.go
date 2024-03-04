57c7f3156f4a56544fbca377b6eb2f2713a8301e
sampocs <sam.pochyly@gmail.com>
2024-03-04 14:26:40 -0600

    Revert "moved wasm permissions to own function"
    This reverts commit 7de7ff07862013177ea52a3f925bd3979e30614b.

--- app/upgrades/v19/upgrades.go
+++ app/upgrades/v19/upgrades.go
@@ -39,8 +39,14 @@ func CreateUpgradeHandler(
 		}
 
 		// Update wasm params so that contracts can only be uploaded through governance
-		if err := SetWasmPermissions(ctx, wasmKeeper); err != nil {
-			return newVm, errorsmod.Wrapf(err, "unable to set wasm permissions")
+		wasmParams := wasmKeeper.GetParams(ctx)
+		wasmParams.CodeUploadAccess = wasmtypes.AccessConfig{
+			Permission: wasmtypes.AccessTypeNobody,
+			Addresses:  []string{},
+		}
+		wasmParams.InstantiateDefaultPermission = wasmtypes.AccessTypeNobody
+		if err := wasmKeeper.SetParams(ctx, wasmParams); err != nil {
+			return newVm, err
 		}
 
 		return newVm, nil
@@ -63,19 +69,5 @@ func MigrateICSOutstandingDowntime(ctx sdk.Context, ck ccvconsumerkeeper.Keeper)
 
 	ctx.Logger().Info("Finished ICS outstanding downtime")
 
-	return nil
-}
-
-// Update wasm params so that contracts can only be uploaded through governance
-func SetWasmPermissions(ctx sdk.Context, wk wasmkeeper.Keeper) error {
-	wasmParams := wk.GetParams(ctx)
-	wasmParams.CodeUploadAccess = wasmtypes.AccessConfig{
-		Permission: wasmtypes.AccessTypeNobody,
-		Addresses:  []string{},
-	}
-	wasmParams.InstantiateDefaultPermission = wasmtypes.AccessTypeNobody
-	if err := wk.SetParams(ctx, wasmParams); err != nil {
-		return err
-	}
 	return nil
 }
