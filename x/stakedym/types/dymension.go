ae97aab6973544d0fe9750c1b3b1359c1ff4a5b2
vish-stride <vishal@stridelabs.co>
2024-03-07 12:56:26 -0500

    updated dymension.go

--- x/stakedym/types/dymension.go
+++ x/stakedym/types/dymension.go
@@ -5,16 +5,16 @@ const (
 	DymensionChainId                   = "dymension_1100-1"
 	StrideToDymensionTransferChannelId = "channel-197"
 	DymensionNativeTokenDenom          = "adym"
-	DymensionNativeTokenIBCDenom       = "ibc/E1C22332C083574F3418481359733BA8887D171E76C80AD9237422AEABB66018" // #nosec G101
+	DymensionNativeTokenIBCDenom       = "ibc/FF6C2E86490C1C4FBBD24F55032831D2415B9D7882F85C3CC9C2401D79362BEA" // #nosec G101
 
-	DelegationAddressOnDymension = "dym1nwpk5ekw74tl9eswvumvnwr3y7wwg99d8m0vzj" // C0
-	RewardAddressOnDymension     = "dym1exfargyp2lpe2yscpe0406zzj4vfedqn5uvw72" // C1
+	DelegationAddressOnDymension = "dym1gl9j2hyyukqvlmzzcxl99mqfgu4y4frgzlv3zz" // C0
+	RewardAddressOnDymension     = "dym1ww3m6h5e3dk2musft9w8f2zu4rkuxgh6zwu0d0" // C1
 
-	DepositAddress    = "stride1z9n8gk3837pagnnqv23ukruh8t606d0aj8u784" // S0
-	RedemptionAddress = "stride10vmnxwgf4647nqxd9a6sh4qlyeytk5tpy2wtku" // S1
-	ClaimAddress      = "stride13nw9fm4ua8pwzmsx9kdrhefl4puz0tp7ge3gxd" // S2
+	DepositAddress    = "stride1e7j8d6sdq272fqe2jfxjpgcagn04j75w9695fj" // S4
+	RedemptionAddress = "stride1jpsnc0ynufa2aheflj6mxzzzsu7nlwqk7ff69n" // S5
+	ClaimAddress      = "stride1q8juddwptg5yxyghh3n243pp4w8ctpvpmf6ras" // S6
 
-	SafeAddressOnStride             = "stride1gatmguzwv9p6y8amz32457094z8hjevezlfp4m" // S3
-	OperatorAddressOnStride         = "stride15s6xjemlhg3dqqeqhyu273ucfv56fss7l5tgf7" // OP-STRIDE
+	SafeAddressOnStride             = "stride1sj8gyqeqecqhqu7em67hn2tjzhpkdf8wz5plh7" // S7
+	OperatorAddressOnStride         = "stride1ghhu67ttgmxrsyxljfl2tysyayswklvxs7pepw" // OP-STRIDE
 	DymensionUnbondingPeriodSeconds = uint64(21 * 24 * 60 * 60)                       // 21 days
 )
