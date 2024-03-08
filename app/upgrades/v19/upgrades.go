531aaa3bf639f1425f61d186b83d1e114fa55255
Aidan Salzmann <aidan@stridelabs.co>
2024-03-08 00:58:48 -0500

    revert upgrade-ics-4.0 (#1132)

--- app/upgrades/v19/upgrades.go
+++ app/upgrades/v19/upgrades.go
@@ -12,7 +12,6 @@ import (
 	sdk "github.com/cosmos/cosmos-sdk/types"
 	"github.com/cosmos/cosmos-sdk/types/module"
 	upgradetypes "github.com/cosmos/cosmos-sdk/x/upgrade/types"
-	ccvconsumerkeeper "github.com/cosmos/interchain-security/v4/x/ccv/consumer/keeper"
 )
 
 const (
@@ -32,7 +31,6 @@ func CreateUpgradeHandler(
 	mm *module.Manager,
 	configurator module.Configurator,
 	cdc codec.Codec,
-	consumerKeeper ccvconsumerkeeper.Keeper,
 	ratelimitKeeper ratelimitkeeper.Keeper,
 	wasmKeeper wasmkeeper.Keeper,
 ) upgradetypes.UpgradeHandler {
@@ -43,12 +41,7 @@ func CreateUpgradeHandler(
 		ctx.Logger().Info("Running module migrations...")
 		newVm, err := mm.RunMigrations(ctx, configurator, vm)
 		if err != nil {
-			return newVm, errorsmod.Wrapf(err, "unable to run module migrations")
-		}
-
-		// Migrate ICS to v4
-		if err := MigrateICSOutstandingDowntime(ctx, consumerKeeper); err != nil {
-			return newVm, errorsmod.Wrapf(err, "unable to migrate ICS to v14")
+			return newVm, err
 		}
 
 		// Update wasm upload permissions
@@ -66,24 +59,6 @@ func CreateUpgradeHandler(
 
 		return newVm, nil
 	}
-}
-
-// Migrates Outstanding Downtime for upgrade to ICS v4
-// https://github.com/cosmos/interchain-security/blob/release/v4.0.x/UPGRADING.md#v40x
-func MigrateICSOutstandingDowntime(ctx sdk.Context, ck ccvconsumerkeeper.Keeper) error {
-	ctx.Logger().Info("Migrating ICS outstanding downtime...")
-
-	downtimes := ck.GetAllOutstandingDowntimes(ctx)
-	for _, od := range downtimes {
-		consAddr, err := sdk.ConsAddressFromBech32(od.ValidatorConsensusAddress)
-		if err != nil {
-			return err
-		}
-		ck.DeleteOutstandingDowntime(ctx, consAddr)
-	}
-
-	ctx.Logger().Info("Finished ICS outstanding downtime")
-	return nil
 }
 
 // Migrate the rate limit module to the open sourced version
