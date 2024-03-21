36783e3945680880005697cdb1c6feaa1fb1d060
Riley Edmunds <riley@stridelabs.co>
2024-03-20 23:41:51 -0400

    functional with no unit test yet

--- app/upgrades/v20/upgrades.go
+++ app/upgrades/v20/upgrades.go
@@ -1,6 +1,8 @@
 package v20
 
 import (
+	"fmt"
+
 	sdk "github.com/cosmos/cosmos-sdk/types"
 	"github.com/cosmos/cosmos-sdk/types/module"
 	upgradetypes "github.com/cosmos/cosmos-sdk/x/upgrade/types"
@@ -24,6 +26,9 @@ func CreateUpgradeHandler(
 		ctx.Logger().Info("Starting upgrade v20...")
 
 		ctx.Logger().Info("Adding DYDX Community Pool Treasury Address...")
+		if err := SetDydxCommunityPoolTreasuryAddress(ctx, stakeIbcKeeper); err != nil {
+			return vm, err
+		}
 
 		return mm.RunMigrations(ctx, configurator, vm)
 	}
@@ -33,10 +38,16 @@ func CreateUpgradeHandler(
 func SetDydxCommunityPoolTreasuryAddress(ctx sdk.Context, stakeIbcKeeper stakeibckeeper.Keeper) error {
 
 	// Get the dydx host_zone
+	hostZone, found := stakeIbcKeeper.GetHostZone(ctx, dydxChainId)
+	if !found {
+		return fmt.Errorf("dydx host_zone not found")
+	}
 
 	// Set the treasury address
+	hostZone.CommunityPoolTreasuryAddress = dydxCPTreasuryAddress
 
 	// Save the dydx host_zone
+	stakeIbcKeeper.SetHostZone(ctx, hostZone)
 
 	return nil
 }
