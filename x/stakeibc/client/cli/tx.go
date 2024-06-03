bec987a5bd830faae08dbbe8356a4a09434b1acc
sampocs <sam.pochyly@gmail.com>
2024-06-03 16:42:49 -0500

    moved update delegation

--- x/stakeibc/client/cli/tx.go
+++ x/stakeibc/client/cli/tx.go
@@ -537,6 +537,37 @@ ex:
 	return cmd
 }
 
+func CmdUpdateValidatorSharesExchRate() *cobra.Command {
+	cmd := &cobra.Command{
+		Use:   "update-delegation [chainid] [valoper]",
+		Short: "Broadcast message update-delegation",
+		Args:  cobra.ExactArgs(2),
+		RunE: func(cmd *cobra.Command, args []string) (err error) {
+			argHostdenom := args[0]
+			argValoper := args[1]
+
+			clientCtx, err := client.GetClientTxContext(cmd)
+			if err != nil {
+				return err
+			}
+
+			msg := types.NewMsgUpdateValidatorSharesExchRate(
+				clientCtx.GetFromAddress().String(),
+				argHostdenom,
+				argValoper,
+			)
+			if err := msg.ValidateBasic(); err != nil {
+				return err
+			}
+			return tx.GenerateOrBroadcastTxCLI(clientCtx, cmd.Flags(), msg)
+		},
+	}
+
+	flags.AddTxFlagsToCmd(cmd)
+
+	return cmd
+}
+
 func CmdCalibrateDelegation() *cobra.Command {
 	cmd := &cobra.Command{
 		Use:   "calibrate-delegation [chainid] [valoper]",
