93035aeba7de6e11c5baf4e11497756608c37185
sampocs <sam.pochyly@gmail.com>
2024-06-03 17:03:54 -0500

    moved query list trade routes

--- x/stakeibc/client/cli/query.go
+++ x/stakeibc/client/cli/query.go
@@ -267,3 +267,28 @@ func CmdNextPacketSequence() *cobra.Command {
 
 	return cmd
 }
+
+func CmdListTradeRoutes() *cobra.Command {
+	cmd := &cobra.Command{
+		Use:   "list-trade-routes",
+		Short: "list all trade routes",
+		RunE: func(cmd *cobra.Command, args []string) error {
+			clientCtx := client.GetClientContextFromCmd(cmd)
+
+			queryClient := types.NewQueryClient(clientCtx)
+
+			params := &types.QueryAllTradeRoutes{}
+			res, err := queryClient.AllTradeRoutes(context.Background(), params)
+			if err != nil {
+				return err
+			}
+
+			return clientCtx.PrintProto(res)
+		},
+	}
+
+	flags.AddPaginationFlagsToCmd(cmd, cmd.Use)
+	flags.AddQueryFlagsToCmd(cmd)
+
+	return cmd
+}
