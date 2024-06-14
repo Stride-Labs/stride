fa0bde159f3d9231915b23c3f311042d98a371bf
Assaf Morami <assaf.morami@gmail.com>
2024-06-14 18:23:30 +0300

    Update ExecuteCLIExpectError to use clientCtx with codec

--- x/stakeibc/client/cli/cli_test.go
+++ x/stakeibc/client/cli/cli_test.go
@@ -10,12 +10,16 @@ import (
 	"github.com/stretchr/testify/require"
 
 	"github.com/cosmos/cosmos-sdk/client"
+
+	"github.com/Stride-Labs/stride/v22/testutil/network"
 )
 
 func ExecuteCLIExpectError(t *testing.T, cmd *cobra.Command, args []string, errorString string) {
 	sdk.GetConfig().SetBech32PrefixForAccount("stride", "stridepub")
 
-	clientCtx := client.Context{}.WithFromAddress(sdk.MustAccAddressFromBech32("stride10p3xzmnpdeshqctsv9ukzcm0vdhkuat52aucqd"))
+	clientCtx := client.Context{}.
+		WithFromAddress(sdk.MustAccAddressFromBech32("stride10p3xzmnpdeshqctsv9ukzcm0vdhkuat52aucqd")).
+		WithCodec(network.DefaultConfig().Codec)
 
 	_, err := clitestutil.ExecTestCLICmd(clientCtx, cmd, args)
 	require.ErrorContains(t, err, errorString)
