28046faf0ffb583c74eaaf5efc888f294dc56f82
sampocs <sam@stridelabs.co>
2024-06-24 15:42:48 -0500

    added wasm light client (#1224)

--- app/upgrades/v23/upgrades.go
+++ app/upgrades/v23/upgrades.go
@@ -4,6 +4,8 @@ import (
 	sdk "github.com/cosmos/cosmos-sdk/types"
 	"github.com/cosmos/cosmos-sdk/types/module"
 	upgradetypes "github.com/cosmos/cosmos-sdk/x/upgrade/types"
+	ibcwasmtypes "github.com/cosmos/ibc-go/modules/light-clients/08-wasm/types"
+	clientkeeper "github.com/cosmos/ibc-go/v7/modules/core/02-client/keeper"
 
 	stakeibckeeper "github.com/Stride-Labs/stride/v22/x/stakeibc/keeper"
 )
@@ -16,10 +18,14 @@ var (
 func CreateUpgradeHandler(
 	mm *module.Manager,
 	configurator module.Configurator,
+	clientKeeper clientkeeper.Keeper,
 	stakeibcKeeper stakeibckeeper.Keeper,
 ) upgradetypes.UpgradeHandler {
 	return func(ctx sdk.Context, _ upgradetypes.Plan, vm module.VersionMap) (module.VersionMap, error) {
 		ctx.Logger().Info("Starting upgrade v23...")
+
+		ctx.Logger().Info("Adding wasm client...")
+		AddWasmAllowedClient(ctx, clientKeeper)
 
 		ctx.Logger().Info("Migrating trade routes...")
 		MigrateTradeRoutes(ctx, stakeibcKeeper)
@@ -27,6 +33,13 @@ func CreateUpgradeHandler(
 		ctx.Logger().Info("Running module migrations...")
 		return mm.RunMigrations(ctx, configurator, vm)
 	}
+}
+
+// Add the wasm client to the IBC client's allowed clients
+func AddWasmAllowedClient(ctx sdk.Context, k clientkeeper.Keeper) {
+	params := k.GetParams(ctx)
+	params.AllowedClients = append(params.AllowedClients, ibcwasmtypes.Wasm)
+	k.SetParams(ctx, params)
 }
 
 // Migration to deprecate the trade config
