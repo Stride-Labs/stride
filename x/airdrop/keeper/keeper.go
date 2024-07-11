c1ac6c9c30dc03660286c265886096e98d64c46e
Assaf Morami <assaf.morami@gmail.com>
2024-07-11 15:19:55 +0300

    x/airdrop: implement a bit of storage functions
    - GetAirdropRecords
    - SetAirdropRecords
    - GetAllocationRecords
    - SetAllocationRecords

--- x/airdrop/keeper/keeper.go
+++ x/airdrop/keeper/keeper.go
@@ -3,10 +3,11 @@ package keeper
 import (
 	"fmt"
 
-	"cosmossdk.io/core/store"
 	errorsmod "cosmossdk.io/errors"
 	"cosmossdk.io/log"
 	"github.com/cosmos/cosmos-sdk/codec"
+	"github.com/cosmos/cosmos-sdk/store/prefix"
+	storetypes "github.com/cosmos/cosmos-sdk/store/types"
 	sdk "github.com/cosmos/cosmos-sdk/types"
 	sdkerrors "github.com/cosmos/cosmos-sdk/types/errors"
 
@@ -15,9 +16,9 @@ import (
 
 type (
 	Keeper struct {
-		cdc          codec.BinaryCodec
-		storeService store.KVStoreService
-		logger       log.Logger
+		cdc      codec.BinaryCodec
+		storeKey storetypes.StoreKey
+		logger   log.Logger
 
 		accountKeeper types.AccountKeeper
 		bankKeeper    types.BankKeeper
@@ -26,16 +27,16 @@ type (
 
 func NewKeeper(
 	cdc codec.BinaryCodec,
-	storeService store.KVStoreService,
+	storeKey storetypes.StoreKey,
 	logger log.Logger,
 
 	accountKeeper types.AccountKeeper,
 	bankKeeper types.BankKeeper,
 ) Keeper {
 	return Keeper{
-		cdc:          cdc,
-		storeService: storeService,
-		logger:       logger,
+		cdc:      cdc,
+		storeKey: storeKey,
+		logger:   logger,
 
 		accountKeeper: accountKeeper,
 		bankKeeper:    bankKeeper,
@@ -80,10 +81,60 @@ func (k Keeper) ClaimEarly(ctx sdk.Context, claimer string) error {
 	return nil
 }
 
-func (k Keeper) SetAirdropRecords(ctx sdk.Context, airdrop_record []types.AirdropRecord) error {
-	return nil
+func (k Keeper) GetAirdropRecords(ctx sdk.Context) []types.AirdropRecord {
+	// TODO add pagination?
+	store := prefix.NewStore(ctx.KVStore(k.storeKey), types.AirdropRecordsKeyPrefix)
+
+	iterator := store.Iterator(nil, nil)
+	defer iterator.Close()
+
+	allAirdrops := []types.AirdropRecord{}
+	for ; iterator.Valid(); iterator.Next() {
+
+		airdrop := types.AirdropRecord{}
+		k.cdc.MustUnmarshal(iterator.Value(), &airdrop)
+		allAirdrops = append(allAirdrops, airdrop)
+	}
+
+	return allAirdrops
 }
 
-func (k Keeper) SetAllocationRecords(ctx sdk.Context, allocation_record []types.AllocationRecord) error {
-	return nil
+func (k Keeper) SetAirdropRecords(ctx sdk.Context, airdropRecords []types.AirdropRecord) {
+	for _, airdrop := range airdropRecords {
+		store := prefix.NewStore(ctx.KVStore(k.storeKey), types.AirdropRecordsKeyPrefix)
+
+		key := types.AirdropRecordKeyPrefix(airdrop.Id)
+		value := k.cdc.MustMarshal(&airdrop)
+
+		store.Set(key, value)
+	}
+}
+
+func (k Keeper) GetAllocationRecords(ctx sdk.Context) []types.AirdropRecord {
+	// TODO add pagination?
+	store := prefix.NewStore(ctx.KVStore(k.storeKey), types.AllocationRecordsKeyPrefix)
+
+	iterator := store.Iterator(nil, nil)
+	defer iterator.Close()
+
+	allAllocations := []types.AirdropRecord{}
+	for ; iterator.Valid(); iterator.Next() {
+
+		airdrop := types.AirdropRecord{}
+		k.cdc.MustUnmarshal(iterator.Value(), &airdrop)
+		allAllocations = append(allAllocations, airdrop)
+	}
+
+	return allAllocations
+}
+
+func (k Keeper) SetAllocationRecords(ctx sdk.Context, allocationRecord []types.AllocationRecord) {
+	for _, allocation := range allocationRecord {
+		store := prefix.NewStore(ctx.KVStore(k.storeKey), types.AllocationRecordsKeyPrefix)
+
+		key := types.AllocationRecordKeyPrefix(allocation.AirdropId, allocation.UserAddress)
+		value := k.cdc.MustMarshal(&allocation)
+
+		store.Set(key, value)
+	}
 }
--- x/airdrop/types/keys.go
+++ x/airdrop/types/keys.go
@@ -1,5 +1,10 @@
 package types
 
+import (
+	fmt "fmt"
+	"strconv"
+)
+
 const (
 	// ModuleName defines the module name
 	ModuleName = "airdrop"
@@ -14,6 +19,20 @@ const (
 	FeeAddress = "staketia_fee_address"
 )
 
+var (
+	AirdropRecordsKeyPrefix    = KeyPrefix("airdrop-records")
+	AllocationRecordsKeyPrefix = KeyPrefix("allocation-records")
+)
+
+// Generates a key byte prefix from a string
 func KeyPrefix(p string) []byte {
 	return []byte(p)
+}
+
+func AirdropRecordKeyPrefix(airdropId uint64) []byte {
+	return KeyPrefix(strconv.FormatUint(airdropId, 10))
+}
+
+func AllocationRecordKeyPrefix(airdropId uint64, userAddress string) []byte {
+	return KeyPrefix(fmt.Sprintf("/%d/%s", airdropId, userAddress))
 }
