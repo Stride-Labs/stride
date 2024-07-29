3c494ff67cca7e4a496843ab793e0de325867980
Aidan Salzmann <aidan@stridelabs.co>
2024-07-29 16:22:07 -0400

    testing scripts

--- dockernet/scripts/airdrop/setup.sh
+++ dockernet/scripts/airdrop/setup.sh
@@ -31,61 +31,79 @@ current_time_with_minute_offset() {
 	fi
 }
 
-echo ">>> Creating admin accounts..."
-echo $DISTRIBUTOR_MNEMONIC | $STRIDE_MAIN_CMD keys add distributor --recover -- | grep -E "address|name"
-echo $ALLOCATOR_MNEMONIC | $STRIDE_MAIN_CMD keys add allocator --recover | grep -E "address|name"
-echo $LINKER_MNEMONIC | $STRIDE_MAIN_CMD keys add linker --recover | grep -E "address|name"
+# echo ">>> Creating admin accounts..."
+# echo $DISTRIBUTOR_MNEMONIC | $STRIDE_MAIN_CMD keys add distributor --recover -- | grep -E "address|name"
+# echo $ALLOCATOR_MNEMONIC | $STRIDE_MAIN_CMD keys add allocator --recover | grep -E "address|name"
+# echo $LINKER_MNEMONIC | $STRIDE_MAIN_CMD keys add linker --recover | grep -E "address|name"
 
-echo -e "\n>>> Creating claimer accounts..."
-echo $CLAIMER_1_MNEMONIC | $STRIDE_MAIN_CMD keys add claimer1 --recover | grep -E "address|name"
-echo $CLAIMER_2_MNEMONIC | $STRIDE_MAIN_CMD keys add claimer2 --recover | grep -E "address|name"
-echo $CLAIMER_3_MNEMONIC | $STRIDE_MAIN_CMD keys add claimer3 --recover | grep -E "address|name"
+# echo -e "\n>>> Creating claimer accounts..."
+# echo $CLAIMER_1_MNEMONIC | $STRIDE_MAIN_CMD keys add claimer1 --recover | grep -E "address|name"
+# echo $CLAIMER_2_MNEMONIC | $STRIDE_MAIN_CMD keys add claimer2 --recover | grep -E "address|name"
+# echo $CLAIMER_3_MNEMONIC | $STRIDE_MAIN_CMD keys add claimer3 --recover | grep -E "address|name"
 
-distributor_address=$($STRIDE_MAIN_CMD keys show distributor -a)
-allocator_address=$($STRIDE_MAIN_CMD keys show allocator -a)
-linker_address=$($STRIDE_MAIN_CMD keys show linker -a)
+# distributor_address=$($STRIDE_MAIN_CMD keys show distributor -a)
+# allocator_address=$($STRIDE_MAIN_CMD keys show allocator -a)
+# linker_address=$($STRIDE_MAIN_CMD keys show linker -a)
 
-claimer_1_address=$($STRIDE_MAIN_CMD keys show claimer1 -a)
-claimer_2_address=$($STRIDE_MAIN_CMD keys show claimer2 -a)
-claimer_3_address=$($STRIDE_MAIN_CMD keys show claimer3 -a)
+# claimer_1_address=$($STRIDE_MAIN_CMD keys show claimer1 -a)
+# claimer_2_address=$($STRIDE_MAIN_CMD keys show claimer2 -a)
+# claimer_3_address=$($STRIDE_MAIN_CMD keys show claimer3 -a)
 
-echo -e "\n>>> Funding admin accounts..."
-$STRIDE_MAIN_CMD tx bank send val1 $distributor_address 5000000000ustrd --from val1 -y | TRIM_TX
-sleep 3
-$STRIDE_MAIN_CMD tx bank send val1 $allocator_address 1ustrd --from val1 -y | TRIM_TX
-sleep 3
-$STRIDE_MAIN_CMD tx bank send val1 $linker_address 1ustrd --from val1 -y | TRIM_TX
-sleep 3
+# echo -e "\n>>> Funding admin accounts..."
+# $STRIDE_MAIN_CMD tx bank send val1 $distributor_address 5000000000ustrd --from val1 -y | TRIM_TX
+# sleep 3
+# $STRIDE_MAIN_CMD tx bank send val1 $allocator_address 1ustrd --from val1 -y | TRIM_TX
+# sleep 3
+# $STRIDE_MAIN_CMD tx bank send val1 $linker_address 1ustrd --from val1 -y | TRIM_TX
+# sleep 3
 
-echo -e "\n>>> Funding claimer accounts..."
-$STRIDE_MAIN_CMD tx bank send val1 $claimer_1_address 1ustrd --from val1 -y | TRIM_TX
-sleep 3
-$STRIDE_MAIN_CMD tx bank send val1 $claimer_2_address 1ustrd --from val1 -y | TRIM_TX
-sleep 3
-$STRIDE_MAIN_CMD tx bank send val1 $claimer_3_address 1ustrd --from val1 -y | TRIM_TX
-sleep 3
+# echo -e "\n>>> Funding claimer accounts..."
+# $STRIDE_MAIN_CMD tx bank send val1 $claimer_1_address 1ustrd --from val1 -y | TRIM_TX
+# sleep 3
+# $STRIDE_MAIN_CMD tx bank send val1 $claimer_2_address 1ustrd --from val1 -y | TRIM_TX
+# sleep 3
+# $STRIDE_MAIN_CMD tx bank send val1 $claimer_3_address 1ustrd --from val1 -y | TRIM_TX
+# sleep 3
 
-echo -e "\n>>> Creating airdrop..."
-$STRIDE_MAIN_CMD tx airdrop create-airdrop $AIRDROP_NAME \
-  	--distribution-start-date  $(current_time_with_minute_offset 1) \
-	--distribution-end-date    $(current_time_with_minute_offset 20) \
-	--clawback-date            $(current_time_with_minute_offset 30) \
-	--claim-type-deadline-date $(current_time_with_minute_offset 10) \
-	--early-claim-penalty      0.5 \
-	--distributor-address      $distributor_address \
-	--allocator-address        $allocator_address \
-	--linker-address           $linker_address \
-	--from admin -y | TRIM_TX
-sleep 3
+# echo -e "\n>>> Creating airdrop..."
+# $STRIDE_MAIN_CMD tx airdrop create-airdrop $AIRDROP_NAME \
+#   	--distribution-start-date  $(current_time_with_minute_offset 1) \
+# 	--distribution-end-date    $(current_time_with_minute_offset 20) \
+# 	--clawback-date            $(current_time_with_minute_offset 30) \
+# 	--claim-type-deadline-date $(current_time_with_minute_offset 10) \
+# 	--early-claim-penalty      0.3 \
+# 	--distributor-address      $distributor_address \
+# 	--allocator-address        $allocator_address \
+# 	--linker-address           $linker_address \
+# 	--from admin -y | TRIM_TX
+# sleep 3
 
-echo -e "\n>>> Adding allocations..."
-$STRIDE_MAIN_CMD tx airdrop add-allocations $AIRDROP_NAME ${SCRIPT_DIR}/allocations.csv \
-    --from allocator -y --gas 1000000 | TRIM_TX 
-sleep 3
+# echo -e "\n>>> Adding allocations..."
+# $STRIDE_MAIN_CMD tx airdrop add-allocations $AIRDROP_NAME ${SCRIPT_DIR}/allocations.csv \
+#     --from allocator -y --gas 1000000 | TRIM_TX 
+# sleep 3
 
-echo -e "\n>>> Airdrops:"
-$STRIDE_MAIN_CMD q airdrop airdrops
+# echo -e "\n>>> Airdrops:"
+# $STRIDE_MAIN_CMD q airdrop airdrops
 
-echo -e "\n>>> Allocations:"
-$STRIDE_MAIN_CMD q airdrop all-allocations $AIRDROP_NAME | head -n 10 
-echo "..."
+# echo -e "\n>>> Allocations:"
+# $STRIDE_MAIN_CMD q airdrop all-allocations $AIRDROP_NAME | head -n 10 
+# echo "..."
+
+# TEST 0.3 forfeiture
+# claimer1=stride1qtzlx93h8xlmej42pjqyez6yp9nscfgxsmtt59
+# $STRIDE_MAIN_CMD q airdrop user-allocation $AIRDROP_NAME stride1qtzlx93h8xlmej42pjqyez6yp9nscfgxsmtt59 | head -n 30
+# $STRIDE_MAIN_CMD tx airdrop claim-early $AIRDROP_NAME --from claimer1 -y --gas 1000000 | TRIM_TX
+# $STRIDE_MAIN_CMD q bank balances stride1qtzlx93h8xlmej42pjqyez6yp9nscfgxsmtt59 --> 700001
+# 700000 / 1000000 => 0.7
+
+
+# TEST DISTRIBUTOR NOT FUNDED
+# $STRIDE_MAIN_CMD q bank balances stride1wxv8jnusl5s2che9kp33jrsxz3kxcwm6fgfs0z
+# $STRIDE_MAIN_CMD tx bank send stride1wxv8jnusl5s2che9kp33jrsxz3kxcwm6fgfs0z stride1v0t265yce4pwdvmzs4ew0ll4fnk2t6updfg6hf 4999300000ustrd --from distributor -y --gas 1000000 | TRIM_TX 
+# $STRIDE_MAIN_CMD q bank balances stride1v0t265yce4pwdvmzs4ew0ll4fnk2t6updfg6hf
+# $STRIDE_MAIN_CMD tx airdrop claim-early $AIRDROP_NAME --from claimer2 -y --gas 1000000 | TRIM_TX
+# $STRIDE_MAIN_CMD q tx BC965ED7A2BAF680E2A082130DDD1B267F6CD56881279FF0D88BA7D64FA338AC
+# raw_log: 'failed to execute message; message index: 0: unable to distribute rewards:
+#   spendable balance  is smaller than 4900000ustrd: insufficient funds'
+# timestamp: "2024-07-29T20:07:57Z"
