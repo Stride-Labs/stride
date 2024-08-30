00b9f40b964698bb51239984494303d51ce22409
sampocs <sam@stridelabs.co>
2024-08-29 20:20:08 -0500

    loosened outer redemption rate bounds for all host zones (#1280)

--- app/upgrades/v24/upgrades_test.go
+++ app/upgrades/v24/upgrades_test.go
@@ -3,15 +3,22 @@ package v24_test
 import (
 	"testing"
 
-	"github.com/stretchr/testify/suite"
-
 	sdkmath "cosmossdk.io/math"
+	sdk "github.com/cosmos/cosmos-sdk/types"
+	"github.com/stretchr/testify/suite"
 
 	"github.com/Stride-Labs/stride/v24/app/apptesting"
 	v24 "github.com/Stride-Labs/stride/v24/app/upgrades/v24"
 	recordstypes "github.com/Stride-Labs/stride/v24/x/records/types"
 	stakeibctypes "github.com/Stride-Labs/stride/v24/x/stakeibc/types"
 )
+
+type UpdateRedemptionRateBounds struct {
+	ChainId                        string
+	CurrentRedemptionRate          sdk.Dec
+	ExpectedMinOuterRedemptionRate sdk.Dec
+	ExpectedMaxOuterRedemptionRate sdk.Dec
+}
 
 type UpgradeTestSuite struct {
 	apptesting.AppTestHelper
@@ -59,6 +66,8 @@ func (s *UpgradeTestSuite) TestUpgrade() {
 		},
 	})
 
+	checkRedemptionRatesAfterUpgrade := s.SetupTestUpdateRedemptionRateBounds()
+
 	// Run the upgrade
 	s.ConfirmUpgradeSucceededs(v24.UpgradeName, upgradeHeight)
 
@@ -79,6 +88,8 @@ func (s *UpgradeTestSuite) TestUpgrade() {
 	s.Require().Equal(stTokens.Int64(), hostZoneUnbonding.StTokensToBurn.Int64(), "sttokens to burn")
 	s.Require().Equal(nativeTokens.Int64(), hostZoneUnbonding.NativeTokensToUnbond.Int64(), "native to unbond")
 	s.Require().Zero(hostZoneUnbonding.ClaimableNativeTokens.Int64(), "claimable tokens")
+
+	checkRedemptionRatesAfterUpgrade()
 }
 
 func (s *UpgradeTestSuite) TestMigrateHostZones() {
@@ -254,5 +265,51 @@ func (s *UpgradeTestSuite) TestMigrateEpochUnbondingRecords() {
 			"%s claimable native", tc.chainId)
 		s.Require().Equal(tc.expectedUndelegationTxsInProgress, hostZoneUnbonding.UndelegationTxsInProgress,
 			"%s undelegation txs in progress", tc.chainId)
+	}
+}
+
+func (s *UpgradeTestSuite) SetupTestUpdateRedemptionRateBounds() func() {
+	// Define test cases consisting of an initial redemption rate and expected bounds
+	testCases := []UpdateRedemptionRateBounds{
+		{
+			ChainId:                        "chain-0",
+			CurrentRedemptionRate:          sdk.MustNewDecFromStr("1.0"),
+			ExpectedMinOuterRedemptionRate: sdk.MustNewDecFromStr("0.95"), // 1 - 5% = 0.95
+			ExpectedMaxOuterRedemptionRate: sdk.MustNewDecFromStr("1.10"), // 1 + 10% = 1.1
+		},
+		{
+			ChainId:                        "chain-1",
+			CurrentRedemptionRate:          sdk.MustNewDecFromStr("1.1"),
+			ExpectedMinOuterRedemptionRate: sdk.MustNewDecFromStr("1.045"), // 1.1 - 5% = 1.045
+			ExpectedMaxOuterRedemptionRate: sdk.MustNewDecFromStr("1.210"), // 1.1 + 10% = 1.21
+		},
+		{
+			// Max outer for osmo uses 12% instead of 10%
+			ChainId:                        v24.OsmosisChainId,
+			CurrentRedemptionRate:          sdk.MustNewDecFromStr("1.25"),
+			ExpectedMinOuterRedemptionRate: sdk.MustNewDecFromStr("1.1875"), // 1.25 - 5% = 1.1875
+			ExpectedMaxOuterRedemptionRate: sdk.MustNewDecFromStr("1.4000"), // 1.25 + 12% = 1.400
+		},
+	}
+
+	// Create a host zone for each test case
+	for _, tc := range testCases {
+		hostZone := stakeibctypes.HostZone{
+			ChainId:        tc.ChainId,
+			RedemptionRate: tc.CurrentRedemptionRate,
+		}
+		s.App.StakeibcKeeper.SetHostZone(s.Ctx, hostZone)
+	}
+
+	// Return callback to check store after upgrade
+	return func() {
+		// Confirm they were all updated
+		for _, tc := range testCases {
+			hostZone, found := s.App.StakeibcKeeper.GetHostZone(s.Ctx, tc.ChainId)
+			s.Require().True(found)
+
+			s.Require().Equal(tc.ExpectedMinOuterRedemptionRate, hostZone.MinRedemptionRate, "%s - min outer", tc.ChainId)
+			s.Require().Equal(tc.ExpectedMaxOuterRedemptionRate, hostZone.MaxRedemptionRate, "%s - max outer", tc.ChainId)
+		}
 	}
 }
