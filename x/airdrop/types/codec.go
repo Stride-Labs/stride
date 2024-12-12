4e2d3bf8b26b12506c780c2bf77e78267d47e13a
Assaf Morami <assaf.morami@gmail.com>
2024-12-12 15:03:19 +0200

    added gov codec registration for legacy amino support

--- x/airdrop/types/codec.go
+++ x/airdrop/types/codec.go
@@ -7,6 +7,7 @@ import (
 	cryptocodec "github.com/cosmos/cosmos-sdk/crypto/codec"
 	sdk "github.com/cosmos/cosmos-sdk/types"
 	"github.com/cosmos/cosmos-sdk/types/msgservice"
+	govcodec "github.com/cosmos/cosmos-sdk/x/gov/codec"
 )
 
 func RegisterLegacyAminoCodec(cdc *codec.LegacyAmino) {
@@ -42,4 +43,8 @@ func init() {
 	RegisterLegacyAminoCodec(Amino)
 	cryptocodec.RegisterCrypto(Amino)
 	sdk.RegisterLegacyAminoCodec(Amino)
+
+	// Register all Amino interfaces and concrete types on the authz and gov Amino codec so that this can later be
+	// used to properly serialize MsgSubmitProposal instances
+	RegisterLegacyAminoCodec(govcodec.Amino)
 }
