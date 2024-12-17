97cccc7772373739ef39d274dd4484779d475a07
Assaf Morami <assaf.morami@gmail.com>
2024-12-17 16:14:15 +0200

    removes unused bank keeper functions

--- x/auction/types/expected_keepers.go
+++ x/auction/types/expected_keepers.go
@@ -13,10 +13,7 @@ type AccountKeeper interface {
 // Required BankKeeper functions
 type BankKeeper interface {
 	GetBalance(ctx sdk.Context, addr sdk.AccAddress, denom string) sdk.Coin
-	GetSupply(ctx sdk.Context, denom string) sdk.Coin
-	MintCoins(ctx sdk.Context, moduleName string, amt sdk.Coins) error
 	SendCoins(ctx sdk.Context, from, to sdk.AccAddress, amt sdk.Coins) error
-	SendCoinsFromAccountToModule(ctx sdk.Context, senderAddr sdk.AccAddress, recipientModule string, amt sdk.Coins) error
 	SendCoinsFromModuleToAccount(ctx sdk.Context, senderModule string, recipientAddr sdk.AccAddress, amt sdk.Coins) error
 }
 
