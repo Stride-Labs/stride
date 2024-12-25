31e5c977cf1ef04ea9b237b08acff7d24594e4b5
Assaf Morami <assaf.morami@gmail.com>
2024-12-25 20:01:23 +0200

    dockernet: fix GET_VAL_ADDR() for sdk 0.50

--- dockernet/config.sh
+++ dockernet/config.sh
@@ -512,7 +512,7 @@ GET_VAL_ADDR() {
   val_index=$2
 
   MAIN_CMD=$(GET_VAR_VALUE ${chain}_MAIN_CMD)
-  $MAIN_CMD q staking validators | grep ${chain}_${val_index} -A 6 | grep operator | awk '{print $2}'
+  $MAIN_CMD q staking validators 2>&1 | grep ${chain}_${val_index} -A 6 | grep operator | awk '{print $2}'
 }
 
 GET_ICA_ADDR() {
