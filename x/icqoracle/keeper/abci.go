cd727f275f1201e53ad5b2f8722b3551a269e8a3
sampocs <sam.pochyly@gmail.com>
2025-02-04 09:49:46 -0600

    updated abci

--- x/icqoracle/keeper/abci.go
+++ x/icqoracle/keeper/abci.go
@@ -7,41 +7,25 @@ import (
 )
 
 func (k Keeper) BeginBlocker(ctx sdk.Context) {
-	// Get all token prices
 	params, err := k.GetParams(ctx)
 	if err != nil {
-		// Can't really do anything but log
-		// A panic would halt the chain
 		ctx.Logger().Error("failed to get icqoracle params: %w", err)
 		return
 	}
 
 	currentTime := ctx.BlockTime()
 
-	// Iterate through each token price
-	tokenPrices := k.GetAllTokenPrices(ctx)
-	for _, tokenPrice := range tokenPrices {
+	for _, tokenPrice := range k.GetAllTokenPrices(ctx) {
 		// Get last update time for this token
 		lastUpdate := tokenPrice.LastRequestTime
-
-		// If never updated or update interval has passed
 		isNewToken := lastUpdate.IsZero()
 		updateIntervalPassed := currentTime.Sub(lastUpdate) >= time.Second*time.Duration(params.UpdateIntervalSec)
-		if isNewToken || updateIntervalPassed {
-			// If the token is in progress, time it out
-			if tokenPrice.QueryInProgress {
-				queryId, err := k.GetOsmosisCLPoolQueryId(ctx, tokenPrice)
-				if err != nil {
-					ctx.Logger().Error("Failed to get osmosis query ID: %w", err)
-					continue
-				}
-				k.IcqKeeper.ForceQueryTimeout(ctx, queryId)
-			}
 
-			// Update price for this specific token
-			err := k.SubmitOsmosisClPoolICQ(ctx, tokenPrice)
-			if err != nil {
-				// Can't really do anything but log
+		// If never updated or update interval has passed, submit a new query for the price
+		// If a query was already in progress, it will be replaced with this new one that will
+		// will have the same query ID
+		if isNewToken || updateIntervalPassed {
+			if err := k.SubmitOsmosisClPoolICQ(ctx, tokenPrice); err != nil {
 				ctx.Logger().Error(
 					"failed to submit Osmosis CL pool ICQ baseToken='%s' quoteToken='%s' poolId='%s': %w",
 					err,
