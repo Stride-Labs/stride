6904256bdb3d3c388b1a4d441529836d160bcddf
sampocs <sam@stridelabs.co>
2025-02-04 14:39:39 -0600

    moved abci stuff to own func (#1348)

--- x/icqoracle/keeper/abci.go
+++ x/icqoracle/keeper/abci.go
@@ -1,40 +1,53 @@
 package keeper
 
 import (
+	"fmt"
 	"time"
 
+	errorsmod "cosmossdk.io/errors"
 	sdk "github.com/cosmos/cosmos-sdk/types"
+
+	"github.com/Stride-Labs/stride/v25/x/icqoracle/types"
 )
 
 func (k Keeper) BeginBlocker(ctx sdk.Context) {
 	params, err := k.GetParams(ctx)
 	if err != nil {
-		ctx.Logger().Error("failed to get icqoracle params: %w", err)
+		ctx.Logger().Error("Unable to fetch params")
 		return
 	}
 
-	currentTime := ctx.BlockTime()
-
 	for _, tokenPrice := range k.GetAllTokenPrices(ctx) {
-		// Get last update time for this token
-		lastUpdate := tokenPrice.LastRequestTime
-		isNewToken := lastUpdate.IsZero()
-		updateIntervalPassed := currentTime.Sub(lastUpdate) >= time.Second*time.Duration(params.UpdateIntervalSec)
-
-		// If never updated or update interval has passed, submit a new query for the price
-		// If a query was already in progress, it will be replaced with a new one that will
-		// have the same query ID
-		if isNewToken || updateIntervalPassed {
-			if err := k.SubmitOsmosisClPoolICQ(ctx, tokenPrice); err != nil {
-				ctx.Logger().Error(
-					"failed to submit Osmosis CL pool ICQ baseToken='%s' quoteToken='%s' poolId='%s': %w",
-					tokenPrice.BaseDenom,
-					tokenPrice.QuoteDenom,
-					tokenPrice.OsmosisPoolId,
-					err,
-				)
-				continue
-			}
+		if err := k.RefreshTokenPrice(ctx, tokenPrice, params.UpdateIntervalSec); err != nil {
+			ctx.Logger().Error(fmt.Sprintf("failed to refresh token price: %s", err.Error()))
+			continue
 		}
 	}
+}
+
+// Refreshes the price of a token (if applicable)
+func (k Keeper) RefreshTokenPrice(ctx sdk.Context, tokenPrice types.TokenPrice, updateIntervalSec uint64) error {
+	// Get last update time for this token
+	currentTime := ctx.BlockTime()
+	lastUpdate := tokenPrice.LastRequestTime
+	isNewToken := lastUpdate.IsZero()
+	updateIntervalPassed := currentTime.Sub(lastUpdate) >= time.Second*time.Duration(updateIntervalSec)
+
+	// If the update interval has not passed, don't update
+	if !isNewToken && !updateIntervalPassed {
+		return nil
+	}
+
+	// If never updated or update interval has passed, submit a new query for the price
+	// If a query was already in progress, it will be replaced with a new one that will
+	// have the same query ID
+	if err := k.SubmitOsmosisClPoolICQ(ctx, tokenPrice); err != nil {
+		return errorsmod.Wrapf(err,
+			"failed to submit Osmosis CL pool ICQ baseToken='%s' quoteToken='%s' poolId='%s'",
+			tokenPrice.BaseDenom,
+			tokenPrice.QuoteDenom,
+			tokenPrice.OsmosisPoolId)
+	}
+
+	return nil
 }
