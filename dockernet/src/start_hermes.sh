0a0556c91f9718c5646500b3de4554877abf86ae
sampocs <sam.pochyly@gmail.com>
2025-02-03 19:04:36 -0600

    updated dockernet scripts

---
+++ dockernet/src/start_hermes.sh
@@ -0,0 +1,35 @@
+#!/bin/bash
+
+set -eu 
+SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
+
+source ${SCRIPT_DIR}/../config.sh
+
+for chain in ${HOST_CHAINS[@]:-}; do
+    chain_id=$(GET_VAR_VALUE     ${chain}_CHAIN_ID)
+    chain_name=$(printf "$chain" | awk '{ print tolower($0) }')
+    account_name=$(GET_VAR_VALUE RELAYER_${chain}_ACCT)
+    mnemonic=$(GET_VAR_VALUE     RELAYER_${chain}_MNEMONIC)
+
+    hermes_logs=${LOGS}/hermes-${chain_name}.log
+    hermes_config=$STATE/hermes-${chain_name}
+    hermes_exec="$DOCKER_COMPOSE run --rm hermes-$chain_name hermes"
+
+    mkdir -p $hermes_config
+    cp ${DOCKERNET_HOME}/config/hermes_config_stride.toml $hermes_config/config.toml
+    echo "$mnemonic" > $hermes_config/mnemonic.txt
+    chmod -R 777 $hermes_config
+
+    printf "STRIDE <> $chain - Adding hermes keys..."
+    $hermes_exec keys add --chain STRIDE --mnemonic-file .hermes/mnemonic.txt >> $hermes_logs 2>&1
+    $hermes_exec keys add --chain $chain_id --mnemonic-file .hermes/mnemonic.txt >> $hermes_logs 2>&1
+    echo "Done"
+
+    printf "STRIDE <> $chain - Creating client, connection, and transfer channel..." | tee -a $hermes_logs
+    $hermes_exec create channel --a-chain STRIDE --b-chain $chain_id \
+            --a-port transfer --b-port transfer --new-client-connection --yes >> $hermes_logs 2>&1
+    echo "Done"
+
+    $DOCKER_COMPOSE up -d hermes-${chain_name}
+    SAVE_DOCKER_LOGS hermes-${chain_name} $hermes_logs
+done
