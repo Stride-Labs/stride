62d02bd28618e5fc5bfefffea5766aeb050da6bf
sampocs <sam@stridelabs.co>
2025-02-04 08:18:40 -0600

    v26 upgrade test (#1343)

--- app/upgrades/v26/upgrades_test.go
+++ app/upgrades/v26/upgrades_test.go
@@ -6,6 +6,7 @@ import (
 	"github.com/stretchr/testify/suite"
 
 	"github.com/Stride-Labs/stride/v25/app/apptesting"
+	v26 "github.com/Stride-Labs/stride/v25/app/upgrades/v26"
 )
 
 type UpgradeTestSuite struct {
@@ -21,4 +22,14 @@ func TestKeeperTestSuite(t *testing.T) {
 }
 
 func (s *UpgradeTestSuite) TestUpgrade() {
+	upgradeHeight := int64(4)
+
+	s.ConfirmUpgradeSucceededs(v26.UpgradeName, upgradeHeight)
+
+	params, err := s.App.ICQOracleKeeper.GetParams(s.Ctx)
+	s.Require().NoError(err, "No error expected when getting params")
+	s.Require().Equal(v26.OsmosisChainId, params.OsmosisChainId, "Osmosis chain ID")
+	s.Require().Equal(v26.OsmosisConnectionId, params.OsmosisConnectionId, "Osmosis connection ID")
+	s.Require().Equal(v26.ICQOracleUpdateIntervalSec, params.UpdateIntervalSec, "Update interval")
+	s.Require().Equal(v26.ICQOraclePriceExpirationTimeoutSec, params.PriceExpirationTimeoutSec, "Timeout")
 }
