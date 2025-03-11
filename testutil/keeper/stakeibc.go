26f8e347334a1e213391611ff6857ef6310fefda
Assaf Morami <assaf.morami@gmail.com>
2025-03-11 20:20:30 +0200

    s/NewContext/NewContextLegacy/

--- testutil/keeper/stakeibc.go
+++ testutil/keeper/stakeibc.go
@@ -14,7 +14,7 @@ import (
 func StakeibcKeeper(t testing.TB) (*keeper.Keeper, sdk.Context) {
 	app := strideapp.InitStrideTestApp(true)
 	stakeibcKeeper := app.StakeibcKeeper
-	ctx := app.BaseApp.NewContext(false, tmproto.Header{Height: 1, ChainID: "stride-1", Time: time.Now().UTC()})
+	ctx := app.BaseApp.NewContextLegacy(false, tmproto.Header{Height: 1, ChainID: "stride-1", Time: time.Now().UTC()})
 
 	return &stakeibcKeeper, ctx
 }
