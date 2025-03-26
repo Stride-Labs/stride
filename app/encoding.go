bc3efc99356d01167ddf1d92e618edc0abc6acf9
Assaf Morami <assaf.morami@gmail.com>
2025-03-26 15:18:27 +0200

    unit tests: use stride prefix

--- app/encoding.go
+++ app/encoding.go
@@ -3,6 +3,7 @@ package app
 import (
 	"github.com/cosmos/cosmos-sdk/client"
 	"github.com/cosmos/cosmos-sdk/codec"
+	"github.com/cosmos/cosmos-sdk/codec/testutil"
 	"github.com/cosmos/cosmos-sdk/codec/types"
 	"github.com/cosmos/cosmos-sdk/std"
 	"github.com/cosmos/cosmos-sdk/x/auth/tx"
@@ -19,7 +20,7 @@ type EncodingConfig struct {
 // MakeEncodingConfig creates an EncodingConfig for an amino based test configuration.
 func MakeEncodingConfig() EncodingConfig {
 	amino := codec.NewLegacyAmino()
-	interfaceRegistry := types.NewInterfaceRegistry()
+	interfaceRegistry := testutil.CodecOptions{AccAddressPrefix: "stride", ValAddressPrefix: "stridevaloper"}.NewInterfaceRegistry()
 	codec := codec.NewProtoCodec(interfaceRegistry)
 	txCfg := tx.NewTxConfig(codec, tx.DefaultSignModes)
 
