3bc253fe7dad854b539de1ac2087f4c19a561cdc
Assaf Morami <assaf.morami@gmail.com>
2025-03-28 11:30:56 +0300

    checkpoint
    now need to generate a new non validator address and try with it (https://github.com/cosmos/cosmos-sdk/blob/7b9d2ff98d/testutil/network/network.go#L487-L490), bc it might be succeeding emptily due to the validator already being registered

--- x/icaoracle/client/cli/cli_test.go
+++ x/icaoracle/client/cli/cli_test.go
@@ -2,12 +2,15 @@ package cli_test
 
 import (
 	"fmt"
+	"os"
 	"testing"
 
 	tmcli "github.com/cometbft/cometbft/libs/cli"
 	"github.com/cosmos/cosmos-sdk/client/flags"
+	"github.com/cosmos/cosmos-sdk/codec/address"
 	clitestutil "github.com/cosmos/cosmos-sdk/testutil/cli"
 	sdk "github.com/cosmos/cosmos-sdk/types"
+	stakingcli "github.com/cosmos/cosmos-sdk/x/staking/client/cli"
 	"github.com/spf13/cobra"
 	"github.com/stretchr/testify/suite"
 
@@ -18,9 +21,7 @@ import (
 	"github.com/Stride-Labs/stride/v26/x/icaoracle/types"
 )
 
-var (
-	HostChainId = "chain-1"
-)
+var HostChainId = "chain-1"
 
 type ClientTestSuite struct {
 	suite.Suite
@@ -57,6 +58,44 @@ func (s *ClientTestSuite) SetupSuite() {
 	s.val = s.network.Validators[0]
 
 	cmdcfg.RegisterDenoms()
+}
+
+func (s *ClientTestSuite) TestCmdCreateValidator() {
+	addrCodec := address.NewBech32Codec(sdk.GetConfig().GetBech32AccountAddrPrefix())
+	clientCtx := s.network.Validators[0].ClientCtx
+
+	f, err := os.CreateTemp("", "")
+	s.Require().NoError(err)
+	defer f.Close()
+
+	defaultFlags := []string{
+		fmt.Sprintf("--%s=%s", flags.FlagFrom, s.val.Address.String()),
+		fmt.Sprintf("--%s=true", flags.FlagSkipConfirmation),
+		fmt.Sprintf("--%s=%s", flags.FlagBroadcastMode, flags.BroadcastSync),
+		fmt.Sprintf("--%s=json", tmcli.OutputFlag),
+		strideclitestutil.DefaultFeeString(s.cfg),
+	}
+
+	validatorPubkey := `{"@type":"/cosmos.crypto.ed25519.PubKey","key":"efnXQZK3MZ6RM3rCdSA8RQbGh5L5NXoQNC3XX7J5gaY="}`
+	_, err = f.WriteString(`{
+		"pubkey": ` + validatorPubkey + `,
+		"amount": "1stake",
+		"moniker": "test-validator",
+		"commission-rate": "0.1",
+		"commission-max-rate": "0.2",
+		"commission-max-change-rate": "0.01",
+		"min-self-delegation": "1"
+		}`)
+	s.Require().NoError(err)
+
+	args := []string{
+		f.Name(),
+	}
+	args = append(args, defaultFlags...)
+
+	cmd := stakingcli.NewCreateValidatorCmd(addrCodec)
+	_, err = clitestutil.ExecTestCLICmd(clientCtx, cmd, args)
+	s.Require().ErrorContains(err, `invalid coin expression`)
 }
 
 func (s *ClientTestSuite) ExecuteTxAndCheckSuccessful(cmd *cobra.Command, args []string, description string) {
