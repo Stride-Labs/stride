10aaa19db2f6bb495f1804736f5854c960bcc802
sampocs <sam.pochyly@gmail.com>
2025-06-05 11:22:52 -0500

    scaffolded upgrade handler

---
+++ app/upgrades/v27/upgrades.go
@@ -0,0 +1,33 @@
+package v27
+
+import (
+	sdk "github.com/cosmos/cosmos-sdk/types"
+	"github.com/cosmos/cosmos-sdk/types/module"
+	upgradetypes "github.com/cosmos/cosmos-sdk/x/upgrade/types"
+
+	stakeibckeeper "github.com/Stride-Labs/stride/v26/x/stakeibc/keeper"
+)
+
+var (
+	UpgradeName = "v27"
+)
+
+// CreateUpgradeHandler creates an SDK upgrade handler for v23
+func CreateUpgradeHandler(
+	mm *module.Manager,
+	configurator module.Configurator,
+	stakeibcKeeper stakeibckeeper.Keeper,
+) upgradetypes.UpgradeHandler {
+	return func(ctx sdk.Context, _ upgradetypes.Plan, vm module.VersionMap) (module.VersionMap, error) {
+		ctx.Logger().Info("Starting upgrade v27...")
+
+		// Run migrations first
+		ctx.Logger().Info("Running module migrations...")
+		versionMap, err := mm.RunMigrations(ctx, configurator, vm)
+		if err != nil {
+			return nil, err
+		}
+
+		return versionMap, nil
+	}
+}
---
+++ app/upgrades/v27/upgrades_test.go
@@ -0,0 +1,28 @@
+package v27_test
+
+import (
+	"testing"
+
+	"github.com/stretchr/testify/suite"
+
+	"github.com/Stride-Labs/stride/v26/app/apptesting"
+	v27 "github.com/Stride-Labs/stride/v26/app/upgrades/v27"
+)
+
+type UpgradeTestSuite struct {
+	apptesting.AppTestHelper
+}
+
+func (s *UpgradeTestSuite) SetupTest() {
+	s.Setup()
+}
+
+func TestKeeperTestSuite(t *testing.T) {
+	suite.Run(t, new(UpgradeTestSuite))
+}
+
+func (s *UpgradeTestSuite) TestUpgrade() {
+	upgradeHeight := int64(4)
+
+	s.ConfirmUpgradeSucceededs(v27.UpgradeName, upgradeHeight)
+}
