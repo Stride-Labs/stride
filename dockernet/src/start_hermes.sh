560eec62c4d4dc79f88b39eb69c1e0eaaf92beda
sampocs <sam.pochyly@gmail.com>
2025-06-04 21:15:50 -0500

    updated hermes setup

--- dockernet/src/start_hermes.sh
+++ dockernet/src/start_hermes.sh
@@ -11,25 +11,26 @@ for chain in ${HOST_CHAINS[@]:-}; do
     account_name=$(GET_VAR_VALUE RELAYER_${chain}_ACCT)
     mnemonic=$(GET_VAR_VALUE     RELAYER_${chain}_MNEMONIC)
 
-    hermes_logs=${LOGS}/hermes-${chain_name}.log
-    hermes_config=$STATE/hermes-${chain_name}
-    hermes_exec="$DOCKER_COMPOSE run --rm hermes-$chain_name hermes"
+    hermes_logs=${LOGS}/hermes.log
+    hermes_config=$STATE/hermes
+    hermes_exec="$DOCKER_COMPOSE run --rm hermes hermes"
 
     mkdir -p $hermes_config
-    cp ${DOCKERNET_HOME}/config/hermes_config_stride.toml $hermes_config/config.toml
+    cp ${DOCKERNET_HOME}/config/hermes_config.toml $hermes_config/config.toml
     echo "$mnemonic" > $hermes_config/mnemonic.txt
     chmod -R 777 $hermes_config
 
     printf "STRIDE <> $chain - Adding hermes keys..."
-    $hermes_exec keys add --chain STRIDE --mnemonic-file .hermes/mnemonic.txt >> $hermes_logs 2>&1
-    $hermes_exec keys add --chain $chain_id --mnemonic-file .hermes/mnemonic.txt >> $hermes_logs 2>&1
+    $hermes_exec keys add --chain STRIDE --mnemonic-file /home/hermes/.hermes/mnemonic.txt >> $hermes_logs 2>&1
+    $hermes_exec keys add --chain $chain_id --mnemonic-file /home/hermes/.hermes/mnemonic.txt >> $hermes_logs 2>&1
     echo "Done"
 
-    printf "STRIDE <> $chain - Creating client, connection, and transfer channel..." | tee -a $hermes_logs
-    $hermes_exec create channel --a-chain STRIDE --b-chain $chain_id \
-            --a-port transfer --b-port transfer --new-client-connection --yes >> $hermes_logs 2>&1
-    echo "Done"
+    # Create clients with relayer 
+    # printf "STRIDE <> $chain - Creating client, connection, and transfer channel..." | tee -a $hermes_logs
+    # $hermes_exec create channel --a-chain STRIDE --b-chain $chain_id \
+    #         --a-port transfer --b-port transfer --new-client-connection --yes >> $hermes_logs 2>&1
+    # echo "Done"
 
-    $DOCKER_COMPOSE up -d hermes-${chain_name}
-    SAVE_DOCKER_LOGS hermes-${chain_name} $hermes_logs
+    $DOCKER_COMPOSE up -d hermes
+    SAVE_DOCKER_LOGS hermes $hermes_logs
 done
