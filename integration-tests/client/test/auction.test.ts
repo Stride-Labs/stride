4cf14ea414fd0356d6624acf747c44c47110325c
sampocs <sam.pochyly@gmail.com>
2025-08-02 11:36:11 -0500

    added expedited field to submit proposal in tests

--- integration-tests/client/test/auction.test.ts
+++ integration-tests/client/test/auction.test.ts
@@ -307,6 +307,7 @@ describe("Buyback and Burn", () => {
           metadata: "Update icqoracle params",
           title: "Update icqoracle params",
           summary: "Update icqoracle params",
+          expedited: false,
         }),
       ]);
       const proposalId = BigInt(getValueFromEvents(tx.events, "submit_proposal.proposal_id"));
