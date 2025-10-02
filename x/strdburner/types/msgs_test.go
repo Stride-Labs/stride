1612aad48ae0c45dc2cc8c7464d00e543a91a9d9
sampocs <sam.pochyly@gmail.com>
2025-10-02 09:29:52 -0500

    added msg types

---
+++ x/strdburner/types/msgs_test.go
@@ -0,0 +1,64 @@
+package types_test
+
+import (
+	"testing"
+
+	sdkmath "cosmossdk.io/math"
+	sdkerrors "github.com/cosmos/cosmos-sdk/types/errors"
+	"github.com/stretchr/testify/require"
+
+	"github.com/Stride-Labs/stride/v28/app/apptesting"
+	"github.com/Stride-Labs/stride/v28/x/strdburner/types"
+)
+
+func TestMsgBurn_ValidateBasic(t *testing.T) {
+	validAddress, _ := apptesting.GenerateTestAddrs()
+
+	tests := []struct {
+		name          string
+		msg           types.MsgBurn
+		expectedError string
+	}{
+		{
+			name: "valid inputs",
+			msg: types.MsgBurn{
+				Burner: validAddress,
+				Amount: sdkmath.NewInt(1200000),
+			},
+		},
+		{
+			name: "invalid address",
+			msg: types.MsgBurn{
+				Burner: "invalid_address",
+				Amount: sdkmath.NewInt(1000000),
+			},
+			expectedError: sdkerrors.ErrInvalidAddress.Error(),
+		},
+		{
+			name: "invalid address: wrong chain's bech32prefix",
+			msg: types.MsgBurn{
+				Burner: "celestia1yjq0n2ewufluenyyvj2y9sead9jfstpxnqv2xz",
+				Amount: sdkmath.NewInt(1000000),
+			},
+			expectedError: sdkerrors.ErrInvalidAddress.Error(),
+		},
+		{
+			name: "amount below threshold",
+			msg: types.MsgBurn{
+				Burner: validAddress,
+				Amount: sdkmath.NewInt(999999),
+			},
+			expectedError: "amount (999999ustrd) is below 1 STRD minimum",
+		},
+	}
+	for _, tc := range tests {
+		t.Run(tc.name, func(t *testing.T) {
+			actualError := tc.msg.ValidateBasic()
+			if tc.expectedError != "" {
+				require.ErrorContains(t, actualError, tc.expectedError)
+				return
+			}
+			require.NoError(t, actualError)
+		})
+	}
+}
