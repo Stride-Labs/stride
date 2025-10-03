90589e3ee79f81d63fb4bfa9b10df0048c0eb29e
sampocs <sam.pochyly@gmail.com>
2025-10-03 10:20:38 -0500

    added migration unit tests

--- app/upgrades/v29/upgrades_test.go
+++ app/upgrades/v29/upgrades_test.go
@@ -5,8 +5,12 @@ import (
 
 	"github.com/stretchr/testify/suite"
 
+	sdkmath "cosmossdk.io/math"
+	sdk "github.com/cosmos/cosmos-sdk/types"
+
 	"github.com/Stride-Labs/stride/v28/app/apptesting"
 	v29 "github.com/Stride-Labs/stride/v28/app/upgrades/v29"
+	strdburnertypes "github.com/Stride-Labs/stride/v28/x/strdburner/types"
 )
 
 type UpgradeTestSuite struct {
@@ -22,9 +26,30 @@ func TestKeeperTestSuite(t *testing.T) {
 }
 
 func (s *UpgradeTestSuite) TestUpgrade() {
+	// Set an initial total STRD burned
+	protocolBurnedAmount := sdkmath.NewInt(1000)
+	s.StoreLegacyTotalBurned(protocolBurnedAmount)
+
+	// Run the upgrade
 	s.ConfirmUpgradeSucceeded(v29.UpgradeName)
 
-	// Confirm state after upgrade
+	// Confirm CCV state after upgrade
 	consumerParams := s.App.ConsumerKeeper.GetConsumerParams(s.Ctx)
 	s.Require().Equal(consumerParams.ConsumerRedistributionFraction, "1.0")
+
+	// Confirm the burned total state after upgrade
+	actualTotal := s.App.StrdBurnerKeeper.GetTotalStrdBurned(s.Ctx)
+	actualProtocol := s.App.StrdBurnerKeeper.GetProtocolStrdBurned(s.Ctx)
+	actualUser := s.App.StrdBurnerKeeper.GetTotalUserStrdBurned(s.Ctx)
+
+	s.Require().Equal(protocolBurnedAmount, actualTotal, "total")
+	s.Require().Equal(protocolBurnedAmount, actualProtocol, "protocol")
+	s.Require().Equal(sdkmath.ZeroInt(), actualUser, "user")
+}
+
+// Helper to write to the legacy total store
+func (s *UpgradeTestSuite) StoreLegacyTotalBurned(amount sdkmath.Int) {
+	amountBz := sdk.Uint64ToBigEndian(amount.Uint64())
+	store := s.Ctx.KVStore(s.App.GetKey(strdburnertypes.StoreKey))
+	store.Set([]byte(strdburnertypes.TotalStrdBurnedKey), amountBz)
 }
