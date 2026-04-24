948e48c0821909defe7bb5617745d12da6ebdb99
sampocs <sam.pochyly@gmail.com>
2026-04-24 09:06:08 -0500

    tests for min deposit and max val weights

--- app/upgrades/v32/upgrades_test.go
+++ app/upgrades/v32/upgrades_test.go
@@ -7,6 +7,7 @@ import (
 
 	"github.com/Stride-Labs/stride/v32/app/apptesting"
 	v32 "github.com/Stride-Labs/stride/v32/app/upgrades/v32"
+	"github.com/Stride-Labs/stride/v32/utils"
 )
 
 type UpgradeTestSuite struct {
@@ -22,5 +23,35 @@ func TestKeeperTestSuite(t *testing.T) {
 }
 
 func (s *UpgradeTestSuite) TestUpgrade() {
+	// Setup state for steps that require it
+	checkValidatorWeights := s.SetupTestUpdateValidatorWeights()
+
+	// Run upgrade
 	s.ConfirmUpgradeSucceeded(v32.UpgradeName)
+
+	// Verify post-upgrade state
+	s.VerifyMinDepositIncreased()
+	s.VerifyMaxValidatorWeightIncreased()
+	checkValidatorWeights()
+}
+
+func (s *UpgradeTestSuite) VerifyMinDepositIncreased() {
+	params, err := s.App.GovKeeper.Params.Get(s.Ctx)
+	s.Require().NoError(err, "no error expected when getting gov params")
+	s.Require().Equal(utils.BaseStrideDenom, params.MinDeposit[0].Denom, "min deposit denom")
+	s.Require().Equal(v32.MinDeposit, params.MinDeposit[0].Amount, "min deposit amount")
+}
+
+func (s *UpgradeTestSuite) VerifyMaxValidatorWeightIncreased() {
+	params := s.App.StakeibcKeeper.GetParams(s.Ctx)
+	s.Require().Equal(v32.ValidatorWeightCap, params.ValidatorWeightCap, "validator weight cap")
+}
+
+func (s *UpgradeTestSuite) SetupTestUpdateValidatorWeights() func() {
+	// TODO: seed host zones with validators whose weights should be updated
+
+	// Return callback to check store after upgrade
+	return func() {
+		// TODO: assert validator weights were updated as expected
+	}
 }
