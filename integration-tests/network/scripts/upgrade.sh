e2023c2248149112d9b4da372f51de87339382ea
sampocs <sam.pochyly@gmail.com>
2026-04-30 21:50:41 -0500

    feat(rehearsal): vote from val1-val5 in upgrade.sh

--- integration-tests/network/scripts/upgrade.sh
+++ integration-tests/network/scripts/upgrade.sh
@@ -5,46 +5,46 @@
 set -eu
 
 NAMESPACE=integration
-
-EXEC0="kubectl exec -it stride-validator-0 -c validator -n $NAMESPACE -- "
-STRIDED0="kubectl exec -it stride-validator-0 -c validator -n $NAMESPACE -- strided"
-STRIDED1="kubectl exec -it stride-validator-1 -c validator -n $NAMESPACE -- strided"
-STRIDED2="kubectl exec -it stride-validator-2 -c validator -n $NAMESPACE -- strided"
-
 UPGRADE_BUFFER=45 # blocks
+
+# Helper to invoke strided in a specific validator pod.
+strided_in() {
+    pod_index="$1"; shift
+    kubectl exec -it stride-validator-$pod_index -c validator -n $NAMESPACE -- strided "$@"
+}
 
 trim_tx() {
     grep -E "code:|txhash:" | sed 's/^[[:space:]]*//'
 }
 
-upgrade_name=$(kubectl exec stride-validator-0 -c validator -- printenv UPGRADE_NAME)
-latest_height=$($STRIDED0 status | jq -r 'if .SyncInfo then .SyncInfo.latest_block_height else .sync_info.latest_block_height end')
+upgrade_name=$(kubectl exec stride-validator-0 -c validator -n $NAMESPACE -- printenv UPGRADE_NAME)
+latest_height=$(strided_in 0 status | jq -r 'if .SyncInfo then .SyncInfo.latest_block_height else .sync_info.latest_block_height end')
 upgrade_height=$((latest_height+UPGRADE_BUFFER))
 
 echo -e "\nSubmitting proposal for $upgrade_name at height $upgrade_height...\n"
-$EXEC0 bash scripts/propose_upgrade.sh $upgrade_name $upgrade_height | trim_tx
+kubectl exec -it stride-validator-0 -c validator -n $NAMESPACE -- \
+    bash scripts/propose_upgrade.sh $upgrade_name $upgrade_height | trim_tx
 
 sleep 5
 echo -e "\nProposal:\n"
-proposal_id=$($STRIDED0 q gov proposals --output json | jq -r '.proposals | max_by(.id | tonumber).id')
-$STRIDED0 query gov proposal $proposal_id
+proposal_id=$(strided_in 0 q gov proposals --output json | jq -r '.proposals | max_by(.id | tonumber).id')
+strided_in 0 query gov proposal $proposal_id
 
 sleep 1
 echo -e "\nVoting on proposal #$proposal_id...\n"
-echo "Val1:"
-$STRIDED0 tx gov vote $proposal_id yes --from val1 -y | trim_tx
-echo "Val2:"
-$STRIDED1 tx gov vote $proposal_id yes --from val2 -y | trim_tx
-echo "Val3:"
-$STRIDED2 tx gov vote $proposal_id yes --from val3 -y | trim_tx
+for i in 0 1 2 3 4; do
+    val_name="val$((i + 1))"
+    echo "${val_name}:"
+    strided_in "$i" tx gov vote $proposal_id yes --from "$val_name" -y | trim_tx
+done
 
 sleep 5
 echo -e "\nVote confirmation:\n"
-$STRIDED0 query gov tally $proposal_id
+strided_in 0 query gov tally $proposal_id
 
 echo -e "\nProposal Status:\n"
 while true; do
-    status=$($STRIDED0 query gov proposal $proposal_id --output json | jq -r '.proposal.status')
+    status=$(strided_in 0 query gov proposal $proposal_id --output json | jq -r '.proposal.status')
     if [[ "$status" == "PROPOSAL_STATUS_VOTING_PERIOD" ]]; then
         echo "Proposal still in progress..."
         sleep 5
@@ -54,7 +54,7 @@ while true; do
     elif [[ "$status" == "PROPOSAL_STATUS_REJECTED" ]]; then
         echo "Proposal Failed!"
         exit 1
-    else 
+    else
         echo "Unknown proposal status: $status"
         exit 1
     fi
